3072062f3024f9d1e84fff7cf6b1d484de78d553
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2023-11-12 19:28:13 +0200

    Improve MergeTwoSortedListsTests

--- LeetCode.Tests/Algorithms/MergeTwoSortedListsTests.cs
+++ LeetCode.Tests/Algorithms/MergeTwoSortedListsTests.cs
@@ -11,13 +11,13 @@ public class MergeTwoSortedListsTests
     [DataRow(new int[] { }, new int[] { }, new int[] { })]
     [DataRow(new int[] { }, new[] { 0 }, new[] { 0 })]
     [DataRow(new[] { -9, 3 }, new[] { 5, 7 }, new[] { -9, 3, 5, 7 })]
-    public void MergeTwoSortedLists_GetResult_ShouldMergeAndMatchExpectedResult(int[] array1, int[] array2, int[] expectedResultArray)
+    public void MergeTwoSortedLists_GetResult_ShouldMergeAndMatchExpectedResult(int[] array1, int[] array2,
+        int[] expectedResultArray)
     {
         // Arrange
-        var list1 = array1.Reverse().Aggregate<int, ListNode?>(null, (current, t) => new ListNode(t, current));
-        var list2 = array2.Reverse().Aggregate<int, ListNode?>(null, (current, t) => new ListNode(t, current));
-        var expectedResultList = expectedResultArray.Reverse()
-            .Aggregate<int, ListNode?>(null, (current, t) => new ListNode(t, current));
+        var list1 = array1.Reverse().Aggregate<int, ListNode?>(null, AggregateListNode);
+        var list2 = array2.Reverse().Aggregate<int, ListNode?>(null, AggregateListNode);
+        var expectedResultList = expectedResultArray.Reverse().Aggregate<int, ListNode?>(null, AggregateListNode);
 
         // Act
         var actualResultList = MergeTwoSortedLists.GetResult(list1, list2);
@@ -32,5 +32,10 @@ public class MergeTwoSortedListsTests
 
         Assert.IsNull(expectedResultList, "Expected result should not have more nodes.");
         Assert.IsNull(actualResultList, "Actual result should not have more nodes.");
+    }
+
+    private static ListNode AggregateListNode(ListNode? next, int val)
+    {
+        return new ListNode(val, next);
     }
 }
