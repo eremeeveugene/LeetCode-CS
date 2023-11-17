d4647e2f5b0ce1741e1b551102abb636e5bd2f55
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2023-11-17 22:36:22 +0200

    PrintInOrder with SemaphoreSlim implementation

---
+++ LeetCode/Concurrency/PrintInOrderTask/PrintInOrderSemaphoreSlim.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Concurrency.PrintInOrderTask;
+
+public class PrintInOrderSemaphoreSlim
+{
+    private static readonly SemaphoreSlim FirstPrint = new(0, 1);
+    private static readonly SemaphoreSlim SecondPrint = new(0, 1);
+
+    public void First(Action printFirst)
+    {
+        printFirst();
+
+        FirstPrint.Release();
+    }
+
+    public void Second(Action printSecond)
+    {
+        FirstPrint.Wait();
+
+        printSecond();
+
+        SecondPrint.Release();
+    }
+
+    public void Third(Action printThird)
+    {
+        SecondPrint.Wait();
+
+        printThird();
+    }
+}
