c03f926be2ca7bdd10d9cc083539b66ab643686e
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2023-11-17 22:32:13 +0200

    PrintInOrder with Monitor implementation

--- LeetCode.Tests/Concurrency/PrintInOrderTests.cs
+++ LeetCode.Tests/Concurrency/PrintInOrderTests.cs
@@ -80,7 +80,6 @@ public class PrintInOrderTests
         Assert.AreEqual(expectedResult, actualResult);
     }
 
-
     [TestMethod]
     [DataRow(new[] { 1, 2, 3 })]
     [DataRow(new[] { 2, 3, 1 })]
@@ -93,6 +92,41 @@ public class PrintInOrderTests
 
         // Act
         var printInOrder = new PrintInOrderAutoResetEvent();
+
+        var tasks = new List<Task>();
+
+        foreach (var num in nums)
+            switch (num)
+            {
+                case 1:
+                    tasks.Add(Task.Run(() => printInOrder.First(() => actualResult += First)));
+                    break;
+                case 2:
+                    tasks.Add(Task.Run(() => printInOrder.Second(() => actualResult += Second)));
+                    break;
+                case 3:
+                    tasks.Add(Task.Run(() => printInOrder.Third(() => actualResult += Third)));
+                    break;
+            }
+
+        await Task.WhenAll(tasks);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+
+    [TestMethod]
+    [DataRow(new[] { 1, 2, 3 })]
+    [DataRow(new[] { 2, 3, 1 })]
+    [DataRow(new[] { 3, 1, 2 })]
+    public async Task PrintInOrder_Monitor_ExecutesActionsInSequentialOrder(int[] nums)
+    {
+        // Arrange
+        var expectedResult = FirstSecondThird;
+        var actualResult = string.Empty;
+
+        // Act
+        var printInOrder = new PrintInOrderMonitor();
 
         var tasks = new List<Task>();
 
---
+++ LeetCode/Concurrency/PrintInOrderTask/PrintInOrderMonitor.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode.Concurrency.PrintInOrderTask;
+
+public class PrintInOrderMonitor
+{
+    private readonly object _lock = new();
+    private int _step = 1;
+
+    public void First(Action printFirst)
+    {
+        lock (_lock)
+        {
+            while (_step != 1)
+                Monitor.Wait(_lock);
+
+            printFirst();
+
+            _step = 2;
+            Monitor.PulseAll(_lock);
+        }
+    }
+
+    public void Second(Action printSecond)
+    {
+        lock (_lock)
+        {
+            while (_step != 2)
+                Monitor.Wait(_lock);
+
+            printSecond();
+
+            _step = 3;
+            Monitor.PulseAll(_lock);
+        }
+    }
+
+    public void Third(Action printThird)
+    {
+        lock (_lock)
+        {
+            while (_step != 3)
+                Monitor.Wait(_lock);
+
+            printThird();
+
+            _step = 1;
+            Monitor.PulseAll(_lock);
+        }
+    }
+}
