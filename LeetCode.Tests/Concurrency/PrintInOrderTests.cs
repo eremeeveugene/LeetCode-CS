63cb5b0202524c0e72fad4381109c493aef7b1c8
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2023-11-17 21:57:15 +0200

    PrintInOrder solution with thread sleep

---
+++ LeetCode.Tests/Concurrency/PrintInOrderTests.cs
@@ -0,0 +1,53 @@
+﻿using LeetCode.Concurrency.PrintInOrderTask;
+
+namespace LeetCode.Tests.Concurrency;
+
+[TestClass]
+public class PrintInOrderTests
+{
+    private string _actualResult = string.Empty;
+
+    [TestMethod]
+    [DataRow(new[] { 1, 2, 3 }, "firstsecondthird")]
+    [DataRow(new[] { 3, 2, 1 }, "firstsecondthird")]
+    [DataRow(new[] { 2, 3, 1 }, "firstsecondthird")]
+    public async Task PrintInOrderThreadSleep_ExecutesActionsInSequentialOrder(int[] nums, string expectedResult)
+    {
+        var printInOrder = new PrintInOrderThreadSleep();
+
+        var tasks = new List<Task>();
+
+        foreach (var num in nums)
+            switch (num)
+            {
+                case 1:
+                    tasks.Add(Task.Run(() => printInOrder.First(PrintFirst)));
+                    break;
+                case 2:
+                    tasks.Add(Task.Run(() => printInOrder.Second(PrintSecond)));
+                    break;
+                case 3:
+                    tasks.Add(Task.Run(() => printInOrder.Third(PrintThird)));
+                    break;
+            }
+
+        await Task.WhenAll(tasks);
+
+        Assert.AreEqual(expectedResult, _actualResult);
+    }
+
+    private void PrintFirst()
+    {
+        _actualResult += "first";
+    }
+
+    private void PrintSecond()
+    {
+        _actualResult += "second";
+    }
+
+    private void PrintThird()
+    {
+        _actualResult += "third";
+    }
+}
---
+++ LeetCode/Concurrency/PrintInOrderTask/PrintInOrderThreadSleep.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCode.Concurrency.PrintInOrderTask;
+
+public class PrintInOrderThreadSleep
+{
+    private const int ThreadTimeoutMs = 100;
+    private bool _isFirstPrinted;
+    private bool _isSecondPrinted;
+
+    public void First(Action printFirst)
+    {
+        printFirst();
+
+        _isFirstPrinted = true;
+    }
+
+    public void Second(Action printSecond)
+    {
+        while (!_isFirstPrinted) Thread.Sleep(ThreadTimeoutMs);
+
+        printSecond();
+
+        _isSecondPrinted = true;
+    }
+
+    public void Third(Action printThird)
+    {
+        while (!_isSecondPrinted) Thread.Sleep(ThreadTimeoutMs);
+
+        printThird();
+    }
+}
