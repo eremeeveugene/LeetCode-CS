bb39c67760436ee09bf4cf03b19c028bc94b151b
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2023-11-18 21:04:11 +0200

    FizzBuzzMultithreaded solution

---
+++ LeetCode.Tests/Concurrency/FizzBuzzMultithreadedTests.cs
@@ -0,0 +1,58 @@
+﻿using LeetCode.Concurrency.FizzBuzzMultithreadedTask;
+using Newtonsoft.Json;
+
+namespace LeetCode.Tests.Concurrency;
+
+[TestClass]
+public class FizzBuzzMultithreadedTests
+{
+    [TestMethod]
+    [DataRow(1, "1")]
+    [DataRow(2, "1, 2")]
+    [DataRow(3, "1, 2, fizz")]
+    [DataRow(5, "1, 2, fizz, 4, buzz")]
+    [DataRow(15, "1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzbuzz")]
+    public async Task Test(int length, string expectedResultJson)
+    {
+        // Arrange
+        var expectedResult = DeserializeToList(expectedResultJson);
+        var actualResult = new List<object>();
+
+        // Act
+        var fizzBuzzMultithreaded = new FizzBuzzMultithreaded(length);
+
+        var tasks = new List<Task>
+        {
+            Task.Run(() => fizzBuzzMultithreaded.Fizz(() => actualResult.Add("fizz"))),
+            Task.Run(() => fizzBuzzMultithreaded.Buzz(() => actualResult.Add("buzz"))),
+            Task.Run(() => fizzBuzzMultithreaded.Fizzbuzz(() => actualResult.Add("fizzbuzz"))),
+            Task.Run(() => fizzBuzzMultithreaded.Number(n => actualResult.Add(n)))
+        };
+
+        await Task.WhenAll(tasks);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+
+    static List<object> DeserializeToList(string input)
+    {
+        var resultList = new List<object>();
+        var elements = input.Split(',');
+
+        foreach (var element in elements)
+        {
+            var trimmedElement = element.Trim();
+            if (int.TryParse(trimmedElement, out int number))
+            {
+                resultList.Add(number);
+            }
+            else
+            {
+                resultList.Add(trimmedElement);
+            }
+        }
+
+        return resultList;
+    }
+}
---
+++ LeetCode/Concurrency/FizzBuzzMultithreadedTask/FizzBuzzMultithreaded.cs
@@ -0,0 +1,100 @@
+﻿namespace LeetCode.Concurrency.FizzBuzzMultithreadedTask;
+
+public class FizzBuzzMultithreaded
+{
+    private readonly int _length;
+    private readonly object _lock = new();
+    private int _number = 1;
+
+    public FizzBuzzMultithreaded(int length)
+    {
+        _length = length;
+    }
+
+    public void Fizz(Action printFizz)
+    {
+        lock (_lock)
+        {
+            while (_number <= _length)
+            {
+                while (_number <= _length && (_number % 3 != 0 || _number % 5 == 0))
+                {
+                    Monitor.Wait(_lock);
+
+                    if (_number > _length) return;
+                }
+
+                printFizz.Invoke();
+
+                IncrementNumber();
+            }
+        }
+    }
+
+    public void Buzz(Action printBuzz)
+    {
+        lock (_lock)
+        {
+            while (_number <= _length)
+            {
+                while (_number % 3 == 0 || _number % 5 != 0)
+                {
+                    Monitor.Wait(_lock);
+
+                    if (_number > _length) return;
+                }
+
+                printBuzz.Invoke();
+
+                IncrementNumber();
+            }
+        }
+    }
+
+    public void Fizzbuzz(Action printFizzBuzz)
+    {
+        lock (_lock)
+        {
+            while (_number <= _length)
+            {
+                while (_number % 3 != 0 || _number % 5 != 0)
+                {
+                    Monitor.Wait(_lock);
+
+                    if (_number > _length) return;
+                }
+
+                printFizzBuzz.Invoke();
+
+                IncrementNumber();
+            }
+        }
+    }
+
+    public void Number(Action<int> printNumber)
+    {
+        lock (_lock)
+        {
+            while (_number <= _length)
+            {
+                while (_number % 3 == 0 || _number % 5 == 0)
+                {
+                    Monitor.Wait(_lock);
+
+                    if (_number > _length) return;
+                }
+
+                printNumber.Invoke(_number);
+
+                IncrementNumber();
+            }
+        }
+    }
+
+    private void IncrementNumber()
+    {
+        _number++;
+
+        Monitor.PulseAll(_lock);
+    }
+}
