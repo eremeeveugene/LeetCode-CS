cef3c08a23b55df6124e320b9a66aa3cedac3cc7
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2023-12-30 20:57:30 +0200

    Solution for search insert position

---
+++ source/LeetCode/Algorithms/SearchInsertPosition/SearchInsertPositionIterative.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2023 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SearchInsertPosition;
+
+/// <summary>
+///     https://leetcode.com/problems/search-insert-position/description/
+/// </summary>
+public static class SearchInsertPositionIterative
+{
+    /// <summary>
+    ///     Time complexity - O (n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    public static int GetResult(int[] nums, int target)
+    {
+        return nums.TakeWhile(t => t < target).Count();
+    }
+}
