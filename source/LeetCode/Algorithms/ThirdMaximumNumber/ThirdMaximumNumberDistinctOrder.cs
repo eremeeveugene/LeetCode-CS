665e021c67c09d4f06d5f5ff21afdb84317d8e7b
eugene <eremeeveugene@gmail.com>
2024-01-06 13:28:20 +0200

    Third maximum number implementation (#51)
    * Third maximum number implementation
    
    * Name unit tests

---
+++ source/LeetCode/Algorithms/ThirdMaximumNumber/ThirdMaximumNumberDistinctOrder.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ThirdMaximumNumber;
+
+/// <summary>
+///     https://leetcode.com/problems/third-maximum-number/description/
+/// </summary>
+public static class ThirdMaximumNumberDistinctOrder
+{
+    /// <summary>
+    ///     Time complexity - O (n log n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public static int GetResult(int[] nums)
+    {
+        var numsLength = nums.Length;
+
+        switch (numsLength)
+        {
+            case 1:
+                return nums[0];
+            case 2:
+                return Math.Max(nums[0], nums[1]);
+        }
+
+        var distinct = nums.Distinct().Order().ToArray();
+
+        var distinctLength = distinct.Length;
+
+        return distinctLength < 3 ? distinct.Last() : distinct[distinctLength - 3];
+    }
+}
