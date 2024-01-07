a8f657fbaad78f7e69bc514a46bfeff102fd2f45
eugene <eremeeveugene@gmail.com>
2024-01-07 16:59:58 +0200

    Buy two chocolates solution (#57)

---
+++ source/LeetCode/Algorithms/BuyTwoChocolates/BuyTwoChocolatesMinimumTwoPrices.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.BuyTwoChocolates;
+
+public static class BuyTwoChocolatesMinimumTwoPrices
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="prices"></param>
+    /// <param name="money"></param>
+    /// <returns></returns>
+    public static int GetResult(int[] prices, int money)
+    {
+        var min1 = prices[0];
+        var min2 = prices[1];
+
+        for (var i = 1; i < prices.Length; i++)
+        {
+            if (prices[i] < min1)
+            {
+                min2 = min1;
+                min1 = prices[i];
+            }
+            else if (prices[i] < min2)
+            {
+                min2 = prices[i];
+            }
+        }
+
+        var leftover = money - min1 - min2;
+
+        return leftover < 0 ? money : leftover;
+    }
+}
