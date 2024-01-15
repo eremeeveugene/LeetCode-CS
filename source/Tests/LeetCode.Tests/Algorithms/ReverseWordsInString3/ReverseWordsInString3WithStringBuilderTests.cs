52c9d95d4b82c85b61a26628094f59108ffcf924
eugene <eremeeveugene@gmail.com>
2024-01-15 21:22:01 +0200

    Reverse Words in a String III (#67)

---
+++ source/Tests/LeetCode.Tests/Algorithms/ReverseWordsInString3/ReverseWordsInString3WithStringBuilderTests.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ReverseWordsInString3;
+
+namespace LeetCode.Tests.Algorithms.ReverseWordsInString3;
+
+[TestClass]
+public class ReverseWordsInString3WithStringBuilderTests
+{
+    [TestMethod]
+    [DataRow("Let's take LeetCode contest", "s'teL ekat edoCteeL tsetnoc")]
+    [DataRow("Mr Ding", "rM gniD")]
+    public void ReverseWordsInString3WithStringBuilder_ReverseWords_WithStringInput_ReturnsReversedWords(string s,
+        string expectedResult)
+    {
+        // Act
+        var actualResult = ReverseWordsInString3WithStringBuilder.ReverseWords(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
