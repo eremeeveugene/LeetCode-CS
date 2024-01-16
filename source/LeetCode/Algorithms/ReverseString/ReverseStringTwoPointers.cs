553eecc25b51bba4c1432da2ded3d4252b3def30
eugene <eremeeveugene@gmail.com>
2024-01-16 22:15:43 +0200

    Reverse String (#71)

---
+++ source/LeetCode/Algorithms/ReverseString/ReverseStringTwoPointers.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ReverseString;
+
+public static class ReverseStringTwoPointers
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    public static void ReverseString(char[] s)
+    {
+        for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+        {
+            (s[i], s[j]) = (s[j], s[i]);
+        }
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ReverseString/ReverseStringTwoPointersTests.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ReverseString;
+
+namespace LeetCode.Tests.Algorithms.ReverseString;
+
+[TestClass]
+public class ReverseStringTwoPointersTests
+{
+    [TestMethod]
+    [DataRow(new[] { 'h', 'e', 'l', 'l', 'o' }, new[] { 'o', 'l', 'l', 'e', 'h' })]
+    [DataRow(new[] { 'H', 'a', 'n', 'n', 'a', 'h' }, new[] { 'h', 'a', 'n', 'n', 'a', 'H' })]
+    public void ReverseStringTwoPointers_ReverseString_WithCharArray_ReturnsReversedArray(char[] s,
+        char[] expectedResult)
+    {
+        // Act
+        ReverseStringTwoPointers.ReverseString(s);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, s);
+    }
+}
