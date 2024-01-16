55362054459842d5ed5ec0edac2765700be7d7d1
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-01-16 21:50:44 +0200

    Reverse string 2

---
+++ source/LeetCode/Algorithms/ReverseString2/ReverseString2WithCharArray.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ReverseString2;
+
+/// <summary>
+///     https://leetcode.com/problems/reverse-string-ii/
+/// </summary>
+public static class ReverseString2WithCharArray
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public static string ReverseStr(string s, int k)
+    {
+        var charArray = s.ToCharArray();
+
+        for (var start = 0; start < charArray.Length; start += 2 * k)
+        {
+            var i = start;
+            var j = Math.Min(start + k - 1, charArray.Length - 1);
+
+            while (i < j)
+            {
+                (charArray[i], charArray[j]) = (charArray[j], charArray[i]);
+
+                i++;
+                j--;
+            }
+        }
+
+        return new string(charArray);
+    }
+}
---
+++ source/LeetCode/Algorithms/ReverseString2/ReverseString2WithStringBuilder.cs
@@ -0,0 +1,61 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Text;
+
+namespace LeetCode.Algorithms.ReverseString2;
+
+/// <summary>
+///     https://leetcode.com/problems/reverse-string-ii/
+/// </summary>
+public static class ReverseString2WithStringBuilder
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public static string ReverseStr(string s, int k)
+    {
+        var reverseStringBuilder = new StringBuilder();
+
+        var reverse = true;
+        var sIndex = 0;
+
+        while (sIndex < s.Length)
+        {
+            IEnumerable<char> charArray;
+
+            if (reverse)
+            {
+                charArray = s.Skip(sIndex).Take(k).Reverse();
+
+                reverse = false;
+            }
+            else
+            {
+                charArray = s.Skip(sIndex).Take(k);
+
+                reverse = true;
+            }
+
+            foreach (var c in charArray)
+            {
+                reverseStringBuilder.Append(c);
+            }
+
+            sIndex += k;
+        }
+
+        return reverseStringBuilder.ToString();
+    }
+}
