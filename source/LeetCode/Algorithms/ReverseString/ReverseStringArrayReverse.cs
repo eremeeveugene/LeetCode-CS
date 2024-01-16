651d1f7648f5bcebc1992253c358e92f75d56a0d
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-01-16 22:11:32 +0200

    Reverse String

---
+++ source/LeetCode/Algorithms/ReverseString/ReverseStringArrayReverse.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ReverseString;
+
+public static class ReverseStringArrayReverse
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    public static void ReverseString(char[] s)
+    {
+        Array.Reverse(s);
+    }
+}
