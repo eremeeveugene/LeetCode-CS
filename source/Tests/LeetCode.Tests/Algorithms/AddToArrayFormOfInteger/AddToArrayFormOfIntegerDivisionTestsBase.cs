d7211d727c675ee27fbb30908609956e07554512
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-01-20 20:36:52 +0200

    Update AddToArrayFormOfIntegerDivisionTestsBase.cs

--- source/Tests/LeetCode.Tests/Algorithms/AddToArrayFormOfInteger/AddToArrayFormOfIntegerDivisionTestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/AddToArrayFormOfInteger/AddToArrayFormOfIntegerDivisionTestsBase.cs
@@ -19,7 +19,7 @@ public abstract class AddToArrayFormOfIntegerDivisionTestsBase
     [DataRow(new[] { 1, 2, 0, 0 }, 34, new[] { 1, 2, 3, 4 })]
     [DataRow(new[] { 2, 7, 4 }, 181, new[] { 4, 5, 5 })]
     [DataRow(new[] { 2, 1, 5 }, 806, new[] { 1, 0, 2, 1 })]
-    public void Test(int[] num, int k, int[] expectedResult)
+    public void AddToArrayForm_WithArrayAndInteger_ReturnsSumAsArray(int[] num, int k, int[] expectedResult)
     {
         // Arrange
         var solution = GetSolution();
