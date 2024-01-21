5aff6c8c75c9eff1f26edd5d79c9f05d945359b2
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-01-21 21:05:54 +0200

    Palindrome number

---
+++ source/LeetCode/Algorithms/PalindromeNumber/PalindromeNumberByReversingDigits.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.PalindromeNumber;
+
+/// <inheritdoc />
+public class PalindromeNumberByReversingDigits : IPalindromeNumber
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public bool IsPalindrome(int x)
+    {
+        switch (x)
+        {
+            case < 0:
+                return false;
+            case < 10:
+                return true;
+        }
+
+        if (x % 10 == 0) return false;
+
+        var reversedHalf = 0;
+
+        while (x > reversedHalf)
+        {
+            reversedHalf = (reversedHalf * 10) + (x % 10);
+
+            x /= 10;
+        }
+
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+}
