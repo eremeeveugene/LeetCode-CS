133b11db9141b224601da1316aa48e44e2664e08
eugene <eremeeveugene@gmail.com>
2024-01-21 21:12:08 +0200

    Palindrome number (#84)
    * Palindrome number
    
    * Update PalindromeNumberByConvertingToStringTests.cs

---
+++ source/LeetCode/Algorithms/PalindromeNumber/PalindromeNumberByConvertingToString.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.PalindromeNumber;
+
+/// <inheritdoc />
+public class PalindromeNumberByConvertingToString : IPalindromeNumber
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public bool IsPalindrome(int x)
+    {
+        switch (x)
+        {
+            case < 0:
+                return false;
+            case < 10:
+                return true;
+        }
+
+        if (x % 10 == 0) return false;
+
+        var xString = x.ToString();
+        var i = 0;
+        var j = xString.Length - 1;
+
+        while (i < j)
+        {
+            if (xString[i].Equals(xString[j]))
+            {
+                i++;
+                j--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/PalindromeNumber/PalindromeNumberTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.PalindromeNumber;
+
+namespace LeetCode.Tests.Algorithms.PalindromeNumber;
+
+public abstract class PalindromeNumberTestsBase<T> where T : IPalindromeNumber, new()
+{
+    [TestMethod]
+    [DataRow(121, true)]
+    [DataRow(-121, false)]
+    [DataRow(10, false)]
+    public void IsPalindrome_WithNumber_ChecksIfNumberIsPalindrome(int x, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsPalindrome(x);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
