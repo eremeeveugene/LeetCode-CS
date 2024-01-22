70d369bd5279a6c65672b0c0e7dbaa5492ae84aa
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-01-22 21:54:21 +0200

    Calid parenthesis

---
+++ source/LeetCode/Algorithms/ValidParentheses/ValidParentheses1.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ValidParentheses;
+
+/// <inheritdoc />
+public class ValidParentheses1 : IValidParentheses
+{
+    public bool IsValid(string s)
+    {
+        var charsQueue = new Stack<char>();
+
+        foreach (var c in s)
+        {
+            if (charsQueue.Count == 0)
+            {
+                charsQueue.Push(c);
+            }
+            else
+            {
+                switch (charsQueue.Peek())
+                {
+                    case '(' when c == ')':
+                    case '{' when c == '}':
+                    case '[' when c == ']':
+                        charsQueue.Pop();
+                        break;
+                    case '(':
+                    case '{':
+                    case '[':
+                        charsQueue.Push(c);
+                        break;
+                }
+            }
+        }
+
+        return charsQueue.Count == 0;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ValidParentheses/ValidParenthesesTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ValidParentheses;
+
+namespace LeetCode.Tests.Algorithms.ValidParentheses;
+
+public abstract class ValidParenthesesTestsBase<T> where T : IValidParentheses, new()
+{
+    [TestMethod]
+    [DataRow("()", true)]
+    [DataRow("()[]{}", true)]
+    [DataRow("(]", false)]
+    [DataRow("{[]}", true)]
+    [DataRow("[[[[]]]]", true)]
+    public void SearchInsert_WithSortedArrayAndTarget_ReturnsCorrectInsertionIndex(string s, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsValid(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
