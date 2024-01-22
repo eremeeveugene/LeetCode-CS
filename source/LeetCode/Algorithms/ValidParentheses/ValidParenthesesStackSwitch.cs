ad5650a8bbde9a8868cc1ddff97701e8415eabe8
eugene <eremeeveugene@gmail.com>
2024-01-22 22:13:24 +0200

    Valid parenthesis (#88)

---
+++ source/LeetCode/Algorithms/ValidParentheses/ValidParenthesesStackSwitch.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ValidParentheses;
+
+/// <inheritdoc />
+public class ValidParenthesesStackSwitch : IValidParentheses
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public bool IsValid(string s)
+    {
+        var parenthesesStack = new Stack<char>();
+
+        foreach (var c in s)
+        {
+            if (parenthesesStack.Count == 0)
+            {
+                parenthesesStack.Push(c);
+            }
+            else
+            {
+                switch (parenthesesStack.Peek())
+                {
+                    case '(' when c == ')':
+                    case '{' when c == '}':
+                    case '[' when c == ']':
+                        parenthesesStack.Pop();
+                        break;
+                    case '(':
+                    case '{':
+                    case '[':
+                        parenthesesStack.Push(c);
+                        break;
+                }
+            }
+        }
+
+        return parenthesesStack.Count == 0;
+    }
+}
