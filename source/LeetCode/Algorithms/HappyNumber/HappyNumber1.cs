796014ed3ed1f1a83eb8879558b270fb1f4670c1
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-01-24 22:24:54 +0200

    Happy number solution

---
+++ source/LeetCode/Algorithms/HappyNumber/HappyNumber1.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.HappyNumber;
+
+/// <inheritdoc />
+public class HappyNumber1 : IHappyNumber
+{
+    private readonly HashSet<int> _hashSet = [];
+
+    public bool IsHappy(int n)
+    {
+        if (n == 1)
+        {
+            return true;
+        }
+
+        while (n > 1)
+        {
+            n = n.ToString().Sum(c => (int)Math.Pow(char.GetNumericValue(c), 2));
+
+            if (!_hashSet.Add(n))
+            {
+                break;
+            }
+        }
+
+        return n == 1;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/HappyNumber/HappyNumberTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.HappyNumber;
+
+namespace LeetCode.Tests.Algorithms.HappyNumber;
+
+public abstract class HappyNumberTestsBase<T> where T : IHappyNumber, new()
+{
+    [TestMethod]
+    [DataRow(1, true)]
+    [DataRow(2, false)]
+    [DataRow(3, false)]
+    [DataRow(7, true)]
+    [DataRow(19, true)]
+    public void Test(int s, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsHappy(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
