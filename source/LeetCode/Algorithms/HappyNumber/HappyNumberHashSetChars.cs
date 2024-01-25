4fa161dbb7db9a70dfa5d252d3a85be47889188f
eugene <eremeeveugene@gmail.com>
2024-01-25 21:53:42 +0200

    Happy number (#94)

---
+++ source/LeetCode/Algorithms/HappyNumber/HappyNumberHashSetChars.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.HappyNumber;
+
+/// <inheritdoc />
+public class HappyNumberHashSetChars : IHappyNumber
+{
+    /// <summary>
+    ///     Time complexity - O(log n)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public bool IsHappy(int n)
+    {
+        if (n == 1)
+        {
+            return true;
+        }
+
+        var hashSet = new HashSet<int>();
+
+        while (n > 1 && !hashSet.Contains(n))
+        {
+            hashSet.Add(n);
+            n = n.ToString().Sum(c => (int)Math.Pow(char.GetNumericValue(c), 2));
+        }
+
+        return n == 1;
+    }
+}
---
+++ source/LeetCode/Algorithms/HappyNumber/HappyNumberHashSetNumbers.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.HappyNumber;
+
+/// <inheritdoc />
+public class HappyNumberHashSetNumbers : IHappyNumber
+{
+    /// <summary>
+    ///     Time complexity - O(log n)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public bool IsHappy(int n)
+    {
+        if (n == 1)
+        {
+            return true;
+        }
+
+        var hashSet = new HashSet<int>();
+
+        while (n > 1 && !hashSet.Contains(n))
+        {
+            hashSet.Add(n);
+            n = GetNext(n);
+        }
+
+        return n == 1;
+    }
+
+    private static int GetNext(int n)
+    {
+        var sum = 0;
+
+        while (n > 0)
+        {
+            var digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
