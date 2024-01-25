2a118c7179c78764e168a79be50789de54be5701
eugene <eremeeveugene@gmail.com>
2024-01-25 21:53:18 +0200

    Add digits (#93)

---
+++ source/LeetCode/Algorithms/AddDigits/AddDigitsIterative.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.AddDigits;
+
+/// <inheritdoc />
+public class AddDigitsIterative : IAddDigits
+{
+    /// <summary>
+    ///     Time complexity - O(log(n))
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+    public int AddDigits(int num)
+    {
+        while (num > 9)
+        {
+            num = num.ToString().Sum(c => (int)char.GetNumericValue(c));
+        }
+
+        return num;
+    }
+}
