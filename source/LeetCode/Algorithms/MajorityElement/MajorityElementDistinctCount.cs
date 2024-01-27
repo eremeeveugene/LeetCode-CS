941eb524bd4c616d173d60fa242068ffe3024246
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-01-27 18:58:55 +0200

    Majority Element

---
+++ source/LeetCode/Algorithms/MajorityElement/MajorityElementDistinctCount.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MajorityElement;
+
+/// <inheritdoc />
+public class MajorityElementDistinctCount : IMajorityElement
+{
+    /// <summary>
+    ///     Time complexity - O(n^2)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MajorityElement(int[] nums)
+    {
+        var result = 0;
+        var resultFrequency = 0;
+
+        foreach (var distinctNum in nums.Distinct())
+        {
+            var frequency = nums.Count(num => num.Equals(distinctNum));
+
+            if (frequency <= resultFrequency)
+            {
+                continue;
+            }
+
+            resultFrequency = frequency;
+            result = distinctNum;
+        }
+
+        return result;
+    }
+}
