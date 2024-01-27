465c258b2cdec47a3bbd8ef986032cc0498bff19
eugene <eremeeveugene@gmail.com>
2024-01-27 18:21:47 +0200

    Isomorphic Strings (#102)

---
+++ source/LeetCode/Algorithms/IsomorphicStrings/IsomorphicStringsTwoDictionaries.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.IsomorphicStrings;
+
+/// <inheritdoc />
+public class IsomorphicStringsTwoDictionaries : IIsomorphicStrings
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public bool IsIsomorphic(string s, string t)
+    {
+        var sDictionary = new Dictionary<char, char>();
+        var tDictionary = new Dictionary<char, char>();
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            var sChar = s[i];
+            var tChar = t[i];
+
+            if (sDictionary.TryGetValue(sChar, out var mappedTChar))
+            {
+                if (mappedTChar != tChar)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                sDictionary[sChar] = tChar;
+            }
+
+            if (tDictionary.TryGetValue(tChar, out var mappedSChar))
+            {
+                if (mappedSChar != sChar)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                tDictionary[tChar] = sChar;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/IsomorphicStrings/IsomorphicStringsTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.IsomorphicStrings;
+
+namespace LeetCode.Tests.Algorithms.IsomorphicStrings;
+
+public abstract class IsomorphicStringsTestsBase<T> where T : IIsomorphicStrings, new()
+{
+    [TestMethod]
+    [DataRow("egg", "add", true)]
+    [DataRow("foo", "bar", false)]
+    [DataRow("paper", "title", true)]
+    [DataRow("badc", "baba", false)]
+    public void IsIsomorphic_WithTwoStrings_ReturnsExpectedBoolean(string s, string t, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsIsomorphic(s, t);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
