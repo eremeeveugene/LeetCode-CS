9c222de29325d615c28708043a5d99c4fb8b53c2
eugene <eremeeveugene@gmail.com>
2024-01-27 14:01:01 +0200

    Climbing stairs (#96)

---
+++ source/LeetCode/Algorithms/ClimbingStairs/ClimbingStairsIterativeFibonacci.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ClimbingStairs;
+
+/// <inheritdoc />
+public class ClimbingStairsIterativeFibonacci : IClimbingStairs
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public int ClimbStairs(int n)
+    {
+        switch (n)
+        {
+            case 1:
+                return 1;
+            case 2:
+                return 2;
+        }
+
+        var step1 = 1;
+        var step2 = 1;
+
+        for (var i = 2; i < n + 1; i++)
+        {
+            var step3 = step1 + step2;
+            step1 = step2;
+            step2 = step3;
+        }
+
+        return step2;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ClimbingStairs/ClimbingStairsTestsBase.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ClimbingStairs;
+
+namespace LeetCode.Tests.Algorithms.ClimbingStairs;
+
+public abstract class ClimbingStairsTestsBase<T> where T : IClimbingStairs, new()
+{
+    [TestMethod]
+    [DataRow(1, 1)]
+    [DataRow(2, 2)]
+    [DataRow(3, 3)]
+    [DataRow(4, 5)]
+    [DataRow(5, 8)]
+    [DataRow(6, 13)]
+    [DataRow(7, 21)]
+    [DataRow(8, 34)]
+    [DataRow(9, 55)]
+    public void ClimbStairs_WithGivenN_ReturnsExpectedWaysToClimb(int n, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.ClimbStairs(n);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
