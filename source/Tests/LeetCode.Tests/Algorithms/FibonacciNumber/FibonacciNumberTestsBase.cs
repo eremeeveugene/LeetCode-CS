b468957e6c06cf954717087486424bb1f3bde198
eugene <eremeeveugene@gmail.com>
2024-01-27 14:44:53 +0200

    Fibonacci number (#98)

---
+++ source/Tests/LeetCode.Tests/Algorithms/FibonacciNumber/FibonacciNumberTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FibonacciNumber;
+
+namespace LeetCode.Tests.Algorithms.FibonacciNumber;
+
+public abstract class FibonacciNumberTestsBase<T> where T : IFibonacciNumber, new()
+{
+    [TestMethod]
+    [DataRow(0, 0)]
+    [DataRow(1, 1)]
+    [DataRow(2, 1)]
+    [DataRow(3, 2)]
+    [DataRow(4, 3)]
+    public void Fib_WithInputN_ReturnsNthFibonacciNumber(int n, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.Fib(n);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
