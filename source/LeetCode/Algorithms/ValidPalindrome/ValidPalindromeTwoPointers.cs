f2ac85b49b0343451ad1bafa4345f94a72694148
eugene <eremeeveugene@gmail.com>
2024-01-28 13:01:43 +0200

    Valid Palindrome (#106)

---
+++ source/LeetCode/Algorithms/ValidPalindrome/ValidPalindromeTwoPointers.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ValidPalindrome;
+
+/// <inheritdoc />
+public class ValidPalindromeTwoPointers : IValidPalindrome
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public bool IsPalindrome(string s)
+    {
+        var left = 0;
+        var right = s.Length - 1;
+
+        while (left < right)
+        {
+            while (left < right && !char.IsAsciiLetterOrDigit(s[left]))
+            {
+                left++;
+            }
+
+            while (left < right && !char.IsAsciiLetterOrDigit(s[right]))
+            {
+                right--;
+            }
+
+            if (char.ToLower(s[left]) != char.ToLower(s[right]))
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ValidPalindrome/ValidPalindromeTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ValidPalindrome;
+
+namespace LeetCode.Tests.Algorithms.ValidPalindrome;
+
+public abstract class ValidPalindromeTestsBase<T> where T : IValidPalindrome, new()
+{
+    [TestMethod]
+    [DataRow("A man, a plan, a canal: Panama", true)]
+    [DataRow("race a car", false)]
+    [DataRow(" ", true)]
+    [DataRow(".,", true)]
+    [DataRow("0P", false)]
+    public void IsPalindrome_WithStringInput_ReturnsBoolean(string s, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsPalindrome(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
