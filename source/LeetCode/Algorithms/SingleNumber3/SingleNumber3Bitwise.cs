1ef09e352672b797ee2bcde38fa3238bada71312
eugene <eremeeveugene@gmail.com>
2024-01-30 20:10:11 +0200

    Single Number III (#114)

---
+++ source/LeetCode/Algorithms/SingleNumber3/SingleNumber3Bitwise.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SingleNumber3;
+
+/// <inheritdoc />
+public class SingleNumber3Bitwise : ISingleNumber3
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int[] SingleNumber(int[] nums)
+    {
+        var xor = nums.Aggregate((current, num) => current ^ num);
+
+        var rightmostSetBit = xor & ~(xor - 1);
+        var result = new int[2];
+
+        foreach (var num in nums)
+        {
+            if ((num & rightmostSetBit) != 0)
+            {
+                result[0] ^= num;
+            }
+            else
+            {
+                result[1] ^= num;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ source/LeetCode/Algorithms/SingleNumber3/SingleNumber3Dictionary.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SingleNumber3;
+
+/// <inheritdoc />
+public class SingleNumber3Dictionary : ISingleNumber3
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int[] SingleNumber(int[] nums)
+    {
+        var numsDictionary = new Dictionary<int, int>();
+
+        foreach (var num in nums)
+        {
+            if (!numsDictionary.TryAdd(num, 1))
+            {
+                numsDictionary[num]++;
+            }
+        }
+
+        return numsDictionary.Where(num => num.Value < 2).Select(num => num.Key).ToArray();
+    }
+}
