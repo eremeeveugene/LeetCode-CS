99ebe6a20219ad059640f76227c6821d82a4f699
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-02-01 19:14:00 +0200

    Add Two Integers

---
+++ source/LeetCode/Algorithms/AddTwoIntegers/AddTwoIntegersMath.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.AddTwoIntegers;
+
+/// <inheritdoc />
+public class AddTwoIntegersMath : IAddTwoIntegers
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="num1"></param>
+    /// <param name="num2"></param>
+    /// <returns></returns>
+    public int Sum(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/AddTwoIntegers/AddTwoIntegersTestsBase.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.AddTwoIntegers;
+
+namespace LeetCode.Tests.Algorithms.AddTwoIntegers;
+
+public abstract class AddTwoIntegersTestsBase<T> where T : IAddTwoIntegers, new()
+{
+    [TestMethod]
+    [DataRow(12, 5, 17)]
+    [DataRow(-10, 4, -6)]
+    public void Sum_WhenCalledWithVariousNumbers_ReturnsExpectedSum(int num1, int num2, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.Sum(num1, num2);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
