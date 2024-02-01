75287a2c5b79f73db0cc2dd1ff54833c4278f65f
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-02-01 19:43:10 +0200

    Distribute Money to Maximum Children

---
+++ source/LeetCode/Algorithms/DistributeMoneyToMaximumChildren/DistributeMoneyToMaximumChildrenLinear.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.DistributeMoneyToMaximumChildren;
+
+/// <inheritdoc />
+public class DistributeMoneyToMaximumChildrenLinear : IDistributeMoneyToMaximumChildren
+{
+    /// <summary>
+    ///     Time complexity - O(1)
+    /// </summary>
+    /// <param name="money"></param>
+    /// <param name="children"></param>
+    /// <returns></returns>
+    public int DistMoney(int money, int children)
+    {
+        if (money < children)
+        {
+            return -1;
+        }
+
+        money -= children;
+
+        var max = money / 7;
+        var rest = money % 7;
+
+        if (max == children)
+        {
+            return rest == 0 ? children : children - 1;
+        }
+
+        if (max < children)
+        {
+            return rest == 3 && children - max == 1 ? max - 1 : max;
+        }
+
+        return children - 1;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/DistributeMoneyToMaximumChildren/DistributeMoneyToMaximumChildrenTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.DistributeMoneyToMaximumChildren;
+
+namespace LeetCode.Tests.Algorithms.DistributeMoneyToMaximumChildren;
+
+public abstract class DistributeMoneyToMaximumChildrenTestsBase<T> where T : IDistributeMoneyToMaximumChildren, new()
+{
+    [TestMethod]
+    [DataRow(20, 3, 1)]
+    [DataRow(16, 2, 2)]
+    [DataRow(4, 2, 0)]
+    [DataRow(12, 3, 1)]
+    public void DistMoney_WithVariousMoneyAndChildrenValues_ReturnsExpectedResults(int money, int children,
+        int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.DistMoney(money, children);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
