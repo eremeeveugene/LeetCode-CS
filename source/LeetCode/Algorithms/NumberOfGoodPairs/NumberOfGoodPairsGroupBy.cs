a9ca90e98c00780b1ad66be7b0035f2c10fe0f98
eugene <eremeeveugene@gmail.com>
2024-02-01 20:47:55 +0200

    Number of Good Pairs (#130)

---
+++ source/LeetCode/Algorithms/NumberOfGoodPairs/NumberOfGoodPairsGroupBy.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.NumberOfGoodPairs;
+
+/// <inheritdoc />
+public class NumberOfGoodPairsGroupBy : INumberOfGoodPairs
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int NumIdenticalPairs(int[] nums)
+    {
+        var groups = nums.GroupBy(n => n);
+
+        return groups.Sum(group => GetPairsCount(group.Count()));
+    }
+
+    private static int GetPairsCount(int count)
+    {
+        return count * (count - 1) / 2;
+    }
+}
---
+++ source/LeetCode/Algorithms/NumberOfGoodPairs/NumberOfGoodPairsIterative.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.NumberOfGoodPairs;
+
+/// <inheritdoc />
+public class NumberOfGoodPairsIterative : INumberOfGoodPairs
+{
+    /// <summary>
+    ///     Time complexity - O(n^2)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int NumIdenticalPairs(int[] nums)
+    {
+        var pairs = 0;
+
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    pairs++;
+                }
+            }
+        }
+
+        return pairs;
+    }
+}
