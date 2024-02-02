6563ebe463a6096a798f59c42807fb3a0a937242
eugene <eremeeveugene@gmail.com>
2024-02-02 18:53:43 +0200

    Smallest Even Multiple (#136)

---
+++ source/LeetCode/Algorithms/SmallestEvenMultiple/SmallestEvenMultipleConstant.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SmallestEvenMultiple;
+
+/// <inheritdoc />
+public class SmallestEvenMultipleConstant : ISmallestEvenMultiple
+{
+    /// <summary>
+    ///     Time complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public int SmallestEvenMultiple(int n)
+    {
+        if (n % 2 == 0)
+        {
+            return n;
+        }
+
+        return n * 2;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/SmallestEvenMultiple/SmallestEvenMultipleTestsBase.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SmallestEvenMultiple;
+
+namespace LeetCode.Tests.Algorithms.SmallestEvenMultiple;
+
+public abstract class SmallestEvenMultipleTestsBase<T> where T : ISmallestEvenMultiple, new()
+{
+    [TestMethod]
+    [DataRow(5, 10)]
+    [DataRow(6, 6)]
+    public void SmallestEvenMultiple_WithPositiveAndEqualInputs_ReturnsExpectedResult(int n, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.SmallestEvenMultiple(n);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
