be45d47e057f50d46161449b5bb5e1ca8ce66df0
eugene <eremeeveugene@gmail.com>
2024-02-02 22:34:50 +0200

    Calculate Delayed Arrival Time (#150)

---
+++ source/LeetCode/Algorithms/CalculateDelayedArrivalTime/CalculateDelayedArrivalTimeConstant.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CalculateDelayedArrivalTime;
+
+/// <inheritdoc />
+public class CalculateDelayedArrivalTimeConstant : ICalculateDelayedArrivalTime
+{
+    /// <summary>
+    ///     Time complexity - O(1)
+    /// </summary>
+    /// <param name="arrivalTime"></param>
+    /// <param name="delayedTime"></param>
+    /// <returns></returns>
+    public int FindDelayedArrivalTime(int arrivalTime, int delayedTime)
+    {
+        return (arrivalTime + delayedTime) % 24;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CalculateDelayedArrivalTime/CalculateDelayedArrivalTimeTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CalculateDelayedArrivalTime;
+
+namespace LeetCode.Tests.Algorithms.CalculateDelayedArrivalTime;
+
+public abstract class CalculateDelayedArrivalTimeTestsBase<T> where T : ICalculateDelayedArrivalTime, new()
+{
+    [TestMethod]
+    [DataRow(15, 5, 20)]
+    [DataRow(13, 11, 0)]
+    public void FindDelayedArrivalTime_WithArrivalAndDelayedTime_ReturnsExpectedArrivalTime(int arrivalTime,
+        int delayedTime, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FindDelayedArrivalTime(arrivalTime, delayedTime);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
