97f0266f0846927099589dede947569dae6032d6
eugene <eremeeveugene@gmail.com>
2024-02-02 20:27:35 +0200

    XOR Operation in an Array (#143)

---
+++ source/LeetCode/Algorithms/XOROperationInAnArray/XOROperationInAnArrayIterative.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.XOROperationInAnArray;
+
+/// <inheritdoc />
+public class XOROperationInAnArrayIterative : IXOROperationInAnArray
+{
+    public int XorOperation(int n, int start)
+    {
+        var result = 0;
+
+        for (var i = 0; i < n; i++)
+        {
+            result ^= start + (2 * i);
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/XOROperationInAnArray/XOROperationInAnArrayTestsBase.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.XOROperationInAnArray;
+
+namespace LeetCode.Tests.Algorithms.XOROperationInAnArray;
+
+public abstract class XOROperationInAnArrayTestsBase<T> where T : IXOROperationInAnArray, new()
+{
+    [TestMethod]
+    [DataRow(5, 0, 8)]
+    [DataRow(4, 3, 8)]
+    public void XorOperation_WithStartAndNumberOfElements_ReturnsExpectedXorValue(int n, int start, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.XorOperation(n, start);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
