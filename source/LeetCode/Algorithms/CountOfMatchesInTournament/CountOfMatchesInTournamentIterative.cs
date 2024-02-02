abb30993e065c33789428cad11b36d9eeabed96d
eugene <eremeeveugene@gmail.com>
2024-02-02 20:12:58 +0200

    Count of Matches in Tournament (#140)

---
+++ source/LeetCode/Algorithms/CountOfMatchesInTournament/CountOfMatchesInTournamentIterative.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountOfMatchesInTournament;
+
+/// <inheritdoc />
+public class CountOfMatchesInTournamentIterative : ICountOfMatchesInTournament
+{
+    /// <summary>
+    /// Time complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public int NumberOfMatches(int n)
+    {
+        return n - 1;
+        //var numberOfMatches = 0;
+
+        //while (n > 1)
+        //{
+        //    var roundMatches = n / 2;
+
+        //    numberOfMatches += roundMatches;
+
+        //    n -= roundMatches;
+        //}
+
+        //return numberOfMatches;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountOfMatchesInTournament/CountOfMatchesInTournamentTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountOfMatchesInTournament;
+
+namespace LeetCode.Tests.Algorithms.CountOfMatchesInTournament;
+
+public abstract class CountOfMatchesInTournamentTestsBase<T> where T : ICountOfMatchesInTournament, new()
+{
+    [TestMethod]
+    [DataRow(7, 6)]
+    [DataRow(14, 13)]
+    public void NumberOfMatches_WithSingleEliminationTournamentInputs_ReturnsExpectedMatchesCount(int n,
+        int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.NumberOfMatches(n);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
