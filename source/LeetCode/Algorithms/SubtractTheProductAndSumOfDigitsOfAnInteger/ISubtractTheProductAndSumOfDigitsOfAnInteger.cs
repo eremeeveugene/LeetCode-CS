16e815b3ed24391b50aa53a5c7fc719b673332e2
eugene <eremeeveugene@gmail.com>
2024-02-02 19:20:05 +0200

    Subtract the Product and Sum of Digits of an Integer (#138)

---
+++ source/LeetCode/Algorithms/SubtractTheProductAndSumOfDigitsOfAnInteger/ISubtractTheProductAndSumOfDigitsOfAnInteger.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SubtractTheProductAndSumOfDigitsOfAnInteger;
+
+/// <summary>
+///     https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+/// </summary>
+public interface ISubtractTheProductAndSumOfDigitsOfAnInteger
+{
+    int SubtractProductAndSum(int n);
+}
