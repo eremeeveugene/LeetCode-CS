f7b27c26f73c325f5f7051edf558af8f43724ab8
eugene <eremeeveugene@gmail.com>
2024-02-02 22:18:54 +0200

    Maximum 69 Number (#148)

---
+++ source/LeetCode/Algorithms/Maximum69Number/Maximum69NumberIterative.cs
@@ -0,0 +1,56 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.Maximum69Number;
+
+/// <inheritdoc />
+public class Maximum69NumberIterative : IMaximum69Number
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+    public int Maximum69Number(int num)
+    {
+        var multiplier = 1;
+        var maximum = 9;
+
+        while (num != maximum)
+        {
+            if (num < maximum)
+            {
+                if (num + (3 * multiplier) > maximum)
+                {
+                    if (num + (3 * multiplier / 10) > maximum)
+                    {
+                        multiplier /= 10;
+                    }
+                    else
+                    {
+                        return num + (3 * multiplier / 10);
+                    }
+                }
+                else
+                {
+                    return num + (3 * multiplier);
+                }
+            }
+            else
+            {
+                multiplier *= 10;
+                maximum = (maximum * 10) + 9;
+            }
+        }
+
+        return num;
+    }
+}
---
+++ source/LeetCode/Algorithms/Maximum69Number/Maximum69NumberString.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.Maximum69Number;
+
+/// <inheritdoc />
+public class Maximum69NumberString : IMaximum69Number
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+    public int Maximum69Number(int num)
+    {
+        var numStr = num.ToString();
+
+        var index = numStr.IndexOf('6');
+
+        if (index != -1)
+        {
+            numStr = numStr[..index] + '9' + numStr[(index + 1)..];
+        }
+
+        return int.Parse(numStr);
+    }
+}
