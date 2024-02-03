a96cd69085f373967a964fcbc87cb9a0f9546b2d
eugene <eremeeveugene@gmail.com>
2024-02-03 19:49:20 +0200

    Find the Pivot Integer (#152)

---
+++ source/Tests/LeetCode.Tests/Algorithms/FindThePivotInteger/FindThePivotIntegerTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindThePivotInteger;
+
+namespace LeetCode.Tests.Algorithms.FindThePivotInteger;
+
+public abstract class FindThePivotIntegerTestsBase<T> where T : IFindThePivotInteger, new()
+{
+    [TestMethod]
+    [DataRow(8, 6)]
+    [DataRow(1, 1)]
+    [DataRow(4, -1)]
+    [DataRow(12, -1)]
+    public void PivotInteger_WithInput_ReturnsExpectedPivotOrMinusOne(int n, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.PivotInteger(n);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
