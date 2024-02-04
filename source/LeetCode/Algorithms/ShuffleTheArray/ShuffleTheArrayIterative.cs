b68f4584571dbb07205446ced383b74a5774aba1
eugene <eremeeveugene@gmail.com>
2024-02-04 21:09:30 +0200

    Shuffle the Array (#158)

---
+++ source/LeetCode/Algorithms/ShuffleTheArray/ShuffleTheArrayIterative.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ShuffleTheArray;
+
+/// <inheritdoc />
+public class ShuffleTheArrayIterative : IShuffleTheArray
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public int[] Shuffle(int[] nums, int n)
+    {
+        var result = new int[n * 2];
+
+        for (var i = 0; i < n; i++)
+        {
+            result[i * 2] = nums[i];
+        }
+
+        for (var i = 0; i < n; i++)
+        {
+            result[(i * 2) + 1] = nums[i + n];
+        }
+
+        return result;
+    }
+}
