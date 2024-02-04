1f366a2c81b19f38a795b1032b3f2ad8d89f477c
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-02-04 20:07:54 +0200

    Concatenation of Array

---
+++ source/LeetCode/Algorithms/ConcatenationOfArray/ConcatenationOfArrayIterative.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ConcatenationOfArray;
+
+public class ConcatenationOfArrayIterative : IConcatenationOfArray
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int[] GetConcatenation(int[] nums)
+    {
+        var result = new int[nums.Length * 2];
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            result[i] = nums[i];
+            result[nums.Length + i] = nums[i];
+        }
+
+        return result;
+    }
+}
