31eb232d12bba1dded4cfaa9d86215893de885bc
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-02-08 15:06:13 +0200

    Goal Parser Interpretation

---
+++ source/LeetCode/Algorithms/GoalParserInterpretation/GoalParserInterpretationReplace.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.GoalParserInterpretation;
+
+/// <inheritdoc />
+public class GoalParserInterpretationReplace : IGoalParserInterpretation
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="command"></param>
+    /// <returns></returns>
+    public string Interpret(string command)
+    {
+        return command.Replace("()", "o").Replace("(al)", "al");
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/GoalParserInterpretation/GoalParserInterpretationTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.GoalParserInterpretation;
+
+namespace LeetCode.Tests.Algorithms.GoalParserInterpretation;
+
+public abstract class GoalParserInterpretationTestsBase<T> where T : IGoalParserInterpretation, new()
+{
+    [TestMethod]
+    [DataRow("G()(al)", "Goal")]
+    [DataRow("G()()()()(al)", "Gooooal")]
+    [DataRow("(al)G(al)()()G", "alGalooG")]
+    public void Interpret_GivenCommandString_ReturnsExpectedString(string command, string expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.Interpret(command);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
