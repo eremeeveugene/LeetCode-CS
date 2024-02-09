d40812e69701f816125f3881ececb64b23694e82
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-02-09 20:26:52 +0200

    Perfect Squares

---
+++ source/LeetCode/Algorithms/PerfectSquares/IPerfectSquares.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.PerfectSquares;
+
+/// <summary>
+///     https://leetcode.com/problems/perfect-squares/description/
+/// </summary>
+public interface IPerfectSquares
+{
+    int NumSquares(int n);
+}
---
+++ source/LeetCode/Algorithms/PerfectSquares/PerfectSquaresIterative.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.PerfectSquares;
+
+/// <inheritdoc />
+public class PerfectSquaresIterative : IPerfectSquares
+{
+    /// <summary>
+    ///     Time complexity - O(n sqrt(n))
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public int NumSquares(int n)
+    {
+        var dp = new int[n + 1];
+
+        for (var i = 0; i <= n; i++)
+        {
+            dp[i] = int.MaxValue;
+        }
+
+        dp[0] = 0;
+
+        for (var i = 1; i <= n; i++)
+        {
+            for (var j = 1; j * j <= i; j++)
+            {
+                dp[i] = Math.Min(dp[i], dp[i - (j * j)] + 1);
+            }
+        }
+
+        return dp[n];
+    }
+}
