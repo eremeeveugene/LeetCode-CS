6e05354b940368809a9e7d2f48503f37bb36d265
eugene <eremeeveugene@gmail.com>
2024-02-09 20:29:02 +0200

    Perfect Squares (#178)

---
+++ source/Tests/LeetCode.Tests/Algorithms/PerfectSquares/PerfectSquaresTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.PerfectSquares;
+
+namespace LeetCode.Tests.Algorithms.PerfectSquares;
+
+public abstract class PerfectSquaresTestsBase<T> where T : IPerfectSquares, new()
+{
+    [TestMethod]
+    [DataRow(3, 3)]
+    [DataRow(12, 3)]
+    [DataRow(13, 2)]
+    public void Test(int n, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.NumSquares(n);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
