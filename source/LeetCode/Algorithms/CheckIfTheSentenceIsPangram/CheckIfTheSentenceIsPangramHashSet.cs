356d7c41d094e4a14235d9b7e67be9623403070d
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-02-10 19:40:51 +0200

    Check if the Sentence Is Pangram

---
+++ source/LeetCode/Algorithms/CheckIfTheSentenceIsPangram/CheckIfTheSentenceIsPangramHashSet.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CheckIfTheSentenceIsPangram;
+
+/// <inheritdoc />
+public class CheckIfTheSentenceIsPangramHashSet : ICheckIfTheSentenceIsPangram
+{
+    private readonly HashSet<char> _alphabetHashSet =
+    [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+        'w', 'x', 'y', 'z'
+    ];
+
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="sentence"></param>
+    /// <returns></returns>
+    public bool CheckIfPangram(string sentence)
+    {
+        if (sentence.Length < 26)
+        {
+            return false;
+        }
+
+        var sentenceHashSet = new HashSet<char>(sentence);
+
+        return _alphabetHashSet.All(sentenceHashSet.Contains);
+    }
+}
---
+++ source/LeetCode/Algorithms/CheckIfTheSentenceIsPangram/CheckIfTheSentenceIsPangramIterative.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CheckIfTheSentenceIsPangram;
+
+/// <inheritdoc />
+public class CheckIfTheSentenceIsPangramIterative : ICheckIfTheSentenceIsPangram
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    /// </summary>
+    /// <param name="sentence"></param>
+    /// <returns></returns>
+    public bool CheckIfPangram(string sentence)
+    {
+        if (sentence.Length < 26)
+        {
+            return false;
+        }
+
+        var seen = 0;
+
+        foreach (var index in sentence.Select(c => c - 'a'))
+        {
+            if (index is >= 0 and < 26)
+            {
+                seen |= 1 << index;
+            }
+
+            if (seen == 0x3FFFFFF)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
