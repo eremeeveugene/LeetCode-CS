432a0e6a80896f6f1a5ed87d9c4c58fc89e99528
eugene <eremeeveugene@gmail.com>
2024-02-10 19:46:43 +0200

    Check if the Sentence Is Pangram (#183)

---
+++ source/LeetCode/Algorithms/CheckIfTheSentenceIsPangram/ICheckIfTheSentenceIsPangram.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CheckIfTheSentenceIsPangram;
+
+/// <summary>
+///     https://leetcode.com/problems/check-if-the-sentence-is-pangram/description/
+/// </summary>
+public interface ICheckIfTheSentenceIsPangram
+{
+    bool CheckIfPangram(string sentence);
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CheckIfTheSentenceIsPangram/CheckIfTheSentenceIsPangramTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CheckIfTheSentenceIsPangram;
+
+namespace LeetCode.Tests.Algorithms.CheckIfTheSentenceIsPangram;
+
+public abstract class CheckIfTheSentenceIsPangramTestsBase<T> where T : ICheckIfTheSentenceIsPangram, new()
+{
+    [TestMethod]
+    [DataRow("abcdefghijklmnopqrstuvxwyz", true)]
+    [DataRow("thequickbrownfoxjumpsoverthelazydog", true)]
+    [DataRow("leetcode", false)]
+    [DataRow("jwtucoucmdfwxxqnxzkaxoglszmfrcvjoiunqqausaxxaaijyqdqgvdnqcaihwilqkpivenpnekioyqujrdrovqrlxovcucjqzjsxmllfgndfprctxvxwlzjtciqxgsxfwhmuzqvlksyuztoetyjugmswfjtawwaqmwyxmvo", false)]
+    public void CheckIfPangram_WithGivenSentence_ReturnsTrueIfPangramFalseOtherwise(string sentence,
+        bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CheckIfPangram(sentence);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
