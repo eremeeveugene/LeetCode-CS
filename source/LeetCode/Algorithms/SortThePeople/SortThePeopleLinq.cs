605561895d4e2526562b81565640da71c684cb74
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-02-11 12:20:12 +0200

    Sort the People

---
+++ source/LeetCode/Algorithms/SortThePeople/SortThePeopleLinq.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SortThePeople;
+
+/// <inheritdoc />
+public class SortThePeopleLinq : ISortThePeople
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    /// </summary>
+    /// <param name="names"></param>
+    /// <param name="heights"></param>
+    /// <returns></returns>
+    public string[] SortPeople(string[] names, int[] heights)
+    {
+        return names.Zip(heights, (name, height) => new { Name = name, Height = height })
+            .OrderByDescending(person => person.Height)
+            .Select(person => person.Name)
+            .ToArray();
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/SortThePeople/SortThePeopleTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SortThePeople;
+
+namespace LeetCode.Tests.Algorithms.SortThePeople;
+
+public abstract class SortThePeopleTestsBase<T> where T : ISortThePeople, new()
+{
+    [TestMethod]
+    [DataRow(new[] { "Mary", "John", "Emma" }, new[] { 180, 165, 170 }, new[] { "Mary", "Emma", "John" })]
+    [DataRow(new[] { "Alice", "Bob", "Bob" }, new[] { 155, 185, 150 }, new[] { "Bob", "Alice", "Bob" })]
+    public void SortPeople_WithNamesAndHeights_ReturnsSortedNamesByHeight(string[] names, int[] heights,
+        string[] expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.SortPeople(names, heights);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
