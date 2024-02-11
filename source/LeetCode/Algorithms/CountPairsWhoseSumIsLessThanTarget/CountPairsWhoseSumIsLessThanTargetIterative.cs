9737164700a0bdbbfaa9a38d9cb31a5deeaeab3d
eugene <eremeeveugene@gmail.com>
2024-02-11 12:04:43 +0200

    Count Pairs Whose Sum is Less than Target (#187)

---
+++ source/LeetCode/Algorithms/CountPairsWhoseSumIsLessThanTarget/CountPairsWhoseSumIsLessThanTargetIterative.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountPairsWhoseSumIsLessThanTarget;
+
+/// <inheritdoc />
+public class CountPairsWhoseSumIsLessThanTargetIterative : ICountPairsWhoseSumIsLessThanTarget
+{
+    /// <summary>
+    ///     Time complexity - O(n^2)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    public int CountPairs(IList<int> nums, int target)
+    {
+        var count = 0;
+
+        for (var i = 0; i < nums.Count - 1; i++)
+        {
+            for (var j = i + 1; j < nums.Count; j++)
+            {
+                if (nums[i] + nums[j] < target)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
