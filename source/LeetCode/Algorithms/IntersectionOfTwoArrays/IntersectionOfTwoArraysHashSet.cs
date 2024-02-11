59e1d6fdbdb1208b8cddea269f6e7b4886199ff5
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-02-11 20:04:21 +0200

    Intersection of Two Arrays

---
+++ source/LeetCode/Algorithms/IntersectionOfTwoArrays/IntersectionOfTwoArraysHashSet.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.IntersectionOfTwoArrays;
+
+/// <inheritdoc />
+public class IntersectionOfTwoArraysHashSet : IIntersectionOfTwoArrays
+{
+    /// <summary>
+    ///     Time complexity - O(n + m)
+    /// </summary>
+    /// <param name="nums1"></param>
+    /// <param name="nums2"></param>
+    /// <returns></returns>
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        var nums1HashSet = new HashSet<int>(nums1);
+        var intersectionHashSet = new HashSet<int>();
+
+        foreach (var num in nums2)
+        {
+            if (nums1HashSet.Contains(num))
+            {
+                intersectionHashSet.Add(num);
+            }
+        }
+
+        return [.. intersectionHashSet];
+    }
+}
---
+++ source/LeetCode/Algorithms/IntersectionOfTwoArrays/IntersectionOfTwoArraysLinq.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.IntersectionOfTwoArrays;
+
+/// <inheritdoc />
+public class IntersectionOfTwoArraysLinq : IIntersectionOfTwoArrays
+{
+    /// <summary>
+    ///     Time complexity - O(n + m)
+    /// </summary>
+    /// <param name="nums1"></param>
+    /// <param name="nums2"></param>
+    /// <returns></returns>
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        var set1 = new HashSet<int>(nums1);
+        var intersection = new HashSet<int>();
+
+        foreach (var num in nums2)
+        {
+            if (set1.Contains(num))
+            {
+                intersection.Add(num);
+            }
+        }
+
+        return [.. intersection];
+    }
+}
