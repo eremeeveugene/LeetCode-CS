a297d903450f92e9575aa06cf1d99766a54c9282
eugene <eremeeveugene@gmail.com>
2024-02-12 20:18:15 +0200

    Number of Steps to Reduce a Number to Zero (#197)

---
+++ source/LeetCode/Algorithms/NumberOfStepsToReduceNumberToZero/NumberOfStepsToReduceNumberToZeroBitwise.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Numerics;
+
+namespace LeetCode.Algorithms.NumberOfStepsToReduceNumberToZero;
+
+/// <inheritdoc />
+public class NumberOfStepsToReduceNumberToZeroBitwise : INumberOfStepsToReduceNumberToZero
+{
+    /// <summary>
+    ///     Time complexity - O(1)
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+    public int NumberOfSteps(int num)
+    {
+        return BitOperations.Log2((uint)num) + BitOperations.PopCount((uint)num);
+    }
+}
---
+++ source/LeetCode/Algorithms/NumberOfStepsToReduceNumberToZero/NumberOfStepsToReduceNumberToZeroIterative.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.NumberOfStepsToReduceNumberToZero;
+
+/// <inheritdoc />
+public class NumberOfStepsToReduceNumberToZeroIterative : INumberOfStepsToReduceNumberToZero
+{
+    /// <summary>
+    ///     Time complexity - O(log n)
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+    public int NumberOfSteps(int num)
+    {
+        var steps = 0;
+
+        while (num > 0)
+        {
+            if (num % 2 == 0)
+            {
+                num /= 2;
+            }
+            else
+            {
+                num--;
+            }
+
+            steps++;
+        }
+
+        return steps;
+    }
+}
