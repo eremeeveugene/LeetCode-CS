4860823e7bd0b25109e4684832ad7252c1f84248
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-02-17 11:35:14 +0200

    Binary Tree Inorder Traversal

---
+++ source/LeetCode/Algorithms/BinaryTreeInorderTraversal/BinaryTreeInorderTraversalStack.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.BinaryTreeInorderTraversal;
+
+/// <inheritdoc />
+public class BinaryTreeInorderTraversalStack : IBinaryTreeInorderTraversal
+{
+    /// <summary>
+    ///     Time complexity - O (n)
+    /// </summary>
+    /// <param name="treeNode"></param>
+    /// <returns></returns>
+    public IList<int> InorderTraversal(TreeNode? treeNode)
+    {
+        var result = new List<int>();
+        var stack = new Stack<TreeNode>();
+        var current = treeNode;
+
+        while (current != null || stack.Count > 0)
+        {
+            while (current != null)
+            {
+                stack.Push(current);
+                current = current.left;
+            }
+
+            current = stack.Pop();
+            result.Add(current.val);
+
+            current = current.right;
+        }
+
+        return result;
+    }
+}
