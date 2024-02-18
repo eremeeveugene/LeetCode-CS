b9d130197815346394500a524ed34bdff91256ce
eugene <eremeeveugene@gmail.com>
2024-02-18 14:04:59 +0200

    Is Subsequence (#215)

---
+++ source/LeetCode/Algorithms/IsSubsequence/IsSubsequenceIterative.cs
@@ -0,0 +1,63 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.IsSubsequence;
+
+/// <inheritdoc />
+public class IsSubsequenceIterative : IIsSubsequence
+{
+    /// <summary>
+    ///     Time complexity - O(n*m)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public bool IsSubsequence(string s, string t)
+    {
+        if (s.Length == 0)
+        {
+            return true;
+        }
+
+        if (t.Length == 0)
+        {
+            return false;
+        }
+
+        var tIndex = 0;
+        var sIndex = 0;
+
+        while (tIndex < t.Length && sIndex < s.Length)
+        {
+            var isFound = false;
+
+            while (tIndex < t.Length && !isFound)
+            {
+                if (s[sIndex] == t[tIndex])
+                {
+                    isFound = true;
+                }
+
+                tIndex++;
+            }
+
+            if (!isFound)
+            {
+                return false;
+            }
+
+            sIndex++;
+        }
+
+        return sIndex == s.Length;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/IsSubsequence/IsSubsequenceTestsBase.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.IsSubsequence;
+
+namespace LeetCode.Tests.Algorithms.IsSubsequence;
+
+public abstract class IsSubsequenceTestsBase<T> where T : IIsSubsequence, new()
+{
+    [TestMethod]
+    [DataRow("", "ahbgdc", true)]
+    [DataRow("abc", "", false)]
+    [DataRow("abc", "ahbgdc", true)]
+    [DataRow("axc", "ahbgdc", false)]
+    [DataRow("acb", "ahbgdc", false)]
+    [DataRow("aaaaaa", "bbaaaa", false)]
+    [DataRow("bcd",
+        "uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuubcd",
+        true)]
+    public void IsSubsequence_WithStringSAndT_VerifiesIfSSubsequenceOfT(string s, string t, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsSubsequence(s, t);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
