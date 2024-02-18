69c00e70b27aa564d25cf42de8ec67c8e4c289f2
eugene <eremeeveugene@gmail.com>
2024-02-18 13:14:31 +0200

    Best Time to Buy and Sell Stock (#213)

---
+++ source/LeetCode/Algorithms/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStockDynamicProgramming.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.BestTimeToBuyAndSellStock;
+
+/// <inheritdoc />
+public class BestTimeToBuyAndSellStockDynamicProgramming : IBestTimeToBuyAndSellStock
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="prices"></param>
+    /// <returns></returns>
+    public int MaxProfit(int[] prices)
+    {
+        var maxProfit = 0;
+
+        var min = prices[0];
+
+        for (var i = 1; i < prices.Length; i++)
+        {
+            if (min > prices[i])
+            {
+                min = prices[i];
+            }
+            else
+            {
+                var profit = prices[i] - min;
+
+                if (profit > maxProfit)
+                {
+                    maxProfit = profit;
+                }
+            }
+        }
+
+        return maxProfit;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStockTestsBase.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.BestTimeToBuyAndSellStock;
+
+namespace LeetCode.Tests.Algorithms.BestTimeToBuyAndSellStock;
+
+public abstract class BestTimeToBuyAndSellStockTestsBase<T> where T : IBestTimeToBuyAndSellStock, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 7, 1, 5, 3, 6, 4 }, 5)]
+    [DataRow(new[] { 7, 6, 4, 3, 1 }, 0)]
+    public void MaxProfit_GivenPriceArray_ReturnsMaximumProfit(int[] prices, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaxProfit(prices);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
