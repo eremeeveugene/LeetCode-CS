5af1e8ef0fc0aafa459ff5b4981aef61da21712d
eugene <eremeeveugene@gmail.com>
2024-02-19 22:22:10 +0200

    Count the Digits That Divide a Number (#219)

---
+++ source/LeetCode/Algorithms/CountTheDigitsThatDivideNumber/CountTheDigitsThatDivideNumberIterative.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountTheDigitsThatDivideNumber;
+
+/// <inheritdoc />
+public class CountTheDigitsThatDivideNumberIterative : ICountTheDigitsThatDivideNumber
+{
+    /// <summary>
+    ///     Time complexity - O(log n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+    public int CountDigits(int num)
+    {
+        var count = 0;
+
+        var currentNum = num;
+
+        while (currentNum > 0)
+        {
+            var digit = currentNum % 10;
+
+            if (num % digit == 0)
+            {
+                count++;
+            }
+
+            currentNum /= 10;
+        }
+
+        return count;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountTheDigitsThatDivideNumber/CountTheDigitsThatDivideNumberTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountTheDigitsThatDivideNumber;
+
+namespace LeetCode.Tests.Algorithms.CountTheDigitsThatDivideNumber;
+
+public abstract class CountTheDigitsThatDivideNumberTestsBase<T> where T : ICountTheDigitsThatDivideNumber, new()
+{
+    [TestMethod]
+    [DataRow(7, 1)]
+    [DataRow(121, 2)]
+    [DataRow(1248, 4)]
+    public void CountDigits_GivenNumber_ReturnsNumberOfDigits(int n, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountDigits(n);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
