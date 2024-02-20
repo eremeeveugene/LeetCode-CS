262f5595ceca522745e123dde6c477b100c54a2f
eugene <eremeeveugene@gmail.com>
2024-02-20 17:00:28 +0200

    Min Cost Climbing Stairs (#223)

---
+++ source/LeetCode/Algorithms/MinCostClimbingStairs/MinCostClimbingStairsDynamicProgramming.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinCostClimbingStairs;
+
+/// <inheritdoc />
+public class MinCostClimbingStairsDynamicProgramming : IMinCostClimbingStairs
+{
+    public int MinCostClimbingStairs(int[] cost)
+    {
+        if (cost.Length == 0)
+        {
+            return 0;
+        }
+
+        var costs = new int[cost.Length + 1];
+
+        costs[0] = 0;
+        costs[1] = 0;
+
+        for (var i = 2; i <= cost.Length; i++)
+        {
+            costs[i] = Math.Min(costs[i - 2] + cost[i - 2], costs[i - 1] + cost[i - 1]);
+        }
+
+        return costs[cost.Length];
+    }
+}
