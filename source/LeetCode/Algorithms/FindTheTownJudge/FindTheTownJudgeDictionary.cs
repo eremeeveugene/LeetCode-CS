2ff6c85cec46567daead3d253bd6b41655978a54
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-02-22 19:54:50 +0200

    Find the Town Judge

---
+++ source/LeetCode/Algorithms/FindTheTownJudge/FindTheTownJudgeDictionary.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindTheTownJudge;
+
+public class FindTheTownJudgeDictionary : IFindTheTownJudge
+{
+    /// <summary>
+    ///     Time complexity - O(n + t)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="trust"></param>
+    /// <returns></returns>
+    public int FindJudge(int n, int[][] trust)
+    {
+        var trustCounts = new int[n + 1];
+        var trustsSomeone = new bool[n + 1];
+
+        foreach (var t in trust)
+        {
+            trustCounts[t[0]]--;
+            trustCounts[t[1]]++;
+            trustsSomeone[t[0]] = true;
+        }
+
+        for (var i = 1; i <= n; i++)
+        {
+            if (trustCounts[i] == n - 1 && !trustsSomeone[i])
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FindTheTownJudge/FindTheTownJudgeTestsBase.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindTheTownJudge;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.FindTheTownJudge;
+
+public abstract class FindTheTownJudgeTestsBase<T> where T : IFindTheTownJudge, new()
+{
+    [TestMethod]
+    [DataRow(1, "[]", 1)]
+    [DataRow(2, "[]", -1)]
+    [DataRow(2, "[[1,2]]", 2)]
+    [DataRow(3, "[[1,3],[2,3]]", 3)]
+    [DataRow(3, "[[1,3],[2,3],[3,1]]", -1)]
+    public void FindJudge_WithDifferentTrustRelationships_ReturnsCorrectJudgeOrMinusOne(int n, string jsonTrust,
+        int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        var trust = JsonConvertHelper<int>.JsonArrayToJaggedArray(jsonTrust);
+
+        // Act
+        var actualResult = solution.FindJudge(n, trust);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
