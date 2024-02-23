1f1ff84b6804bf0b4ccc67e2f8413f6a363c1ab5
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-02-23 19:47:42 +0200

    Cheapest Flights Within K Stops

---
+++ source/LeetCode/Algorithms/CheapestFlightsWithinKStops/ICheapestFlightsWithinKStops.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CheapestFlightsWithinKStops;
+
+/// <summary>
+///     https://leetcode.com/problems/cheapest-flights-within-k-stops/
+/// </summary>
+public interface ICheapestFlightsWithinKStops
+{
+    int FindCheapestPrice(int n, int[][] flights, int src, int dst, int k);
+}
