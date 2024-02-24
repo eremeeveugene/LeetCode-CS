314d9af85d11cce1ca0e7a856f4723f7c7e0e523
eugene <eremeeveugene@gmail.com>
2024-02-24 14:59:23 +0200

    Minimum Number Game (#238)

---
+++ source/LeetCode/Algorithms/MinimumNumberGame/MinimumNumberGameSorting.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumNumberGame;
+
+/// <inheritdoc />
+public class MinimumNumberGameSorting : IMinimumNumberGame
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int[] NumberGame(int[] nums)
+    {
+        Array.Sort(nums);
+
+        var result = new int[nums.Length];
+
+        for (var i = 0; i < nums.Length; i += 2)
+        {
+            result[i] = nums[i + 1];
+            result[i + 1] = nums[i];
+        }
+
+        return result;
+    }
+}
