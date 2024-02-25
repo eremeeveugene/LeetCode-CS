97fc2a4439f974a8187587cf0693271c47a2c919
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-02-25 13:22:05 +0200

    Symmetric Tree

---
+++ source/LeetCode/Algorithms/SymmetricTree/SymmetricTreeRecursive.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.SymmetricTree;
+
+/// <inheritdoc />
+public class SymmetricTreeRecursive : ISymmetricTree
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="root"></param>
+    /// <returns></returns>
+    public bool IsSymmetric(TreeNode? root)
+    {
+        return IsSymmetric(root?.left, root?.right);
+    }
+
+    private static bool IsSymmetric(TreeNode? left, TreeNode? right)
+    {
+        if (left == null && right == null)
+        {
+            return true;
+        }
+
+        if (left == null || right == null)
+        {
+            return false;
+        }
+
+        if (left.val != right.val)
+        {
+            return false;
+        }
+
+        return IsSymmetric(left.left, right.right) && IsSymmetric(left.right, right.left);
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/SymmetricTree/SymmetricTreeTestsBase.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SymmetricTree;
+using LeetCode.Core.Helpers;
+using LeetCode.Core.Models;
+
+namespace LeetCode.Tests.Algorithms.SymmetricTree;
+
+public abstract class SymmetricTreeTestsBase<T> where T : ISymmetricTree, new()
+{
+    [TestMethod]
+    [DataRow("[]", true)]
+    [DataRow("[1,2,2,3,4,4,3]", true)]
+    [DataRow("[1,2,2,null,3,null,3]", false)]
+    public void IsSymmetric_GivenBinaryTreeAsJsonArray_ReturnsExpectedSymmetryOutcome(string jsonArrayRoot,
+        bool expectedResult)
+    {
+        // Arrange
+        IList<int?> arrayRoot = JsonConvertHelper<int?>.JsonArrayToList(jsonArrayRoot);
+        var root = TreeNode.BuildTree(arrayRoot);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsSymmetric(root);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
