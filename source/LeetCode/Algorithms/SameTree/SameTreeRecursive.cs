a3e624bfb0d72f16ed597bda5bf93f74177fc23f
eugene <eremeeveugene@gmail.com>
2024-02-25 12:46:03 +0200

    Same Tree (#242)

---
+++ source/LeetCode/Algorithms/SameTree/SameTreeRecursive.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.SameTree;
+
+/// <inheritdoc />
+public class SameTreeRecursive : ISameTree
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(log n) for a balanced tree, O(n) for an unbalanced tree
+    /// </summary>
+    /// <param name="p"></param>
+    /// <param name="q"></param>
+    /// <returns></returns>
+    public bool IsSameTree(TreeNode? p, TreeNode? q)
+    {
+        if (p == null && q == null)
+        {
+            return true;
+        }
+
+        if (p == null || q == null)
+        {
+            return false;
+        }
+
+        if (p.val != q.val)
+        {
+            return false;
+        }
+
+        return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/SameTree/SameTreeTestsBase.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SameTree;
+using LeetCode.Core.Helpers;
+using LeetCode.Core.Models;
+
+namespace LeetCode.Tests.Algorithms.SameTree;
+
+public abstract class SameTreeTestsBase<T> where T : ISameTree, new()
+{
+    [TestMethod]
+    [DataRow("[1,2,3]", "[1,2,3]", true)]
+    [DataRow("[1,2]", "[1,null,2]", false)]
+    [DataRow("[1,2,1]", "[1,1,2]", false)]
+    public void IsSameTree_GivenTwoBinaryTreesFromJsonArrays_ReturnsExpectedBoolean(string jsonArrayP,
+        string jsonArrayQ, bool expectedResult)
+    {
+        // Arrange
+        IList<int?> pArray = JsonConvertHelper<int?>.JsonArrayToList(jsonArrayP);
+        var p = TreeNode.BuildTree(pArray);
+
+        IList<int?> qArray = JsonConvertHelper<int?>.JsonArrayToList(jsonArrayQ);
+        var q = TreeNode.BuildTree(qArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsSameTree(p, q);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
