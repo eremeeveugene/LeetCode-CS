c08749db9ed02cbfe1bd2222d6875d02e818c88e
eugene <eremeeveugene@gmail.com>
2024-02-27 21:01:13 +0200

    Diameter of Binary Tree (#248)

---
+++ source/LeetCode/Algorithms/DiameterOfBinaryTree/DiameterOfBinaryTreeDepthFirst.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.DiameterOfBinaryTree;
+
+/// <inheritdoc />
+public class DiameterOfBinaryTreeDepthFirst : IDiameterOfBinaryTree
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(log n) for a balanced tree, O(n) for a skewed tree
+    /// </summary>
+    /// <param name="root"></param>
+    /// <returns></returns>
+    public int DiameterOfBinaryTree(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        var maxDiameter = 0;
+
+        GetMaxDepth(root, ref maxDiameter);
+
+        return maxDiameter;
+    }
+
+    private static int GetMaxDepth(TreeNode node, ref int maxDiameter)
+    {
+        var leftDepth = 0;
+
+        if (node.left != null)
+        {
+            leftDepth = GetMaxDepth(node.left, ref maxDiameter);
+        }
+
+        var rightDepth = 0;
+
+        if (node.right != null)
+        {
+            rightDepth = GetMaxDepth(node.right, ref maxDiameter);
+        }
+
+        maxDiameter = Math.Max(maxDiameter, leftDepth + rightDepth);
+
+        return Math.Max(leftDepth, rightDepth) + 1;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/DiameterOfBinaryTree/DiameterOfBinaryTreeTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.DiameterOfBinaryTree;
+using LeetCode.Core.Helpers;
+using LeetCode.Core.Models;
+
+namespace LeetCode.Tests.Algorithms.DiameterOfBinaryTree;
+
+public abstract class DiameterOfBinaryTreeTestsBase<T> where T : IDiameterOfBinaryTree, new()
+{
+    [TestMethod]
+    [DataRow("[1,2,3,4,5]", 3)]
+    [DataRow("[1,2]", 1)]
+    public void DiameterOfBinaryTree_GivenJsonArrayRepresentation_ReturnsExpectedDiameter(string jsonArrayRoot,
+        int expectedResult)
+    {
+        // Arrange
+        IList<int?> arrayRoot = JsonConvertHelper<int?>.JsonArrayToList(jsonArrayRoot);
+        var root = TreeNode.BuildTree(arrayRoot);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.DiameterOfBinaryTree(root);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
