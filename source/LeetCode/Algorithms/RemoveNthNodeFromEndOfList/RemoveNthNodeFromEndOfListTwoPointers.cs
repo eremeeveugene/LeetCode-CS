f28909a0697cfb24d58ade67e9fa2ba8535fb539
eugene <eremeeveugene@gmail.com>
2024-03-03 13:14:24 +0200

    Remove Nth Node From End of List (#259)

---
+++ source/LeetCode/Algorithms/RemoveNthNodeFromEndOfList/RemoveNthNodeFromEndOfListTwoPointers.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.RemoveNthNodeFromEndOfList;
+
+/// <inheritdoc />
+public class RemoveNthNodeFromEndOfListTwoPointers : IRemoveNthNodeFromEndOfList
+{
+    /// <summary>
+    ///     Time complexity - O(n), where n is the  number of nodes in the linked list
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="head"></param>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public ListNode? RemoveNthFromEnd(ListNode? head, int n)
+    {
+        var dummyHead = new ListNode { next = head };
+
+        var slowPointer = dummyHead;
+        var fastPointer = dummyHead;
+
+        for (var i = 0; i <= n; i++)
+        {
+            fastPointer = fastPointer?.next;
+        }
+
+        while (fastPointer != null)
+        {
+            slowPointer = slowPointer?.next;
+            fastPointer = fastPointer.next;
+        }
+
+        if (slowPointer != null)
+        {
+            slowPointer.next = slowPointer.next?.next;
+        }
+
+        return dummyHead.next;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/RemoveNthNodeFromEndOfList/RemoveNthNodeFromEndOfListTestsBase.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.RemoveNthNodeFromEndOfList;
+using LeetCode.Core.Extensions;
+using LeetCode.Tests.Extensions;
+
+namespace LeetCode.Tests.Algorithms.RemoveNthNodeFromEndOfList;
+
+public abstract class RemoveNthNodeFromEndOfListTestsBase<T> where T : IRemoveNthNodeFromEndOfList, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1, 2, 3, 4, 5 }, 2, new[] { 1, 2, 3, 5 })]
+    [DataRow(new[] { 1 }, 1, new int[] { })]
+    [DataRow(new[] { 1, 2 }, 1, new[] { 1 })]
+    [DataRow(new[] { 1, 2 }, 2, new[] { 2 })]
+    public void RemoveNthFromEnd_WhenCalled_RemovesNthNodeFromEndAndReturnsCorrectList(int[] headArray, int n,
+        int[] expectedResultArray)
+    {
+        // Arrange
+        var head = headArray.ToListNode();
+
+        var expectedResult = expectedResultArray.ToListNode();
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.RemoveNthFromEnd(head, n);
+
+        // Assert
+        AssertExtensions.AreListNodesEqual(expectedResult, actualResult);
+    }
+}
