d642262793d9d9f20c07b69ea27034a5c8e45b99
eugene <eremeeveugene@gmail.com>
2024-03-05 16:57:30 +0200

    Bag of Tokens (#261)

---
+++ source/LeetCode/Algorithms/BagOfTokens/BagOfTokensTwoPointers.cs
@@ -0,0 +1,55 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.BagOfTokens;
+
+/// <inheritdoc />
+public class BagOfTokensTwoPointers : IBagOfTokens
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="tokens"></param>
+    /// <param name="power"></param>
+    /// <returns></returns>
+    public int BagOfTokensScore(int[] tokens, int power)
+    {
+        var score = 0;
+
+        Array.Sort(tokens);
+
+        var left = 0;
+        var right = tokens.Length - 1;
+
+        while (left <= right)
+        {
+            if (power >= tokens[left])
+            {
+                power -= tokens[left];
+                score++;
+                left++;
+            }
+            else if (score >= 1 && left < right)
+            {
+                power += tokens[right];
+                score--;
+                right--;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return score;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/BagOfTokens/BagOfTokensTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.BagOfTokens;
+
+namespace LeetCode.Tests.Algorithms.BagOfTokens;
+
+public abstract class BagOfTokensTestsBase<T> where T : IBagOfTokens, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 100 }, 50, 0)]
+    [DataRow(new[] { 200, 100 }, 150, 1)]
+    [DataRow(new[] { 100, 200, 300, 400 }, 200, 2)]
+    public void BagOfTokensScore_WithTokensAndPower_ReturnsExpectedScore(int[] tokens, int power, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.BagOfTokensScore(tokens, power);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
