dbfc6882aab70c9ed1126cb70bfb365c7943177a
eugene <eremeeveugene@gmail.com>
2024-03-06 18:48:21 +0200

    Linked List Cycle (#265)

---
+++ source/LeetCode/Algorithms/LinkedListCycle/ILinkedListCycle.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.LinkedListCycle;
+
+/// <summary>
+///     https://leetcode.com/problems/linked-list-cycle/description/
+/// </summary>
+public interface ILinkedListCycle
+{
+    bool HasCycle(ListNode? head);
+}
---
+++ source/LeetCode/Algorithms/LinkedListCycle/LinkedListCycleTwoPointers.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.LinkedListCycle;
+
+/// <inheritdoc />
+public class LinkedListCycleTwoPointers : ILinkedListCycle
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="head"></param>
+    /// <returns></returns>
+    public bool HasCycle(ListNode? head)
+    {
+        if (head == null)
+        {
+            return false;
+        }
+
+        var slow = head;
+        var fast = head.next;
+
+        while (fast is { next: not null })
+        {
+            if (slow == fast)
+            {
+                return true;
+            }
+
+            slow = slow?.next;
+            fast = fast.next.next;
+        }
+
+        return false;
+    }
+}
