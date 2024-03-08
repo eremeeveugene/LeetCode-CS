83ca3b1fd98b6e2810ec4a457c49c223e352548b
eugene <eremeeveugene@gmail.com>
2024-03-08 13:34:40 +0200

    Count Elements With Maximum Frequency (#267)

---
+++ source/LeetCode/Algorithms/CountElementsWithMaximumFrequency/CountElementsWithMaximumFrequencyDictionary.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountElementsWithMaximumFrequency;
+
+/// <inheritdoc />
+public class CountElementsWithMaximumFrequencyDictionary : ICountElementsWithMaximumFrequency
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(m), where m is the number of unique elements
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MaxFrequencyElements(int[] nums)
+    {
+        var numsDictionary = new Dictionary<int, int>();
+        var maxFrequency = 0;
+        var maxFrequencySum = 0;
+
+        foreach (var num in nums)
+        {
+            if (!numsDictionary.TryAdd(num, 1))
+            {
+                numsDictionary[num]++;
+            }
+
+            if (numsDictionary[num] > maxFrequency)
+            {
+                maxFrequency = numsDictionary[num];
+                maxFrequencySum = numsDictionary[num];
+            }
+            else if (numsDictionary[num] == maxFrequency)
+            {
+                maxFrequencySum += numsDictionary[num];
+            }
+        }
+
+        return maxFrequencySum;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountElementsWithMaximumFrequency/CountElementsWithMaximumFrequencyTestsBase.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountElementsWithMaximumFrequency;
+
+namespace LeetCode.Tests.Algorithms.CountElementsWithMaximumFrequency;
+
+public abstract class CountElementsWithMaximumFrequencyTestsBase<T> where T : ICountElementsWithMaximumFrequency, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1, 2, 2, 3, 1, 4 }, 4)]
+    [DataRow(new[] { 1, 2, 3, 4, 5 }, 5)]
+    public void MaxFrequencyElements_GivenArray_ReturnsMaximumElementFrequency(int[] nums, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaxFrequencyElements(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
