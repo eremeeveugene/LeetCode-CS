f2b2ce8149fb1789fef9ba935a813a96a6fbae9d
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-03-12 15:59:52 +0200

    Remove Zero Sum Consecutive Nodes from Linked List

---
+++ source/LeetCode/Algorithms/RemoveZeroSumConsecutiveNodesFromLinkedList/RemoveZeroSumConsecutiveNodesFromLinkedListBruteForce.cs
@@ -0,0 +1,86 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.RemoveZeroSumConsecutiveNodesFromLinkedList;
+
+/// <inheritdoc />
+public class RemoveZeroSumConsecutiveNodesFromLinkedListBruteForce : IRemoveZeroSumConsecutiveNodesFromLinkedList
+{
+    /// <summary>
+    ///     Time complexity - O (n^3)
+    ///     Space complexity - O (n)
+    /// </summary>
+    /// <param name="head"></param>
+    /// <returns></returns>
+    public ListNode? RemoveZeroSumSublists(ListNode? head)
+    {
+        if (head == null)
+        {
+            return null;
+        }
+
+        var values = new List<int>();
+
+        while (head != null)
+        {
+            values.Add(head.val);
+
+            head = head.next;
+        }
+
+        var i = 0;
+
+        while (i < values.Count)
+        {
+            for (var j = i; j >= 0; j--)
+            {
+                var sum = 0;
+
+                for (var k = j; k <= i; k++)
+                {
+                    sum += values[k];
+                }
+
+                if (sum != 0)
+                {
+                    continue;
+                }
+
+                values.RemoveRange(j, i - j + 1);
+
+                i -= i - j + 1;
+
+                break;
+            }
+
+            i++;
+        }
+
+        if (values.Count == 0)
+        {
+            return null;
+        }
+
+        var dummyHead = new ListNode();
+
+        var current = dummyHead;
+
+        foreach (var value in values)
+        {
+            current.next = new ListNode(value);
+            current = current.next;
+        }
+
+        return dummyHead.next;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/RemoveZeroSumConsecutiveNodesFromLinkedList/RemoveZeroSumConsecutiveNodesFromLinkedListTestsBase.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.RemoveZeroSumConsecutiveNodesFromLinkedList;
+using LeetCode.Core.Extensions;
+using LeetCode.Tests.Extensions;
+
+namespace LeetCode.Tests.Algorithms.RemoveZeroSumConsecutiveNodesFromLinkedList;
+
+public abstract class RemoveZeroSumConsecutiveNodesFromLinkedListTestsBase<T>
+    where T : IRemoveZeroSumConsecutiveNodesFromLinkedList, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1, 2, -3, 3, 1 }, new[] { 3, 1 })]
+    [DataRow(new[] { 1, 2, 3, -3, 4 }, new[] { 1, 2, 4 })]
+    [DataRow(new[] { 1, 2, 3, -3, -2 }, new[] { 1 })]
+    public void RemoveZeroSumSublists_WithVariousLists_ReturnsExpectedResult(int[] headArray, int[] expectedResultArray)
+    {
+        // Arrange
+        var solution = new T();
+
+        var head = headArray.ToListNode();
+        var expectedResult = expectedResultArray.ToListNode();
+
+        // Act
+        var actualResult = solution.RemoveZeroSumSublists(head);
+
+        // Assert
+        AssertExtensions.AreListNodesEqual(expectedResult, actualResult);
+    }
+}
