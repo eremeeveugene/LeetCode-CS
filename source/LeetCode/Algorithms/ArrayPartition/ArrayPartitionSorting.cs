f92ca624233289e6b28988ae0416de8ef529bc77
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-03-13 19:34:27 +0200

    Array Partition

---
+++ source/LeetCode/Algorithms/ArrayPartition/ArrayPartitionSorting.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ArrayPartition;
+
+/// <inheritdoc />
+public class ArrayPartitionSorting : IArrayPartition
+{
+    /// <summary>
+    ///     Time complexity - O (n log n)
+    ///     Space complexity - O (log n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int ArrayPairSum(int[] nums)
+    {
+        Array.Sort(nums);
+
+        var sum = 0;
+
+        for (var i = 0; i < nums.Length; i += 2)
+        {
+            sum += Math.Min(nums[i], nums[i + 1]);
+        }
+
+        return sum;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ArrayPartition/ArrayPartitionTestsBase.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ArrayPartition;
+
+namespace LeetCode.Tests.Algorithms.ArrayPartition;
+
+public abstract class ArrayPartitionTestsBase<T> where T : IArrayPartition, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1, 4, 3, 2 }, 4)]
+    [DataRow(new[] { 6, 2, 6, 5, 1, 2 }, 9)]
+    public void ArrayPairSum_GivenArrayOfIntegers_ReturnsMaximumSumOfMinPairsInEveryPair(int[] nums, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.ArrayPairSum(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
