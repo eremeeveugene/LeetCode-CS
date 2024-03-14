e36439d968cb46bd3b4ac2a8de9baf7a6ef9b8ab
eugene <eremeeveugene@gmail.com>
2024-03-14 20:24:19 +0200

    Binary Subarrays With Sum (#280)

---
+++ source/LeetCode/Algorithms/BinarySubarraysWithSum/BinarySubarraysWithSumDictionary.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.BinarySubarraysWithSum;
+
+/// <inheritdoc />
+public class BinarySubarraysWithSumDictionary : IBinarySubarraysWithSum
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="goal"></param>
+    /// <returns></returns>
+    public int NumSubarraysWithSum(int[] nums, int goal)
+    {
+        var sumDictionary = new Dictionary<int, int> { [0] = 1 };
+
+        int currentSum = 0, count = 0;
+
+        foreach (var num in nums)
+        {
+            currentSum += num;
+
+            if (sumDictionary.ContainsKey(currentSum - goal))
+            {
+                count += sumDictionary[currentSum - goal];
+            }
+
+            if (!sumDictionary.TryAdd(currentSum, 1))
+            {
+                sumDictionary[currentSum]++;
+            }
+        }
+
+        return count;
+    }
+}
