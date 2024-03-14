a18fc8d58efe35bcd4463f9c84b53a5530adbdec
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-03-14 20:17:28 +0200

    Binary Subarrays With Sum

---
+++ source/Tests/LeetCode.Tests/Algorithms/BinarySubarraysWithSum/BinarySubarraysWithSumTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.BinarySubarraysWithSum;
+
+namespace LeetCode.Tests.Algorithms.BinarySubarraysWithSum;
+
+public abstract class BinarySubarraysWithSumTestsBase<T> where T : IBinarySubarraysWithSum, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1, 0, 1, 0, 1 }, 2, 4)]
+    [DataRow(new[] { 0, 0, 0, 0, 0 }, 0, 15)]
+    public void NumSubarraysWithSum_GivenBinaryArrayAndGoal_ReturnsExpectedCount(int[] nums, int goal,
+        int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.NumSubarraysWithSum(nums, goal);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
