97477a6b9d10a8d072153f155d50c882cb7aa45c
eugene <eremeeveugene@gmail.com>
2024-03-18 11:44:54 +0200

    Minimum Number of Arrows to Burst Balloons (#286)

---
+++ source/LeetCode/Algorithms/MinimumNumberOfArrowsToBurstBalloons/MinimumNumberOfArrowsToBurstBalloonsIterative.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumNumberOfArrowsToBurstBalloons;
+
+/// <inheritdoc />
+public class MinimumNumberOfArrowsToBurstBalloonsIterative : IMinimumNumberOfArrowsToBurstBalloons
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="points"></param>
+    /// <returns></returns>
+    public int FindMinArrowShots(int[][] points)
+    {
+        var arrowShotsCount = 1;
+
+        var orderedPoints = points.OrderBy(p => p[1]).ToArray();
+
+        var previousPoint = orderedPoints[0];
+
+        foreach (var point in orderedPoints)
+        {
+            if (point[0] <= previousPoint[1])
+            {
+                continue;
+            }
+
+            arrowShotsCount++;
+            previousPoint = point;
+        }
+
+        return arrowShotsCount;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumNumberOfArrowsToBurstBalloons/MinimumNumberOfArrowsToBurstBalloonsTestsBase.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumNumberOfArrowsToBurstBalloons;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MinimumNumberOfArrowsToBurstBalloons;
+
+public abstract class MinimumNumberOfArrowsToBurstBalloonsTestsBase<T>
+    where T : IMinimumNumberOfArrowsToBurstBalloons, new()
+{
+    [TestMethod]
+    [DataRow("[[10,16],[2,8],[1,6],[7,12]]", 2)]
+    [DataRow("[[1,2],[3,4],[5,6],[7,8]]", 4)]
+    [DataRow("[[1,2],[2,3],[3,4],[4,5]]", 2)]
+    [DataRow("[[3,9],[7,12],[3,8],[6,8],[9,10],[2,9],[0,9],[3,9],[0,6],[2,8]]", 2)]
+    public void FindMinArrowShots_GivenDifferentPointCombinations_ReturnsMinimumArrowsRequired(string pointsJsonArray,
+        int expectedResult)
+    {
+        // Arrange
+        var points = JsonConvertHelper<int>.JsonArrayToJaggedArray(pointsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FindMinArrowShots(points);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
