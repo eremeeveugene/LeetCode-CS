ec7cb14186cad6cc88f168265ab6949791e3879a
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-03-19 16:54:26 +0200

    Task Scheduler

---
+++ source/LeetCode/Algorithms/TaskScheduler/TaskSchedulerDictionary.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.TaskScheduler;
+
+/// <inheritdoc />
+public class TaskSchedulerDictionary : ITaskScheduler
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="tasks"></param>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public int LeastInterval(char[] tasks, int n)
+    {
+        var taskCounts = new Dictionary<char, int>();
+
+        foreach (var task in tasks)
+        {
+            if (!taskCounts.TryAdd(task, 1))
+            {
+                taskCounts[task]++;
+            }
+        }
+
+        var maxFrequency = 0;
+        var tasksWithMaxFrequency = 0;
+
+        foreach (var count in taskCounts.Values)
+        {
+            if (count > maxFrequency)
+            {
+                maxFrequency = count;
+                tasksWithMaxFrequency = 1;
+            }
+            else if (count == maxFrequency)
+            {
+                tasksWithMaxFrequency++;
+            }
+        }
+
+        var partCount = maxFrequency - 1;
+        var partLength = n - (tasksWithMaxFrequency - 1);
+        var emptySlots = partCount * partLength;
+        var availableTasks = tasks.Length - (maxFrequency * tasksWithMaxFrequency);
+        var idles = Math.Max(0, emptySlots - availableTasks);
+
+        return tasks.Length + idles;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/TaskScheduler/TaskSchedulerTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.TaskScheduler;
+
+namespace LeetCode.Tests.Algorithms.TaskScheduler;
+
+public abstract class TaskSchedulerTestsBase<T> where T : ITaskScheduler, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 'A', 'A', 'A', 'B', 'B', 'B' }, 2, 8)]
+    [DataRow(new[] { 'A', 'C', 'A', 'B', 'D', 'B' }, 1, 6)]
+    [DataRow(new[] { 'A', 'A', 'A', 'B', 'B', 'B' }, 3, 10)]
+    public void LeastInterval_GivenTasksAndCooldownPeriod_ReturnsMinimumIntervalsNeeded(char[] tasks, int n,
+        int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.LeastInterval(tasks, n);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
