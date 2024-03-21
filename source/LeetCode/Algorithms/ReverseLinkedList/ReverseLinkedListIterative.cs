fcf631fcb735546d8f4081514951f799fc407f5c
eugene <eremeeveugene@gmail.com>
2024-03-21 15:42:57 +0200

    Reverse Linked List (#290)

---
+++ source/LeetCode/Algorithms/ReverseLinkedList/ReverseLinkedListIterative.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.ReverseLinkedList;
+
+/// <inheritdoc />
+public class ReverseLinkedListIterative : IReverseLinkedList
+{
+    /// <summary>
+    ///     Time complexity - O (n)
+    ///     Space complexity - O (n)
+    /// </summary>
+    /// <param name="head"></param>
+    /// <returns></returns>
+    public ListNode? ReverseList(ListNode? head)
+    {
+        ListNode? reverseHead = null;
+
+        while (head != null)
+        {
+            reverseHead = new ListNode(head.val, reverseHead);
+
+            head = head.next;
+        }
+
+        return reverseHead;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ReverseLinkedList/ReverseLinkedListTestsBase.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ReverseLinkedList;
+using LeetCode.Core.Extensions;
+using LeetCode.Core.Models;
+using LeetCode.Tests.Extensions;
+
+namespace LeetCode.Tests.Algorithms.ReverseLinkedList;
+
+public abstract class ReverseLinkedListTestsBase<T> where T : IReverseLinkedList, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1, 2, 3, 4, 5 }, new[] { 5, 4, 3, 2, 1 })]
+    [DataRow(new[] { 1, 2 }, new[] { 2, 1 })]
+    [DataRow(new int[] { }, new int[] { })]
+    public void ReverseList_WithVariousHeadArrays_ReturnsExpectedReversedList(int[] headArray,
+        int[] expectedResultArray)
+    {
+        // Arrange
+        var head = headArray.ToListNode();
+
+        var expectedResult = expectedResultArray.ToListNode();
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.ReverseList(head);
+
+        // Assert
+        AssertExtensions.AreListNodesEqual(expectedResult, actualResult);
+    }
+}
