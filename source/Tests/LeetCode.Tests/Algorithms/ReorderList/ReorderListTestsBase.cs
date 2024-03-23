40c37243f26222ec5e1b2288226b29b5af076021
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-03-23 14:35:24 +0200

    Reorder List

---
+++ source/Tests/LeetCode.Tests/Algorithms/ReorderList/ReorderListTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ReorderList;
+using LeetCode.Core.Extensions;
+using LeetCode.Tests.Extensions;
+
+namespace LeetCode.Tests.Algorithms.ReorderList;
+
+public abstract class ReorderListTestsBase<T> where T : IReorderList, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1, 2, 3, 4 }, new[] { 1, 4, 2, 3 })]
+    [DataRow(new[] { 1, 2, 3, 4, 5 }, new[] { 1, 5, 2, 4, 3 })]
+    public void ReorderList_GivenHeadArray_ResultsInExpectedOrder(int[] headArray, int[] expectedResultArray)
+    {
+        // Arrange
+        var head = headArray.ToListNode();
+
+        var expectedResult = expectedResultArray.ToListNode();
+
+        var solution = new T();
+
+        // Act
+        solution.ReorderList(head);
+
+        // Assert
+        AssertExtensions.AreListNodesEqual(expectedResult, head);
+    }
+}
