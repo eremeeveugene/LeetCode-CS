747a7c41e987ae8bae77277d4d10b8886325e192
eugene <eremeeveugene@gmail.com>
2024-03-27 18:43:28 +0200

    Subarray Product Less Than K (#302)

---
+++ source/LeetCode/Algorithms/SubarrayProductLessThanK/ISubarrayProductLessThanK.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SubarrayProductLessThanK;
+
+/// <summary>
+///     https://leetcode.com/problems/subarray-product-less-than-k/description/
+/// </summary>
+public interface ISubarrayProductLessThanK
+{
+    int NumSubarrayProductLessThanK(int[] nums, int k);
+}
