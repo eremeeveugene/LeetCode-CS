e9b39939b0f1dc33ecd4c90f1c0db44efbbe12cf
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-03-27 18:40:34 +0200

    Subarray Product Less Than K

---
+++ source/LeetCode/Algorithms/SubarrayProductLessThanK/SubarrayProductLessThanKTwoPointers.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SubarrayProductLessThanK;
+
+/// <inheritdoc />
+public class SubarrayProductLessThanKTwoPointers : ISubarrayProductLessThanK
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int NumSubarrayProductLessThanK(int[] nums, int k)
+    {
+        if (k <= 1)
+        {
+            return 0;
+        }
+
+        var count = 0;
+        var product = 1;
+        var left = 0;
+
+        for (var right = 0; right < nums.Length; right++)
+        {
+            product *= nums[right];
+
+            while (product >= k)
+            {
+                product /= nums[left];
+
+                left++;
+            }
+
+            count += right - left + 1;
+        }
+
+        return count;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/SubarrayProductLessThanK/SubarrayProductLessThanKTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SubarrayProductLessThanK;
+
+namespace LeetCode.Tests.Algorithms.SubarrayProductLessThanK;
+
+public abstract class SubarrayProductLessThanKTestsBase<T> where T : ISubarrayProductLessThanK, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 10, 5, 2, 6 }, 100, 8)]
+    [DataRow(new[] { 1, 2, 3 }, 0, 0)]
+    [DataRow(new[] { 1, 1, 1 }, 2, 6)]
+    public void NumSubarrayProductLessThanK_GivenArrayAndThreshold_ReturnsCount(int[] nums, int k, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.NumSubarrayProductLessThanK(nums, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
