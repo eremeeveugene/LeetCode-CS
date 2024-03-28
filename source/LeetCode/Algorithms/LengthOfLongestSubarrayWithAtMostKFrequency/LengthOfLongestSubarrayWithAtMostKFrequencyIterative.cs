0e13a62b778afda8def9861135e8993adf232b86
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-03-28 13:26:34 +0200

    Length of Longest Subarray With at Most K Frequency

---
+++ source/LeetCode/Algorithms/LengthOfLongestSubarrayWithAtMostKFrequency/LengthOfLongestSubarrayWithAtMostKFrequencyIterative.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.LengthOfLongestSubarrayWithAtMostKFrequency;
+
+/// <inheritdoc />
+public class LengthOfLongestSubarrayWithAtMostKFrequencyIterative : ILengthOfLongestSubarrayWithAtMostKFrequency
+{
+    /// <summary>
+    ///     Time complexity - O(n^2)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int MaxSubarrayLength(int[] nums, int k)
+    {
+        var maxLength = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var dictionary = new Dictionary<int, int>();
+
+            for (var j = i; j < nums.Length; j++)
+            {
+                if (dictionary.TryAdd(nums[j], 1))
+                {
+                    continue;
+                }
+
+                if (dictionary[nums[j]] == k)
+                {
+                    maxLength = Math.Max(maxLength, j - i);
+
+                    break;
+                }
+
+                dictionary[nums[j]]++;
+            }
+
+            maxLength = Math.Max(maxLength, dictionary.Select(d => d.Value).Sum());
+        }
+
+        return maxLength;
+    }
+}
---
+++ source/LeetCode/Algorithms/LengthOfLongestSubarrayWithAtMostKFrequency/LengthOfLongestSubarrayWithAtMostKFrequencyTwoPointers.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.LengthOfLongestSubarrayWithAtMostKFrequency;
+
+/// <inheritdoc />
+public class LengthOfLongestSubarrayWithAtMostKFrequencyTwoPointers : ILengthOfLongestSubarrayWithAtMostKFrequency
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int MaxSubarrayLength(int[] nums, int k)
+    {
+        var maxLength = 0;
+        var left = 0;
+
+        var frequencyDictionary = new Dictionary<int, int>();
+
+        for (var right = 0; right < nums.Length; right++)
+        {
+            if (frequencyDictionary.TryGetValue(nums[right], out var value))
+            {
+                frequencyDictionary[nums[right]] = ++value;
+            }
+            else
+            {
+                frequencyDictionary[nums[right]] = 1;
+            }
+
+            while (frequencyDictionary[nums[right]] > k)
+            {
+                frequencyDictionary[nums[left]]--;
+
+                if (frequencyDictionary[nums[left]] == 0)
+                {
+                    frequencyDictionary.Remove(nums[left]);
+                }
+
+                left++;
+            }
+
+            maxLength = Math.Max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
