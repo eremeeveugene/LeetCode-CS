962e9976560272abd41fee6869360f0d151dd585
eugene <eremeeveugene@gmail.com>
2024-03-29 19:40:17 +0200

    Count Subarrays Where Max Element Appears at Least K Times (#306)

---
+++ source/LeetCode/Algorithms/CountSubarraysWhereMaxElementAppearsAtLeastKTimes/ICountSubarraysWhereMaxElementAppearsAtLeastKTimes.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountSubarraysWhereMaxElementAppearsAtLeastKTimes;
+
+/// <summary>
+///     https://leetcode.com/problems/count-subarrays-where-max-element-appears-at-least-k-times/description/
+/// </summary>
+public interface ICountSubarraysWhereMaxElementAppearsAtLeastKTimes
+{
+    long CountSubarrays(int[] nums, int k);
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountSubarraysWhereMaxElementAppearsAtLeastKTimes/CountSubarraysWhereMaxElementAppearsAtLeastKTimesTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountSubarraysWhereMaxElementAppearsAtLeastKTimes;
+
+namespace LeetCode.Tests.Algorithms.CountSubarraysWhereMaxElementAppearsAtLeastKTimes;
+
+public abstract class CountSubarraysWhereMaxElementAppearsAtLeastKTimesTestsBase<T>
+    where T : ICountSubarraysWhereMaxElementAppearsAtLeastKTimes, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1, 3, 2, 3, 3 }, 2, 6)]
+    [DataRow(new[] { 1, 4, 2, 1 }, 3, 0)]
+    public void CountSubarrays_GivenArrayAndThreshold_ReturnsExpectedCount(int[] nums, int k, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountSubarrays(nums, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
