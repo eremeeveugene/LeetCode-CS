8e5c04d18da75c4f215879890c4a289356965a06
eugene <eremeeveugene@gmail.com>
2024-03-30 15:08:49 +0200

    Maximum Average Subarray I (#312)

---
+++ source/Tests/LeetCode.Tests/Algorithms/MaximumAverageSubarray1/MaximumAverageSubarray1SlidingWindowTests.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaximumAverageSubarray1;
+
+namespace LeetCode.Tests.Algorithms.MaximumAverageSubarray1;
+
+[TestClass]
+public class MaximumAverageSubarray1SlidingWindowTests :
+    MaximumAverageSubarray1TestsBase<MaximumAverageSubarray1SlidingWindow>;
