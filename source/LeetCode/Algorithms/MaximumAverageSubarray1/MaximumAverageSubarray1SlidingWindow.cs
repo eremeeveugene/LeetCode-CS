840989eb477c1038a5f503925970bb48df40e674
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-03-30 15:04:59 +0200

    Maximum Average Subarray I

---
+++ source/LeetCode/Algorithms/MaximumAverageSubarray1/MaximumAverageSubarray1SlidingWindow.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumAverageSubarray1;
+
+/// <inheritdoc />
+public class MaximumAverageSubarray1SlidingWindow : IMaximumAverageSubarray1
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public double FindMaxAverage(int[] nums, int k)
+    {
+        var maxAverage = double.MinValue;
+
+        var left = 0;
+
+        var average = 0.0;
+
+        for (var right = 0; right < nums.Length; right++)
+        {
+            average += nums[right];
+
+            if (right - left + 1 > k)
+            {
+                average -= nums[left];
+
+                left++;
+            }
+
+            if (right - left + 1 == k)
+            {
+                maxAverage = Math.Max(maxAverage, average);
+            }
+        }
+
+        return maxAverage / k;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MaximumAverageSubarray1/MaximumAverageSubarray1TestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaximumAverageSubarray1;
+
+namespace LeetCode.Tests.Algorithms.MaximumAverageSubarray1;
+
+public abstract class MaximumAverageSubarray1TestsBase<T> where T : IMaximumAverageSubarray1, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1, 12, -5, -6, 50, 3 }, 4, 12.75)]
+    [DataRow(new[] { 5 }, 1, 5)]
+    [DataRow(new[] { -1 }, 1, -1)]
+    public void FindMaxAverage_GivenNumsAndK_ReturnsMaxAverage(int[] nums, int k, double expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FindMaxAverage(nums, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
