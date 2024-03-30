e824a0f7c726e6aeb6734f4dd369fa2cb6cace10
eugene <eremeeveugene@gmail.com>
2024-03-30 14:32:47 +0200

    Longest Substring Without Repeating Characters (#310)

---
+++ source/LeetCode/Algorithms/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharactersSlidingWindow.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.LongestSubstringWithoutRepeatingCharacters;
+
+/// <inheritdoc />
+public class LongestSubstringWithoutRepeatingCharactersSlidingWindow : ILongestSubstringWithoutRepeatingCharacters
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public int LengthOfLongestSubstring(string s)
+    {
+        var maxLength = 0;
+
+        var left = 0;
+
+        var charDictionary = new Dictionary<char, int>();
+
+        for (var right = 0; right < s.Length; right++)
+        {
+            if (!charDictionary.TryAdd(s[right], right))
+            {
+                left = Math.Max(left, charDictionary[s[right]] + 1);
+            }
+
+            charDictionary[s[right]] = right;
+            maxLength = Math.Max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/LongestSubstringWithoutRepeatingCharacters/LongestSubstringWithoutRepeatingCharactersTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.LongestSubstringWithoutRepeatingCharacters;
+
+namespace LeetCode.Tests.Algorithms.LongestSubstringWithoutRepeatingCharacters;
+
+public abstract class LongestSubstringWithoutRepeatingCharactersTestsBase<T>
+    where T : ILongestSubstringWithoutRepeatingCharacters, new()
+{
+    [TestMethod]
+    [DataRow("bbbbb", 1)]
+    [DataRow("abcabcbb", 3)]
+    [DataRow("dvdf", 3)]
+    [DataRow("pwwkew", 3)]
+    [DataRow("abcabcd", 4)]
+    public void LengthOfLongestSubstring_GivenString_ReturnsMaxUniqueSubstringLength(string s, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.LengthOfLongestSubstring(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
