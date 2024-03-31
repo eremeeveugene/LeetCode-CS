df06533ffb247b66b69e16dc4671f19cf22a3e61
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-03-31 12:38:44 +0300

    Contains Duplicate II

---
+++ source/LeetCode/Algorithms/ContainsDuplicate2/ContainsDuplicate2Dictionary.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ContainsDuplicate2;
+
+/// <inheritdoc />
+public class ContainsDuplicate2Dictionary : IContainsDuplicate2
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        var numsDictionary = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (numsDictionary.TryAdd(nums[i], i))
+            {
+                continue;
+            }
+
+            if (Math.Abs(numsDictionary[nums[i]] - i) <= k)
+            {
+                return true;
+            }
+
+            numsDictionary[nums[i]] = i;
+        }
+
+        return false;
+    }
+}
