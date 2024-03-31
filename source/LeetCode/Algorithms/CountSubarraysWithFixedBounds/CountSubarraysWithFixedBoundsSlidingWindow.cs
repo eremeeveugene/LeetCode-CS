f8238da934c20f81b815c89ea42fc6b651c60e57
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-03-31 14:45:35 +0300

    Count Subarrays With Fixed Bounds

---
+++ source/LeetCode/Algorithms/CountSubarraysWithFixedBounds/CountSubarraysWithFixedBoundsSlidingWindow.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountSubarraysWithFixedBounds;
+
+/// <inheritdoc />
+public class CountSubarraysWithFixedBoundsSlidingWindow : ICountSubarraysWithFixedBounds
+{
+    public long CountSubarrays(int[] nums, int minK, int maxK)
+    {
+        long count = 0;
+
+        var lastMinKPosition = -1;
+        var lastMaxKPosition = -1;
+
+        var left = 0;
+
+        for (var right = 0; right < nums.Length; right++)
+        {
+            if (nums[right] == minK)
+            {
+                lastMinKPosition = right;
+            }
+
+            if (nums[right] == maxK)
+            {
+                lastMaxKPosition = right;
+            }
+
+            if (nums[right] < minK || nums[right] > maxK)
+            {
+                left = right + 1;
+            }
+
+            if (lastMinKPosition != -1 && lastMaxKPosition != -1)
+            {
+                count += Math.Max(0, Math.Min(lastMinKPosition, lastMaxKPosition) - left + 1);
+            }
+        }
+
+        return count;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountSubarraysWithFixedBounds/CountSubarraysWithFixedBoundsTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountSubarraysWithFixedBounds;
+
+namespace LeetCode.Tests.Algorithms.CountSubarraysWithFixedBounds;
+
+public abstract class CountSubarraysWithFixedBoundsTestsBase<T> where T : ICountSubarraysWithFixedBounds, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1, 3, 5, 2, 7, 5 }, 1, 5, 2)]
+    [DataRow(new[] { 1, 1, 1, 1 }, 1, 1, 10)]
+    public void CountSubarrays_GivenRangeWithMinAndMaxValues_ReturnsExpectedCount(int[] nums, int minK, int maxK,
+        long expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountSubarrays(nums, minK, maxK);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
