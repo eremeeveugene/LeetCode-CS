8f53144c59392cee03b71a03c623b68df1105512
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-04-04 14:34:40 +0300

    Maximum Nesting Depth of the Parentheses

---
+++ source/Tests/LeetCode.Tests/Algorithms/MaximumNestingDepthOfTheParentheses/MaximumNestingDepthOfTheParenthesesTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaximumNestingDepthOfTheParentheses;
+
+namespace LeetCode.Tests.Algorithms.MaximumNestingDepthOfTheParentheses;
+
+public abstract class MaximumNestingDepthOfTheParenthesesTestsBase<T>
+    where T : IMaximumNestingDepthOfTheParentheses, new()
+{
+    [TestMethod]
+    [DataRow("(1+(2*3)+((8)/4))+1", 3)]
+    [DataRow("(1)+((2))+(((3)))", 3)]
+    [DataRow("1+(2*3)/(2-1)", 1)]
+    public void Test(string s, double expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaxDepth(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
