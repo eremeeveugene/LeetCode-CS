fa8f9caa0523bbb43bd9aa2f6cea26d8f5e1bb64
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-04-12 12:54:03 +0300

    Trapping Rain Water

---
+++ source/LeetCode/Algorithms/TrappingRainWater/TrappingRainWaterIterative.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.TrappingRainWater;
+
+/// <inheritdoc />
+public class TrappingRainWaterIterative : ITrappingRainWater
+{
+    public int Trap(int[] height)
+    {
+        var result = 0;
+
+        var left = 0;
+        var right = height.Length - 1;
+
+        var leftMax = 0;
+        var rightMax = 0;
+
+        while (left < right)
+        {
+            if (height[left] <= height[right])
+            {
+                if (height[left] >= leftMax)
+                {
+                    leftMax = height[left];
+                }
+                else
+                {
+                    result += leftMax - height[left];
+                }
+
+                left++;
+            }
+            else
+            {
+                if (height[right] >= rightMax)
+                {
+                    rightMax = height[right];
+                }
+                else
+                {
+                    result += rightMax - height[right];
+                }
+
+                right--;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/TrappingRainWater/TrappingRainWaterTestsBase.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.TrappingRainWater;
+
+namespace LeetCode.Tests.Algorithms.TrappingRainWater;
+
+public abstract class TrappingRainWaterTestsBase<T> where T : ITrappingRainWater, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }, 6)]
+    [DataRow(new[] { 4, 2, 0, 3, 2, 5 }, 9)]
+    public void Test(int[] height, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.Trap(height);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
