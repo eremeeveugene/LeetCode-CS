0c0241a3b220f4a02a4aa9fa3ecf5ee2def6a0ea
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-04-13 13:00:31 +0300

    Maximal Rectangle

---
+++ source/LeetCode/Algorithms/MaximalRectangle/MaximalRectangleStack.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximalRectangle;
+
+/// <inheritdoc />
+public class MaximalRectangleStack : IMaximalRectangle
+{
+    /// <summary>
+    ///     Time complexity - O(n*m)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <returns></returns>
+    public int MaximalRectangle(char[][] matrix)
+    {
+        var maxArea = 0;
+        var height = new int[matrix[0].Length + 1];
+
+        foreach (var item in matrix)
+        {
+            var stack = new Stack<int>();
+
+            for (var col = 0; col <= matrix[0].Length; col++)
+            {
+                if (col < matrix[0].Length)
+                {
+                    if (item[col] == '1')
+                    {
+                        height[col] += 1;
+                    }
+                    else
+                    {
+                        height[col] = 0;
+                    }
+                }
+
+                while (stack.Count > 0 && height[col] < height[stack.Peek()])
+                {
+                    var h = height[stack.Pop()];
+                    var w = stack.Count == 0 ? col : col - stack.Peek() - 1;
+                    maxArea = Math.Max(maxArea, h * w);
+                }
+
+                stack.Push(col);
+            }
+        }
+
+        return maxArea;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MaximalRectangle/MaximalRectangleTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaximalRectangle;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MaximalRectangle;
+
+public abstract class MaximalRectangleTestsBase<T> where T : IMaximalRectangle, new()
+{
+    [TestMethod]
+    [DataRow("[[\"0\"]]", 0)]
+    [DataRow("[[\"1\"]]", 1)]
+    [DataRow("[[\"1\",\"0\",\"1\",\"0\",\"0\"],[\"1\",\"0\",\"1\",\"1\",\"1\"],[\"1\",\"1\",\"1\",\"1\",\"1\"],[\"1\",\"0\",\"0\",\"1\",\"0\"]]", 6)]
+    public void MaximalRectangle_GivenMatrix_ReturnsMaximalArea(string matrixJsonArray, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        var matrix = JsonConvertHelper<char>.JsonArrayToJaggedArray(matrixJsonArray);
+
+        // Act
+        var actualResult = solution.MaximalRectangle(matrix);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
