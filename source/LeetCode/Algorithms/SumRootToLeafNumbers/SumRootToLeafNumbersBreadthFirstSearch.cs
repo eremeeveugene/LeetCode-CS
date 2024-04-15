6bda023c8eaeaee8a816fd839faefc9c0ac86888
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-04-15 16:19:11 +0300

    Sum Root to Leaf Numbers

---
+++ source/LeetCode/Algorithms/SumRootToLeafNumbers/SumRootToLeafNumbersBreadthFirstSearch.cs
@@ -0,0 +1,68 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.SumRootToLeafNumbers;
+
+/// <inheritdoc />
+public class SumRootToLeafNumbersBreadthFirstSearch : ISumRootToLeafNumbers
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="root"></param>
+    /// <returns></returns>
+    public int SumNumbers(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        var sum = 0;
+
+        var queue = new Queue<TreeNode>();
+
+        queue.Enqueue(root);
+
+        while (queue.Count > 0)
+        {
+            var current = queue.Dequeue();
+
+            if (current.left == null && current.right == null)
+            {
+                sum += current.val;
+            }
+            else
+            {
+                if (current.left != null)
+                {
+                    current.left.val += current.val * 10;
+
+                    queue.Enqueue(current.left);
+                }
+
+                if (current.right == null)
+                {
+                    continue;
+                }
+
+                current.right.val += current.val * 10;
+
+                queue.Enqueue(current.right);
+            }
+        }
+
+        return sum;
+    }
+}
---
+++ source/LeetCode/Algorithms/SumRootToLeafNumbers/SumRootToLeafNumbersDepthFirstSearch.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.SumRootToLeafNumbers;
+
+/// <inheritdoc />
+public class SumRootToLeafNumbersDepthFirstSearch : ISumRootToLeafNumbers
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n) for a skewed tree, O(log n) for a balanced tree
+    /// </summary>
+    /// <param name="root"></param>
+    /// <returns></returns>
+    public int SumNumbers(TreeNode? root)
+    {
+        return root == null ? 0 : GetSum(root, 0);
+    }
+
+    private static int GetSum(TreeNode treeNode, int currentSum)
+    {
+        currentSum = (currentSum * 10) + treeNode.val;
+
+        if (treeNode.left == null && treeNode.right == null)
+        {
+            return currentSum;
+        }
+
+        var sum = 0;
+
+        if (treeNode.left != null)
+        {
+            sum += GetSum(treeNode.left, currentSum);
+        }
+
+        if (treeNode.right != null)
+        {
+            sum += GetSum(treeNode.right, currentSum);
+        }
+
+        return sum;
+    }
+}
