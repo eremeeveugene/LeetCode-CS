76bfda36c25f17193d156428e754b41ba67da246
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-04-17 17:47:21 +0300

    Smallest String Starting From Leaf

---
+++ source/LeetCode/Algorithms/SmallestStringStartingFromLeaf/SmallestStringStartingFromLeafBreadthFirstSearch.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.SmallestStringStartingFromLeaf;
+
+/// <inheritdoc />
+public class SmallestStringStartingFromLeafBreadthFirstSearch : ISmallestStringStartingFromLeaf
+{
+    /// <summary>
+    ///     Time complexity - O(n) for a balanced tree, O(n^2) for a skewed tree
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="root"></param>
+    /// <returns></returns>
+    public string? SmallestFromLeaf(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return null;
+        }
+
+        string? smallestString = null;
+
+        var queue = new Queue<(TreeNode node, string path)>();
+
+        queue.Enqueue((root, ""));
+
+        while (queue.Count > 0)
+        {
+            var (currentNode, path) = queue.Dequeue();
+            var currentPath = (char)('a' + currentNode.val) + path;
+
+            if (currentNode.left == null && currentNode.right == null)
+            {
+                var reversePath = new string(currentPath.Reverse().ToArray());
+
+                if (smallestString == null || string.Compare(reversePath, smallestString, StringComparison.Ordinal) < 0)
+                {
+                    smallestString = reversePath;
+                }
+            }
+
+            if (currentNode.left != null)
+            {
+                queue.Enqueue((currentNode.left, currentPath));
+            }
+
+            if (currentNode.right != null)
+            {
+                queue.Enqueue((currentNode.right, currentPath));
+            }
+        }
+
+        return smallestString;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/SmallestStringStartingFromLeaf/SmallestStringStartingFromLeafTestsBase.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SmallestStringStartingFromLeaf;
+using LeetCode.Core.Helpers;
+using LeetCode.Core.Models;
+
+namespace LeetCode.Tests.Algorithms.SmallestStringStartingFromLeaf;
+
+public abstract class SmallestStringStartingFromLeafTestsBase<T> where T : ISmallestStringStartingFromLeaf, new()
+{
+    [TestMethod]
+    [DataRow("[0,1,2,3,4,3,4]", "dba")]
+    [DataRow("[25,1,3,1,3,0,2]", "adz")]
+    [DataRow("[2,2,1,null,1,0,null,0]", "abc")]
+    public void SmallestFromLeaf_WithDifferentTreeConfigurations_ReturnsSmallestLexicographicalStringFromLeaf(
+        string jsonArrayRoot, string? expectedResult)
+    {
+        // Arrange
+        IList<int?> arrayRoot = JsonConvertHelper<int?>.JsonArrayToList(jsonArrayRoot);
+        var root = TreeNode.BuildTree(arrayRoot);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.SmallestFromLeaf(root);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
