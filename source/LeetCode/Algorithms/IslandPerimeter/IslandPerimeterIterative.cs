fe64e01d1cfdb2d7ee016d12c750d496910e9ba4
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-04-18 17:10:47 +0300

    Island Perimeter

---
+++ source/LeetCode/Algorithms/IslandPerimeter/IslandPerimeterIterative.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.IslandPerimeter;
+
+/// <inheritdoc />
+public class IslandPerimeterIterative : IIslandPerimeter
+{
+    /// <summary>
+    ///     Time complexity - O(n * m)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="grid"></param>
+    /// <returns></returns>
+    public int IslandPerimeter(int[][] grid)
+    {
+        var perimeter = 0;
+
+        for (var i = 0; i < grid.Length; i++)
+        {
+            for (var j = 0; j < grid[i].Length; j++)
+            {
+                if (grid[i][j] != 1)
+                {
+                    continue;
+                }
+
+                if (i - 1 < 0 || grid[i - 1][j] == 0)
+                {
+                    perimeter++;
+                }
+
+                if (i + 1 >= grid.Length || grid[i + 1][j] == 0)
+                {
+                    perimeter++;
+                }
+
+                if (j - 1 < 0 || grid[i][j - 1] == 0)
+                {
+                    perimeter++;
+                }
+
+                if (j + 1 >= grid[i].Length || grid[i][j + 1] == 0)
+                {
+                    perimeter++;
+                }
+            }
+        }
+
+        return perimeter;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/IslandPerimeter/IslandPerimeterTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.IslandPerimeter;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.IslandPerimeter;
+
+public abstract class IslandPerimeterTestsBase<T> where T : IIslandPerimeter, new()
+{
+    [TestMethod]
+    [DataRow("[[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]", 16)]
+    [DataRow("[[1]]", 4)]
+    [DataRow("[[1,0]]", 4)]
+    public void IslandPerimeter_GivenGrid_ReturnsCorrectPerimeter(string gridJsonArray, int expectedResult)
+    {
+        // Arrange
+        var grid = JsonConvertHelper<int>.JsonArrayToJaggedArray(gridJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IslandPerimeter(grid);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
