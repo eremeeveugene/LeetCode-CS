655096325acb3b7cdedf65aa134cd4b8e488a710
eugene <eremeeveugene@gmail.com>
2024-04-18 22:47:43 +0300

    Flood Fill (#357)

---
+++ source/LeetCode/Algorithms/FloodFill/FloodFillIterative.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FloodFill;
+
+/// <inheritdoc />
+public class FloodFillIterative : IFloodFill
+{
+    /// <summary>
+    ///     Time complexity - O(n*m)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="image"></param>
+    /// <param name="sr"></param>
+    /// <param name="sc"></param>
+    /// <param name="newColor"></param>
+    /// <returns></returns>
+    public int[][] FloodFill(int[][] image, int sr, int sc, int newColor)
+    {
+        if (image[sr][sc] == newColor)
+        {
+            return image;
+        }
+
+        ReplacePixels(image, sr, sc, image[sr][sc], newColor);
+
+        return image;
+    }
+
+    private static void ReplacePixels(IReadOnlyList<int[]> image, int sr, int sc, int oldColor, int newColor)
+    {
+        if (sr < 0 || sr >= image.Count || sc < 0 || sc >= image[sr].Length || image[sr][sc] != oldColor)
+        {
+            return;
+        }
+
+        image[sr][sc] = newColor;
+
+        ReplacePixels(image, sr - 1, sc, oldColor, newColor);
+        ReplacePixels(image, sr + 1, sc, oldColor, newColor);
+        ReplacePixels(image, sr, sc - 1, oldColor, newColor);
+        ReplacePixels(image, sr, sc + 1, oldColor, newColor);
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FloodFill/FloodFillTestsBase.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FloodFill;
+using LeetCode.Core.Helpers;
+using LeetCode.Tests.Base.Extensions;
+
+namespace LeetCode.Tests.Algorithms.FloodFill;
+
+public abstract class FloodFillTestsBase<T> where T : IFloodFill, new()
+{
+    [TestMethod]
+    [DataRow("[[0,0,0],[0,0,0]]", 0, 0, 0, "[[0,0,0],[0,0,0]]")]
+    [DataRow("[[0,0,0],[0,0,0]]", 1, 0, 2, "[[2,2,2,],[2,2,2]]")]
+    [DataRow("[[1,1,1],[1,1,0],[1,0,1]]", 1, 1, 2, "[[2,2,2],[2,2,0],[2,0,1]]")]
+    public void FloodFill_WithInitialPositionAndNewColor_ReturnsModifiedImage(string imageJsonArray, int sr, int sc,
+        int color, string expectedResultJsonArray)
+    {
+        // Arrange
+        var intervals = JsonConvertHelper<int>.JsonArrayToJaggedArray(imageJsonArray);
+        var expectedResult = JsonConvertHelper<int>.JsonArrayToJaggedArray(expectedResultJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FloodFill(intervals, sr, sc, color);
+
+        // Assert
+        AssertExtensions.AssertJaggedArrayEqual(expectedResult, actualResult);
+    }
+}
