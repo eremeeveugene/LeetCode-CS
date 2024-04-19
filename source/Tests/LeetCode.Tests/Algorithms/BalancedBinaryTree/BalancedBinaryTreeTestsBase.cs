90387515f117f7fa50d907af606a4e812dd3030d
eugene <eremeeveugene@gmail.com>
2024-04-19 21:32:20 +0300

    Balanced Binary Tree (#363)

---
+++ source/Tests/LeetCode.Tests/Algorithms/BalancedBinaryTree/BalancedBinaryTreeTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.BalancedBinaryTree;
+using LeetCode.Core.Helpers;
+using LeetCode.Core.Models;
+
+namespace LeetCode.Tests.Algorithms.BalancedBinaryTree;
+
+public abstract class BalancedBinaryTreeTestsBase<T> where T : IBalancedBinaryTree, new()
+{
+    [TestMethod]
+    [DataRow("[3,9,20,null,null,15,7]", true)]
+    [DataRow("[1,2,2,3,3,null,null,4,4]", false)]
+    [DataRow("[]", true)]
+    public void IsBalanced_TreeWithJsonInput_ReturnsExpectedBalancedStatus(string rootJsonArray, bool expectedResult)
+    {
+        // Arrange
+        IList<int?> rootArray = JsonConvertHelper<int?>.JsonArrayToList(rootJsonArray);
+        var root = TreeNode.BuildTree(rootArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsBalanced(root);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
