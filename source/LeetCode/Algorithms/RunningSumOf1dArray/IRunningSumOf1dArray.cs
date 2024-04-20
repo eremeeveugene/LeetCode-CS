49b6b7335e86c9412065f276b6c1c98415e341ae
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-04-20 20:42:29 +0300

    Running Sum of 1d Array

---
+++ source/LeetCode/Algorithms/RunningSumOf1dArray/IRunningSumOf1dArray.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.RunningSumOf1dArray;
+
+/// <summary>
+///     https://leetcode.com/problems/running-sum-of-1d-array/description/
+/// </summary>
+public interface IRunningSumOf1dArray
+{
+    int[] RunningSum(int[] nums);
+}
---
+++ source/LeetCode/Algorithms/RunningSumOf1dArray/RunningSumOf1dArrayIterative.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.RunningSumOf1dArray;
+
+/// <inheritdoc />
+public class RunningSumOf1dArrayIterative : IRunningSumOf1dArray
+{
+    public int[] RunningSum(int[] nums)
+    {
+        if (nums.Length <= 1)
+        {
+            return nums;
+        }
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            nums[i] += nums[i - 1];
+        }
+
+        return nums;
+    }
+}
