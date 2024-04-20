93ca2145b29316dea60b491fa24cc7be01142602
eugene <eremeeveugene@gmail.com>
2024-04-20 20:47:41 +0300

    Running Sum of 1d Array (#373)

---
+++ source/LeetCode/Algorithms/RunningSumOf1dArray/RunningSumOf1dArrayIterative.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.RunningSumOf1dArray;
+
+/// <inheritdoc />
+public class RunningSumOf1dArrayIterative : IRunningSumOf1dArray
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int[] RunningSum(int[] nums)
+    {
+        if (nums.Length <= 1)
+        {
+            return nums;
+        }
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            nums[i] += nums[i - 1];
+        }
+
+        return nums;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/RunningSumOf1dArray/RunningSumOf1dArrayTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.RunningSumOf1dArray;
+
+namespace LeetCode.Tests.Algorithms.RunningSumOf1dArray;
+
+public abstract class RunningSumOf1dArrayTestsBase<T> where T : IRunningSumOf1dArray, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1, 2, 3, 4 }, new[] { 1, 3, 6, 10 })]
+    [DataRow(new[] { 1, 1, 1, 1, 1 }, new[] { 1, 2, 3, 4, 5 })]
+    [DataRow(new[] { 3, 1, 2, 10, 1 }, new[] { 3, 4, 6, 16, 17 })]
+    public void RunningSum_GivenArrays_ReturnsCumulativeSumsPerElement(int[] nums, int[] expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.RunningSum(nums);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
