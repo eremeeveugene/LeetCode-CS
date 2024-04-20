3a9f66999007c6bdbd89a771cbf7d2e36ae77d54
eugene <eremeeveugene@gmail.com>
2024-04-20 14:35:45 +0300

    Find All Groups of Farmland (#365)

---
+++ source/LeetCode/Algorithms/FindAllGroupOfFarmland/FindAllGroupOfFarmlandIterative.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindAllGroupOfFarmland;
+
+/// <inheritdoc />
+public class FindAllGroupOfFarmlandIterative : IFindAllGroupOfFarmland
+{
+    /// <summary>
+    ///     Time complexity - O(m * n)
+    ///     Space complexity - O(k), where k is the number of distinct rectangles found
+    /// </summary>
+    /// <param name="land"></param>
+    /// <returns></returns>
+    public int[][] FindFarmland(int[][] land)
+    {
+        var result = new List<int[]>();
+
+        for (var i = 0; i < land.Length; i++)
+        {
+            for (var j = 0; j < land[0].Length; j++)
+            {
+                if (land[i][j] == 1)
+                {
+                    result.Add(FindFarmlandCorner(land, i, j));
+                }
+            }
+        }
+
+        return [.. result];
+    }
+
+    private static int[] FindFarmlandCorner(IReadOnlyList<int[]> land, int x, int y)
+    {
+        var dx = x;
+        var dy = y;
+
+        while (dx < land.Count - 1 && land[dx + 1][dy] == 1)
+        {
+            dx++;
+        }
+
+        while (dy < land[0].Length - 1 && land[dx][dy + 1] == 1)
+        {
+            dy++;
+        }
+
+        for (var i = x; i <= dx; i++)
+        {
+            for (var j = y; j <= dy; j++)
+            {
+                land[i][j] = 0;
+            }
+        }
+
+        return [x, y, dx, dy];
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FindAllGroupOfFarmland/FindAllGroupOfFarmlandTestsBase.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindAllGroupOfFarmland;
+using LeetCode.Core.Helpers;
+using LeetCode.Tests.Base.Extensions;
+
+namespace LeetCode.Tests.Algorithms.FindAllGroupOfFarmland;
+
+public abstract class FindAllGroupOfFarmlandTestsBase<T> where T : IFindAllGroupOfFarmland, new()
+{
+    [TestMethod]
+    [DataRow("[[0]]", "[]")]
+    [DataRow("[[1,1],[0,0]]", "[[0,0,0,1]]")]
+    [DataRow("[[1,1],[1,1]]", "[[0,0,1,1]]")]
+    [DataRow("[[0,1],[0,1]]", "[[0,1,1,1]]")]
+    [DataRow("[[1,0,0],[0,1,1],[0,1,1]]", "[[0,0,0,0],[1,1,2,2]]")]
+    [DataRow("[[0,0,0,0,0],[0,1,1,1,0],[0,1,1,1,0],[0,1,1,1,0],[0,0,0,0,0]]", "[[1,1,3,3]]")]
+    public void FindFarmland_GivenLandArray_ReturnsExpectedFarmBoundaries(string landJsonArray,
+        string expectedResultJsonArray)
+    {
+        // Arrange
+        var expectedResult = JsonConvertHelper<int>.JsonArrayToJaggedArray(expectedResultJsonArray);
+
+        var solution = new T();
+
+        var land = JsonConvertHelper<int>.JsonArrayToJaggedArray(landJsonArray);
+
+        // Act
+        var actualResult = solution.FindFarmland(land);
+
+        // Assert
+        AssertExtensions.AssertJaggedArrayEqual(expectedResult, actualResult);
+    }
+}
