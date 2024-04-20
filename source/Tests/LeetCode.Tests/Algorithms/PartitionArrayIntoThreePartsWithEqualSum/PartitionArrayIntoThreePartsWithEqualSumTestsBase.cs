f7c1a4d785992eb3205307bca21e1c17696db279
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-04-20 20:24:01 +0300

    Partition Array Into Three Parts With Equal Sum

---
+++ source/Tests/LeetCode.Tests/Algorithms/PartitionArrayIntoThreePartsWithEqualSum/PartitionArrayIntoThreePartsWithEqualSumTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.PartitionArrayIntoThreePartsWithEqualSum;
+
+namespace LeetCode.Tests.Algorithms.PartitionArrayIntoThreePartsWithEqualSum;
+
+public abstract class PartitionArrayIntoThreePartsWithEqualSumTestsBase<T>
+    where T : IPartitionArrayIntoThreePartsWithEqualSum, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1, 1, 1, 1 }, false)]
+    [DataRow(new[] { 2, 2, 3 }, false)]
+    [DataRow(new[] { 1, -1, 1, -1 }, false)]
+    [DataRow(new[] { 0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1 }, true)]
+    [DataRow(new[] { 0, 2, 1, -6, 6, 7, 9, -1, 2, 0, 1 }, false)]
+    [DataRow(new[] { 3, 3, 6, 5, -2, 2, 5, 1, -9, 4 }, true)]
+    public void CanThreePartsEqualSum_GivenArray_ReturnsExpectedBoolean(int[] arr, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CanThreePartsEqualSum(arr);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
