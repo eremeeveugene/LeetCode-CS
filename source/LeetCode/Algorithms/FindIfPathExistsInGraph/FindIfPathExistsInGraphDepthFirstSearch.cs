f9f7a381b5b111ebc89a26819c46487a4ef2d769
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-04-21 13:37:26 +0300

    Find if Path Exists in Graph

---
+++ source/LeetCode/Algorithms/FindIfPathExistsInGraph/FindIfPathExistsInGraphDepthFirstSearch.cs
@@ -0,0 +1,61 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindIfPathExistsInGraph;
+
+/// <inheritdoc />
+public class FindIfPathExistsInGraphDepthFirstSearch : IFindIfPathExistsInGraph
+{
+    /// <summary>
+    ///     Time complexity - O(v + e), where v is the number of vertices and e is the number of edges
+    ///     Space complexity - O(v + e), where v is the number of vertices and e is the number of edges
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="edges"></param>
+    /// <param name="source"></param>
+    /// <param name="destination"></param>
+    /// <returns></returns>
+    public bool ValidPath(int n, int[][] edges, int source, int destination)
+    {
+        if (source == destination)
+        {
+            return true;
+        }
+
+        var graph = new Dictionary<int, List<int>>();
+
+        for (var i = 0; i < n; i++)
+        {
+            graph[i] = [];
+        }
+
+        foreach (var edge in edges)
+        {
+            graph[edge[0]].Add(edge[1]);
+            graph[edge[1]].Add(edge[0]);
+        }
+
+        return ValidPath(new HashSet<int>(), graph, source, destination);
+    }
+
+    private static bool ValidPath(ISet<int> visited, IReadOnlyDictionary<int, List<int>> graph, int current, int target)
+    {
+        if (current == target)
+        {
+            return true;
+        }
+
+        visited.Add(current);
+
+        return graph[current]
+            .Any(neighbor => !visited.Contains(neighbor) && ValidPath(visited, graph, neighbor, target));
+    }
+}
