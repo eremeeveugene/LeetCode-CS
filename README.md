9d2628dea37cde2f7499f49ab65567da5e2a72be
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-04-22 18:24:42 +0300

    Summary Ranges

--- README.md
+++ README.md
@@ -68,6 +68,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [217. Contains Duplicate](https://leetcode.com/problems/contains-duplicate/description/)
 - [219. Contains Duplicate II](https://leetcode.com/problems/contains-duplicate-ii/description/)
 - [220. Contains Duplicate III](https://leetcode.com/problems/contains-duplicate-iii/description/)
+- [228. Summary Ranges](https://leetcode.com/problems/summary-ranges/description/)
 - [234. Palindrome Linked List](https://leetcode.com/problems/palindrome-linked-list/description/)
 - [257. Binary Tree Paths](https://leetcode.com/problems/binary-tree-paths/description/)
 - [258. Add Digits](https://leetcode.com/problems/add-digits/description/)
---
+++ source/Tests/LeetCode.Tests/Algorithms/SummaryRanges/SummaryRangesTestsBase.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SummaryRanges;
+
+namespace LeetCode.Tests.Algorithms.SummaryRanges;
+
+public abstract class SummaryRangesTestsBase<T> where T : ISummaryRanges, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 0, 1, 2, 4, 5, 7 }, new[] { "0->2", "4->5", "7" })]
+    [DataRow(new[] { 0, 2, 3, 4, 6, 8, 9 }, new[] { "0", "2->4", "6", "8->9" })]
+    public void SummaryRanges_WithSequencesOfIntegers_ReturnsCorrectStringRanges(int[] nums, string[] expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.SummaryRanges(nums);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult.ToArray(), actualResult.ToArray());
+    }
+}
