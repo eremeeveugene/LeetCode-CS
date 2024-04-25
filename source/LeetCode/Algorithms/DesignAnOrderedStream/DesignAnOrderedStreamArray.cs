06ea4d73ef4b7f22377cfeb46ad4e1d2a748362c
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-04-25 16:05:15 +0300

    Design an Ordered Stream

---
+++ source/LeetCode/Algorithms/DesignAnOrderedStream/DesignAnOrderedStreamArray.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.DesignAnOrderedStream;
+
+/// <inheritdoc />
+public class DesignAnOrderedStreamArray(int n) : IDesignAnOrderedStream
+{
+    private readonly string?[] _stream = new string[n];
+    private int _pointer;
+
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="idKey"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public IList<string?> Insert(int idKey, string value)
+    {
+        var result = new List<string?>();
+
+        _stream[idKey - 1] = value;
+
+        while (_pointer < _stream.Length && _stream[_pointer] != null)
+        {
+            result.Add(_stream[_pointer]);
+            _pointer++;
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/DesignAnOrderedStream/DesignAnOrderedStreamTestsBase.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.DesignAnOrderedStream;
+
+namespace LeetCode.Tests.Algorithms.DesignAnOrderedStream;
+
+public abstract class DesignAnOrderedStreamTestsBase<T> where T : IDesignAnOrderedStreamArrayFactory, new()
+{
+    [TestMethod]
+    public void OrderedStream_InsertVariousIndexes_ReturnsExpectedOutput()
+    {
+        // Arrange
+        var designAnOrderedStreamFactory = new T();
+
+        // Act 
+        var designAnOrderedStream = designAnOrderedStreamFactory.Create(5);
+
+        // Assert
+        CollectionAssert.AreEqual(new List<string>(), designAnOrderedStream.Insert(3, "ccccc").ToArray());
+
+        CollectionAssert.AreEqual(new List<string> { "aaaaa" }, designAnOrderedStream.Insert(1, "aaaaa").ToArray());
+
+        CollectionAssert.AreEqual(new List<string> { "bbbbb", "ccccc" },
+            designAnOrderedStream.Insert(2, "bbbbb").ToArray());
+
+        CollectionAssert.AreEqual(new List<string>(), designAnOrderedStream.Insert(5, "eeeee").ToArray());
+
+        CollectionAssert.AreEqual(new List<string> { "ddddd", "eeeee" },
+            designAnOrderedStream.Insert(4, "ddddd").ToArray());
+    }
+}
