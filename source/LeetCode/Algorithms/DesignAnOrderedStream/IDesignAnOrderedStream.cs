e5d0dc1165c26026d268c0b595701300a15a5f7e
eugene <eremeeveugene@gmail.com>
2024-04-25 16:07:54 +0300

    Design an Ordered Stream (#387)

---
+++ source/LeetCode/Algorithms/DesignAnOrderedStream/IDesignAnOrderedStream.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.DesignAnOrderedStream;
+
+/// <summary>
+///     https://leetcode.com/problems/design-an-ordered-stream/
+/// </summary>
+public interface IDesignAnOrderedStream
+{
+    IList<string?> Insert(int idKey, string value);
+}
