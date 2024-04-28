f8a87b4d3d1dab8ecddc21317345042f983c98f7
eugene <eremeeveugene@gmail.com>
2024-04-28 14:31:38 +0300

    Sum of Distances in Tree (#395)

---
+++ source/LeetCode/Algorithms/SumOfDistancesInTree/SumOfDistancesInTreeDepthFirstSearch.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SumOfDistancesInTree;
+
+/// <inheritdoc />
+public class SumOfDistancesInTreeDepthFirstSearch : ISumOfDistancesInTree
+{
+    public int[] SumOfDistancesInTree(int n, int[][] edges)
+    {
+        var graph = new List<int>[n];
+
+        for (var i = 0; i < n; i++)
+        {
+            graph[i] = [];
+        }
+
+        foreach (var edge in edges)
+        {
+            int u = edge[0], v = edge[1];
+
+            graph[u].Add(v);
+            graph[v].Add(u);
+        }
+
+        var count = new int[n];
+        var ans = new int[n];
+
+        Dfs(0, -1, graph, count, ans);
+
+        Dfs2(0, -1, graph, count, ans);
+
+        return ans;
+    }
+
+    private static void Dfs(int node, int parent, List<int>[] graph, int[] count, int[] ans)
+    {
+        count[node] = 1;
+
+        foreach (var child in graph[node].Where(child => child != parent))
+        {
+            Dfs(child, node, graph, count, ans);
+
+            count[node] += count[child];
+            ans[node] += ans[child] + count[child];
+        }
+    }
+
+    private static void Dfs2(int node, int parent, List<int>[] graph, int[] count, int[] ans)
+    {
+        foreach (var child in graph[node].Where(child => child != parent))
+        {
+            ans[child] = ans[node] - count[child] + count.Length - count[child];
+
+            Dfs2(child, node, graph, count, ans);
+        }
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/SumOfDistancesInTree/SumOfDistancesInTreeTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SumOfDistancesInTree;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.SumOfDistancesInTree;
+
+public abstract class SumOfDistancesInTreeTestsBase<T> where T : ISumOfDistancesInTree, new()
+{
+    [TestMethod]
+    [DataRow(6, "[[0,1],[0,2],[2,3],[2,4],[2,5]]", new[] { 8, 12, 6, 10, 10, 10 })]
+    [DataRow(1, "[]", new[] { 0 })]
+    [DataRow(2, "[[1,0]]", new[] { 1, 1 })]
+    public void SumOfDistancesInTree_GivenNumberOfNodesAndEdges_ReturnsDistanceSumsArray(int n, string edgesJsonArray,
+        int[] expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        var edges = JsonConvertHelper<int>.JsonArrayToJaggedArray(edgesJsonArray);
+
+        // Act
+        var actualResult = solution.SumOfDistancesInTree(n, edges);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
