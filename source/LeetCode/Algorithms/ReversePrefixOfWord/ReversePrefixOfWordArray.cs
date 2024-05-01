9f5e9522daaf4702c4b087332d388217d39c340b
eugene <eremeeveugene@gmail.com>
2024-05-01 18:19:29 +0300

    Reverse Prefix of Word (#401)

---
+++ source/LeetCode/Algorithms/ReversePrefixOfWord/ReversePrefixOfWordArray.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ReversePrefixOfWord;
+
+/// <inheritdoc />
+public class ReversePrefixOfWordArray : IReversePrefixOfWord
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="word"></param>
+    /// <param name="ch"></param>
+    /// <returns></returns>
+    public string ReversePrefix(string word, char ch)
+    {
+        var wordArray = word.ToCharArray();
+        var chIndex = word.IndexOf(ch);
+
+        if (chIndex >= 0)
+        {
+            Array.Reverse(wordArray, 0, chIndex + 1);
+        }
+
+        return new string(wordArray);
+    }
+}
---
+++ source/LeetCode/Algorithms/ReversePrefixOfWord/ReversePrefixOfWordStringBuilder.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Text;
+
+namespace LeetCode.Algorithms.ReversePrefixOfWord;
+
+/// <inheritdoc />
+public class ReversePrefixOfWordStringBuilder : IReversePrefixOfWord
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="word"></param>
+    /// <param name="ch"></param>
+    /// <returns></returns>
+    public string ReversePrefix(string word, char ch)
+    {
+        var chIndex = word.IndexOf(ch);
+
+        if (chIndex < 0)
+        {
+            return word;
+        }
+
+        var result = new StringBuilder();
+
+        for (var i = chIndex; i >= 0; i--)
+        {
+            result.Append(word[i]);
+        }
+
+        for (var i = chIndex + 1; i < word.Length; i++)
+        {
+            result.Append(word[i]);
+        }
+
+        return result.ToString();
+    }
+}
