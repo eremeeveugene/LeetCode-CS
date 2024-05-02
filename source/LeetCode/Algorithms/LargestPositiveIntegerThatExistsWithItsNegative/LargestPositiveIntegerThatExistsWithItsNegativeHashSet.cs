1d571074d1ebdddf95a04ea239222415f0327f00
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-05-02 14:47:47 +0300

    LargestPositiveIntegerThatExistsWithItsNegativeSortingTwoPointers

---
+++ source/LeetCode/Algorithms/LargestPositiveIntegerThatExistsWithItsNegative/LargestPositiveIntegerThatExistsWithItsNegativeHashSet.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.LargestPositiveIntegerThatExistsWithItsNegative;
+
+/// <inheritdoc />
+public class LargestPositiveIntegerThatExistsWithItsNegativeHashSet : ILargestPositiveIntegerThatExistsWithItsNegative
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int FindMaxK(int[] nums)
+    {
+        var maxK = -1;
+
+        var numsHashSet = new HashSet<int>();
+
+        foreach (var num in nums)
+        {
+            if (numsHashSet.Contains(-num))
+            {
+                maxK = Math.Max(maxK, Math.Abs(num));
+            }
+            else
+            {
+                numsHashSet.Add(num);
+            }
+        }
+
+        return maxK;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/LargestPositiveIntegerThatExistsWithItsNegative/LargestPositiveIntegerThatExistsWithItsNegativeTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.LargestPositiveIntegerThatExistsWithItsNegative;
+
+namespace LeetCode.Tests.Algorithms.LargestPositiveIntegerThatExistsWithItsNegative;
+
+public abstract class LargestPositiveIntegerThatExistsWithItsNegativeTestsBase<T>
+    where T : ILargestPositiveIntegerThatExistsWithItsNegative, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1 }, -1)]
+    [DataRow(new[] { -1, 2, -3, 3 }, 3)]
+    [DataRow(new[] { -1, 10, 6, 7, -7, 1 }, 7)]
+    [DataRow(new[] { -10, 8, 6, 7, -2, -3 }, -1)]
+    public void FindMaxK_WithInputArray_ReturnsExpectedMaximumK(int[] nums, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FindMaxK(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
