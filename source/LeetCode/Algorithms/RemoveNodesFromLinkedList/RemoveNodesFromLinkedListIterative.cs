975a008b4f5111268aec1cf40d2b78fe1f760791
eugene <eremeeveugene@gmail.com>
2024-05-06 15:07:39 +0300

    Remove Nodes From Linked List (#411)

---
+++ source/LeetCode/Algorithms/RemoveNodesFromLinkedList/RemoveNodesFromLinkedListIterative.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.RemoveNodesFromLinkedList;
+
+/// <inheritdoc />
+public class RemoveNodesFromLinkedListIterative : IRemoveNodesFromLinkedList
+{
+    public ListNode? RemoveNodes(ListNode? head)
+    {
+        head = Reverse(head);
+
+        var dummyHead = new ListNode();
+        var tail = dummyHead;
+        var maxValue = int.MinValue;
+
+        while (head != null)
+        {
+            if (head.val >= maxValue)
+            {
+                maxValue = head.val;
+                tail.next = head;
+                tail = head;
+            }
+
+            head = head.next;
+        }
+
+        tail.next = null;
+
+        return Reverse(dummyHead.next);
+    }
+
+    private static ListNode? Reverse(ListNode? head)
+    {
+        ListNode? prev = null;
+
+        var curr = head;
+
+        while (curr != null)
+        {
+            var next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/RemoveNodesFromLinkedList/RemoveNodesFromLinkedListTestsBase.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.RemoveNodesFromLinkedList;
+using LeetCode.Core.Extensions;
+using LeetCode.Tests.Base.Extensions;
+
+namespace LeetCode.Tests.Algorithms.RemoveNodesFromLinkedList;
+
+public abstract class RemoveNodesFromLinkedListTestsBase<T> where T : IRemoveNodesFromLinkedList, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1 }, new[] { 1 })]
+    [DataRow(new[] { 1, 1, 1, 1 }, new[] { 1, 1, 1, 1 })]
+    [DataRow(new[] { 5, 2, 13, 3, 8 }, new[] { 13, 8 })]
+    [DataRow(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new[] { 9 })]
+    [DataRow(new[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 }, new[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 })]
+    [DataRow(new[] { 1, 2, 3, 1, 2, 3, 1, 2, 3 }, new[] { 3, 3, 3 })]
+    [DataRow(new[] { 3, 2, 1, 3, 2, 1, 3, 2, 1 }, new[] { 3, 3, 3, 2, 1 })]
+    public void RemoveNodes_WithVariousLists_RemovesExpectedNodes(int[] headArray, int[] expectedResultArray)
+    {
+        // Arrange
+        var head = headArray.ToListNode();
+
+        var expectedResult = expectedResultArray.ToListNode();
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.RemoveNodes(head);
+
+        // Assert
+        AssertExtensions.AreListNodesEqual(expectedResult, actualResult);
+    }
+}
