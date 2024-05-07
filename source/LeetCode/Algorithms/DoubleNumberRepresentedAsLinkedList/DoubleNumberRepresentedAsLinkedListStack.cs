482f22eb6a12dfa116e2434dd9ed42d485f24f37
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-05-07 16:12:42 +0300

    Double a Number Represented as a Linked List

---
+++ source/LeetCode/Algorithms/DoubleNumberRepresentedAsLinkedList/DoubleNumberRepresentedAsLinkedListStack.cs
@@ -0,0 +1,70 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.DoubleNumberRepresentedAsLinkedList;
+
+/// <inheritdoc />
+public class DoubleNumberRepresentedAsLinkedListStack : IDoubleNumberRepresentedAsLinkedList
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="head"></param>
+    /// <returns></returns>
+    public ListNode? DoubleIt(ListNode? head)
+    {
+        var stack = new Stack<ListNode>();
+
+        while (head != null)
+        {
+            stack.Push(head);
+
+            head = head.next;
+        }
+
+        var dummyHead = new ListNode();
+
+        var carry = 0;
+
+        while (stack.Count > 0)
+        {
+            var current = stack.Pop();
+
+            var newVal = (current.val * 2) + carry;
+
+            if (newVal > 9)
+            {
+                carry = 1;
+
+                newVal %= 10;
+            }
+            else
+            {
+                carry = 0;
+            }
+
+            current.val = newVal;
+            current.next = dummyHead.next;
+
+            dummyHead.next = current;
+        }
+
+        if (carry > 0)
+        {
+            dummyHead.next = new ListNode(1, dummyHead.next);
+        }
+
+        return dummyHead.next;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/DoubleNumberRepresentedAsLinkedList/DoubleNumberRepresentedAsLinkedListTestsBase.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.DoubleNumberRepresentedAsLinkedList;
+using LeetCode.Core.Extensions;
+using LeetCode.Tests.Base.Extensions;
+
+namespace LeetCode.Tests.Algorithms.DoubleNumberRepresentedAsLinkedList;
+
+public abstract class DoubleNumberRepresentedAsLinkedListTestsBase<T>
+    where T : IDoubleNumberRepresentedAsLinkedList, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1, 8, 9 }, new[] { 3, 7, 8 })]
+    [DataRow(new[] { 9, 9, 9 }, new[] { 1, 9, 9, 8 })]
+    public void ListNode_DoubleIt_GivenListNode_ReturnsDoubledValuesListNode(int[] headArray, int[] expectedResultArray)
+    {
+        // Arrange
+        var head = headArray.ToListNode();
+
+        var expectedResult = expectedResultArray.ToListNode();
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.DoubleIt(head);
+
+        // Assert
+        AssertExtensions.AreListNodesEqual(expectedResult, actualResult);
+    }
+}
