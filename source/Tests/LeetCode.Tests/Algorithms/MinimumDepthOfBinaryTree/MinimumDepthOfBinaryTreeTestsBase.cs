f5d49a7426932f2888e6706ce7fec858db9002e1
eugene <eremeeveugene@gmail.com>
2024-05-08 18:27:55 +0300

    Minimum Depth of Binary Tree (#420)

---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumDepthOfBinaryTree/MinimumDepthOfBinaryTreeTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumDepthOfBinaryTree;
+using LeetCode.Core.Helpers;
+using LeetCode.Core.Models;
+
+namespace LeetCode.Tests.Algorithms.MinimumDepthOfBinaryTree;
+
+public abstract class MinimumDepthOfBinaryTreeTestsBase<T> where T : IMinimumDepthOfBinaryTree, new()
+{
+    [TestMethod]
+    [DataRow("[3,9,20,null,null,15,7]", 2)]
+    [DataRow("[2,null,3,null,4,null,5,null,6]", 5)]
+    public void MinDepth_GivenBinaryTreeAsJsonArray_ReturnsMinimumDepth(string rootJsonArray, int expectedResult)
+    {
+        // Arrange
+        IList<int?> rootArray = JsonConvertHelper<int?>.JsonArrayToList(rootJsonArray);
+        var root = TreeNode.BuildTree(rootArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinDepth(root);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
