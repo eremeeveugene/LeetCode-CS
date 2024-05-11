da5e2e06e09e48724b68d4bf62c0464ab5df56b0
eugene <eremeeveugene@gmail.com>
2024-05-11 14:21:53 +0300

    Minimum Cost to Hire K Workers (#426)

---
+++ source/LeetCode/Algorithms/MinimumCostToHireKWorkers/MinimumCostToHireKWorkersPriorityQueue.cs
@@ -0,0 +1,63 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumCostToHireKWorkers;
+
+/// <summary>
+///     https://leetcode.com/problems/minimum-cost-to-hire-k-workers/
+/// </summary>
+public class MinimumCostToHireKWorkersPriorityQueue : IMinimumCostToHireKWorkers
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="quality"></param>
+    /// <param name="wage"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public double MincostToHireWorkers(int[] quality, int[] wage, int k)
+    {
+        var workers = new (double ratio, int quality)[quality.Length];
+
+        for (var i = 0; i < quality.Length; ++i)
+        {
+            workers[i] = ((double)wage[i] / quality[i], quality[i]);
+        }
+
+        Array.Sort(workers, (a, b) => a.ratio.CompareTo(b.ratio));
+
+        var result = double.MaxValue;
+
+        var priorityQueue = new PriorityQueue<double, double>();
+
+        double sum = 0;
+
+        foreach (var worker in workers)
+        {
+            sum += worker.quality;
+
+            priorityQueue.Enqueue(-worker.quality, -worker.quality);
+
+            if (priorityQueue.Count > k)
+            {
+                sum += priorityQueue.Dequeue();
+            }
+
+            if (priorityQueue.Count == k)
+            {
+                result = Math.Min(result, sum * worker.ratio);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumCostToHireKWorkers/MinimumCostToHireKWorkersTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumCostToHireKWorkers;
+
+namespace LeetCode.Tests.Algorithms.MinimumCostToHireKWorkers;
+
+public abstract class MinimumCostToHireKWorkersTestsBase<T> where T : IMinimumCostToHireKWorkers, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1 }, new[] { 1 }, 1, 1)]
+    [DataRow(new[] { 10, 20, 5 }, new[] { 70, 50, 30 }, 2, 105)]
+    [DataRow(new[] { 3, 1, 10, 10, 1 }, new[] { 4, 8, 2, 2, 7 }, 3, 30.66667)]
+    [DataRow(new[] { 3, 5, 8, 10, 9, 5, 1, 2, 4, 1 }, new[] { 8, 8, 6, 9, 5, 6, 8, 7, 5, 8 }, 3, 21.25)]
+    [DataRow(new[] { 5, 7, 4, 2, 6, 5, 10, 9, 4, 2 }, new[] { 10, 10, 3, 7, 3, 7, 6, 2, 6, 4 }, 3, 14.25)]
+    public void MincostToHireWorkers_GivenQualityAndWageArraysAndK_ReturnsMinimumCostWithPrecision(int[] quality,
+        int[] wage, int k, double expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MincostToHireWorkers(quality, wage, k);
+
+        // Assert
+        Assert.AreEqual(Math.Round(expectedResult, 2), Math.Round(actualResult, 2));
+    }
+}
