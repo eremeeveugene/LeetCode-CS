519983b1f1ee6c0a45513c4088a54ef4b72acbf0
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-05-13 13:24:17 +0300

    Power of Two

---
+++ source/LeetCode/Algorithms/PowerOfTwo/PowerOfTwoBitwise.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.PowerOfTwo;
+
+/// <inheritdoc />
+public class PowerOfTwoBitwise : IPowerOfTwo
+{
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public bool IsPowerOfTwo(int n)
+    {
+        if (n <= 0)
+        {
+            return false;
+        }
+
+        return (n & (n - 1)) == 0;
+    }
+}
