272b9168ac48a4a5282d1ce08da6ee27a599a7be
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-05-17 13:41:45 +0300

    Delete Leaves With a Given Value

---
+++ source/LeetCode/Algorithms/DeleteLeavesWithGivenValue/DeleteLeavesWithGivenValueDepthFirstSearch.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.DeleteLeavesWithGivenValue;
+
+/// <inheritdoc />
+public class DeleteLeavesWithGivenValueDepthFirstSearch : IDeleteLeavesWithGivenValue
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n) for the skewed tree, O(log n) for a balanced tree
+    /// </summary>
+    /// <param name="root"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    public TreeNode? RemoveLeafNodes(TreeNode? root, int target)
+    {
+        if (root == null)
+        {
+            return null;
+        }
+
+        root.left = RemoveLeafNodes(root.left, target);
+        root.right = RemoveLeafNodes(root.right, target);
+
+        if (root.left == null && root.right == null)
+        {
+            return root.val == target ? null : root;
+        }
+
+        return root;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/DeleteLeavesWithGivenValue/DeleteLeavesWithGivenValueTestsBase.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.DeleteLeavesWithGivenValue;
+using LeetCode.Core.Helpers;
+using LeetCode.Core.Models;
+using LeetCode.Tests.Base.Extensions;
+
+namespace LeetCode.Tests.Algorithms.DeleteLeavesWithGivenValue;
+
+public abstract class DeleteLeavesWithGivenValueTestsBase<T> where T : IDeleteLeavesWithGivenValue, new()
+{
+    [TestMethod]
+    [DataRow("[1]", 1, "[]")]
+    [DataRow("[1,2,3,2,null,2,4]", 2, "[1,null,3,null,4]")]
+    [DataRow("[1,3,3,3,2]", 3, "[1,3,null,null,2]")]
+    [DataRow("[1,2,null,2,null,2]", 2, "[1]")]
+    [DataRow("[1,2,2,3,null,null,3,4,null,null,4,5,null,null,5,5,null,null,5,5,null,null,5,5,null,null,5]", 5,
+        "[1,2,2,3,null,null,3,4,null,null,4]")]
+    public void RemoveLeafNodes_GivenTarget_RemovesAllLeafNodesWithTargetValue(string rootJsonArray, int target,
+        string expectedResultJsonArray)
+    {
+        // Arrange
+        IList<int?> rootArray = JsonConvertHelper<int?>.JsonArrayToList(rootJsonArray);
+        var root = TreeNode.BuildTree(rootArray);
+
+        IList<int?> expectedResultArray = JsonConvertHelper<int?>.JsonArrayToList(expectedResultJsonArray);
+        var expectedResult = TreeNode.BuildTree(expectedResultArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.RemoveLeafNodes(root, target);
+
+        // Assert
+        TreeNodeAssertExtensions.AreEqual(expectedResult, actualResult);
+    }
+}
