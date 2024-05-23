91c66b3849fdb78afb8e0adadc32af725571a3c7
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-05-23 18:48:01 +0300

    The Number of Beautiful Subsets

---
+++ source/LeetCode/Algorithms/TheNumberOfBeautifulSubsets/TheNumberOfBeautifulSubsetsBacktracking.cs
@@ -0,0 +1,61 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.TheNumberOfBeautifulSubsets;
+
+/// <inheritdoc />
+public class TheNumberOfBeautifulSubsetsBacktracking : ITheNumberOfBeautifulSubsets
+{
+    /// <summary>
+    ///     Time complexity - O(n * 2^n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int BeautifulSubsets(int[] nums, int k)
+    {
+        if (nums.Length <= 1)
+        {
+            return nums.Length;
+        }
+
+        var count = 0;
+
+        Backtrack(nums, k, 0, new List<int>(), ref count);
+
+        return count - 1;
+    }
+
+    private static void Backtrack(IReadOnlyList<int> nums, int k, int index, IList<int> subset, ref int count)
+    {
+        while (true)
+        {
+            if (index >= nums.Count)
+            {
+                count++;
+
+                return;
+            }
+
+            if (!subset.Contains(nums[index] - k) && !subset.Contains(k + nums[index]))
+            {
+                subset.Add(nums[index]);
+
+                Backtrack(nums, k, index + 1, subset, ref count);
+
+                subset.RemoveAt(subset.Count - 1);
+            }
+
+            index += 1;
+        }
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/TheNumberOfBeautifulSubsets/TheNumberOfBeautifulSubsetsTestsBase.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.TheNumberOfBeautifulSubsets;
+
+namespace LeetCode.Tests.Algorithms.TheNumberOfBeautifulSubsets;
+
+public abstract class TheNumberOfBeautifulSubsetsTestsBase<T> where T : ITheNumberOfBeautifulSubsets, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1 }, 1, 1)]
+    [DataRow(new[] { 1, 3 }, 3, 3)]
+    [DataRow(new[] { 2, 4, 6 }, 2, 4)]
+    [DataRow(new[] { 1000, 1 }, 999, 2)]
+    [DataRow(new[] { 2, 4, 6, 8, 10, 12, 14, 15, 16, 17, 18, 100, 103, 106 }, 3, 4799)]
+    [DataRow(new[] { 1000, 999, 998, 997, 996, 995, 994, 993, 992, 991 }, 1, 143)]
+    [DataRow(
+        new[] { 1000, 999, 998, 997, 996, 995, 994, 993, 992, 991, 1000, 999, 998, 997, 996, 995, 994, 993, 992, 991 },
+        2, 9408)]
+    [DataRow(
+        new[] { 1000, 999, 998, 997, 996, 995, 994, 993, 992, 991, 990, 989, 988, 987, 986, 985, 984, 983, 982, 981 },
+        2, 20735)]
+    public void BeautifulSubsets_WithNumsAndK_ReturnsExpectedResult(int[] nums, int k, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.BeautifulSubsets(nums, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
