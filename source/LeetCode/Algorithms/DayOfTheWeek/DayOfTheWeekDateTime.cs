c8b3ce40a4f2c8970b5cb8c23d4bb863d9aedcfe
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-05-24 14:52:08 +0300

    Day of the Week

---
+++ source/LeetCode/Algorithms/DayOfTheWeek/DayOfTheWeekDateTime.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.DayOfTheWeek;
+
+/// <inheritdoc />
+public class DayOfTheWeekDateTime : IDayOfTheWeek
+{
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="day"></param>
+    /// <param name="month"></param>
+    /// <param name="year"></param>
+    /// <returns></returns>
+    public string DayOfTheWeek(int day, int month, int year)
+    {
+        var date = new DateTime(year, month, day);
+
+        return date.DayOfWeek.ToString();
+    }
+}
