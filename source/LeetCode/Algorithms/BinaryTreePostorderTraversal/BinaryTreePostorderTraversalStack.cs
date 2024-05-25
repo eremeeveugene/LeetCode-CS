306f258ebc905b5a44b5e76fa957cda47637782e
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-05-25 15:25:06 +0300

    Binary Tree Postorder Traversal

---
+++ source/LeetCode/Algorithms/BinaryTreePostorderTraversal/BinaryTreePostorderTraversalStack.cs
@@ -0,0 +1,57 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.BinaryTreePostorderTraversal;
+
+/// <inheritdoc />
+public class BinaryTreePostorderTraversalStack : IBinaryTreePostorderTraversal
+{
+    /// <summary>
+    ///     Time complexity - O(n^2)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="root"></param>
+    /// <returns></returns>
+    public IList<int> PostorderTraversal(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return new List<int>();
+        }
+
+        var result = new List<int>();
+
+        var stack = new Stack<TreeNode>();
+
+        stack.Push(root);
+
+        while (stack.Count > 0)
+        {
+            var treeNode = stack.Pop();
+
+            result.Insert(0, treeNode.val);
+
+            if (treeNode.left != null)
+            {
+                stack.Push(treeNode.left);
+            }
+
+            if (treeNode.right != null)
+            {
+                stack.Push(treeNode.right);
+            }
+        }
+
+        return result;
+    }
+}
---
+++ source/LeetCode/Algorithms/BinaryTreePostorderTraversal/IBinaryTreePostorderTraversal.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.BinaryTreePostorderTraversal;
+
+/// <summary>
+///     https://leetcode.com/problems/binary-tree-postorder-traversal/
+/// </summary>
+public interface IBinaryTreePostorderTraversal
+{
+    IList<int> PostorderTraversal(TreeNode? root);
+}
