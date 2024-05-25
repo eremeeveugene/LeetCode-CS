7cb16a2eab80b609efabc0446a52929de0ddec6d
eugene <eremeeveugene@gmail.com>
2024-05-25 18:04:17 +0300

    N-ary Tree Preorder Traversal (#478)

---
+++ source/LeetCode/Algorithms/NaryTreePreorderTraversal/NaryTreePreorderTraversalRecursive.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.NaryTreePreorderTraversal;
+
+/// <inheritdoc />
+public class NaryTreePreorderTraversalRecursive : INaryTreePreorderTraversal
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="root"></param>
+    /// <returns></returns>
+    public IList<int> Preorder(Node? root)
+    {
+        if (root == null)
+        {
+            return new List<int>();
+        }
+
+        var result = new List<int>();
+
+        Preorder(root, result);
+
+        return result;
+    }
+
+    private static void Preorder(Node root, ICollection<int> result)
+    {
+        result.Add(root.val);
+
+        if (root.children == null)
+        {
+            return;
+        }
+
+        foreach (var child in root.children)
+        {
+            Preorder(child, result);
+        }
+    }
+}
---
+++ source/LeetCode/Algorithms/NaryTreePreorderTraversal/NaryTreePreorderTraversalStack.cs
@@ -0,0 +1,57 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.NaryTreePreorderTraversal;
+
+/// <inheritdoc />
+public class NaryTreePreorderTraversalStack : INaryTreePreorderTraversal
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="root"></param>
+    /// <returns></returns>
+    public IList<int> Preorder(Node? root)
+    {
+        if (root == null)
+        {
+            return new List<int>();
+        }
+
+        var result = new List<int>();
+
+        var stack = new Stack<Node>();
+
+        stack.Push(root);
+
+        while (stack.Count > 0)
+        {
+            var node = stack.Pop();
+
+            result.Add(node.val);
+
+            if (node.children == null)
+            {
+                continue;
+            }
+
+            for (var i = node.children.Count - 1; i >= 0; i--)
+            {
+                stack.Push(node.children[i]);
+            }
+        }
+
+        return result;
+    }
+}
