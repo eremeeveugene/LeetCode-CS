e51a1ae9a7545992243ac1fa957bc8f090c7542d
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-05-26 12:48:37 +0300

    Number of 1 Bits

---
+++ source/LeetCode/Algorithms/NumberOfOneBits/NumberOfOneBitsBitwise.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.NumberOfOneBits;
+
+/// <inheritdoc />
+public class NumberOfOneBitsBitwise : INumberOfOneBits
+{
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public int HammingWeight(int n)
+    {
+        var count = 0;
+
+        while (n > 0)
+        {
+            if ((n & 1) == 1)
+            {
+                count++;
+            }
+
+            n >>= 1;
+        }
+
+        return count;
+    }
+}
---
+++ source/LeetCode/Algorithms/NumberOfOneBits/NumberOfOneBitsLookup.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.NumberOfOneBits;
+
+/// <inheritdoc />
+public class NumberOfOneBitsLookup : INumberOfOneBits
+{
+    private static readonly int[] WordBits = new int[65536];
+
+    static NumberOfOneBitsLookup()
+    {
+        uint i;
+
+        for (i = 0; i <= 0xFFFF; i++)
+        {
+            var x = i;
+
+            int count;
+
+            for (count = 0; x > 0; count++)
+            {
+                x &= x - 1;
+            }
+
+            WordBits[i] = count;
+        }
+    }
+
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public int HammingWeight(int n)
+    {
+        return WordBits[n & 0xFFFF] + WordBits[(n >> 16) & 0xFFFF];
+    }
+}
