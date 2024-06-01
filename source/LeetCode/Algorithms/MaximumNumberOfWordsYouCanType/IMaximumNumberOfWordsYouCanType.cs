263781741a4bb9c92d6c5da0a90304073fe590ee
eugene <eremeeveugene@gmail.com>
2024-06-01 11:05:59 +0300

    Maximum Number of Words You Can Type (#495)

---
+++ source/LeetCode/Algorithms/MaximumNumberOfWordsYouCanType/IMaximumNumberOfWordsYouCanType.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumNumberOfWordsYouCanType;
+
+/// <summary>
+///     https://leetcode.com/problems/maximum-number-of-words-you-can-type/
+/// </summary>
+public interface IMaximumNumberOfWordsYouCanType
+{
+    int CanBeTypedWords(string text, string brokenLetters);
+}
---
+++ source/LeetCode/Algorithms/MaximumNumberOfWordsYouCanType/MaximumNumberOfWordsYouCanTypeHashSet.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumNumberOfWordsYouCanType;
+
+/// <inheritdoc />
+public class MaximumNumberOfWordsYouCanTypeHashSet : IMaximumNumberOfWordsYouCanType
+{
+    /// <summary>
+    ///     Time complexity - O(n + m), where n is the length of the text and m is the length of brokenLetters
+    ///     Space complexity - O(n + m), where n is the length of the text and m is the length of brokenLetters
+    /// </summary>
+    /// <param name="text"></param>
+    /// <param name="brokenLetters"></param>
+    /// <returns></returns>
+    public int CanBeTypedWords(string text, string brokenLetters)
+    {
+        var brokenLettersHashSet = new HashSet<char>(brokenLetters);
+        var words = text.Split(' ');
+        var count = words.Length;
+
+        foreach (var word in words)
+        {
+            if (word.Any(brokenLettersHashSet.Contains))
+            {
+                count--;
+            }
+        }
+
+        return count;
+    }
+}
