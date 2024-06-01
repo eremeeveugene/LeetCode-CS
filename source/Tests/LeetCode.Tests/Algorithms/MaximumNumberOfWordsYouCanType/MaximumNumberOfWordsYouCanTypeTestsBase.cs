724a95937fcc049c6e35230da610fb68df4211e6
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-06-01 11:00:01 +0300

    Maximum Number of Words You Can Type

---
+++ source/Tests/LeetCode.Tests/Algorithms/MaximumNumberOfWordsYouCanType/MaximumNumberOfWordsYouCanTypeTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaximumNumberOfWordsYouCanType;
+
+namespace LeetCode.Tests.Algorithms.MaximumNumberOfWordsYouCanType;
+
+public abstract class MaximumNumberOfWordsYouCanTypeTestsBase<T> where T : IMaximumNumberOfWordsYouCanType, new()
+{
+    [TestMethod]
+    [DataRow("hello world", "ad", 1)]
+    [DataRow("leet code", "lt", 1)]
+    [DataRow("leet code", "e", 0)]
+    public void CanBeTypedWords_WithGivenTextAndBrokenLetters_ReturnsExpectedCount(string text, string brokenLetters,
+        int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CanBeTypedWords(text, brokenLetters);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
