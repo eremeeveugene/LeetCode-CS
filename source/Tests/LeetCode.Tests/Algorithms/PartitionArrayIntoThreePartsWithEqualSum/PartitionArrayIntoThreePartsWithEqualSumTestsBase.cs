d805b4d67cdef68f54c6607abb3d37f60c56cfc5
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-06-02 19:06:05 +0300

    Improve code coverage

--- source/Tests/LeetCode.Tests/Algorithms/PartitionArrayIntoThreePartsWithEqualSum/PartitionArrayIntoThreePartsWithEqualSumTestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/PartitionArrayIntoThreePartsWithEqualSum/PartitionArrayIntoThreePartsWithEqualSumTestsBase.cs
@@ -23,6 +23,15 @@ public abstract class PartitionArrayIntoThreePartsWithEqualSumTestsBase<T>
     [DataRow(new[] { 0, 2, 1, -6, 6, -7, 9, 1, 2, 0, 1 }, true)]
     [DataRow(new[] { 0, 2, 1, -6, 6, 7, 9, -1, 2, 0, 1 }, false)]
     [DataRow(new[] { 3, 3, 6, 5, -2, 2, 5, 1, -9, 4 }, true)]
+    [DataRow(new[] { 0, 0, 0, 0, 0 }, true)]
+    [DataRow(new[] { 0, 0, 0, 0, 1 }, false)]
+    [DataRow(new[] { 1, 2, 3, 0, 3, 2, 1 }, false)]
+    [DataRow(new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, false)]
+    [DataRow(new[] { 1, -1, 1, -1, 1, -1, 1, -1, 1, -1 }, true)]
+    [DataRow(new[] { 6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, true)]
+    [DataRow(new[] { 1, 2, 3, 4, 5, 6 }, false)]
+    [DataRow(new[] { -3, 3, -3, 3, -3, 3 }, true)]
+    [DataRow(new[] { 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1 }, true)]
     public void CanThreePartsEqualSum_GivenArray_ReturnsExpectedBoolean(int[] arr, bool expectedResult)
     {
         // Arrange
--- source/Tests/LeetCode.Tests/Algorithms/PathSum2/PathSum2TestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/PathSum2/PathSum2TestsBase.cs
@@ -22,6 +22,15 @@ public abstract class PathSum2TestsBase<T> where T : IPathSum2, new()
     [DataRow("[5,4,8,11,null,13,4,7,2,null,null,5,1]", 22, "[[5,4,11,2],[5,8,4,5]]")]
     [DataRow("[1,2,3]", 5, "[]")]
     [DataRow("[1,2]", 0, "[]")]
+    [DataRow("[1,2,3]", 3, "[[1,2]]")]
+    [DataRow("[1,-2,-3,1,3,-2,null,-1]", -1, "[[1,-2,1,-1]]")]
+    [DataRow("[1]", 1, "[[1]]")]
+    [DataRow("[1,2]", 1, "[]")]
+    [DataRow("[1,2,null,3]", 6, "[[1,2,3]]")]
+    [DataRow("[1,2,null,3,4]", 7, "[[1,2,4]]")]
+    [DataRow("[5,4,8,11,null,13,4,7,2,null,null,5,1,6,9]", 30, "[]")]
+    [DataRow("[5,4,8,11,null,13,4,7,2,null,null,5,1]", 26, "[[5,8,13]]")]
+    [DataRow("[10,5,-3,3,2,11,3,-2,1]", 18, "[[10,-3,11]]")]
     public void PathSum_GivenTreeAndTargetSum_ReturnsExpectedPaths(string jsonRootArray, int targetSum,
         string expectedJsonResult)
     {
