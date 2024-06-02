d8e191299adb8b15653f2eae633e559b7a078ce2
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-06-02 14:24:22 +0300

    Faulty Keyboard

---
+++ source/Tests/LeetCode.Tests/Algorithms/FaultyKeyboard/FaultyKeyboardTestsBase.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FaultyKeyboard;
+
+namespace LeetCode.Tests.Algorithms.FaultyKeyboard;
+
+public abstract class FaultyKeyboardTestsBase<T> where T : IFaultyKeyboard, new()
+{
+    [TestMethod]
+    [DataRow("string", "rtsng")]
+    [DataRow("poiinter", "ponter")]
+    public void FinalString_WithGivenString_ReturnsTransformedString(string s, string expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FinalString(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
