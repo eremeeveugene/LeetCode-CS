f8bcb303c82e8b9d8dc78dbe8574266f06b682cb
eugene <eremeeveugene@gmail.com>
2024-06-02 14:00:22 +0300

    Reverse Vowels of a String (#497)

---
+++ source/LeetCode/Algorithms/ReverseVowelsOfString/IReverseVowelsOfString.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ReverseVowelsOfString;
+
+/// <summary>
+///     https://leetcode.com/problems/reverse-vowels-of-a-string/
+/// </summary>
+public interface IReverseVowelsOfString
+{
+    string ReverseVowels(string s);
+}
