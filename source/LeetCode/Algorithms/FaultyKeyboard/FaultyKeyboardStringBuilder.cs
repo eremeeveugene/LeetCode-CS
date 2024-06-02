05848e41ea425e9186459ac403d6150922b1e6c9
eugene <eremeeveugene@gmail.com>
2024-06-02 14:35:07 +0300

    Faulty Keyboard (#499)

---
+++ source/LeetCode/Algorithms/FaultyKeyboard/FaultyKeyboardStringBuilder.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Text;
+
+namespace LeetCode.Algorithms.FaultyKeyboard;
+
+/// <inheritdoc />
+public class FaultyKeyboardStringBuilder : IFaultyKeyboard
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public string FinalString(string s)
+    {
+        var stringBuilder = new StringBuilder();
+
+        foreach (var c in s)
+        {
+            if (c == 'i')
+            {
+                var left = 0;
+                var right = stringBuilder.Length - 1;
+
+                while (left < right)
+                {
+                    (stringBuilder[left], stringBuilder[right]) = (stringBuilder[right], stringBuilder[left]);
+
+                    left++;
+                    right--;
+                }
+            }
+            else
+            {
+                stringBuilder.Append(c);
+            }
+        }
+
+        return stringBuilder.ToString();
+    }
+}
