c8cce8d227e7b2cc3e64d906cec1b0172e3f922b
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-06-03 15:18:23 +0300

    Append Characters to String to Make Subsequence

--- README.md
+++ README.md
@@ -235,6 +235,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [2444. Count Subarrays With Fixed Bounds](https://leetcode.com/problems/count-subarrays-with-fixed-bounds/description/)
 - [2469. Convert the Temperature](https://leetcode.com/problems/convert-the-temperature/description/)
 - [2485. Find the Pivot Integer](https://leetcode.com/problems/find-the-pivot-integer/description/)
+- [2486. Append Characters to String to Make Subsequence](https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/description/)
 - [2487. Remove Nodes From Linked List](https://leetcode.com/problems/remove-nodes-from-linked-list/description/)
 - [2520. Count the Digits That Divide a Number](https://leetcode.com/problems/count-the-digits-that-divide-a-number/description/)
 - [2535. Difference Between Element Sum and Digit Sum of an Array](https://leetcode.com/problems/difference-between-element-sum-and-digit-sum-of-an-array/description/)
---
+++ source/Tests/LeetCode.Tests/Algorithms/AppendCharactersToStringToMakeSubsequence/AppendCharactersToStringToMakeSubsequenceTestsBase.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.AppendCharactersToStringToMakeSubsequence;
+
+namespace LeetCode.Tests.Algorithms.AppendCharactersToStringToMakeSubsequence;
+
+public abstract class AppendCharactersToStringToMakeSubsequenceTestsBase<T>
+    where T : IAppendCharactersToStringToMakeSubsequence, new()
+{
+    [TestMethod]
+    [DataRow("a", "a", 0)]
+    [DataRow("a", "z", 1)]
+    [DataRow("z", "abcde", 5)]
+    [DataRow("abz", "abc", 1)]
+    [DataRow("abcde", "a", 0)]
+    [DataRow("abcde", "abc", 0)]
+    [DataRow("abcde", "bdf", 1)]
+    [DataRow("abcdef", "abcdef", 0)]
+    [DataRow("axbyczd", "abcd", 0)]
+    [DataRow("vrykt", "rkge", 2)]
+    [DataRow("coaching", "coding", 4)]
+    public void AppendCharacters_GivenSourceAndTargetStrings_ReturnsExpectedCharacterCount(string s, string t,
+        int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.AppendCharacters(s, t);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
