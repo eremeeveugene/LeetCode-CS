0bc32526843e16852b05be22da39e8516b47bdef
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-06-05 08:00:13 +0300

    Longest Palindrome

---
+++ source/LeetCode/Algorithms/LongestPalindrome/LongestPalindromeArray.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.LongestPalindrome;
+
+/// <inheritdoc />
+public class LongestPalindromeArray : ILongestPalindrome
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public int LongestPalindrome(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+        {
+            return 0;
+        }
+
+        var charCount = new int[128];
+
+        foreach (var c in s)
+        {
+            charCount[c]++;
+        }
+
+        var length = 0;
+        var oddCountFound = false;
+
+        foreach (var count in charCount)
+        {
+            if (count % 2 == 0)
+            {
+                length += count;
+            }
+            else
+            {
+                length += count - 1;
+                oddCountFound = true;
+            }
+        }
+
+        if (oddCountFound)
+        {
+            length++;
+        }
+
+        return length;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/LongestPalindrome/LongestPalindromeTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.LongestPalindrome;
+
+namespace LeetCode.Tests.Algorithms.LongestPalindrome;
+
+public abstract class LongestPalindromeTestsBase<T> where T : ILongestPalindrome, new()
+{
+    [TestMethod]
+    [DataRow("", 0)]
+    [DataRow("a", 1)]
+    [DataRow("bb", 2)]
+    [DataRow("ccc", 3)]
+    [DataRow("abccccdd", 7)]
+    [DataRow(
+        "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth",
+        983)]
+    public void LongestPalindrome_WithVariousInputStrings_ReturnsExpectedLengths(string s, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.LongestPalindrome(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
