c6b6e1a4caf2a1c8493164335b64d2653745c623
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-06-10 13:48:13 +0300

    Height Checker

---
+++ source/LeetCode/Algorithms/HeightChecker/HeightCheckerCounting.cs
@@ -0,0 +1,55 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.HeightChecker;
+
+/// <inheritdoc />
+public class HeightCheckerCounting : IHeightChecker
+{
+    /// <summary>
+    ///     Time complexity - O(n + k), where n is the number of elements in the input array and k is the range of the input
+    ///     values
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="heights"></param>
+    /// <returns></returns>
+    public int HeightChecker(int[] heights)
+    {
+        var maxHeight = heights.Max();
+
+        var count = new int[maxHeight + 1];
+
+        foreach (var height in heights)
+        {
+            count[height]++;
+        }
+
+        var result = 0;
+        var currentHeight = 0;
+
+        foreach (var height in heights)
+        {
+            while (count[currentHeight] == 0)
+            {
+                currentHeight++;
+            }
+
+            if (currentHeight != height)
+            {
+                result++;
+            }
+
+            count[currentHeight]--;
+        }
+
+        return result;
+    }
+}
---
+++ source/LeetCode/Algorithms/HeightChecker/HeightCheckerSorting.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.HeightChecker;
+
+/// <inheritdoc />
+public class HeightCheckerSorting : IHeightChecker
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="heights"></param>
+    /// <returns></returns>
+    public int HeightChecker(int[] heights)
+    {
+        var sortedHeights = heights.Order().ToArray();
+
+        return heights.Where((t, i) => sortedHeights[i] != t).Count();
+    }
+}
