2693bca6b5e19429fa82316a293b51683f3bb6ee
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-06-18 17:41:53 +0300

    Excel Sheet Column Title

---
+++ source/LeetCode/Algorithms/ExcelSheetColumnTitle/ExcelSheetColumnTitleIterative.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace LeetCode.Algorithms.ExcelSheetColumnTitle;
+
+/// <inheritdoc />
+public class ExcelSheetColumnTitleIterative : IExcelSheetColumnTitle
+{
+    /// <summary>
+    ///     Time complexity - O((log n) ^ 2)
+    ///     Time complexity - O(log n)
+    /// </summary>
+    /// <param name="columnNumber"></param>
+    /// <returns></returns>
+    public string ConvertToTitle(int columnNumber)
+    {
+        var stringBuilder = new StringBuilder();
+
+        while (columnNumber > 0)
+        {
+            stringBuilder.Insert(0, (char)(((columnNumber - 1) % 26) + 'A'));
+
+            columnNumber = (columnNumber - 1) / 26;
+        }
+
+        return stringBuilder.ToString();
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ExcelSheetColumnTitle/ExcelSheetColumnTitleIterativeTests.cs
@@ -0,0 +1,6 @@
+﻿using LeetCode.Algorithms.ExcelSheetColumnTitle;
+
+namespace LeetCode.Tests.Algorithms.ExcelSheetColumnTitle;
+
+[TestClass]
+public class ExcelSheetColumnTitleIterativeTests : ExcelSheetColumnTitleTestsBase<ExcelSheetColumnTitleIterative>;
