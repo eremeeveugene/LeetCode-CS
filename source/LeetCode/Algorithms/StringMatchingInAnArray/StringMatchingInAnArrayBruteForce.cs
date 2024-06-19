8e84cada898ece0817a5406e244fa7ac8b19a5ae
eugene <eremeeveugene@gmail.com>
2024-06-19 18:36:28 +0300

    String Matching in an Array (#565)

---
+++ source/LeetCode/Algorithms/StringMatchingInAnArray/StringMatchingInAnArrayBruteForce.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode.Algorithms.StringMatchingInAnArray;
+
+/// <inheritdoc />
+public class StringMatchingInAnArrayBruteForce : IStringMatchingInAnArray
+{
+    /// <summary>
+    ///     Time complexity - O(n^2)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="words"></param>
+    /// <returns></returns>
+    public IList<string> StringMatching(string[] words)
+    {
+        return words.Where((t1, i) => words.Where((t, j) => i != j && t.Contains(t1)).Any()).ToArray();
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/StringMatchingInAnArray/StringMatchingInAnArrayBruteForceTests.cs
@@ -0,0 +1,7 @@
+﻿using LeetCode.Algorithms.StringMatchingInAnArray;
+
+namespace LeetCode.Tests.Algorithms.StringMatchingInAnArray;
+
+[TestClass]
+public class StringMatchingInAnArrayBruteForceTests :
+    StringMatchingInAnArrayTestsBase<StringMatchingInAnArrayBruteForce>;
