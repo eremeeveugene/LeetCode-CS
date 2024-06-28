d764897c9c8aa3abe6f8c5aa794f26bda9898204
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-06-28 18:23:40 +0300

    Valid Perfect Square

---
+++ source/LeetCode/Algorithms/ValidPerfectSquare/ValidPerfectSquareBinarySearch.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Algorithms.ValidPerfectSquare;
+
+/// <inheritdoc />
+public class ValidPerfectSquareBinarySearch : IValidPerfectSquare
+{
+    /// <summary>
+    ///     Time complexity - O(log n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+    public bool IsPerfectSquare(int num)
+    {
+        long left = 1;
+        long right = num;
+
+        while (left <= right)
+        {
+            var mid = left + ((right - left) / 2);
+
+            var sqrt = mid * mid;
+
+            if (sqrt == num)
+            {
+                return true;
+            }
+
+            if (sqrt < num)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ValidPerfectSquare/ValidPerfectSquareTestsBase.cs
@@ -0,0 +1,22 @@
+﻿using LeetCode.Algorithms.ValidPerfectSquare;
+
+namespace LeetCode.Tests.Algorithms.ValidPerfectSquare;
+
+public abstract class ValidPerfectSquareTestsBase<T> where T : IValidPerfectSquare, new()
+{
+    [TestMethod]
+    [DataRow(16, true)]
+    [DataRow(14, false)]
+    [DataRow(2147483647, false)]
+    public void IsPerfectSquare_GivenNumber_ReturnsWhetherNumberIsPerfectSquare(int num, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsPerfectSquare(num);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
