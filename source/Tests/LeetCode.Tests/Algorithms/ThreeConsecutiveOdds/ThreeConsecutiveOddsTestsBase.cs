e4097be02a42f856531df731a1eab79d7fbc9b21
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-07-01 12:03:36 +0300

    Three Consecutive Odds

---
+++ source/Tests/LeetCode.Tests/Algorithms/ThreeConsecutiveOdds/ThreeConsecutiveOddsTestsBase.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Algorithms.ThreeConsecutiveOdds;
+
+namespace LeetCode.Tests.Algorithms.ThreeConsecutiveOdds;
+
+public abstract class ThreeConsecutiveOddsTestsBase<T> where T : IThreeConsecutiveOdds, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 2, 6, 4, 1 }, false)]
+    [DataRow(new[] { 1, 2, 34, 3, 4, 5, 7, 23, 12 }, true)]
+    public void ThreeConsecutiveOdds_WithVariousArrays_ReturnsExpectedResult(int[] arr, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.ThreeConsecutiveOdds(arr);
+
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
