36119eb0c4363ec344571f0c8b15e8270581b467
eugene <eremeeveugene@gmail.com>
2024-07-03 19:49:41 +0300

    Minimum Difference Between Largest and Smallest Value in Three Moves (#593)

---
+++ source/LeetCode/Algorithms/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMovesSorting.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves;
+
+/// <inheritdoc />
+public class MinimumDifferenceBetweenLargestAndSmallestValueInThreeMovesSorting :
+    IMinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(log n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MinDifference(int[] nums)
+    {
+        if (nums.Length <= 4)
+        {
+            return 0;
+        }
+
+        Array.Sort(nums);
+
+        var minDifference = int.MaxValue;
+
+        for (var left = 0; left < 4; left++)
+        {
+            var right = nums.Length - 4 + left;
+
+            minDifference = Math.Min(minDifference, nums[right] - nums[left]);
+        }
+
+        return minDifference;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves/MinimumDifferenceBetweenLargestAndSmallestValueInThreeMovesTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves;
+
+namespace LeetCode.Tests.Algorithms.MinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves;
+
+public abstract class MinimumDifferenceBetweenLargestAndSmallestValueInThreeMovesTestsBase<T>
+    where T : IMinimumDifferenceBetweenLargestAndSmallestValueInThreeMoves, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 5, 3, 2, 4 }, 0)]
+    [DataRow(new[] { 1, 5, 0, 10, 14 }, 1)]
+    [DataRow(new[] { 3, 100, 20 }, 0)]
+    [DataRow(new[] { 6, 6, 0, 1, 1, 4, 6 }, 2)]
+    public void MinDifference_WithGivenNumsArray_ReturnsExpectedResult(int[] nums, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinDifference(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
