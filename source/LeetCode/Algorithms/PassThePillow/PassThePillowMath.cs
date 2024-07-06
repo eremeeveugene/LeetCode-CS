8903db2b8fb7369b780a6b4f82981d3efb2194b9
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-07-06 12:26:19 +0300

    Pass the Pillow

---
+++ source/LeetCode/Algorithms/PassThePillow/PassThePillowMath.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.PassThePillow;
+
+/// <inheritdoc />
+public class PassThePillowMath : IPassThePillow
+{
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="time"></param>
+    /// <returns></returns>
+    public int PassThePillow(int n, int time)
+    {
+        var fullRounds = time / (n - 1);
+
+        var extraTime = time % (n - 1);
+
+        if (fullRounds % 2 == 0)
+        {
+            return extraTime + 1;
+        }
+
+        return n - extraTime;
+    }
+}
---
+++ source/LeetCode/Algorithms/PassThePillow/PassThePillowSimulation.cs
@@ -0,0 +1,57 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.PassThePillow;
+
+/// <inheritdoc />
+public class PassThePillowSimulation : IPassThePillow
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="time"></param>
+    /// <returns></returns>
+    public int PassThePillow(int n, int time)
+    {
+        var currentPerson = 1;
+
+        var isReversed = false;
+
+        var currentTime = 0;
+
+        while (currentTime < time)
+        {
+            if (isReversed)
+            {
+                currentPerson--;
+            }
+            else
+            {
+                currentPerson++;
+            }
+
+            if (currentPerson == n)
+            {
+                isReversed = true;
+            }
+            else if (currentPerson == 1)
+            {
+                isReversed = false;
+            }
+
+            currentTime++;
+        }
+
+        return currentPerson;
+    }
+}
