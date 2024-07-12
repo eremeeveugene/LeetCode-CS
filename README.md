afc959aef0249ce8ce60505683f057e6c1052343
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-07-12 20:02:26 +0300

    Valid Anagram

--- README.md
+++ README.md
@@ -118,6 +118,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [232. Implement Queue using Stacks](https://leetcode.com/problems/implement-queue-using-stacks/description/)
 - [234. Palindrome Linked List](https://leetcode.com/problems/palindrome-linked-list/description/)
 - [237. Delete Node in a Linked List](https://leetcode.com/problems/delete-node-in-a-linked-list/description/)
+- [242. Valid Anagram](https://leetcode.com/problems/valid-anagram/description/)
 - [257. Binary Tree Paths](https://leetcode.com/problems/binary-tree-paths/description/)
 - [258. Add Digits](https://leetcode.com/problems/add-digits/description/)
 - [260. Single Number III](https://leetcode.com/problems/single-number-iii/description/)
---
+++ source/Tests/LeetCode.Tests/Algorithms/ValidAnagram/ValidAnagramTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ValidAnagram;
+
+namespace LeetCode.Tests.Algorithms.ValidAnagram;
+
+public abstract class ValidAnagramTestsBase<T> where T : IValidAnagram, new()
+{
+    [TestMethod]
+    [DataRow("ab", "a", false)]
+    [DataRow("rat", "car", false)]
+    [DataRow("anagram", "nagaram", true)]
+    public void IsAnagram_WithGivenStrings_ReturnsIfStringsAreAnagrams(string s, string t, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsAnagram(s, t);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
