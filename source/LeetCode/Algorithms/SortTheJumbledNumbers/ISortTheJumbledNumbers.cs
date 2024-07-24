29cf80563f0a065ed8adb693e2032ac5a5008988
eugene <eremeeveugene@gmail.com>
2024-07-24 13:36:13 +0300

    Sort the Jumbled Numbers (#632)

---
+++ source/LeetCode/Algorithms/SortTheJumbledNumbers/ISortTheJumbledNumbers.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SortTheJumbledNumbers;
+
+/// <summary>
+///     https://leetcode.com/problems/sort-the-jumbled-numbers/
+/// </summary>
+public interface ISortTheJumbledNumbers
+{
+    int[] SortJumbled(int[] mapping, int[] nums);
+}
