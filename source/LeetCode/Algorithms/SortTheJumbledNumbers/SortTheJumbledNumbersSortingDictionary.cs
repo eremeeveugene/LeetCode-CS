6e9ec5e29b795d723591af073868dd0d32d7dc6a
eugene <eremeeveugene@gmail.com>
2024-07-24 13:31:50 +0300

    Sort the Jumbled Numbers

---
+++ source/LeetCode/Algorithms/SortTheJumbledNumbers/SortTheJumbledNumbersSortingDictionary.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SortTheJumbledNumbers;
+
+/// <inheritdoc />
+public class SortTheJumbledNumbersSortingDictionary : ISortTheJumbledNumbers
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="mapping"></param>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int[] SortJumbled(int[] mapping, int[] nums)
+    {
+        var mappedNums = new (int mapped, int original)[nums.Length];
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var originalNum = nums[i];
+            var mappedNum = 0;
+            var multiplier = 1;
+
+            do
+            {
+                mappedNum += mapping[originalNum % 10] * multiplier;
+                originalNum /= 10;
+                multiplier *= 10;
+            } while (originalNum > 0);
+
+            mappedNums[i] = (mappedNum, nums[i]);
+        }
+
+        Array.Sort(mappedNums, (a, b) => a.mapped.CompareTo(b.mapped));
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            nums[i] = mappedNums[i].original;
+        }
+
+        return nums;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/SortTheJumbledNumbers/SortTheJumbledNumbersTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SortTheJumbledNumbers;
+
+namespace LeetCode.Tests.Algorithms.SortTheJumbledNumbers;
+
+public abstract class SortTheJumbledNumbersTestsBase<T> where T : ISortTheJumbledNumbers, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new[] { 789, 456, 123 }, new[] { 123, 456, 789 })]
+    [DataRow(new[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }, new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+        new[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 })]
+    [DataRow(new[] { 8, 9, 4, 0, 2, 1, 3, 5, 7, 6 }, new[] { 991, 338, 38 }, new[] { 338, 38, 991 })]
+    public void SortJumbled_WithGivenMappingAndNumbers_ReturnsCorrectOrder(int[] mapping, int[] nums,
+        int[] expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.SortJumbled(mapping, nums);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
