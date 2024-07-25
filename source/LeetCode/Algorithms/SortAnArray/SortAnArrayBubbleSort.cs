c3da07df55d1921e157408e7aabb38d80c62a1f1
eugene <eremeeveugene@gmail.com>
2024-07-25 16:19:59 +0300

    Sort an Array (#634)

---
+++ source/LeetCode/Algorithms/SortAnArray/SortAnArrayBubbleSort.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SortAnArray;
+
+/// <inheritdoc />
+public class SortAnArrayBubbleSort : ISortAnArray
+{
+    /// <summary>
+    ///     Time complexity - O(n^2)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int[] SortArray(int[] nums)
+    {
+        for (var i = 0; i < nums.Length; i++)
+        {
+            for (var j = 0; j < nums.Length - i - 1; j++)
+            {
+                if (nums[j] > nums[j + 1])
+                {
+                    (nums[j], nums[j + 1]) = (nums[j + 1], nums[j]);
+                }
+            }
+        }
+
+        return nums;
+    }
+}
---
+++ source/LeetCode/Algorithms/SortAnArray/SortAnArrayQuickSort.cs
@@ -0,0 +1,61 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SortAnArray;
+
+/// <inheritdoc />
+public class SortAnArrayQuickSort : ISortAnArray
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(n log n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int[] SortArray(int[] nums)
+    {
+        SortArray(nums, 0, nums.Length - 1);
+
+        return nums;
+    }
+
+    private static void SortArray(IList<int> nums, int left, int right)
+    {
+        if (left >= right)
+        {
+            return;
+        }
+
+        var partitionIndex = Partition(nums, left, right);
+
+        SortArray(nums, left, partitionIndex - 1);
+        SortArray(nums, partitionIndex + 1, right);
+    }
+
+    private static int Partition(IList<int> array, int low, int high)
+    {
+        for (var j = low; j < high; j++)
+        {
+            if (array[j] > array[high])
+            {
+                continue;
+            }
+
+            (array[low], array[j]) = (array[j], array[low]);
+
+            low++;
+        }
+
+        (array[low], array[high]) = (array[high], array[low]);
+
+        return low;
+    }
+}
