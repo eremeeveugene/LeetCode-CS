fca01f62de77905c38b133224e36f64e4840ee07
eugene <eremeeveugene@gmail.com>
2024-07-26 18:22:12 +0300

    Arranging Coins (#636)

---
+++ source/LeetCode/Algorithms/ArrangingCoins/ArrangingCoinsMath.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ArrangingCoins;
+
+/// <inheritdoc />
+public class ArrangingCoinsMath : IArrangingCoins
+{
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public int ArrangeCoins(int n)
+    {
+        return (int)((-1 + Math.Sqrt(1 + (8 * (long)n))) / 2);
+    }
+}
