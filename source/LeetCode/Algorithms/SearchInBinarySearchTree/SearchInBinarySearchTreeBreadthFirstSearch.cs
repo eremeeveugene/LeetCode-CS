fff87568843d303adfd2dbdf5022c9296bc81c77
eugene <eremeeveugene@gmail.com>
2024-07-28 14:49:22 +0300

    Search in a Binary Search Tree (#641)

---
+++ source/LeetCode/Algorithms/SearchInBinarySearchTree/SearchInBinarySearchTreeBreadthFirstSearch.cs
@@ -0,0 +1,64 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.SearchInBinarySearchTree;
+
+/// <inheritdoc />
+public class SearchInBinarySearchTreeBreadthFirstSearch : ISearchInBinarySearchTree
+{
+    /// <summary>
+    ///     Time complexity - O(n) for a skewed tree, O(log n) for a balanced tree
+    ///     Space complexity - O(n) for a skewed tree, O(log n) for a balanced tree
+    /// </summary>
+    /// <param name="root"></param>
+    /// <param name="val"></param>
+    /// <returns></returns>
+    public TreeNode? SearchBST(TreeNode? root, int val)
+    {
+        if (root == null)
+        {
+            return null;
+        }
+
+        var queue = new Queue<TreeNode>();
+
+        queue.Enqueue(root);
+
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+
+            if (node.val == val)
+            {
+                return node;
+            }
+
+            if (node.val < val)
+            {
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+            }
+            else
+            {
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+            }
+        }
+
+        return null;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/SearchInBinarySearchTree/SearchInBinarySearchTreeTestsBase.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SearchInBinarySearchTree;
+using LeetCode.Core.Helpers;
+using LeetCode.Core.Models;
+using LeetCode.Tests.Base.Extensions;
+
+namespace LeetCode.Tests.Algorithms.SearchInBinarySearchTree;
+
+public abstract class SearchInBinarySearchTreeTestsBase<T> where T : ISearchInBinarySearchTree, new()
+{
+    [TestMethod]
+    [DataRow("[4,2,7,1,3]", 2, "[2,1,3]")]
+    [DataRow("[4,2,7,1,3]", 5, "[]")]
+    public void SearchBST_WithGivenRootAndValue_ReturnsSubtree(string rootJsonArray, int val,
+        string expectedResultJsonArray)
+    {
+        // Arrange
+        var rootArray = JsonHelper<int?>.JsonArrayToList(rootJsonArray);
+        var root = TreeNode.BuildTree(rootArray);
+
+        var expectedResultArray = JsonHelper<int?>.JsonArrayToList(expectedResultJsonArray);
+        var expectedResult = TreeNode.BuildTree(expectedResultArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.SearchBST(root, val);
+
+        // Assert
+        TreeNodeAssert.AreEqual(expectedResult, actualResult);
+    }
+}
