2c594b7026a9a8aa699edd936e26ef9e653cb650
eugene <eremeeveugene@gmail.com>
2024-07-29 21:53:57 +0300

    Find Minimum Operations to Make All Elements Divisible by Three

---
+++ source/LeetCode/Algorithms/FindMinimumOperationsToMakeAllElementsDivisibleByThree/FindMinimumOperationsToMakeAllElementsDivisibleByThreeIterative.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindMinimumOperationsToMakeAllElementsDivisibleByThree;
+
+/// <inheritdoc />
+public class FindMinimumOperationsToMakeAllElementsDivisibleByThreeIterative :
+    IFindMinimumOperationsToMakeAllElementsDivisibleByThree
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MinimumOperations(int[] nums)
+    {
+        return nums.Select(num => num % 3).Count(remainder => remainder != 0);
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FindMinimumOperationsToMakeAllElementsDivisibleByThree/FindMinimumOperationsToMakeAllElementsDivisibleByThreeTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindMinimumOperationsToMakeAllElementsDivisibleByThree;
+
+namespace LeetCode.Tests.Algorithms.FindMinimumOperationsToMakeAllElementsDivisibleByThree;
+
+public abstract class FindMinimumOperationsToMakeAllElementsDivisibleByThreeTestsBase<T>
+    where T : IFindMinimumOperationsToMakeAllElementsDivisibleByThree, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1, 2, 3, 4 }, 3)]
+    [DataRow(new[] { 3, 6, 9 }, 0)]
+    public void MinimumOperations_WithGivenNums_ReturnsMinimumOperationsCount(int[] nums, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinimumOperations(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
