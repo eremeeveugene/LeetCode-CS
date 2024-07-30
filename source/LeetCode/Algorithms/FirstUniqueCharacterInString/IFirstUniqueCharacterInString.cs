20fbec1acf4ccab497343df5886789edb2e0ef3b
eugene <eremeeveugene@gmail.com>
2024-07-30 17:12:55 +0300

    First Unique Character in a String

---
+++ source/LeetCode/Algorithms/FirstUniqueCharacterInString/IFirstUniqueCharacterInString.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FirstUniqueCharacterInString;
+
+/// <summary>
+///     https://leetcode.com/problems/first-unique-character-in-a-string/description/
+/// </summary>
+public interface IFirstUniqueCharacterInString
+{
+    int FirstUniqChar(string s);
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FirstUniqueCharacterInString/FirstUniqueCharacterInStringTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FirstUniqueCharacterInString;
+
+namespace LeetCode.Tests.Algorithms.FirstUniqueCharacterInString;
+
+public abstract class FirstUniqueCharacterInStringTestsBase<T> where T : IFirstUniqueCharacterInString, new()
+{
+    [TestMethod]
+    [DataRow("leetcode", 0)]
+    [DataRow("loveleetcode", 2)]
+    [DataRow("aabb", -1)]
+    public void FirstUniqChar_GivenString_ReturnsIndexOfFirstUniqueChar(string s, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FirstUniqChar(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
