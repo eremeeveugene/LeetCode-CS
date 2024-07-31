2c7694dfb1b97a06714da24ac3f4523daa8be1b6
eugene <eremeeveugene@gmail.com>
2024-07-31 19:53:44 +0300

    Minimum Absolute Difference in BST (#649)

---
+++ source/LeetCode/Algorithms/MinimumAbsoluteDifferenceInBST/MinimumAbsoluteDifferenceInBSTDepthFirstSearch.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.MinimumAbsoluteDifferenceInBST;
+
+/// <inheritdoc />
+public class MinimumAbsoluteDifferenceInBSTDepthFirstSearch : IMinimumAbsoluteDifferenceInBST
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Time complexity - O(n) for a skewed tree, O(log n) for a balanced tree
+    /// </summary>
+    /// <param name="root"></param>
+    /// <returns></returns>
+    public int GetMinimumDifference(TreeNode root)
+    {
+        TreeNode? prev = null;
+
+        return GetMinimumDifference(root, ref prev, int.MaxValue);
+    }
+
+    private static int GetMinimumDifference(TreeNode node, ref TreeNode? prev, int minDiff)
+    {
+        if (node.left != null)
+        {
+            minDiff = GetMinimumDifference(node.left, ref prev, minDiff);
+        }
+
+        if (prev != null)
+        {
+            minDiff = Math.Min(minDiff, node.val - prev.val);
+        }
+
+        prev = node;
+
+        if (node.right != null)
+        {
+            minDiff = GetMinimumDifference(node.right, ref prev, minDiff);
+        }
+
+        return minDiff;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumAbsoluteDifferenceInBST/MinimumAbsoluteDifferenceInBSTTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumAbsoluteDifferenceInBST;
+using LeetCode.Core.Helpers;
+using LeetCode.Core.Models;
+
+namespace LeetCode.Tests.Algorithms.MinimumAbsoluteDifferenceInBST;
+
+public abstract class MinimumAbsoluteDifferenceInBSTTestsBase<T> where T : IMinimumAbsoluteDifferenceInBST, new()
+{
+    [TestMethod]
+    [DataRow("[4,2,6,1,3]", 1)]
+    [DataRow("[1,0,48,null,null,12,49]", 1)]
+    [DataRow("[90,69,null,49,89,null,52]", 1)]
+    public void GetMinimumDifference_GivenBST_ReturnsMinimumAbsoluteDifference(string rootJsonArray, int expectedResult)
+    {
+        // Arrange
+        var rootArray = JsonHelper<int?>.JsonArrayToList(rootJsonArray);
+        var root = TreeNode.BuildTree(rootArray) ?? throw new InvalidOperationException();
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.GetMinimumDifference(root);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
