d58511e1a0d7fca72f858ff67b40112ce985d208
eugene <eremeeveugene@gmail.com>
2024-08-02 16:15:49 +0300

    Minimum Swaps to Group All 1's Together II

---
+++ source/LeetCode/Algorithms/MinimumSwapsToGroupAll1Together2/IMinimumSwapsToGroupAll1Together2.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumSwapsToGroupAll1Together2;
+
+/// <summary>
+///     https://leetcode.com/problems/minimum-swaps-to-group-all-1s-together-ii/description/
+/// </summary>
+public interface IMinimumSwapsToGroupAll1Together2
+{
+    int MinSwaps(int[] nums);
+}
---
+++ source/LeetCode/Algorithms/MinimumSwapsToGroupAll1Together2/MinimumSwapsToGroupAll1Together2SlidingWindow.cs
@@ -0,0 +1,63 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumSwapsToGroupAll1Together2;
+
+/// <inheritdoc />
+public class MinimumSwapsToGroupAll1Together2SlidingWindow : IMinimumSwapsToGroupAll1Together2
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MinSwaps(int[] nums)
+    {
+        var totalOnes = nums.Count(num => num == 1);
+
+        if (totalOnes == 0)
+        {
+            return 0;
+        }
+
+        var windowedArray = nums.Concat(nums.Take(totalOnes)).ToArray();
+
+        var currentZeroCount = 0;
+
+        for (var i = 0; i < totalOnes; i++)
+        {
+            if (windowedArray[i] == 0)
+            {
+                currentZeroCount++;
+            }
+        }
+
+        var minSwaps = currentZeroCount;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (windowedArray[i - 1] == 0)
+            {
+                currentZeroCount--;
+            }
+
+            if (windowedArray[i + totalOnes - 1] == 0)
+            {
+                currentZeroCount++;
+            }
+
+            minSwaps = Math.Min(minSwaps, currentZeroCount);
+        }
+
+        return minSwaps;
+    }
+}
