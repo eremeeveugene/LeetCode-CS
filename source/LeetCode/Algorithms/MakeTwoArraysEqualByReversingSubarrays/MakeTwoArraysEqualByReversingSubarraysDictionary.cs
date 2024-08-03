a95d0dbde7bcc0b9fa74eeed9525ed87b28f3abb
eugene <eremeeveugene@gmail.com>
2024-08-03 13:24:52 +0300

    Make Two Arrays Equal by Reversing Subarrays

---
+++ source/LeetCode/Algorithms/MakeTwoArraysEqualByReversingSubarrays/MakeTwoArraysEqualByReversingSubarraysDictionary.cs
@@ -0,0 +1,55 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MakeTwoArraysEqualByReversingSubarrays;
+
+/// <inheritdoc />
+public class MakeTwoArraysEqualByReversingSubarraysDictionary : IMakeTwoArraysEqualByReversingSubarrays
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="target"></param>
+    /// <param name="arr"></param>
+    /// <returns></returns>
+    public bool CanBeEqual(int[] target, int[] arr)
+    {
+        var targetDictionary = new Dictionary<int, int>();
+
+        foreach (var targetKey in target)
+        {
+            if (!targetDictionary.TryAdd(targetKey, 1))
+            {
+                targetDictionary[targetKey]++;
+            }
+        }
+
+        foreach (var arrKey in arr)
+        {
+            if (targetDictionary.TryGetValue(arrKey, out var targetValue))
+            {
+                if (targetValue == 0)
+                {
+                    return false;
+                }
+
+                targetDictionary[arrKey] = --targetValue;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
