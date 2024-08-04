a464decbd24c67f7edece3827b7a1404a22f4287
eugene <eremeeveugene@gmail.com>
2024-08-04 12:15:08 +0300

    Range Sum of Sorted Subarray Sums (#657)

---
+++ source/LeetCode/Algorithms/RangeSumOfSortedSubarraySums/RangeSumOfSortedSubarraySumsBruteForce.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.RangeSumOfSortedSubarraySums;
+
+/// <inheritdoc />
+public class RangeSumOfSortedSubarraySumsBruteForce : IRangeSumOfSortedSubarraySums
+{
+    /// <summary>
+    ///     Time complexity - O(n^2 log n)
+    ///     Space complexity - O(n^2)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="n"></param>
+    /// <param name="left"></param>
+    /// <param name="right"></param>
+    /// <returns></returns>
+    public int RangeSum(int[] nums, int n, int left, int right)
+    {
+        var sumsList = new List<int>();
+
+        for (var i = 0; i < n; i++)
+        {
+            var sum = 0;
+
+            for (var j = i; j < n; j++)
+            {
+                sum += nums[j];
+
+                sumsList.Add(sum);
+            }
+        }
+
+        var sortedSums = sumsList.Order().ToArray();
+
+        var rangeSum = 0;
+
+        for (var i = left - 1; i < right; i++)
+        {
+            rangeSum += sortedSums[i];
+            rangeSum %= 1000000007;
+        }
+
+        return rangeSum;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/RangeSumOfSortedSubarraySums/RangeSumOfSortedSubarraySumsTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.RangeSumOfSortedSubarraySums;
+
+namespace LeetCode.Tests.Algorithms.RangeSumOfSortedSubarraySums;
+
+public abstract class RangeSumOfSortedSubarraySumsTestsBase<T> where T : IRangeSumOfSortedSubarraySums, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 1, 2, 3, 4 }, 4, 1, 5, 13)]
+    [DataRow(new[] { 1, 2, 3, 4 }, 4, 3, 4, 6)]
+    [DataRow(new[] { 1, 2, 3, 4 }, 4, 1, 10, 50)]
+    public void RangeSum_WithValidInputs_ReturnsCorrectSubarraySum(int[] nums, int n, int left, int right,
+        int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.RangeSum(nums, n, left, right);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
