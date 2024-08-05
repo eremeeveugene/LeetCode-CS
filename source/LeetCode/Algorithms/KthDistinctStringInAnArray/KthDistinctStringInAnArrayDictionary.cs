ae9c963fa1c4ba660b6c1f5303c69d88f4346195
eugene <eremeeveugene@gmail.com>
2024-08-05 16:23:26 +0300

    Kth Distinct String in an Array

---
+++ source/LeetCode/Algorithms/KthDistinctStringInAnArray/KthDistinctStringInAnArrayDictionary.cs
@@ -0,0 +1,55 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.KthDistinctStringInAnArray;
+
+/// <inheritdoc />
+public class KthDistinctStringInAnArrayDictionary : IKthDistinctStringInAnArray
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public string KthDistinct(string[] arr, int k)
+    {
+        var countDictionary = new Dictionary<string, int>();
+
+        foreach (var item in arr)
+        {
+            if (!countDictionary.TryAdd(item, 1))
+            {
+                countDictionary[item]++;
+            }
+        }
+
+        var distinctCount = 0;
+
+        foreach (var item in arr)
+        {
+            if (countDictionary[item] != 1)
+            {
+                continue;
+            }
+
+            distinctCount++;
+
+            if (distinctCount == k)
+            {
+                return item;
+            }
+        }
+
+        return string.Empty;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/KthDistinctStringInAnArray/KthDistinctStringInAnArrayTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.KthDistinctStringInAnArray;
+
+namespace LeetCode.Tests.Algorithms.KthDistinctStringInAnArray;
+
+public abstract class KthDistinctStringInAnArrayTestsBase<T> where T : IKthDistinctStringInAnArray, new()
+{
+    [TestMethod]
+    [DataRow(new[] { "d", "b", "c", "b", "c", "a" }, 2, "a")]
+    [DataRow(new[] { "aaa", "aa", "a" }, 1, "aaa")]
+    [DataRow(new[] { "a", "b", "a" }, 3, "")]
+    public void KthDistinct_GivenArrayAndK_ReturnsKthDistinctStringOrEmpty(string[] arr, int k, string expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.KthDistinct(arr, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
