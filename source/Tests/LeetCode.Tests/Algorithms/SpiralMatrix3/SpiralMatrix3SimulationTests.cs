29183b4414edd21aaa6f0e7a30524daf94ea18b8
eugene <eremeeveugene@gmail.com>
2024-08-08 21:41:19 +0300

    Spiral Matrix III (#665)

---
+++ source/Tests/LeetCode.Tests/Algorithms/SpiralMatrix3/SpiralMatrix3SimulationTests.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SpiralMatrix3;
+
+namespace LeetCode.Tests.Algorithms.SpiralMatrix3;
+
+[TestClass]
+public class SpiralMatrix3SimulationTests : SpiralMatrix3TestsBase<SpiralMatrix3Simulation>;
