fd98f0c68a4aad1a5cc485ddd89a29cbe7f9ca0f
eugene <eremeeveugene@gmail.com>
2024-08-10 16:34:00 +0300

    Magic Squares In Grid

---
+++ source/LeetCode/Algorithms/MagicSquaresInGrid/MagicSquaresInGridBruteForce.cs
@@ -0,0 +1,83 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MagicSquaresInGrid;
+
+/// <inheritdoc />
+public class MagicSquaresInGridBruteForce : IMagicSquaresInGrid
+{
+    /// <summary>
+    ///     Time complexity - O(m * n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="grid"></param>
+    /// <returns></returns>
+    public int NumMagicSquaresInside(int[][] grid)
+    {
+        var result = 0;
+
+        for (var i = 0; i < grid.Length - 2; i++)
+        {
+            for (var j = 0; j < grid[i].Length - 2; j++)
+            {
+                if (IsMagicSquare(grid, i, j))
+                {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static bool IsMagicSquare(IReadOnlyList<int[]> grid, int i, int j)
+    {
+        if (grid[i + 1][j + 1] != 5 || grid[i + 1][j + 1] == grid[i][j + 1] || grid[i + 1][j + 1] == grid[i + 1][j])
+        {
+            return false;
+        }
+
+        const int sum = 15;
+
+        for (var k = 0; k < 3; k++)
+        {
+            var rowSum = 0;
+            var columnSum = 0;
+
+            for (var l = 0; l < 3; l++)
+            {
+                if (grid[i + k][j + l] > 9)
+                {
+                    return false;
+                }
+
+                rowSum += grid[i + k][j + l];
+                columnSum += grid[i + l][j + k];
+            }
+
+            if (rowSum != sum || columnSum != sum)
+            {
+                return false;
+            }
+        }
+
+        var leftDiagonalSum = 0;
+        var rightDiagonalSum = 0;
+
+        for (var k = 0; k < 3; k++)
+        {
+            leftDiagonalSum += grid[i + k][j + k];
+            rightDiagonalSum += grid[i + k][2 + j - k];
+        }
+
+        return leftDiagonalSum == sum && rightDiagonalSum == sum;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MagicSquaresInGrid/MagicSquaresInGridTestsBase.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MagicSquaresInGrid;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MagicSquaresInGrid;
+
+public abstract class MagicSquaresInGridTestsBase<T> where T : IMagicSquaresInGrid, new()
+{
+    [TestMethod]
+    [DataRow("[[8]]", 0)]
+    [DataRow("[[1,1,1],[1,1,1],[1,1,1]]", 0)]
+    [DataRow("[[5,5,5],[5,5,5],[5,5,5]]", 0)]
+    [DataRow("[[7,0,5],[2,4,6],[3,8,1]]", 0)]
+    [DataRow("[[4,3,8,4],[9,5,1,9],[2,7,6,2]]", 1)]
+    [DataRow("[[4,3,8,4],[9,5,1,9],[2,7,6,2],[4,3,8,1],[1,6,7,5]]", 1)]
+    [DataRow(
+        "[[9,9,5,1,9,5,5,7,2,5],[9,1,8,3,4,6,7,2,8,9],[4,1,1,5,9,1,5,9,6,4],[5,5,6,7,2,8,3,4,0,6],[1,9,1,8,3,1,4,2,9,4],[2,8,6,4,2,7,3,2,7,6],[9,2,5,0,7,8,2,9,5,1],[2,1,4,4,7,6,2,4,3,8],[1,2,5,3,0,5,10,8,5,2],[6,9,6,8,8,4,3,6,0,9]]",
+        3)]
+    public void NumMagicSquaresInside_GivenGridJsonArray_ReturnsCorrectMagicSquareCount(string gridJsonArray,
+        int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        var grid = JsonHelper<int>.JsonArrayToJaggedArray(gridJsonArray);
+
+        // Act
+        var actualResult = solution.NumMagicSquaresInside(grid);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
