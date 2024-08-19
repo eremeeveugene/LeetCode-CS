59a3310a63267f52926dfb5f44a475bab635c8b8
eugene <eremeeveugene@gmail.com>
2024-08-19 22:03:04 +0300

    Fix CA1861 messages

--- source/Tests/LeetCode.Tests/Algorithms/ArithmeticSubarrays/ArithmeticSubarraysTestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/ArithmeticSubarrays/ArithmeticSubarraysTestsBase.cs
@@ -10,23 +10,29 @@
 // --------------------------------------------------------------------------------
 
 using LeetCode.Algorithms.ArithmeticSubarrays;
+using LeetCode.Core.Helpers;
 
 namespace LeetCode.Tests.Algorithms.ArithmeticSubarrays;
 
 public abstract class ArithmeticSubarraysTestsBase<T> where T : IArithmeticSubarrays, new()
 {
     [TestMethod]
-    [DataRow(new[] { 4, 6, 5, 9, 3, 7 }, new[] { 0, 0, 2 }, new[] { 2, 3, 5 }, new[] { true, false, true })]
-    [DataRow(new[] { -12, -9, -3, -12, -6, 15, 20, -25, -20, -15, -10 }, new[] { 0, 1, 6, 4, 8, 7 },
-        new[] { 4, 4, 9, 7, 9, 10 }, new[] { false, true, false, false, true, true })]
-    [DataRow(new[] { 1, 2, 3 }, new[] { 0, 1 }, new[] { 1, 2 }, new[] { true, true })]
-    [DataRow(new[] { 10, 1, 3, 5 }, new[] { 0, 1 }, new[] { 2, 3 }, new[] { false, true })]
-    [DataRow(new[] { 1, 3, 5, 7, 9 }, new[] { 0, 1, 2 }, new[] { 4, 3, 4 }, new[] { true, true, true })]
-    [DataRow(new[] { 1, 2, 4 }, new[] { 0, 1 }, new[] { 2, 2 }, new[] { false, true })]
-    public void CheckArithmeticSubarrays_WithVariousRanges_VerifiesIfSubarraysAreArithmetic(int[] nums, int[] l,
-        int[] r, bool[] expectedResult)
+    [DataRow("[4, 6, 5, 9, 3, 7]", "[0, 0, 2]", "[2, 3, 5]", "[true, false, true]")]
+    [DataRow("[-12, -9, -3, -12, -6, 15, 20, -25, -20, -15, -10]", "[0, 1, 6, 4, 8, 7]", "[4, 4, 9, 7, 9, 10]",
+        "[false, true, false, false, true, true]")]
+    [DataRow("[1, 2, 3]", "[0, 1]", "[1, 2]", "[true, true]")]
+    [DataRow("[10, 1, 3, 5]", "[0, 1]", "[2, 3]", "[false, true]")]
+    [DataRow("[1, 3, 5, 7, 9]", "[0, 1, 2]", "[4, 3, 4]", "[true, true, true]")]
+    [DataRow("[1, 2, 4]", "[0, 1]", "[2, 2]", "[false, true]")]
+    public void CheckArithmeticSubarrays_WithVariousRanges_VerifiesIfSubarraysAreArithmetic(string numsJsonArray,
+        string lJsonArray, string rJsonArray, string expectedResultJsonArray)
     {
         // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+        var l = JsonHelper<int>.DeserializeToArray(lJsonArray);
+        var r = JsonHelper<int>.DeserializeToArray(rJsonArray);
+        var expectedResult = JsonHelper<bool>.DeserializeToArray(expectedResultJsonArray);
+
         var solution = new T();
 
         // Act
