9c9f4909bcff54b5d44fb12a00bc8b32c3f08a0d
eugene <eremeeveugene@gmail.com>
2024-08-24 18:53:57 +0300

    Number Complement (#699)

---
+++ source/Tests/LeetCode.Tests/Algorithms/NumberComplement/NumberComplementTestsBase.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.NumberComplement;
+
+namespace LeetCode.Tests.Algorithms.NumberComplement;
+
+public abstract class NumberComplementTestsBase<T> where T : INumberComplement, new()
+{
+    [TestMethod]
+    [DataRow(1, 0)]
+    [DataRow(2, 1)]
+    [DataRow(3, 0)]
+    [DataRow(4, 3)]
+    [DataRow(5, 2)]
+    [DataRow(6, 1)]
+    [DataRow(7, 0)]
+    [DataRow(8, 7)]
+    [DataRow(9, 6)]
+    [DataRow(16, 15)]
+    [DataRow(69, 58)]
+    [DataRow(911, 112)]
+    [DataRow(911, 112)]
+    [DataRow(1073741824, 1073741823)]
+    [DataRow(2100003647, 47480000)]
+    [DataRow(2147483647, 0)]
+    public void FindComplement_WithGivenNumber_ReturnsBitwiseComplement(int num, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FindComplement(num);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
