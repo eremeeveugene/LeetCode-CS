688ae72cb0f70c38897541c1d8043f4ac39752b9
eugene <eremeeveugene@gmail.com>
2024-08-24 16:10:23 +0300

    Fraction Addition and Subtraction (#697)

---
+++ source/LeetCode/Algorithms/FractionAdditionAndSubtraction/FractionAdditionAndSubtractionSimulation.cs
@@ -0,0 +1,95 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FractionAdditionAndSubtraction;
+
+/// <inheritdoc />
+public class FractionAdditionAndSubtractionSimulation : IFractionAdditionAndSubtraction
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="expression"></param>
+    /// <returns></returns>
+    public string FractionAddition(string expression)
+    {
+        var numerator = 0;
+        var denominator = 1;
+        var i = 0;
+
+        while (i < expression.Length)
+        {
+            var sign = 1;
+
+            switch (expression[i])
+            {
+                case '-':
+                    sign = -1;
+
+                    i++;
+                    break;
+                case '+':
+                    i++;
+                    break;
+            }
+
+            var currentNumerator = 0;
+
+            while (i < expression.Length && char.IsDigit(expression[i]))
+            {
+                currentNumerator = (currentNumerator * 10) + (int)char.GetNumericValue(expression[i]);
+
+                i++;
+            }
+
+            currentNumerator *= sign;
+
+            i++;
+
+            var currentDenominator = 0;
+
+            while (i < expression.Length && char.IsDigit(expression[i]))
+            {
+                currentDenominator = (currentDenominator * 10) + (int)char.GetNumericValue(expression[i]);
+                i++;
+            }
+
+            numerator = (numerator * currentDenominator) + (currentNumerator * denominator);
+            denominator *= currentDenominator;
+
+            var divisor = GetDivisor(numerator, denominator);
+
+            numerator /= divisor;
+            denominator /= divisor;
+        }
+
+        if (denominator < 0)
+        {
+            numerator = -numerator;
+            denominator = -denominator;
+        }
+
+        return $"{numerator}/{denominator}";
+    }
+
+    private static int GetDivisor(int numerator, int denominator)
+    {
+        while (denominator != 0)
+        {
+            var temp = denominator;
+            denominator = numerator % denominator;
+            numerator = temp;
+        }
+
+        return numerator;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FractionAdditionAndSubtraction/FractionAdditionAndSubtractionTestsBase.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FractionAdditionAndSubtraction;
+
+namespace LeetCode.Tests.Algorithms.FractionAdditionAndSubtraction;
+
+public abstract class FractionAdditionAndSubtractionTestsBase<T> where T : IFractionAdditionAndSubtraction, new()
+{
+    [TestMethod]
+    [DataRow("-1/2+1/2", "0/1")]
+    [DataRow("1/6-1/6+2/3-2/3", "0/1")]
+    [DataRow("1/10-1/10", "0/1")]
+    [DataRow("1/4+3/4", "1/1")]
+    [DataRow("-1/2+1/2+1/3", "1/3")]
+    [DataRow("1/3-1/2", "-1/6")]
+    [DataRow("-1/3+1/4-1/5+1/6-1/7+1/8-1/9+1/10-1/9+1/10", "-79/504")]
+    [DataRow("9/4-5/3+7/2-9/5+10/6-1/7+1/8-2/9+2/10", "9853/2520")]
+    [DataRow("-1/1-1/1-1/1-1/1-1/1-1/1-1/1-1/1-1/1-1/1", "-10/1")]
+    [DataRow("-1/3-1/4-1/5-1/6-1/7-1/8-1/9-1/10-1/10-6/10", "-1073/504")]
+    public void FractionAddition_GivenExpression_ReturnsSimplifiedResult(string expression, string expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FractionAddition(expression);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
