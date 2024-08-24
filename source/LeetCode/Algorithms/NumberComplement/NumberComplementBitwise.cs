632fb08596b7f12033042cabafc1a3597c49ed56
eugene <eremeeveugene@gmail.com>
2024-08-24 18:47:00 +0300

    Number Complement

---
+++ source/LeetCode/Algorithms/NumberComplement/NumberComplementBitwise.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.NumberComplement;
+
+/// <inheritdoc />
+public class NumberComplementBitwise : INumberComplement
+{
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+    public int FindComplement(int num)
+    {
+        var bitLengthLog = Math.Log(num, 2);
+
+        var bitLength = (int)Math.Floor(bitLengthLog);
+
+        var totalBits = bitLength + 1;
+
+        var mask = (1 << totalBits) - 1;
+
+        return num ^ mask;
+    }
+}
---
+++ source/LeetCode/Algorithms/NumberComplement/NumberComplementString.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Text;
+
+namespace LeetCode.Algorithms.NumberComplement;
+
+/// <inheritdoc />
+public class NumberComplementString : INumberComplement
+{
+    /// <summary>
+    ///     Time complexity - O(log n)
+    ///     Space complexity - O(log n)
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+    public int FindComplement(int num)
+    {
+        var numBinaryString = Convert.ToString(num, 2);
+        var stringBuilder = new StringBuilder(numBinaryString.Length);
+
+        foreach (var bit in numBinaryString)
+        {
+            stringBuilder.Append(bit == '0' ? '1' : '0');
+        }
+
+        return Convert.ToInt32(stringBuilder.ToString(), 2);
+    }
+}
