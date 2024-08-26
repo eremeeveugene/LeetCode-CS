577658e32da222d7e64be7ae91c772dce815592c
eugene <eremeeveugene@gmail.com>
2024-08-26 14:14:19 +0300

    Ransom Note (#705)

---
+++ source/LeetCode/Algorithms/RansomNote/RansomNoteCounting.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.RansomNote;
+
+/// <inheritdoc />
+public class RansomNoteCounting : IRansomNote
+{
+    /// <summary>
+    ///     Time complexity - O(m + n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="ransomNote"></param>
+    /// <param name="magazine"></param>
+    /// <returns></returns>
+    public bool CanConstruct(string ransomNote, string magazine)
+    {
+        var magazineChars = new int['z' - 'a'];
+
+        foreach (var magazineChar in magazine)
+        {
+            magazineChars[magazineChar - 'a']++;
+        }
+
+        foreach (var magazineChar in ransomNote.Select(ransomNoteChar => ransomNoteChar - 'a'))
+        {
+            if (magazineChars[magazineChar] > 0)
+            {
+                magazineChars[magazineChar]--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/RansomNote/RansomNoteTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.RansomNote;
+
+namespace LeetCode.Tests.Algorithms.RansomNote;
+
+public abstract class RansomNoteTestsBase<T> where T : IRansomNote, new()
+{
+    [TestMethod]
+    [DataRow("a", "b", false)]
+    [DataRow("aa", "ab", false)]
+    [DataRow("aa", "aab", true)]
+    public void CanConstruct_WithRansomNoteAndMagazine_ReturnsWhetherConstructIsPossible(string ransomNote,
+        string magazine, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CanConstruct(ransomNote, magazine);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
