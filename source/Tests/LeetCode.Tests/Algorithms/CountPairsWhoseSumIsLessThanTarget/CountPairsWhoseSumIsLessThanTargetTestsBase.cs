be3a5b78e666e635dba9310280604d6cce688d18
eugene <eremeeveugene@gmail.com>
2024-08-26 22:26:19 +0300

    Fix CA1861 messages

--- source/Tests/LeetCode.Tests/Algorithms/CountPairsWhoseSumIsLessThanTarget/CountPairsWhoseSumIsLessThanTargetTestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/CountPairsWhoseSumIsLessThanTarget/CountPairsWhoseSumIsLessThanTargetTestsBase.cs
@@ -10,6 +10,7 @@
 // --------------------------------------------------------------------------------
 
 using LeetCode.Algorithms.CountPairsWhoseSumIsLessThanTarget;
+using LeetCode.Core.Helpers;
 
 namespace LeetCode.Tests.Algorithms.CountPairsWhoseSumIsLessThanTarget;
 
@@ -17,11 +18,14 @@ public abstract class CountPairsWhoseSumIsLessThanTargetTestsBase<T>
     where T : ICountPairsWhoseSumIsLessThanTarget, new()
 {
     [TestMethod]
-    [DataRow(new[] { -1, 1, 2, 3, 1 }, 2, 3)]
-    [DataRow(new[] { -6, 2, 5, -2, -7, -1, 3 }, -2, 10)]
-    public void CountPairs_WithNumsAndTarget_ReturnsExpectedPairCount(IList<int> nums, int target, int expectedResult)
+    [DataRow("[-1, 1, 2, 3, 1]", 2, 3)]
+    [DataRow("[-6, 2, 5, -2, -7, -1, 3]", -2, 10)]
+    public void CountPairs_WithNumsAndTarget_ReturnsExpectedPairCount(string numsJsonArray, int target,
+        int expectedResult)
     {
         // Arrange
+        var nums = JsonHelper<int>.DeserializeToList(numsJsonArray);
+
         var solution = new T();
 
         // Act
