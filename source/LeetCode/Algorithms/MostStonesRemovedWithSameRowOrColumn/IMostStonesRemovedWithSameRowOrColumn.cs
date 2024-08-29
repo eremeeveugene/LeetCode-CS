67a0f93ad17803210fc5c458850e3b117f45c71b
eugene <eremeeveugene@gmail.com>
2024-08-29 18:53:32 +0300

    Most Stones Removed with Same Row or Column (#712)

---
+++ source/LeetCode/Algorithms/MostStonesRemovedWithSameRowOrColumn/IMostStonesRemovedWithSameRowOrColumn.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetCode.Algorithms.MostStonesRemovedWithSameRowOrColumn;
+
+/// <summary>
+///     https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/description/
+/// </summary>
+public interface IMostStonesRemovedWithSameRowOrColumn
+{
+    int RemoveStones(int[][] stones);
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MostStonesRemovedWithSameRowOrColumn/MostStonesRemovedWithSameRowOrColumnTestsBase.cs
@@ -0,0 +1,32 @@
+﻿using LeetCode.Algorithms.MostStonesRemovedWithSameRowOrColumn;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MostStonesRemovedWithSameRowOrColumn;
+
+public abstract class MostStonesRemovedWithSameRowOrColumnTestsBase<T>
+    where T : IMostStonesRemovedWithSameRowOrColumn, new()
+{
+    [TestMethod]
+    [DataRow("[[0,0]]", 0)]
+    [DataRow("[[0,0],[2,2],[10000,2]]", 1)]
+    [DataRow("[[0,0],[0,2],[1,1],[2,0],[2,2]]", 3)]
+    [DataRow("[[0,0],[0,1],[1,0],[1,2],[2,1],[2,2]]", 5)]
+    [DataRow("[[0,0],[1,0],[2,0],[1,1],[1,2],[2,2]]", 5)]
+    [DataRow("[[0,0],[0,1],[0,2],[1,4],[1,5],[2,4],[2,6]]", 5)]
+    [DataRow("[[0,0],[0,1],[0,2],[0,3],[1,4],[1,5],[2,4],[2,6]]", 6)]
+    [DataRow("[[0,0],[0,1],[1,0],[1,2],[2,0],[2,3],[3,0],[3,4]]", 7)]
+    [DataRow("[[0,5],[1,5],[2,5],[3,5],[0,4],[0,6],[1,3],[1,7],[2,2],[2,8],[3,1],[3,9]]", 11)]
+    public void RemoveStones_WithGridOfStones_ReturnsMaxRemovableStones(string stonesJsonArray, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        var stones = JsonHelper<int>.JsonArrayToJaggedArray(stonesJsonArray);
+
+        // Act
+        var actualResult = solution.RemoveStones(stones);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
