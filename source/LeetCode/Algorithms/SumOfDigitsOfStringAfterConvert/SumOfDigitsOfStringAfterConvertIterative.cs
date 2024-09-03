ccebffc9bb9b2661fb3b2ea7aa67c322e513177f
eugene <eremeeveugene@gmail.com>
2024-09-03 21:15:03 +0300

    Sum of Digits of String After Convert (#729)

---
+++ source/LeetCode/Algorithms/SumOfDigitsOfStringAfterConvert/SumOfDigitsOfStringAfterConvertIterative.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode.Algorithms.SumOfDigitsOfStringAfterConvert;
+
+/// <inheritdoc />
+public class SumOfDigitsOfStringAfterConvertIterative : ISumOfDigitsOfStringAfterConvert
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int GetLucky(string s, int k)
+    {
+        var sum = 0;
+
+        foreach (var c in s)
+        {
+            var number = c - 'a' + 1;
+
+            while (number > 0)
+            {
+                sum += number % 10;
+
+                number /= 10;
+            }
+        }
+
+        while (k > 1)
+        {
+            var currentSum = 0;
+
+            while (sum > 0)
+            {
+                currentSum += sum % 10;
+
+                sum /= 10;
+            }
+
+            sum = currentSum;
+
+            k--;
+        }
+
+        return sum;
+    }
+}
