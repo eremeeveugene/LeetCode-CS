40a0f0cb2b57f2017daca94d3a2642ba18d88113
eugene <eremeeveugene@gmail.com>
2024-09-05 16:53:25 +0300

    Find Missing Observations (#733)

---
+++ source/LeetCode/Algorithms/FindMissingObservations/FindMissingObservationsMath.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.Algorithms.FindMissingObservations;
+
+/// <inheritdoc />
+public class FindMissingObservationsMath : IFindMissingObservations
+{
+    /// <summary>
+    ///     Time complexity - O(m + n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="rolls"></param>
+    /// <param name="mean"></param>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public int[] MissingRolls(int[] rolls, int mean, int n)
+    {
+        var missingSum = (mean * (rolls.Length + n)) - rolls.Sum();
+
+        if (missingSum < n || missingSum > n * 6)
+        {
+            return [];
+        }
+
+        var result = new int[n];
+
+        var baseValue = missingSum / n;
+        var remainder = missingSum - (baseValue * n);
+
+        for (var i = 0; i < n; i++)
+        {
+            result[i] = baseValue;
+
+            if (remainder <= 0)
+            {
+                continue;
+            }
+
+            result[i]++;
+
+            remainder--;
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FindMissingObservations/FindMissingObservationsTestsBase.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Algorithms.FindMissingObservations;
+
+namespace LeetCode.Tests.Algorithms.FindMissingObservations;
+
+public abstract class FindMissingObservationsTestsBase<T> where T : IFindMissingObservations, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 3, 2, 4, 3 }, 4, 2, new[] { 6, 6 })]
+    [DataRow(new[] { 1, 5, 6 }, 3, 4, new[] { 2, 3, 2, 2 })]
+    [DataRow(new[] { 1, 2, 3, 4 }, 6, 4, new int[] { })]
+    [DataRow(new[] { 6, 3, 4, 3, 5, 3 }, 1, 6, new int[] { })]
+    [DataRow(
+        new[]
+        {
+            4, 2, 2, 5, 4, 5, 4, 5, 3, 3, 6, 1, 2, 4, 2, 1, 6, 5, 4, 2, 3, 4, 2, 3, 3, 5, 4, 1, 4, 4, 5, 3, 6, 1, 5,
+            2, 3, 3, 6, 1, 6, 4, 1, 3
+        }, 2, 53, new int[] { })]
+    public void MissingRolls_WithRollsMeanAndN_ReturnsMissingRolls(int[] rolls, int mean, int n, int[] expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MissingRolls(rolls, mean, n);
+
+        // Assert
+        CollectionAssert.AreEquivalent(expectedResult, actualResult);
+    }
+}
