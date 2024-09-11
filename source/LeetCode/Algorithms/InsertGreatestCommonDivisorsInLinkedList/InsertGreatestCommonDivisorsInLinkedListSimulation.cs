7426cda06f412873524e1921e525aa17717b77b2
eugene <eremeeveugene@gmail.com>
2024-09-11 15:27:21 +0300

    Insert Greatest Common Divisors in Linked List (#745)

---
+++ source/LeetCode/Algorithms/InsertGreatestCommonDivisorsInLinkedList/InsertGreatestCommonDivisorsInLinkedListSimulation.cs
@@ -0,0 +1,56 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.InsertGreatestCommonDivisorsInLinkedList;
+
+/// <inheritdoc />
+public class InsertGreatestCommonDivisorsInLinkedListSimulation : IInsertGreatestCommonDivisorsInLinkedList
+{
+    /// <summary>
+    ///     Time complexity - O(n * log min(a,b))
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="head"></param>
+    /// <returns></returns>
+    public ListNode? InsertGreatestCommonDivisors(ListNode head)
+    {
+        var dummyHead = new ListNode(0, head);
+
+        var previousNode = head;
+        var currentNode = head.next;
+
+        while (currentNode != null)
+        {
+            var greatestCommonDivisor = GetGreatestCommonDivisor(previousNode.val, currentNode.val);
+
+            previousNode.next = new ListNode(greatestCommonDivisor, currentNode);
+
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+
+        return dummyHead.next;
+    }
+
+    private static int GetGreatestCommonDivisor(int a, int b)
+    {
+        while (b != 0)
+        {
+            var temp = a % b;
+            a = b;
+            b = temp;
+        }
+
+        return a;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/InsertGreatestCommonDivisorsInLinkedList/InsertGreatestCommonDivisorsInLinkedListTestsBase.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.InsertGreatestCommonDivisorsInLinkedList;
+using LeetCode.Core.Models;
+using LeetCode.Tests.Base.Extensions;
+
+namespace LeetCode.Tests.Algorithms.InsertGreatestCommonDivisorsInLinkedList;
+
+public abstract class InsertGreatestCommonDivisorsInLinkedListTestsBase<T>
+    where T : IInsertGreatestCommonDivisorsInLinkedList, new()
+{
+    [TestMethod]
+    [DataRow(new[] { 7 }, new[] { 7 })]
+    [DataRow(new[] { 18, 6, 10, 3 }, new[] { 18, 6, 6, 2, 10, 1, 3 })]
+    public void InsertGreatestCommonDivisors_GivenLinkedList_ReturnsListWithGCDInserted(int[] headArray,
+        int[] expectedResultArray)
+    {
+        // Arrange
+        var head = ListNode.ToListNode(headArray) ?? throw new InvalidOperationException();
+        var expectedResult = ListNode.ToListNode(expectedResultArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.InsertGreatestCommonDivisors(head);
+
+        // Assert
+        ListNodeAssert.AreEqual(expectedResult, actualResult);
+    }
+}
