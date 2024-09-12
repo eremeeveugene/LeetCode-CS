dff848d3d640ffde848ebb26e7b94c29d725b3fd
eugene <eremeeveugene@gmail.com>
2024-09-12 12:44:16 +0300

    Count the Number of Consistent Strings (#747)

---
+++ source/LeetCode/Algorithms/CountTheNumberOfConsistentStrings/ICountTheNumberOfConsistentStrings.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountTheNumberOfConsistentStrings;
+
+/// <summary>
+///     https://leetcode.com/problems/count-the-number-of-consistent-strings/
+/// </summary>
+public interface ICountTheNumberOfConsistentStrings
+{
+    int CountConsistentStrings(string allowed, string[] words);
+}
