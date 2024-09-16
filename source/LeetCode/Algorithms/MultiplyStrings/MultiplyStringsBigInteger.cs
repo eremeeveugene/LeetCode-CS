0a8088d24f8852fc082bf569ce804bd0d499d150
eugene <eremeeveugene@gmail.com>
2024-09-16 21:32:42 +0300

    Multiply Strings (#760)

---
+++ source/LeetCode/Algorithms/MultiplyStrings/MultiplyStringsBigInteger.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Numerics;
+
+namespace LeetCode.Algorithms.MultiplyStrings;
+
+/// <inheritdoc />
+public class MultiplyStringsBigInteger : IMultiplyStrings
+{
+    /// <summary>
+    ///     Time complexity - O(n * m)
+    ///     Space complexity - O(n + m)
+    /// </summary>
+    /// <param name="num1"></param>
+    /// <param name="num2"></param>
+    /// <returns></returns>
+    public string Multiply(string num1, string num2)
+    {
+        var n1 = BigInteger.Parse(num1);
+        var n2 = BigInteger.Parse(num2);
+
+        var result = n1 * n2;
+
+        return result.ToString();
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MultiplyStrings/MultiplyStringsTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MultiplyStrings;
+
+namespace LeetCode.Tests.Algorithms.MultiplyStrings;
+
+public abstract class MultiplyStringsTestsBase<T> where T : IMultiplyStrings, new()
+{
+    [TestMethod]
+    [DataRow("0", "0", "0")]
+    [DataRow("2", "3", "6")]
+    [DataRow("123", "456", "56088")]
+    [DataRow("999", "999", "998001")]
+    [DataRow("999", "999999", "998999001")]
+    public void Multiply_WithTwoStringNumbers_ReturnsProduct(string num1, string num2, string expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.Multiply(num1, num2);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
