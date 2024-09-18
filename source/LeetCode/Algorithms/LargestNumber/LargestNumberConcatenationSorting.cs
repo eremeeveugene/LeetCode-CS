d5a3c2d88ed770608c0f38d62757007c3b89a48d
eugene <eremeeveugene@gmail.com>
2024-09-18 15:09:44 +0300

    Largest Number (#767)

---
+++ source/LeetCode/Algorithms/LargestNumber/LargestNumberConcatenationSorting.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.LargestNumber;
+
+/// <inheritdoc />
+public class LargestNumberConcatenationSorting : ILargestNumber
+{
+    /// <summary>
+    ///     Time complexity - O(n log n * k), where n is the number of numbers and k is the number of digits in the largest
+    ///     number
+    ///     Space complexity - O(n * k), where n is the number of numbers and k is the number of digits in the largest number
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public string LargestNumber(int[] nums)
+    {
+        var numsStrings = nums.Select(n => n.ToString()).ToArray();
+
+        Array.Sort(numsStrings, (a, b) => string.Compare(b + a, a + b, StringComparison.Ordinal));
+
+        return numsStrings[0] == "0" ? "0" : string.Join("", numsStrings);
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/LargestNumber/LargestNumberTestsBase.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.LargestNumber;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.LargestNumber;
+
+public abstract class LargestNumberTestsBase<T> where T : ILargestNumber, new()
+{
+    [TestMethod]
+    [DataRow("[0,0,0]", "0")]
+    [DataRow("[10,2]", "210")]
+    [DataRow("[3,30,34,5,9]", "9534330")]
+    [DataRow("[34323,3432]", "343234323")]
+    [DataRow("[121,12]", "12121")]
+    [DataRow("[121,12,120]", "12121120")]
+    [DataRow("[1,11,111,1112]", "1112111111")]
+    [DataRow("[999999,999999998,999999997]", "999999999999998999999997")]
+    public void LargestNumber_WithArrayOfIntegers_ReturnsMaxConcatenatedNumber(string numsJsonArray,
+        string expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.LargestNumber(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
