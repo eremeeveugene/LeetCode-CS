57abd37f29d087f2ecd48fc564a4a23f9e16678f
eugene <eremeeveugene@gmail.com>
2024-09-22 12:48:52 +0300

    K-th Smallest in Lexicographical Order

---
+++ source/LeetCode/Algorithms/KthSmallestInLexicographicalOrder/KthSmallestInLexicographicalOrderPrefixTree.cs
@@ -0,0 +1,64 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.KthSmallestInLexicographicalOrder;
+
+/// <inheritdoc />
+public class KthSmallestInLexicographicalOrderPrefixTree : IKthSmallestInLexicographicalOrder
+{
+    /// <summary>
+    ///     Time complexity - O(k * log n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int FindKthNumber(int n, int k)
+    {
+        var result = 1;
+
+        k--;
+
+        while (k > 0)
+        {
+            var nodesCount = GetNodesCount(result, n);
+
+            if (nodesCount <= k)
+            {
+                k -= nodesCount;
+                result++;
+            }
+            else
+            {
+                result *= 10;
+                k--;
+            }
+        }
+
+        return result;
+    }
+
+    private static int GetNodesCount(int prefix, int n)
+    {
+        var count = 0;
+        long current = prefix;
+        long next = prefix + 1;
+
+        while (current <= n)
+        {
+            count += (int)(Math.Min(n + 1, next) - current);
+            current *= 10;
+            next *= 10;
+        }
+
+        return count;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/KthSmallestInLexicographicalOrder/KthSmallestInLexicographicalOrderTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.KthSmallestInLexicographicalOrder;
+
+namespace LeetCode.Tests.Algorithms.KthSmallestInLexicographicalOrder;
+
+public abstract class KthSmallestInLexicographicalOrderTestsBase<T> where T : IKthSmallestInLexicographicalOrder, new()
+{
+    [TestMethod]
+    [DataRow(1, 1, 1)]
+    [DataRow(13, 2, 10)]
+    [DataRow(272938424, 142648193, 22838337)]
+    [DataRow(1000000000, 374924728, 43743225)]
+    [DataRow(1000000000, 500000000, 549999998)]
+    [DataRow(1000000000, 999945398, 999950855)]
+    [DataRow(1000000000, 1000000000, 999999999)]
+    [DataRow(1000000000, 1, 1)]
+    public void FindKthNumber_WithNAndK_ReturnsKthLexicographicalNumber(int n, int k, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FindKthNumber(n, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
