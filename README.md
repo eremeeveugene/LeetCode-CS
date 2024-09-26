4d6f92a77591c5bb2686e68aaea3d49d57d88f31
eugene <eremeeveugene@gmail.com>
2024-09-26 19:42:38 +0300

    My Calendar I

--- README.md
+++ README.md
@@ -208,6 +208,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [719. Find K-th Smallest Pair Distance](https://leetcode.com/problems/find-k-th-smallest-pair-distance/description/)
 - [724. Find Pivot Index](https://leetcode.com/problems/find-pivot-index/description/)
 - [725. Split Linked List in Parts](https://leetcode.com/problems/split-linked-list-in-parts/description/)
+- [729. My Calendar I](https://leetcode.com/problems/my-calendar-i/description/)
 - [733. Flood Fill](https://leetcode.com/problems/flood-fill/description/)
 - [746. Min Cost Climbing Stairs](https://leetcode.com/problems/min-cost-climbing-stairs/description/)
 - [752. Open the Lock](https://leetcode.com/problems/open-the-lock/description/)
---
+++ source/LeetCode/Algorithms/MyCalendar1/MyCalendar1BinarySearch.cs
@@ -0,0 +1,62 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MyCalendar1;
+
+/// <inheritdoc />
+public class MyCalendar1BinarySearch : IMyCalendar1
+{
+    private readonly List<Item> _items = [];
+
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="start"></param>
+    /// <param name="end"></param>
+    /// <returns></returns>
+    public bool Book(int start, int end)
+    {
+        var item = new Item(start, end);
+
+        var index = _items.BinarySearch(item);
+
+        if (index < 0)
+        {
+            index = ~index;
+        }
+
+        if ((index < _items.Count && _items[index].Start < end) || (index > 0 && _items[index - 1].End > start))
+        {
+            return false;
+        }
+
+        _items.Insert(index, item);
+
+        return true;
+    }
+
+    private class Item(int start, int end) : IComparable<Item>
+    {
+        public int Start { get; } = start;
+        public int End { get; } = end;
+
+        public int CompareTo(Item? other)
+        {
+            if (other == null)
+            {
+                return -1;
+            }
+
+            return Start.CompareTo(other.Start);
+        }
+    }
+}
