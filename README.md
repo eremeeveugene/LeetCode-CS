cb9820ce60bfb682e5c9f20ce444531208f9e2ba
eugene <eremeeveugene@gmail.com>
2024-10-02 11:23:44 +0300

    Determine if Two Events Have Conflict (#800)

--- README.md
+++ README.md
@@ -363,6 +363,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [2433. Find The Original Array of Prefix Xor](https://leetcode.com/problems/find-the-original-array-of-prefix-xor/description/)
 - [2441. Largest Positive Integer That Exists With Its Negative](https://leetcode.com/problems/largest-positive-integer-that-exists-with-its-negative/description/)
 - [2444. Count Subarrays With Fixed Bounds](https://leetcode.com/problems/count-subarrays-with-fixed-bounds/description/)
+- [2446. Determine if Two Events Have Conflict](https://leetcode.com/problems/determine-if-two-events-have-conflict/description/)
 - [2469. Convert the Temperature](https://leetcode.com/problems/convert-the-temperature/description/)
 - [2485. Find the Pivot Integer](https://leetcode.com/problems/find-the-pivot-integer/description/)
 - [2486. Append Characters to String to Make Subsequence](https://leetcode.com/problems/append-characters-to-string-to-make-subsequence/description/)
---
+++ source/LeetCode/Algorithms/DetermineIfTwoEventsHaveConflict/DetermineIfTwoEventsHaveConflictNumberComparison.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.DetermineIfTwoEventsHaveConflict;
+
+/// <inheritdoc />
+public class DetermineIfTwoEventsHaveConflictNumberComparison : IDetermineIfTwoEventsHaveConflict
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="event1"></param>
+    /// <param name="event2"></param>
+    /// <returns></returns>
+    public bool HaveConflict(string[] event1, string[] event2)
+    {
+        var event1Start = ConvertToMinutes(event1[0]);
+        var event1End = ConvertToMinutes(event1[1]);
+        var event2Start = ConvertToMinutes(event2[0]);
+        var event2End = ConvertToMinutes(event2[1]);
+
+        return event1Start <= event2End && event1End >= event2Start;
+    }
+
+    private static int ConvertToMinutes(string time)
+    {
+        var parts = time.Split(':');
+
+        return (int.Parse(parts[0]) * 60) + int.Parse(parts[1]);
+    }
+}
