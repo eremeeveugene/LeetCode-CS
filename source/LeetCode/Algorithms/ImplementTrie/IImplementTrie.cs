8642365c6b807462712f169175909d7354b12308
eugene <eremeeveugene@gmail.com>
2024-10-02 12:55:07 +0300

    Implement Trie (Prefix Tree) (#801)

---
+++ source/LeetCode/Algorithms/ImplementTrie/IImplementTrie.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ImplementTrie;
+
+/// <summary>
+///     https://leetcode.com/problems/implement-trie-prefix-tree/
+/// </summary>
+public interface IImplementTrie
+{
+    void Insert(string word);
+    bool Search(string word);
+    bool StartsWith(string prefix);
+}
