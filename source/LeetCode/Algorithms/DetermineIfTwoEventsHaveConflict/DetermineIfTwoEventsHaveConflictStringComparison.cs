8df8b4b9cd0848f36e3be44030d1521f4853f1c9
eugene <eremeeveugene@gmail.com>
2024-10-02 11:18:13 +0300

    Determine if Two Events Have Conflict

---
+++ source/LeetCode/Algorithms/DetermineIfTwoEventsHaveConflict/DetermineIfTwoEventsHaveConflictStringComparison.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.DetermineIfTwoEventsHaveConflict;
+
+/// <inheritdoc />
+public class DetermineIfTwoEventsHaveConflictStringComparison : IDetermineIfTwoEventsHaveConflict
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="event1"></param>
+    /// <param name="event2"></param>
+    /// <returns></returns>
+    public bool HaveConflict(string[] event1, string[] event2)
+    {
+        return string.CompareOrdinal(event1[0], event2[1]) <= 0 && string.CompareOrdinal(event1[1], event2[0]) >= 0;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/DetermineIfTwoEventsHaveConflict/DetermineIfTwoEventsHaveConflictTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.DetermineIfTwoEventsHaveConflict;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.DetermineIfTwoEventsHaveConflict;
+
+public abstract class DetermineIfTwoEventsHaveConflictTestsBase<T> where T : IDetermineIfTwoEventsHaveConflict, new()
+{
+    [TestMethod]
+    [DataRow("[\"01:15\",\"02:00\"]", "[\"02:00\",\"03:00\"]", true)]
+    [DataRow("[\"01:00\",\"02:00\"]", "[\"01:20\",\"03:00\"]", true)]
+    [DataRow("[\"10:00\",\"11:00\"]", "[\"14:00\",\"15:00\"]", false)]
+    public void HaveConflict_GivenTwoEventTimeRanges_ReturnsIfConflictExists(string event1JsonArray,
+        string event2JsonArray, bool expectedResult)
+    {
+        // Arrange
+        var event1 = JsonHelper<string>.DeserializeToArray(event1JsonArray);
+        var event2 = JsonHelper<string>.DeserializeToArray(event2JsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.HaveConflict(event1, event2);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
