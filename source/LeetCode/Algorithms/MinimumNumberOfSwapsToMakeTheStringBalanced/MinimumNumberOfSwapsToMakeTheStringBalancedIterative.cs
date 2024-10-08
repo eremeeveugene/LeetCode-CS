94305306770e497e7a79bae0bf525bab09d12076
eugene <eremeeveugene@gmail.com>
2024-10-08 21:23:38 +0300

    Minimum Number of Swaps to Make the String Balanced (#818)

---
+++ source/LeetCode/Algorithms/MinimumNumberOfSwapsToMakeTheStringBalanced/MinimumNumberOfSwapsToMakeTheStringBalancedIterative.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumNumberOfSwapsToMakeTheStringBalanced;
+
+/// <inheritdoc />
+public class MinimumNumberOfSwapsToMakeTheStringBalancedIterative : IMinimumNumberOfSwapsToMakeTheStringBalanced
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public int MinSwaps(string s)
+    {
+        var count = 0;
+
+        foreach (var c in s)
+        {
+            if (c == '[')
+            {
+                count++;
+            }
+            else
+            {
+                if (count > 0)
+                {
+                    count--;
+                }
+            }
+        }
+
+        return (count + 1) / 2;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumNumberOfSwapsToMakeTheStringBalanced/MinimumNumberOfSwapsToMakeTheStringBalancedTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumNumberOfSwapsToMakeTheStringBalanced;
+
+namespace LeetCode.Tests.Algorithms.MinimumNumberOfSwapsToMakeTheStringBalanced;
+
+public abstract class MinimumNumberOfSwapsToMakeTheStringBalancedTestsBase<T>
+    where T : IMinimumNumberOfSwapsToMakeTheStringBalanced, new()
+{
+    [TestMethod]
+    [DataRow("[]", 0)]
+    [DataRow("][][", 1)]
+    [DataRow("]]][[[", 2)]
+    public void MinSwaps_GivenUnbalancedBracketsString_ReturnsMinimumSwapsToBalance(string s, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinSwaps(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
