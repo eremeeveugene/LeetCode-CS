a9d56edb46b623fcc48f5b3a243674f122fca76a
eugene <eremeeveugene@gmail.com>
2024-10-11 14:20:31 +0300

    The Number of the Smallest Unoccupied Chair (#824)

---
+++ source/LeetCode/Algorithms/TheNumberOfTheSmallestUnoccupiedChair/TheNumberOfTheSmallestUnoccupiedChairSortedSet.cs
@@ -0,0 +1,87 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.TheNumberOfTheSmallestUnoccupiedChair;
+
+/// <inheritdoc />
+public class TheNumberOfTheSmallestUnoccupiedChairSortedSet : ITheNumberOfTheSmallestUnoccupiedChair
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="times"></param>
+    /// <param name="targetFriend"></param>
+    /// <returns></returns>
+    public int SmallestChair(int[][] times, int targetFriend)
+    {
+        var freeChairs = new SortedSet<int>();
+
+        for (var i = 0; i < times.Length; i++)
+        {
+            freeChairs.Add(i);
+        }
+
+        var chairAssigned = new int[times.Length];
+
+        var events = new List<Event>();
+
+        for (var i = 0; i < times.Length; i++)
+        {
+            events.Add(new Event(times[i][0], i, true));
+            events.Add(new Event(times[i][1], i, false));
+        }
+
+        events.Sort();
+
+        foreach (var @event in events)
+        {
+            if (@event.IsArrival)
+            {
+                var assignedChair = freeChairs.Min;
+
+                freeChairs.Remove(assignedChair);
+
+                chairAssigned[@event.FriendId] = assignedChair;
+
+                if (@event.FriendId == targetFriend)
+                {
+                    return assignedChair;
+                }
+            }
+            else
+            {
+                freeChairs.Add(chairAssigned[@event.FriendId]);
+            }
+        }
+
+        return -1;
+    }
+
+    private class Event(int time, int friendId, bool isArrival) : IComparable<Event>
+    {
+        private readonly int _time = time;
+        public int FriendId { get; } = friendId;
+        public bool IsArrival { get; } = isArrival;
+
+        public int CompareTo(Event? other)
+        {
+            if (other == null)
+            {
+                return -1;
+            }
+
+            var timeComparison = _time.CompareTo(other._time);
+
+            return timeComparison == 0 ? IsArrival.CompareTo(other.IsArrival) : timeComparison;
+        }
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/TheNumberOfTheSmallestUnoccupiedChair/TheNumberOfTheSmallestUnoccupiedChairTestsBase.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.TheNumberOfTheSmallestUnoccupiedChair;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.TheNumberOfTheSmallestUnoccupiedChair;
+
+public abstract class TheNumberOfTheSmallestUnoccupiedChairTestsBase<T>
+    where T : ITheNumberOfTheSmallestUnoccupiedChair, new()
+{
+    [TestMethod]
+    [DataRow("[[1,4],[2,3],[4,6]]", 1, 1)]
+    [DataRow("[[1,4],[2,3],[4,6]]", 2, 0)]
+    [DataRow("[[3,10],[1,5],[2,6]]", 0, 2)]
+    [DataRow(
+        "[[33,35],[26,29],[9,28],[4,31],[8,10],[32,34],[15,24],[27,39],[14,36],[1,14],[25,39],[5,27],[6,15],[2,38],[19,36],[24,34],[3,26]]",
+        0, 3)]
+    public void SmallestChair_WithSeatingTimesAndTargetFriend_ReturnsCorrectChair(string timesJsonArray,
+        int targetFriend, int expectedResult)
+    {
+        // Arrange
+        var times = JsonHelper<int>.DeserializeToJaggedArray(timesJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.SmallestChair(times, targetFriend);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
