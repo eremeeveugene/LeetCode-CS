6a0f9577cfdb4446061494bb6f88fe22eabcdc79
eugene <eremeeveugene@gmail.com>
2024-10-12 16:39:16 +0300

    Monotonic Array (#830)

---
+++ source/LeetCode/Algorithms/MonotonicArray/MonotonicArrayIterative.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MonotonicArray;
+
+/// <inheritdoc />
+public class MonotonicArrayIterative : IMonotonicArray
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public bool IsMonotonic(int[] nums)
+    {
+        var increasing = true;
+        var decreasing = true;
+
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] > nums[i + 1])
+            {
+                increasing = false;
+            }
+
+            if (nums[i] < nums[i + 1])
+            {
+                decreasing = false;
+            }
+        }
+
+        return increasing || decreasing;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MonotonicArray/MonotonicArrayTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MonotonicArray;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MonotonicArray;
+
+public abstract class MonotonicArrayTestsBase<T> where T : IMonotonicArray, new()
+{
+    [TestMethod]
+    [DataRow("[1,2,2,3]", true)]
+    [DataRow("[6,5,4,4]", true)]
+    [DataRow("[1,3,2]", false)]
+    public void IsMonotonic_WithJsonArrayInput_ReturnsCorrectMonotonicity(string numsJsonArray, bool expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsMonotonic(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
