46dac346fb7da4bdb33da588b93624d435dcbccb
eugene <eremeeveugene@gmail.com>
2024-10-14 14:31:23 +0300

    Sign of the Product of an Array (#839)

---
+++ source/LeetCode/Algorithms/SignOfTheProductOfAnArray/SignOfTheProductOfAnArrayIterative.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SignOfTheProductOfAnArray;
+
+/// <inheritdoc />
+public class SignOfTheProductOfAnArrayIterative : ISignOfTheProductOfAnArray
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int ArraySign(int[] nums)
+    {
+        var sign = 1;
+
+        foreach (var num in nums)
+        {
+            if (num == 0)
+            {
+                return 0;
+            }
+
+            sign = Math.Sign(num * sign);
+        }
+
+        return sign;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/SignOfTheProductOfAnArray/SignOfTheProductOfAnArrayTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SignOfTheProductOfAnArray;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.SignOfTheProductOfAnArray;
+
+public abstract class SignOfTheProductOfAnArrayTestsBase<T> where T : ISignOfTheProductOfAnArray, new()
+{
+    [TestMethod]
+    [DataRow("[-1,-2,-3,-4,3,2,1]", 1)]
+    [DataRow("[1,5,0,2,-3]", 0)]
+    [DataRow("[-1,1,-1,1,-1]", -1)]
+    public void ArraySign_GivenArrayOfIntegers_ReturnsProductSign(string numsJsonArray, int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.ArraySign(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
