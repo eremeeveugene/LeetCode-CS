926e46ec610563bdd8d3f1ebfec9c5494bf8fa16
eugene <eremeeveugene@gmail.com>
2024-10-14 13:28:27 +0300

    Maximal Score After Applying K Operations

---
+++ source/LeetCode/Algorithms/MaximalScoreAfterApplyingKOperations/MaximalScoreAfterApplyingKOperationsPriorityQueue.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximalScoreAfterApplyingKOperations;
+
+/// <inheritdoc />
+public class MaximalScoreAfterApplyingKOperationsPriorityQueue : IMaximalScoreAfterApplyingKOperations
+{
+    /// <summary>
+    ///     Time complexity - O(k log n + n log n), where n is the number of elements in the queue and k is the number of new
+    ///     elements
+    ///     Space complexity - O(n + k), where n is the number of elements in the queue and k is the number of new elements
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public long MaxKelements(int[] nums, int k)
+    {
+        var priorityQueue = new PriorityQueue<int, int>(Comparer<int>.Create((a, b) => b.CompareTo(a)));
+
+        foreach (var num in nums)
+        {
+            priorityQueue.Enqueue(num, num);
+        }
+
+        long result = 0;
+
+        for (var i = 0; i < k; i++)
+        {
+            var maxElement = priorityQueue.Dequeue();
+
+            result += maxElement;
+
+            var newElement = (int)Math.Ceiling(maxElement / 3.0);
+
+            priorityQueue.Enqueue(newElement, newElement);
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MaximalScoreAfterApplyingKOperations/MaximalScoreAfterApplyingKOperationsTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaximalScoreAfterApplyingKOperations;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MaximalScoreAfterApplyingKOperations;
+
+public abstract class MaximalScoreAfterApplyingKOperationsTestsBase<T>
+    where T : IMaximalScoreAfterApplyingKOperations, new()
+{
+    [TestMethod]
+    [DataRow("[10,10,10,10,10]", 5, 50)]
+    [DataRow("[1,10,3,3,3]", 3, 17)]
+    public void MaxKelements_WithArrayAndK_ReturnsCorrectMaxSum(string numsJsonArray, int k, long expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaxKelements(nums, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
