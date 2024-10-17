7e4715f29aae6687512760c809f3751a2327bbc4
eugene <eremeeveugene@gmail.com>
2024-10-17 20:43:25 +0300

    Maximum Swap (#848)

---
+++ source/Tests/LeetCode.Tests/Algorithms/MaximumSwap/MaximumSwapTestsBase.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaximumSwap;
+
+namespace LeetCode.Tests.Algorithms.MaximumSwap;
+
+public abstract class MaximumSwapTestsBase<T> where T : IMaximumSwap, new()
+{
+    [TestMethod]
+    [DataRow(2736, 7236)]
+    [DataRow(9973, 9973)]
+    public void MaximumSwap_GivenNumber_ReturnsMaxPossibleSwap(int num, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaximumSwap(num);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
