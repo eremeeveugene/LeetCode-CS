f825da7215df4cd4a5b97abb523d5b9321a135c1
eugene <eremeeveugene@gmail.com>
2024-10-19 15:04:03 +0300

    Baseball Game

---
+++ source/LeetCode/Algorithms/BaseballGame/BaseballGameIterative.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.BaseballGame;
+
+/// <inheritdoc />
+public class BaseballGameIterative : IBaseballGame
+{
+    public int CalPoints(string[] operations)
+    {
+        var scores = new List<int>();
+
+        foreach (var operation in operations)
+        {
+            switch (operation)
+            {
+                case "D":
+                    scores.Add(scores[^1] * 2);
+                    break;
+                case "C":
+                    scores.RemoveAt(scores.Count - 1);
+                    break;
+                case "+":
+                    scores.Add(scores[^1] + scores[^2]);
+                    break;
+                default:
+                    scores.Add(int.Parse(operation));
+                    break;
+            }
+        }
+
+        return scores.Sum();
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/BaseballGame/BaseballGameTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.BaseballGame;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.BaseballGame;
+
+public abstract class BaseballGameTestsBase<T> where T : IBaseballGame, new()
+{
+    [TestMethod]
+    [DataRow("[\"5\",\"2\",\"C\",\"D\",\"+\"]", 30)]
+    [DataRow("[\"5\",\"-2\",\"4\",\"C\",\"D\",\"9\",\"+\",\"+\"]", 27)]
+    public void CalPoints_WithOperationsArray_ReturnsTotalPoints(string operationsJsonArray, int expectedResult)
+    {
+        // Arrange
+        var operations = JsonHelper<string>.DeserializeToArray(operationsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CalPoints(operations);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
