33c572bb1f9028f6a5fa9a680009dfd2c936ebe4
eugene <eremeeveugene@gmail.com>
2024-10-19 13:04:14 +0300

    Find Kth Bit in Nth Binary String

---
+++ source/LeetCode/Algorithms/FindKthBitInNthBinaryString/FindKthBitInNthBinaryStringBruteForce.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Text;
+
+namespace LeetCode.Algorithms.FindKthBitInNthBinaryString;
+
+/// <inheritdoc />
+public class FindKthBitInNthBinaryStringBruteForce : IFindKthBitInNthBinaryString
+{
+    /// <summary>
+    ///     Time complexity - O(2^n)
+    ///     Space complexity - O(2^n)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public char FindKthBit(int n, int k)
+    {
+        var s = "0";
+
+        for (var i = 0; i < n - 1; i++)
+        {
+            s = ModifyString(s);
+        }
+
+        return s[k - 1];
+    }
+
+    private static string ModifyString(string s)
+    {
+        var stringBuilder = new StringBuilder();
+
+        stringBuilder.Append(s);
+        stringBuilder.Append('1');
+
+        for (var i = s.Length - 1; i >= 0; i--)
+        {
+            stringBuilder.Append(s[i] == '0' ? '1' : '0');
+        }
+
+        return stringBuilder.ToString();
+    }
+}
---
+++ source/LeetCode/Algorithms/FindKthBitInNthBinaryString/FindKthBitInNthBinaryStringRecursive.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindKthBitInNthBinaryString;
+
+/// <inheritdoc />
+public class FindKthBitInNthBinaryStringRecursive : IFindKthBitInNthBinaryString
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public char FindKthBit(int n, int k)
+    {
+        if (n == 1)
+        {
+            return '0';
+        }
+
+        var length = (1 << n) - 1;
+        var mid = (length / 2) + 1;
+
+        if (k == mid)
+        {
+            return '1';
+        }
+
+        if (k < mid)
+        {
+            return FindKthBit(n - 1, k);
+        }
+
+        var mirroredIndex = length - k + 1;
+        var mirroredBit = FindKthBit(n - 1, mirroredIndex);
+
+        return mirroredBit == '0' ? '1' : '0';
+    }
+}
