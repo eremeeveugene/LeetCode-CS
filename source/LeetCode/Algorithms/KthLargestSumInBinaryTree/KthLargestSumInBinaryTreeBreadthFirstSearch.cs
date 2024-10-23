968f6c9f76c49e2f2fb649fb36b07839ee037502
eugene <eremeeveugene@gmail.com>
2024-10-23 14:52:44 +0300

    Kth Largest Sum in a Binary Tree

---
+++ source/LeetCode/Algorithms/KthLargestSumInBinaryTree/KthLargestSumInBinaryTreeBreadthFirstSearch.cs
@@ -0,0 +1,65 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.KthLargestSumInBinaryTree;
+
+/// <inheritdoc />
+public class KthLargestSumInBinaryTreeBreadthFirstSearch : IKthLargestSumInBinaryTree
+{
+    public long KthLargestLevelSum(TreeNode root, int k)
+    {
+        var nodesQueue = new Queue<TreeNode>();
+
+        nodesQueue.Enqueue(root);
+
+        var sumsPriorityQueue = new PriorityQueue<long, long>();
+
+        while (nodesQueue.Count > 0)
+        {
+            var levelCount = nodesQueue.Count;
+
+            long sum = 0;
+
+            for (var j = 0; j < levelCount; j++)
+            {
+                var node = nodesQueue.Dequeue();
+
+                sum += node.val;
+
+                if (node.left != null)
+                {
+                    nodesQueue.Enqueue(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    nodesQueue.Enqueue(node.right);
+                }
+            }
+
+            sumsPriorityQueue.Enqueue(sum, sum);
+
+            if (sumsPriorityQueue.Count > k)
+            {
+                sumsPriorityQueue.Dequeue();
+            }
+        }
+
+        if (sumsPriorityQueue.Count < k)
+        {
+            return -1;
+        }
+
+        return sumsPriorityQueue.Dequeue();
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/KthLargestSumInBinaryTree/KthLargestSumInBinaryTreeTestsBase.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.KthLargestSumInBinaryTree;
+using LeetCode.Core.Exceptions;
+using LeetCode.Core.Helpers;
+using LeetCode.Core.Models;
+
+namespace LeetCode.Tests.Algorithms.KthLargestSumInBinaryTree;
+
+public abstract class KthLargestSumInBinaryTreeTestsBase<T> where T : IKthLargestSumInBinaryTree, new()
+{
+    [TestMethod]
+    [DataRow("[5,8,9,2,1,3,7,4,6]", 2, 13)]
+    [DataRow("[1,2,null,3]", 1, 3)]
+    public void KthLargestLevelSum_GivenTreeNodeArrayAndK_ReturnsCorrectLevelSum(string rootJsonArray, int k,
+        long expectedResult)
+    {
+        // Arrange
+        var rootArray = JsonHelper<int?>.DeserializeToArray(rootJsonArray);
+        var root = TreeNode.ToTreeNode(rootArray) ?? throw new TreeNodeBuildException();
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.KthLargestLevelSum(root, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
