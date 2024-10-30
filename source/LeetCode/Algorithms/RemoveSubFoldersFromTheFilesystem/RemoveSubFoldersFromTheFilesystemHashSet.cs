06936d69532f881223d1a475c3ab4d4495e5a949
eugene <eremeeveugene@gmail.com>
2024-10-30 18:42:29 +0200

    Remove Sub-Folders from the Filesystem

---
+++ source/LeetCode/Algorithms/RemoveSubFoldersFromTheFilesystem/RemoveSubFoldersFromTheFilesystemHashSet.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.RemoveSubFoldersFromTheFilesystem;
+
+/// <inheritdoc />
+public class RemoveSubFoldersFromTheFilesystemHashSet : IRemoveSubFoldersFromTheFilesystem
+{
+    /// <summary>
+    ///     Time complexity - O(n * m^2)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="folders"></param>
+    /// <returns></returns>
+    public IList<string> RemoveSubfolders(string[] folders)
+    {
+        var result = new List<string>();
+
+        var foldersHashSet = new HashSet<string>(folders);
+
+        foreach (var folder in folders)
+        {
+            var i = 1;
+            var add = true;
+
+            while (i < folder.Length)
+            {
+                if (folder[i] is '/' && foldersHashSet.Contains(folder[..i]))
+                {
+                    add = false;
+
+                    break;
+                }
+
+                i++;
+            }
+
+            if (add)
+            {
+                result.Add(folder);
+            }
+        }
+
+        return result;
+    }
+}
