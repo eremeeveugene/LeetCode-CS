3ba4d4b711a64666acc6f9cf38f92b14c67680b1
eugene <eremeeveugene@gmail.com>
2024-11-03 11:03:32 +0200

    Rotate String (#881)

---
+++ source/LeetCode/Algorithms/RotateString/RotateStringBruteForce.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.RotateString;
+
+/// <inheritdoc />
+public class RotateStringBruteForce : IRotateString
+{
+    /// <summary>
+    ///     Time complexity - O(n^2)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="goal"></param>
+    /// <returns></returns>
+    public bool RotateString(string s, string goal)
+    {
+        return s.Length == goal.Length && s.Where((t, i) => CanRotateString(s, goal, i)).Any();
+    }
+
+    private static bool CanRotateString(string s, string goal, int i)
+    {
+        return !goal.Where((t, j) => s[(i + j) % s.Length] != t).Any();
+    }
+}
---
+++ source/LeetCode/Algorithms/RotateString/RotateStringConcatenation.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.RotateString;
+
+/// <inheritdoc />
+public class RotateStringConcatenation : IRotateString
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="goal"></param>
+    /// <returns></returns>
+    public bool RotateString(string s, string goal)
+    {
+        return s.Length == goal.Length && (s + s).Contains(goal);
+    }
+}
