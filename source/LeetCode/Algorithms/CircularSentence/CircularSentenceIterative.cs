5c0eff2c13d460d789413f7b23f47ce625a3cc01
eugene <eremeeveugene@gmail.com>
2024-11-04 14:25:58 +0200

    Circular Sentence

---
+++ source/LeetCode/Algorithms/CircularSentence/CircularSentenceIterative.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CircularSentence;
+
+/// <inheritdoc />
+public class CircularSentenceIterative : ICircularSentence
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(m)
+    /// </summary>
+    /// <param name="sentence"></param>
+    /// <returns></returns>
+    public bool IsCircularSentence(string sentence)
+    {
+        var words = sentence.Split(' ');
+
+        if (words[0][0] != words[^1][^1])
+        {
+            return false;
+        }
+
+        for (var i = 0; i < words.Length - 1; i++)
+        {
+            if (words[i][^1] != words[i + 1][0])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
