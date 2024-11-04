86c6a86869ce57a0e52bb89b7b81c62bf55900a2
eugene <eremeeveugene@gmail.com>
2024-11-04 14:31:09 +0200

    Circular Sentence (#884)

---
+++ source/Tests/LeetCode.Tests/Algorithms/CircularSentence/CircularSentenceTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CircularSentence;
+
+namespace LeetCode.Tests.Algorithms.CircularSentence;
+
+public abstract class CircularSentenceTestsBase<T> where T : ICircularSentence, new()
+{
+    [TestMethod]
+    [DataRow("leetcode exercises sound delightful", true)]
+    [DataRow("eetcode", true)]
+    [DataRow("Leetcode is cool", false)]
+    public void IsCircularSentence_GivenSentence_ReturnsIfSentenceIsCircular(string sentence, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsCircularSentence(sentence);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
