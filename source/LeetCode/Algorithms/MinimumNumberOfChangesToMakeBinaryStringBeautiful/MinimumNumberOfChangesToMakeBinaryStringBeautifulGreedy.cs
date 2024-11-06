937a0d5db31630e3371da2550b389749e4858b91
eugene <eremeeveugene@gmail.com>
2024-11-06 13:56:56 +0200

    Minimum Number of Changes to Make Binary String Beautiful (#887)

---
+++ source/LeetCode/Algorithms/MinimumNumberOfChangesToMakeBinaryStringBeautiful/MinimumNumberOfChangesToMakeBinaryStringBeautifulGreedy.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumNumberOfChangesToMakeBinaryStringBeautiful;
+
+/// <inheritdoc />
+public class MinimumNumberOfChangesToMakeBinaryStringBeautifulGreedy :
+    IMinimumNumberOfChangesToMakeBinaryStringBeautiful
+{
+    public int MinChanges(string s)
+    {
+        var result = 0;
+
+        for (var i = 0; i < s.Length - 1; i += 2)
+        {
+            if (s[i] != s[i + 1])
+            {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumNumberOfChangesToMakeBinaryStringBeautiful/MinimumNumberOfChangesToMakeBinaryStringBeautifulTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumNumberOfChangesToMakeBinaryStringBeautiful;
+
+namespace LeetCode.Tests.Algorithms.MinimumNumberOfChangesToMakeBinaryStringBeautiful;
+
+public abstract class MinimumNumberOfChangesToMakeBinaryStringBeautifulTestsBase<T>
+    where T : IMinimumNumberOfChangesToMakeBinaryStringBeautiful, new()
+{
+    [TestMethod]
+    [DataRow("0000", 0)]
+    [DataRow("10", 1)]
+    [DataRow("1001", 2)]
+    public void MinChanges_GivenBinaryString_ReturnsMinimumChangeCount(string s, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinChanges(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
