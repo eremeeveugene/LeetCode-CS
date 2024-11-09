993124db39c270934d39d95843355a731b0be582
eugene <eremeeveugene@gmail.com>
2024-11-09 10:28:50 +0200

    Minimum Array End

---
+++ source/LeetCode/Algorithms/MinimumArrayEnd/MinimumArrayEndIterative.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumArrayEnd;
+
+/// <inheritdoc />
+public class MinimumArrayEndIterative : IMinimumArrayEnd
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public long MinEnd(int n, int x)
+    {
+        long result = x;
+
+        while (--n > 0)
+        {
+            result = (result + 1) | (uint)x;
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumArrayEnd/MinimumArrayEndTestsBase.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumArrayEnd;
+
+namespace LeetCode.Tests.Algorithms.MinimumArrayEnd;
+
+public abstract class MinimumArrayEndTestsBase<T> where T : IMinimumArrayEnd, new()
+{
+    [TestMethod]
+    [DataRow(3, 4, 6)]
+    [DataRow(2, 7, 15)]
+    public void MinEnd_GivenNAndX_ReturnsExpectedResult(int n, int x, long expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinEnd(n, x);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
