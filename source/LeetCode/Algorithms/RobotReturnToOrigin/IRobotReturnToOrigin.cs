9405f1da98d74dc526f426eb40bd4a86009408b3
eugene <eremeeveugene@gmail.com>
2024-11-09 12:33:58 +0200

    Robot Return to Origin (#898)

---
+++ source/LeetCode/Algorithms/RobotReturnToOrigin/IRobotReturnToOrigin.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.RobotReturnToOrigin;
+
+/// <summary>
+///     https://leetcode.com/problems/robot-return-to-origin/description/
+/// </summary>
+public interface IRobotReturnToOrigin
+{
+    bool JudgeCircle(string moves);
+}
