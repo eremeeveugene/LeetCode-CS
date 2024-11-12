324f5e9314ddec820997bf41d2be4cf1b43811bc
eugene <eremeeveugene@gmail.com>
2024-11-12 10:42:16 +0200

    Most Beautiful Item for Each Query (#905)

---
+++ source/LeetCode/Algorithms/MostBeautifulItemForEachQuery/IMostBeautifulItemForEachQuery.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MostBeautifulItemForEachQuery;
+
+/// <summary>
+///     https://leetcode.com/problems/most-beautiful-item-for-each-query/description/
+/// </summary>
+public interface IMostBeautifulItemForEachQuery
+{
+    int[] MaximumBeauty(int[][] items, int[] queries);
+}
---
+++ source/LeetCode/Algorithms/MostBeautifulItemForEachQuery/MostBeautifulItemForEachQueryBinarySearch.cs
@@ -0,0 +1,69 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MostBeautifulItemForEachQuery;
+
+/// <inheritdoc />
+public class MostBeautifulItemForEachQueryBinarySearch : IMostBeautifulItemForEachQuery
+{
+    /// <summary>
+    ///     Time complexity - O((m + n) log m)
+    ///     Space complexity - O(m + n)
+    /// </summary>
+    /// <param name="items"></param>
+    /// <param name="queries"></param>
+    /// <returns></returns>
+    public int[] MaximumBeauty(int[][] items, int[] queries)
+    {
+        var result = new int[queries.Length];
+
+        Array.Sort(items, (a, b) => a[0].CompareTo(b[0]));
+
+        var maxBeautyUpToPrice = new List<int[]>();
+        var maxBeauty = 0;
+
+        foreach (var item in items)
+        {
+            maxBeauty = Math.Max(maxBeauty, item[1]);
+            maxBeautyUpToPrice.Add([item[0], maxBeauty]);
+        }
+
+        foreach (var queryWithIndex in queries.Select((query, index) => new[] { query, index }).OrderBy(q => q[0]))
+        {
+            var query = queryWithIndex[0];
+            var originalIndex = queryWithIndex[1];
+
+            var low = 0;
+            var high = maxBeautyUpToPrice.Count - 1;
+            var beauty = 0;
+
+            while (low <= high)
+            {
+                var mid = low + ((high - low) / 2);
+
+                if (maxBeautyUpToPrice[mid][0] <= query)
+                {
+                    beauty = maxBeautyUpToPrice[mid][1];
+
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+
+            result[originalIndex] = beauty;
+        }
+
+        return result;
+    }
+}
