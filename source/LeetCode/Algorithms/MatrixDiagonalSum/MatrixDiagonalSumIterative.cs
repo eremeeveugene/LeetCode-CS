bbfe79a7142687d8795b46bf61720aff3428cba2
eugene <eremeeveugene@gmail.com>
2024-11-12 21:11:23 +0200

    Matrix Diagonal Sum (#907)

---
+++ source/LeetCode/Algorithms/MatrixDiagonalSum/MatrixDiagonalSumIterative.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MatrixDiagonalSum;
+
+/// <inheritdoc />
+public class MatrixDiagonalSumIterative : IMatrixDiagonalSum
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="mat"></param>
+    /// <returns></returns>
+    public int DiagonalSum(int[][] mat)
+    {
+        var result = 0;
+
+        for (var i = 0; i < mat.Length; i++)
+        {
+            result += mat[i][i];
+            result += mat[i][mat.Length - i - 1];
+        }
+
+        if (mat.Length % 2 != 0)
+        {
+            result -= mat[mat.Length / 2][mat.Length / 2];
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MatrixDiagonalSum/MatrixDiagonalSumTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MatrixDiagonalSum;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MatrixDiagonalSum;
+
+public abstract class MatrixDiagonalSumTestsBase<T> where T : IMatrixDiagonalSum, new()
+{
+    [TestMethod]
+    [DataRow("[[1,2,3],[4,5,6],[7,8,9]]", 25)]
+    [DataRow("[[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]]", 8)]
+    [DataRow("[[5]]", 5)]
+    public void DiagonalSum_WithMatrix_ReturnsSumOfDiagonalElements(string matJsonArray, int expectedResult)
+    {
+        // Arrange
+        var mat = JsonHelper<int>.DeserializeToJaggedArray(matJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.DiagonalSum(mat);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
