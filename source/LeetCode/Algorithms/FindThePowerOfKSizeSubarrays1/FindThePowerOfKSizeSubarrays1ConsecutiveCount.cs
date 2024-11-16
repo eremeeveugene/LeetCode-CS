1a2bb56fcf5a91b8c960a4cabb350345f759d335
eugene <eremeeveugene@gmail.com>
2024-11-16 19:31:04 +0200

    3254 Find the Power of K-Size Subarrays I

---
+++ source/LeetCode/Algorithms/FindThePowerOfKSizeSubarrays1/FindThePowerOfKSizeSubarrays1ConsecutiveCount.cs
@@ -0,0 +1,56 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindThePowerOfKSizeSubarrays1;
+
+/// <inheritdoc />
+public class FindThePowerOfKSizeSubarrays1ConsecutiveCount : IFindThePowerOfKSizeSubarrays1
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int[] ResultsArray(int[] nums, int k)
+    {
+        if (k == 1)
+        {
+            return nums;
+        }
+
+        var result = new int[nums.Length - k + 1];
+
+        Array.Fill(result, -1);
+
+        var consecutiveCount = 1;
+
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] + 1 == nums[i + 1])
+            {
+                consecutiveCount++;
+            }
+            else
+            {
+                consecutiveCount = 1;
+            }
+
+            if (consecutiveCount >= k)
+            {
+                result[i - k + 2] = nums[i + 1];
+            }
+        }
+
+        return result;
+    }
+}
---
+++ source/LeetCode/Algorithms/FindThePowerOfKSizeSubarrays1/IFindThePowerOfKSizeSubarrays1.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindThePowerOfKSizeSubarrays1;
+
+/// <summary>
+///     https://leetcode.com/problems/find-the-power-of-k-size-subarrays-i/description/
+/// </summary>
+public interface IFindThePowerOfKSizeSubarrays1
+{
+    int[] ResultsArray(int[] nums, int k);
+}
