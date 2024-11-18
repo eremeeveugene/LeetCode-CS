8a9078fc5410922cf6633b25a50a5a406e165b8c
eugene <eremeeveugene@gmail.com>
2024-11-18 13:06:50 +0200

    Defuse the Bomb (#917)

---
+++ source/LeetCode/Algorithms/DefuseTheBomb/DefuseTheBombSlidingWindow.cs
@@ -0,0 +1,56 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.DefuseTheBomb;
+
+/// <inheritdoc />
+public class DefuseTheBombSlidingWindow : IDefuseTheBomb
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="code"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int[] Decrypt(int[] code, int k)
+    {
+        var result = new int[code.Length];
+
+        if (k == 0)
+        {
+            return result;
+        }
+
+        var sum = 0;
+        var start = k > 0 ? 1 : code.Length + k;
+        var end = k > 0 ? k : code.Length - 1;
+
+        for (var i = start; i <= end; i++)
+        {
+            sum += code[i % code.Length];
+        }
+
+        for (var i = 0; i < code.Length; i++)
+        {
+            result[i] = sum;
+
+            sum -= code[start % code.Length];
+
+            start++;
+            end++;
+
+            sum += code[end % code.Length];
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/DefuseTheBomb/DefuseTheBombTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.DefuseTheBomb;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.DefuseTheBomb;
+
+public abstract class DefuseTheBombTestsBase<T> where T : IDefuseTheBomb, new()
+{
+    [TestMethod]
+    [DataRow("[5,7,1,4]", 3, "[12,10,16,13]")]
+    [DataRow("[1,2,3,4]", 0, "[0,0,0,0]")]
+    [DataRow("[2,4,9,3]", -2, "[12,5,6,13]")]
+    public void Decrypt_WithCodeArrayAndShiftValue_ReturnsDecryptedArray(string codeJsonArray, int k,
+        string expectedResultJsonArray)
+    {
+        // Arrange
+        var code = JsonHelper<int>.DeserializeToArray(codeJsonArray);
+        var expectedResult = JsonHelper<int>.DeserializeToArray(expectedResultJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.Decrypt(code, k);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
