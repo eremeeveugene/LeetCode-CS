25456ff7625713c9e79984693da38563bbee97b3
eugene <eremeeveugene@gmail.com>
2024-11-26 21:17:32 +0200

    Count Odd Numbers in an Interval Range (#929)

---
+++ source/LeetCode/Algorithms/CountOddNumbersInAnIntervalRange/CountOddNumbersInAnIntervalRangeMath.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountOddNumbersInAnIntervalRange;
+
+/// <inheritdoc />
+public class CountOddNumbersInAnIntervalRangeMath : ICountOddNumbersInAnIntervalRange
+{
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="low"></param>
+    /// <param name="high"></param>
+    /// <returns></returns>
+    public int CountOdds(int low, int high)
+    {
+        return ((high + 1) / 2) - (low / 2);
+    }
+}
