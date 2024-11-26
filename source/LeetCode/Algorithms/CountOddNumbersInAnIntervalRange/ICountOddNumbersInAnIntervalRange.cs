eb34f87143cb1717f9a4555f66dab59e62463708
eugene <eremeeveugene@gmail.com>
2024-11-26 21:14:41 +0200

    Count Odd Numbers in an Interval Range

---
+++ source/LeetCode/Algorithms/CountOddNumbersInAnIntervalRange/ICountOddNumbersInAnIntervalRange.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountOddNumbersInAnIntervalRange;
+
+/// <summary>
+///     https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/description/
+/// </summary>
+public interface ICountOddNumbersInAnIntervalRange
+{
+    int CountOdds(int low, int high);
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountOddNumbersInAnIntervalRange/CountOddNumbersInAnIntervalRangeTestsBase.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountOddNumbersInAnIntervalRange;
+
+namespace LeetCode.Tests.Algorithms.CountOddNumbersInAnIntervalRange;
+
+public abstract class CountOddNumbersInAnIntervalRangeTestsBase<T> where T : ICountOddNumbersInAnIntervalRange, new()
+{
+    [TestMethod]
+    [DataRow(3, 7, 3)]
+    [DataRow(8, 10, 1)]
+    public void CountOdds_GivenLowAndHighRange_ReturnsCountOfOddNumbers(int low, int high, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountOdds(low, high);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
