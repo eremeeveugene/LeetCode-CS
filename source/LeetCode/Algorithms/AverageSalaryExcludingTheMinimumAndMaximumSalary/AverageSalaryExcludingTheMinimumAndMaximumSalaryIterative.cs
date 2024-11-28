09fb2851234197455c27022a951366325e56922b
eugene <eremeeveugene@gmail.com>
2024-11-28 21:16:42 +0200

    Average Salary Excluding the Minimum and Maximum Salary (#935)

---
+++ source/LeetCode/Algorithms/AverageSalaryExcludingTheMinimumAndMaximumSalary/AverageSalaryExcludingTheMinimumAndMaximumSalaryIterative.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.AverageSalaryExcludingTheMinimumAndMaximumSalary;
+
+/// <inheritdoc />
+public class AverageSalaryExcludingTheMinimumAndMaximumSalaryIterative :
+    IAverageSalaryExcludingTheMinimumAndMaximumSalary
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="salary"></param>
+    /// <returns></returns>
+    public double Average(int[] salary)
+    {
+        var min = salary[0];
+        var max = salary[0];
+
+        double sum = 0;
+
+        foreach (var item in salary)
+        {
+            sum += item;
+
+            min = Math.Min(min, item);
+            max = Math.Max(max, item);
+        }
+
+        return (sum - min - max) / (salary.Length - 2);
+    }
+}
