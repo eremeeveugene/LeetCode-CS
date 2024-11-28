bcc4c9e2e2897231cef42baba3ace2a59eec333c
eugene <eremeeveugene@gmail.com>
2024-11-28 21:13:40 +0200

    Average Salary Excluding the Minimum and Maximum Salary

--- README.md
+++ README.md
@@ -307,6 +307,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [1470. Shuffle the Array](https://leetcode.com/problems/shuffle-the-array/description/)
 - [1480. Running Sum of 1d Array](https://leetcode.com/problems/running-sum-of-1d-array/description/)
 - [1486. XOR Operation in an Array](https://leetcode.com/problems/xor-operation-in-an-array/description/)
+- [1491. Average Salary Excluding the Minimum and Maximum Salary](https://leetcode.com/problems/average-salary-excluding-the-minimum-and-maximum-salary/description/)
 - [1497. Check If Array Pairs Are Divisible by k](https://leetcode.com/problems/check-if-array-pairs-are-divisible-by-k/description/)
 - [1500. Three Consecutive Odds](https://leetcode.com/problems/three-consecutive-odds/description/)
 - [1502. Can Make Arithmetic Progression From Sequences](https://leetcode.com/problems/can-make-arithmetic-progression-from-sequence/description/)
---
+++ source/Tests/LeetCode.Tests/Algorithms/AverageSalaryExcludingTheMinimumAndMaximumSalary/AverageSalaryExcludingTheMinimumAndMaximumSalaryTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.AverageSalaryExcludingTheMinimumAndMaximumSalary;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.AverageSalaryExcludingTheMinimumAndMaximumSalary;
+
+public abstract class AverageSalaryExcludingTheMinimumAndMaximumSalaryTestsBase<T>
+    where T : IAverageSalaryExcludingTheMinimumAndMaximumSalary, new()
+{
+    [TestMethod]
+    [DataRow("[4000,3000,1000,2000]", 2500.00000)]
+    [DataRow("[1000,2000,3000]", 2000.00000)]
+    public void Average_WithSalaryArray_ComputesCorrectAverage(string salaryJsonArray, double expectedResult)
+    {
+        // Arrange
+        var salary = JsonHelper<int>.DeserializeToArray(salaryJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.Average(salary);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
