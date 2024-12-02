1ed7cb19a84bac2a1a56f927e5ef946b5dd01d4e
eugene <eremeeveugene@gmail.com>
2024-12-02 18:54:08 +0200

    Shortest Distance to Target String in a Circular Array

---
+++ source/LeetCode/Algorithms/ShortestDistanceToTargetStringInCircularArray/IShortestDistanceToTargetStringInCircularArray.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ShortestDistanceToTargetStringInCircularArray;
+
+/// <summary>
+///     https://leetcode.com/problems/shortest-distance-to-target-string-in-a-circular-array/description/
+/// </summary>
+public interface IShortestDistanceToTargetStringInCircularArray
+{
+    int ClosetTarget(string[] words, string target, int startIndex);
+}
---
+++ source/LeetCode/Algorithms/ShortestDistanceToTargetStringInCircularArray/ShortestDistanceToTargetStringInCircularArrayIterative.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ShortestDistanceToTargetStringInCircularArray;
+
+/// <inheritdoc />
+public class ShortestDistanceToTargetStringInCircularArrayIterative : IShortestDistanceToTargetStringInCircularArray
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="words"></param>
+    /// <param name="target"></param>
+    /// <param name="startIndex"></param>
+    /// <returns></returns>
+    public int ClosetTarget(string[] words, string target, int startIndex)
+    {
+        var closestDistance = int.MaxValue;
+        var targetFound = false;
+
+        for (var i = 0; i < words.Length; i++)
+        {
+            if (words[i] != target)
+            {
+                continue;
+            }
+
+            targetFound = true;
+
+            var directDistance = Math.Abs(i - startIndex);
+            var circularDistance = words.Length - directDistance;
+            var distance = Math.Min(directDistance, circularDistance);
+
+            closestDistance = Math.Min(closestDistance, distance);
+        }
+
+        return targetFound ? closestDistance : -1;
+    }
+}
