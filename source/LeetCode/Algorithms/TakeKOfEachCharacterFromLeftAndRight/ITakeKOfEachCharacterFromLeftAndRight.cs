5c1eedf4a43360aba0085f101c8cc6ae43f04c1e
eugene <eremeeveugene@gmail.com>
2024-12-03 14:47:05 +0200

    Take K of Each Character From Left and Right

---
+++ source/LeetCode/Algorithms/TakeKOfEachCharacterFromLeftAndRight/ITakeKOfEachCharacterFromLeftAndRight.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.TakeKOfEachCharacterFromLeftAndRight;
+
+/// <summary>
+///     https://leetcode.com/problems/take-k-of-each-character-from-left-and-right/description/
+/// </summary>
+public interface ITakeKOfEachCharacterFromLeftAndRight
+{
+    int TakeCharacters(string s, int k);
+}
