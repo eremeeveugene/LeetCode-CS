8c14ac70a0eddb484341b3b5da1164190a84c6ff
eugene <eremeeveugene@gmail.com>
2024-12-03 14:06:52 +0200

    Adding Spaces to a String (#948)

---
+++ source/LeetCode/Algorithms/AddingSpacesToString/AddingSpacesToStringStringBuilder.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Text;
+
+namespace LeetCode.Algorithms.AddingSpacesToString;
+
+/// <inheritdoc />
+public class AddingSpacesToStringStringBuilder : IAddingSpacesToString
+{
+    /// <summary>
+    ///     Time complexity - O(n + m)
+    ///     Space complexity - O(n + m)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="spaces"></param>
+    /// <returns></returns>
+    public string AddSpaces(string s, int[] spaces)
+    {
+        var stringBuilder = new StringBuilder();
+
+        var spaceIndex = 0;
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (spaceIndex < spaces.Length && i == spaces[spaceIndex])
+            {
+                stringBuilder.Append(' ');
+
+                spaceIndex++;
+            }
+
+            stringBuilder.Append(s[i]);
+        }
+
+        return stringBuilder.ToString();
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/AddingSpacesToString/AddingSpacesToStringTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.AddingSpacesToString;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.AddingSpacesToString;
+
+public abstract class AddingSpacesToStringTestsBase<T> where T : IAddingSpacesToString, new()
+{
+    [TestMethod]
+    [DataRow("LeetcodeHelpsMeLearn", "[8,13,15]", "Leetcode Helps Me Learn")]
+    [DataRow("icodeinpython", "[1,5,7,9]", "i code in py thon")]
+    [DataRow("spacing", "[0,1,2,3,4,5,6]", " s p a c i n g")]
+    public void AddSpaces_WithStringAndSpacesArray_ReturnsCorrectlyFormattedString(string s, string spacesJsonArray,
+        string expectedResult)
+    {
+        // Arrange
+        var spaces = JsonHelper<int>.DeserializeToArray(spacesJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.AddSpaces(s, spaces);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
