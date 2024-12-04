ea501cd9ac7b5e0f1d47ddd19fc9ed49b4f6cd86
eugene <eremeeveugene@gmail.com>
2024-12-04 14:49:14 +0200

    Make String a Subsequence Using Cyclic Increments (#951)

--- README.md
+++ README.md
@@ -447,6 +447,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [2812. Find the Safest Path in a Grid](https://leetcode.com/problems/find-the-safest-path-in-a-grid/description/)
 - [2816. Double a Number Represented as a Linked List](https://leetcode.com/problems/double-a-number-represented-as-a-linked-list/description/)
 - [2824. Count Pairs Whose Sum is Less than Target](https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/description/)
+- [2825. Make String a Subsequence Using Cyclic Increments](https://leetcode.com/problems/make-string-a-subsequence-using-cyclic-increments/description/)
 - [2864. Maximum Odd Binary Number](https://leetcode.com/problems/maximum-odd-binary-number/description/)
 - [2894. Divisible and Non-divisible Sums Difference](https://leetcode.com/problems/divisible-and-non-divisible-sums-difference/description/)
 - [2938. Separate Black and White Balls](https://leetcode.com/problems/separate-black-and-white-balls/description/)
---
+++ source/Tests/LeetCode.Tests/Algorithms/MakeStringSubsequenceUsingCyclicIncrements/MakeStringSubsequenceUsingCyclicIncrementsTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MakeStringSubsequenceUsingCyclicIncrements;
+
+namespace LeetCode.Tests.Algorithms.MakeStringSubsequenceUsingCyclicIncrements;
+
+public abstract class MakeStringSubsequenceUsingCyclicIncrementsTestsBase<T>
+    where T : IMakeStringSubsequenceUsingCyclicIncrements, new()
+{
+    [TestMethod]
+    [DataRow("abc", "ad", true)]
+    [DataRow("zc", "ad", true)]
+    [DataRow("ab", "d", false)]
+    public void CanMakeSubsequence_WithGivenStrings_ReturnsTrueIfPossible(string str1, string str2, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CanMakeSubsequence(str1, str2);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
