7b25074000df5eace4509a5bce50fa4c74a8d321
eugene <eremeeveugene@gmail.com>
2024-12-07 18:55:17 +0200

    Minimum Limit of Balls in a Bag

---
+++ source/LeetCode/Algorithms/MinimumLimitOfBallsInBag/MinimumLimitOfBallsInBagBinarySearch.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumLimitOfBallsInBag;
+
+/// <inheritdoc />
+public class MinimumLimitOfBallsInBagBinarySearch : IMinimumLimitOfBallsInBag
+{
+    /// <summary>
+    ///     Time complexity - O(n log m)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="maxOperations"></param>
+    /// <returns></returns>
+    public int MinimumSize(int[] nums, int maxOperations)
+    {
+        var left = 1;
+        var right = nums.Max();
+
+        var result = right;
+
+        while (left <= right)
+        {
+            var mid = left + ((right - left) / 2);
+
+            if (nums.Where(num => num > mid).Sum(num => (num - 1) / mid) <= maxOperations)
+            {
+                result = mid;
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumLimitOfBallsInBag/MinimumLimitOfBallsInBagTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumLimitOfBallsInBag;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MinimumLimitOfBallsInBag;
+
+public abstract class MinimumLimitOfBallsInBagTestsBase<T> where T : IMinimumLimitOfBallsInBag, new()
+{
+    [TestMethod]
+    [DataRow("[9]", 2, 3)]
+    [DataRow("[2,4,8,2]", 4, 2)]
+    public void MinimumSize_WithNumsArrayAndMaxOperations_ReturnsMinimumPossibleSize(string numsJsonArray,
+        int maxOperations, int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinimumSize(nums, maxOperations);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
