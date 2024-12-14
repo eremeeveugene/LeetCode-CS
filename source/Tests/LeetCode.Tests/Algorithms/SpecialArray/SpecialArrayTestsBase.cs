a6cb76b3f5a920fccecf8eb4f9aa6af58c9d5c09
eugene <eremeeveugene@gmail.com>
2024-12-14 20:00:40 +0200

    Special Array I (#979)

---
+++ source/Tests/LeetCode.Tests/Algorithms/SpecialArray/SpecialArrayTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SpecialArray;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.SpecialArray;
+
+public abstract class SpecialArrayTestsBase<T> where T : ISpecialArray, new()
+{
+    [TestMethod]
+    [DataRow("[1]", true)]
+    [DataRow("[2,1,4]", true)]
+    [DataRow("[4,3,1,6]", false)]
+    public void IsArraySpecial_WithArrayInput_ReturnsWhetherArrayIsSpecial(string numsJsonArray, bool expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsArraySpecial(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
