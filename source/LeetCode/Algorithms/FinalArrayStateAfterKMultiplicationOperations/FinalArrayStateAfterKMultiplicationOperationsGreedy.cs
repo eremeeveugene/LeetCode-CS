a8b12724f8c8f2de625ab80a6cac79261b512783
eugene <eremeeveugene@gmail.com>
2024-12-16 14:37:17 +0200

    Final Array State After K Multiplication Operations I (#983)

---
+++ source/LeetCode/Algorithms/FinalArrayStateAfterKMultiplicationOperations/FinalArrayStateAfterKMultiplicationOperationsGreedy.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FinalArrayStateAfterKMultiplicationOperations;
+
+/// <inheritdoc />
+public class FinalArrayStateAfterKMultiplicationOperationsGreedy : IFinalArrayStateAfterKMultiplicationOperations
+{
+    /// <summary>
+    ///     Time complexity - O(n * k)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <param name="multiplier"></param>
+    /// <returns></returns>
+    public int[] GetFinalState(int[] nums, int k, int multiplier)
+    {
+        for (var i = 0; i < k; i++)
+        {
+            var min = int.MaxValue;
+            var minIndex = 0;
+
+            for (var j = 0; j < nums.Length; j++)
+            {
+                if (nums[j] >= min)
+                {
+                    continue;
+                }
+
+                minIndex = j;
+                min = nums[j];
+            }
+
+            nums[minIndex] *= multiplier;
+        }
+
+        return nums;
+    }
+}
---
+++ source/LeetCode/Algorithms/FinalArrayStateAfterKMultiplicationOperations/FinalArrayStateAfterKMultiplicationOperationsPriorityQueue.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FinalArrayStateAfterKMultiplicationOperations;
+
+/// <inheritdoc />
+public class FinalArrayStateAfterKMultiplicationOperationsPriorityQueue : IFinalArrayStateAfterKMultiplicationOperations
+{
+    /// <summary>
+    ///     Time complexity - O(n + k log n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <param name="multiplier"></param>
+    /// <returns></returns>
+    public int[] GetFinalState(int[] nums, int k, int multiplier)
+    {
+        var numsPriorityQueue = new PriorityQueue<(int Value, int Index), (int Value, int Index)>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            numsPriorityQueue.Enqueue((nums[i], i), (nums[i], i));
+        }
+
+        for (var i = 0; i < k; i++)
+        {
+            var num = numsPriorityQueue.Dequeue();
+
+            num.Value *= multiplier;
+
+            numsPriorityQueue.Enqueue((num.Value, num.Index), (num.Value, num.Index));
+        }
+
+        while (numsPriorityQueue.Count > 0)
+        {
+            var num = numsPriorityQueue.Dequeue();
+
+            nums[num.Index] = num.Value;
+        }
+
+        return nums;
+    }
+}
