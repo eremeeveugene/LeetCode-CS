9bed79845ab5afe1d90c9bb5b5b92a3c37a737b0
eugene <eremeeveugene@gmail.com>
2024-12-17 18:45:30 +0200

    Construct String With Repeat Limit (#985)

---
+++ source/LeetCode/Algorithms/ConstructStringWithRepeatLimit/ConstructStringWithRepeatLimitGreedy.cs
@@ -0,0 +1,81 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Text;
+
+namespace LeetCode.Algorithms.ConstructStringWithRepeatLimit;
+
+/// <inheritdoc />
+public class ConstructStringWithRepeatLimitGreedy : IConstructStringWithRepeatLimit
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="repeatLimit"></param>
+    /// <returns></returns>
+    public string RepeatLimitedString(string s, int repeatLimit)
+    {
+        var countArray = new int['z' - 'a' + 1];
+
+        foreach (var c in s)
+        {
+            countArray[c - 'a']++;
+        }
+
+        var resultStringBuilder = new StringBuilder();
+
+        var character = 'z' - 'a';
+
+        while (character >= 0)
+        {
+            if (countArray[character] == 0)
+            {
+                character--;
+
+                continue;
+            }
+
+            var count = Math.Min(countArray[character], repeatLimit);
+
+            countArray[character] -= count;
+
+            for (var i = 0; i < count; i++)
+            {
+                resultStringBuilder.Append((char)('a' + character));
+            }
+
+            if (count < repeatLimit || countArray[character] == 0)
+            {
+                continue;
+            }
+
+            var nextCharacter = character - 1;
+
+            while (nextCharacter >= 0 && countArray[nextCharacter] == 0)
+            {
+                nextCharacter--;
+            }
+
+            if (nextCharacter < 0)
+            {
+                break;
+            }
+
+            resultStringBuilder.Append((char)('a' + nextCharacter));
+
+            countArray[nextCharacter]--;
+        }
+
+        return resultStringBuilder.ToString();
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ConstructStringWithRepeatLimit/ConstructStringWithRepeatLimitTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ConstructStringWithRepeatLimit;
+
+namespace LeetCode.Tests.Algorithms.ConstructStringWithRepeatLimit;
+
+public abstract class ConstructStringWithRepeatLimitTestsBase<T> where T : IConstructStringWithRepeatLimit, new()
+{
+    [TestMethod]
+    [DataRow("cczazcc", 3, "zzcccac")]
+    [DataRow("aababab", 2, "bbabaa")]
+    public void RepeatLimitedString_WithInputStringAndRepeatLimit_ReturnsLexicographicallyLargestString(string s,
+        int repeatLimit, string expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.RepeatLimitedString(s, repeatLimit);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
