92baf41dfffbfe7b7d497a0723bb1ee0a0aba6df
eugene <eremeeveugene@gmail.com>
2024-12-17 18:42:42 +0200

    Construct String With Repeat Limit

---
+++ source/LeetCode/Algorithms/ConstructStringWithRepeatLimit/ConstructStringWithRepeatLimitPriorityQueue.cs
@@ -0,0 +1,80 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Text;
+
+namespace LeetCode.Algorithms.ConstructStringWithRepeatLimit;
+
+/// <inheritdoc />
+public class ConstructStringWithRepeatLimitPriorityQueue : IConstructStringWithRepeatLimit
+{
+    /// <summary>
+    ///     Time complexity - O(n log k)
+    ///     Space complexity - O(k)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="repeatLimit"></param>
+    /// <returns></returns>
+    public string RepeatLimitedString(string s, int repeatLimit)
+    {
+        var frequencyDictionary = new Dictionary<char, int>();
+
+        foreach (var character in s.Where(character => !frequencyDictionary.TryAdd(character, 1)))
+        {
+            frequencyDictionary[character]++;
+        }
+
+        var frequencyPriorityQueue = new PriorityQueue<char, char>();
+
+        foreach (var character in frequencyDictionary.Keys)
+        {
+            frequencyPriorityQueue.Enqueue(character, (char)('z' - character));
+        }
+
+        var resultStringBuilder = new StringBuilder();
+
+        while (frequencyPriorityQueue.Count > 0)
+        {
+            var character = frequencyPriorityQueue.Dequeue();
+
+            var count = frequencyDictionary[character];
+
+            var charactersCount = Math.Min(count, repeatLimit);
+
+            for (var i = 0; i < charactersCount; i++)
+            {
+                resultStringBuilder.Append(character);
+            }
+
+            frequencyDictionary[character] -= charactersCount;
+
+            if (frequencyDictionary[character] <= 0 || frequencyPriorityQueue.Count <= 0)
+            {
+                continue;
+            }
+
+            var nextCharacter = frequencyPriorityQueue.Dequeue();
+
+            resultStringBuilder.Append(nextCharacter);
+
+            frequencyDictionary[nextCharacter]--;
+
+            if (frequencyDictionary[nextCharacter] > 0)
+            {
+                frequencyPriorityQueue.Enqueue(nextCharacter, (char)('z' - nextCharacter));
+            }
+
+            frequencyPriorityQueue.Enqueue(character, (char)('z' - character));
+        }
+
+        return resultStringBuilder.ToString();
+    }
+}
