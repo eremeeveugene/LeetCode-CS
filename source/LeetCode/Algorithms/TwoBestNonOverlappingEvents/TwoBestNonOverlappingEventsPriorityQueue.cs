87f1b184dbf19a08e8f5528bc8723af06a2cca2e
eugene <eremeeveugene@gmail.com>
2024-12-18 17:38:08 +0200

    Two Best Non-Overlapping Events (#988)

---
+++ source/LeetCode/Algorithms/TwoBestNonOverlappingEvents/TwoBestNonOverlappingEventsPriorityQueue.cs
@@ -0,0 +1,52 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.TwoBestNonOverlappingEvents;
+
+/// <inheritdoc />
+public class TwoBestNonOverlappingEventsPriorityQueue : ITwoBestNonOverlappingEvents
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="events"></param>
+    /// <returns></returns>
+    public int MaxTwoEvents(int[][] events)
+    {
+        Array.Sort(events, (a, b) => a[0] - b[0]);
+
+        var completedEventsPriorityQueue = new PriorityQueue<(int EndTime, int Value), int>();
+
+        var maxValueBeforeCurrent = 0;
+        var maxSum = 0;
+
+        foreach (var @event in events)
+        {
+            var startTime = @event[0];
+            var endTime = @event[1];
+            var value = @event[2];
+
+            while (completedEventsPriorityQueue.Count > 0 && completedEventsPriorityQueue.Peek().EndTime < startTime)
+            {
+                var completedEvent = completedEventsPriorityQueue.Dequeue();
+
+                maxValueBeforeCurrent = Math.Max(maxValueBeforeCurrent, completedEvent.Value);
+            }
+
+            maxSum = Math.Max(maxSum, maxValueBeforeCurrent + value);
+
+            completedEventsPriorityQueue.Enqueue((endTime, value), endTime);
+        }
+
+        return maxSum;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/TwoBestNonOverlappingEvents/TwoBestNonOverlappingEventsTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.TwoBestNonOverlappingEvents;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.TwoBestNonOverlappingEvents;
+
+public abstract class TwoBestNonOverlappingEventsTestsBase<T> where T : ITwoBestNonOverlappingEvents, new()
+{
+    [TestMethod]
+    [DataRow("[[1,3,2],[4,5,2],[2,4,3]]", 4)]
+    [DataRow("[[1,3,2],[4,5,2],[1,5,5]]", 5)]
+    [DataRow("[[1,5,3],[1,5,1],[6,6,5]]", 8)]
+    public void MaxTwoEvents_WithJaggedArrayOfEvents_ReturnsMaxSumOfTwoNonOverlappingEvents(string eventsJsonArray,
+        int expectedResult)
+    {
+        // Arrange
+        var events = JsonHelper<int>.DeserializeToJaggedArray(eventsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaxTwoEvents(events);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
