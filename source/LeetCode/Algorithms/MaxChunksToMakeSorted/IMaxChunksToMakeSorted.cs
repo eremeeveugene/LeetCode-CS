444eb5c2e269fbc49fa83d1c12d0a10838eedc02
eugene <eremeeveugene@gmail.com>
2024-12-20 14:47:57 +0200

    Max Chunks To Make Sorted (#990)

---
+++ source/LeetCode/Algorithms/MaxChunksToMakeSorted/IMaxChunksToMakeSorted.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaxChunksToMakeSorted;
+
+/// <summary>
+///     https://leetcode.com/problems/max-chunks-to-make-sorted/description/
+/// </summary>
+public interface IMaxChunksToMakeSorted
+{
+    int MaxChunksToSorted(int[] arr);
+}
---
+++ source/LeetCode/Algorithms/MaxChunksToMakeSorted/MaxChunksToMakeSortedIterative.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaxChunksToMakeSorted;
+
+/// <inheritdoc />
+public class MaxChunksToMakeSortedIterative : IMaxChunksToMakeSorted
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <returns></returns>
+    public int MaxChunksToSorted(int[] arr)
+    {
+        var max = 0;
+        var chunks = 0;
+
+        for (var i = 0; i < arr.Length; i++)
+        {
+            max = Math.Max(max, arr[i]);
+
+            if (max == i)
+            {
+                chunks++;
+            }
+        }
+
+        return chunks;
+    }
+}
