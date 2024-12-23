cc8e277018ea43f17917a562124e84455b7ba7f1
eugene <eremeeveugene@gmail.com>
2024-12-23 19:20:47 +0200

    Minimum Number of Operations to Sort a Binary Tree by Level

---
+++ source/LeetCode/Algorithms/MinimumNumberOfOperationsToSortBinaryTreeByLevel/MinimumNumberOfOperationsToSortBinaryTreeByLevelQueue.cs
@@ -0,0 +1,93 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.MinimumNumberOfOperationsToSortBinaryTreeByLevel;
+
+/// <inheritdoc />
+public class MinimumNumberOfOperationsToSortBinaryTreeByLevelQueue : IMinimumNumberOfOperationsToSortBinaryTreeByLevel
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="root"></param>
+    /// <returns></returns>
+    public int MinimumOperations(TreeNode root)
+    {
+        var result = 0;
+
+        var treeNodesQueue = new Queue<TreeNode>();
+
+        if (root.left != null)
+        {
+            treeNodesQueue.Enqueue(root.left);
+        }
+
+        if (root.right != null)
+        {
+            treeNodesQueue.Enqueue(root.right);
+        }
+
+        while (treeNodesQueue.Count > 0)
+        {
+            var values = new int[treeNodesQueue.Count];
+
+            for (var i = 0; i < values.Length; i++)
+            {
+                var node = treeNodesQueue.Dequeue();
+
+                values[i] = node.val;
+
+                if (node.left != null)
+                {
+                    treeNodesQueue.Enqueue(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    treeNodesQueue.Enqueue(node.right);
+                }
+            }
+
+            var indexDictionary = values
+                .Select((value, index) => new { Value = value, Index = index })
+                .OrderBy(x => x.Value)
+                .Select((x, sortedIndex) => new { x.Index, SortedIndex = sortedIndex })
+                .ToDictionary(x => x.Index, x => x.SortedIndex);
+
+            var visited = new bool[values.Length];
+
+            for (var i = 0; i < values.Length; i++)
+            {
+                if (visited[i] || indexDictionary[i] == i)
+                {
+                    continue;
+                }
+
+                var cycleLength = 0;
+                var current = i;
+
+                while (!visited[current])
+                {
+                    visited[current] = true;
+                    current = indexDictionary[current];
+                    cycleLength++;
+                }
+
+                result += cycleLength - 1;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumNumberOfOperationsToSortBinaryTreeByLevel/MinimumNumberOfOperationsToSortBinaryTreeByLevelTestsBase.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumNumberOfOperationsToSortBinaryTreeByLevel;
+using LeetCode.Core.Exceptions;
+using LeetCode.Core.Helpers;
+using LeetCode.Core.Models;
+
+namespace LeetCode.Tests.Algorithms.MinimumNumberOfOperationsToSortBinaryTreeByLevel;
+
+public abstract class MinimumNumberOfOperationsToSortBinaryTreeByLevelTestsBase<T>
+    where T : IMinimumNumberOfOperationsToSortBinaryTreeByLevel, new()
+{
+    [TestMethod]
+    [DataRow("[1,2,3,4,5,6]", 0)]
+    [DataRow("[1,3,2,7,6,5,4]", 3)]
+    [DataRow("[1,4,3,7,6,8,5,null,null,null,null,9,null,10]", 3)]
+    public void MinimumOperations_WithBinaryTreeInput_ReturnsMinOperationsToSortLevelOrders(string rootJsonArray,
+        int expectedResult)
+    {
+        // Arrange
+        var rootArray = JsonHelper<int?>.DeserializeToArray(rootJsonArray);
+        var root = TreeNode.ToTreeNode(rootArray) ?? throw new TreeNodeBuildException();
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinimumOperations(root);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
