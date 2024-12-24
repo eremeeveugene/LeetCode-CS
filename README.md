e6aed28fcf110669f4e697caf7a38b7ad5cd42dc
eugene <eremeeveugene@gmail.com>
2024-12-24 16:36:47 +0200

    Find Minimum Diameter After Merging Two Trees (#1001)

--- README.md
+++ README.md
@@ -489,6 +489,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [3152. Special Array II](https://leetcode.com/problems/special-array-ii/description/)
 - [3163. String Compression III](https://leetcode.com/problems/string-compression-iii/description/)
 - [3190. Find Minimum Operations to Make All Elements Divisible by Three](https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three/description/)
+- [3203. Find Minimum Diameter After Merging Two Trees](https://leetcode.com/problems/find-minimum-diameter-after-merging-two-trees/description/)
 - [3217. Delete Nodes From Linked List Present in Array](https://leetcode.com/problems/delete-nodes-from-linked-list-present-in-array/description/)
 - [3254. Find the Power of K-Size Subarrays I](https://leetcode.com/problems/find-the-power-of-k-size-subarrays-i/description/)
 - [3264. Final Array State After K Multiplication Operations I](https://leetcode.com/problems/final-array-state-after-k-multiplication-operations-i/description/)
---
+++ source/LeetCode/Algorithms/FindMinimumDiameterAfterMergingTwoTrees/IFindMinimumDiameterAfterMergingTwoTrees.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindMinimumDiameterAfterMergingTwoTrees;
+
+/// <summary>
+///     https://leetcode.com/problems/find-minimum-diameter-after-merging-two-trees/description/
+/// </summary>
+public interface IFindMinimumDiameterAfterMergingTwoTrees
+{
+    int MinimumDiameterAfterMerge(int[][] edges1, int[][] edges2);
+}
