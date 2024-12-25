91327c2aace9037f8b33f9ecb31303459e501422
eugene <eremeeveugene@gmail.com>
2024-12-25 12:31:58 +0200

    Find Largest Value in Each Tree Ro

---
+++ source/LeetCode/Algorithms/FindLargestValueInEachTreeRow/IFindLargestValueInEachTreeRow.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.FindLargestValueInEachTreeRow;
+
+/// <summary>
+///     https://leetcode.com/problems/find-largest-value-in-each-tree-row/description/
+/// </summary>
+public interface IFindLargestValueInEachTreeRow
+{
+    IList<int> LargestValues(TreeNode? root);
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FindLargestValueInEachTreeRow/FindLargestValueInEachTreeRowTestsBase.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindLargestValueInEachTreeRow;
+using LeetCode.Core.Helpers;
+using LeetCode.Core.Models;
+
+namespace LeetCode.Tests.Algorithms.FindLargestValueInEachTreeRow;
+
+public abstract class FindLargestValueInEachTreeRowTestsBase<T> where T : IFindLargestValueInEachTreeRow, new()
+{
+    [TestMethod]
+    [DataRow("[]", "[]")]
+    [DataRow("[1,2,3]", "[1,3]")]
+    [DataRow("[1,3,2,5,3,null,9]", "[1,3,9]")]
+    public void LargestValues_WithTreeRoot_ReturnsListOfRowMaxima(string rootJsonArray, string expectedResultJsonArray)
+    {
+        // Arrange
+        var rootArray = JsonHelper<int?>.DeserializeToArray(rootJsonArray);
+        var root = TreeNode.ToTreeNode(rootArray);
+        var expectedResult = JsonHelper<int>.DeserializeToArray(expectedResultJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.LargestValues(root);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult.ToArray());
+    }
+}
