df52430cb327fe8b573a7c669cfaeb74d4a93eeb
eugene <eremeeveugene@gmail.com>
2024-12-27 14:02:23 +0200

    Best Sightseeing Pair

---
+++ source/LeetCode/Algorithms/BestSightseeingPair/BestSightseeingPairGreedy.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.BestSightseeingPair;
+
+/// <inheritdoc />
+public class BestSightseeingPairGreedy : IBestSightseeingPair
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="values"></param>
+    /// <returns></returns>
+    public int MaxScoreSightseeingPair(int[] values)
+    {
+        var maxScore = 0;
+        var maxSeen = values[0] + 0;
+
+        for (var j = 1; j < values.Length; j++)
+        {
+            maxScore = Math.Max(maxScore, maxSeen + values[j] - j);
+
+            maxSeen = Math.Max(maxSeen, values[j] + j);
+        }
+
+        return maxScore;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/BestSightseeingPair/BestSightseeingPairTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.BestSightseeingPair;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.BestSightseeingPair;
+
+public abstract class BestSightseeingPairTestsBase<T> where T : IBestSightseeingPair, new()
+{
+    [TestMethod]
+    [DataRow("[1,2]", 2)]
+    [DataRow("[8,1,5,2,6]", 11)]
+    public void MaxScoreSightseeingPair_WithValuesArray_ReturnsMaxScore(string valuesJsonArray, int expectedResult)
+    {
+        // Arrange
+        var values = JsonHelper<int>.DeserializeToArray(valuesJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaxScoreSightseeingPair(values);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
