283636435b3512622ebcf29563bf770e5dfdfe38
eugene <eremeeveugene@gmail.com>
2024-12-30 13:20:18 +0200

    Minimum Number of Operations to Make Elements in Array Distinct

---
+++ source/LeetCode/Algorithms/MinimumNumberOfOperationsToMakeElementsInArrayDistinct/MinimumNumberOfOperationsToMakeElementsInArrayDistinctIterative.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumNumberOfOperationsToMakeElementsInArrayDistinct;
+
+/// <inheritdoc />
+public class MinimumNumberOfOperationsToMakeElementsInArrayDistinctIterative :
+    IMinimumNumberOfOperationsToMakeElementsInArrayDistinct
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MinimumOperations(int[] nums)
+    {
+        var frequencyArray = new int[101];
+
+        for (var i = nums.Length - 1; i >= 0; i--)
+        {
+            frequencyArray[nums[i]]++;
+
+            if (frequencyArray[nums[i]] > 1)
+            {
+                return (i + 3) / 3;
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumNumberOfOperationsToMakeElementsInArrayDistinct/MinimumNumberOfOperationsToMakeElementsInArrayDistinctTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumNumberOfOperationsToMakeElementsInArrayDistinct;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MinimumNumberOfOperationsToMakeElementsInArrayDistinct;
+
+public abstract class MinimumNumberOfOperationsToMakeElementsInArrayDistinctTestsBase<T>
+    where T : IMinimumNumberOfOperationsToMakeElementsInArrayDistinct, new()
+{
+    [TestMethod]
+    [DataRow("[1,2,3,4,2,3,3,5,7]", 2)]
+    [DataRow("[4,5,6,4,4]", 2)]
+    [DataRow("[6,7,8,9]", 0)]
+    public void MinimumOperations_GivenArrayOfNumbers_ReturnsMinimumOperationsCount(string numsJsonArray,
+        int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinimumOperations(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
