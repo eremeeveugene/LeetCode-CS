ae7f1e3f299b97ad8b4bac48a78ff069cf73eaaa
eugene <eremeeveugene@gmail.com>
2024-12-31 13:17:56 +0200

    Check Balanced String (#1017)

---
+++ source/LeetCode/Algorithms/CheckBalancedString/CheckBalancedStringIterative.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CheckBalancedString;
+
+/// <inheritdoc />
+public class CheckBalancedStringIterative : ICheckBalancedString
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+    public bool IsBalanced(string num)
+    {
+        var sum = 0;
+
+        for (var i = 0; i < num.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                sum += num[i] - '0';
+            }
+            else
+            {
+                sum -= num[i] - '0';
+            }
+        }
+
+        return sum == 0;
+    }
+}
---
+++ source/LeetCode/Algorithms/CheckBalancedString/ICheckBalancedString.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CheckBalancedString;
+
+/// <summary>
+///     https://leetcode.com/problems/check-balanced-string/description/
+/// </summary>
+public interface ICheckBalancedString
+{
+    bool IsBalanced(string num);
+}
