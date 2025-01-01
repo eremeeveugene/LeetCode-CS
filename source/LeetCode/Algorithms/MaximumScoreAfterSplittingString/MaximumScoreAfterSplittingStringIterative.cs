563a2a01bf9e544b09a96180a197758aec025ed0
eugene <eremeeveugene@gmail.com>
2025-01-01 15:35:45 +0200

    Maximum Score After Splitting a String

---
+++ source/LeetCode/Algorithms/MaximumScoreAfterSplittingString/MaximumScoreAfterSplittingStringIterative.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumScoreAfterSplittingString;
+
+/// <inheritdoc />
+public class MaximumScoreAfterSplittingStringIterative : IMaximumScoreAfterSplittingString
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public int MaxScore(string s)
+    {
+        var zeros = 0;
+
+        if (s[0] == '0')
+        {
+            zeros++;
+        }
+
+        var ones = 0;
+
+        for (var i = 1; i < s.Length; i++)
+        {
+            if (s[i] == '1')
+            {
+                ones++;
+            }
+        }
+
+        var maxScore = zeros + ones;
+
+        for (var i = 1; i < s.Length; i++)
+        {
+            maxScore = Math.Max(maxScore, zeros + ones);
+
+            if (s[i] == '0')
+            {
+                zeros++;
+            }
+            else
+            {
+                ones--;
+            }
+        }
+
+        return maxScore;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MaximumScoreAfterSplittingString/MaximumScoreAfterSplittingStringTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaximumScoreAfterSplittingString;
+
+namespace LeetCode.Tests.Algorithms.MaximumScoreAfterSplittingString;
+
+public abstract class MaximumScoreAfterSplittingStringTestsBase<T> where T : IMaximumScoreAfterSplittingString, new()
+{
+    [TestMethod]
+    [DataRow("1111", 3)]
+    [DataRow("011101", 5)]
+    [DataRow("00111", 5)]
+    public void MaxScore_WithBinaryString_ReturnsMaximumScore(string s, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaxScore(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
