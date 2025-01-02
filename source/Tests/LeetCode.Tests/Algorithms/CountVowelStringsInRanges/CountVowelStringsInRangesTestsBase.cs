30b498c83974ec7cd16d1b18d9481c317413810a
eugene <eremeeveugene@gmail.com>
2025-01-02 13:28:57 +0200

    Count Vowel Strings in Ranges (#1021)

---
+++ source/Tests/LeetCode.Tests/Algorithms/CountVowelStringsInRanges/CountVowelStringsInRangesTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountVowelStringsInRanges;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.CountVowelStringsInRanges;
+
+public abstract class CountVowelStringsInRangesTestsBase<T> where T : ICountVowelStringsInRanges, new()
+{
+    [TestMethod]
+    [DataRow("[\"aba\",\"bcb\",\"ece\",\"aa\",\"e\"]", "[[0,2],[1,4],[1,1]]", "[2,3,0]")]
+    [DataRow("[\"a\",\"e\",\"i\"]", "[[0,2],[0,1],[2,2]]", "[3,2,1]")]
+    public void VowelStrings_WithWordsAndQueries_ModifiesWordsAccordingToQueries(string wordsJsonArray,
+        string queriesJsonArray, string expectedResultJsonArray)
+    {
+        // Arrange
+        var words = JsonHelper<string>.DeserializeToArray(wordsJsonArray);
+        var queries = JsonHelper<int>.DeserializeToJaggedArray(queriesJsonArray);
+        var expectedResult = JsonHelper<int>.DeserializeToArray(expectedResultJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.VowelStrings(words, queries);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
