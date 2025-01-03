15997c1163c130118cc87667ffeb4ba01b562e01
eugene <eremeeveugene@gmail.com>
2025-01-03 10:36:19 +0200

    Number of Ways to Split Array (#1023)

--- README.md
+++ README.md
@@ -413,6 +413,8 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [2220. Minimum Bit Flips to Convert Number](https://leetcode.com/problems/minimum-bit-flips-to-convert-number/description/)
 - [2235. Add Two Integers](https://leetcode.com/problems/add-two-integers/description/)
 - [2236. Root Equals Sum of Children](https://leetcode.com/problems/root-equals-sum-of-children/description/)
+- [2270. Number of Ways to Split Array](https://leetcode.com/problems/number-of-ways-to-split-array/description/)
+- [2275. Largest Combination With Bitwise AND Greater Than Zero](https://leetcode.com/problems/largest-combination-with-bitwise-and-greater-than-zero/description/)
 - [2275. Largest Combination With Bitwise AND Greater Than Zero](https://leetcode.com/problems/largest-combination-with-bitwise-and-greater-than-zero/description/)
 - [2288. Apply Discount to Prices](https://leetcode.com/problems/apply-discount-to-prices/description/)
 - [2326. Spiral Matrix IV](https://leetcode.com/problems/spiral-matrix-iv/description/)
---
+++ source/LeetCode/Algorithms/NumberOfWaysToSplitArray/NumberOfWaysToSplitArrayPrefixSum.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.NumberOfWaysToSplitArray;
+
+/// <inheritdoc />
+public class NumberOfWaysToSplitArrayPrefixSum : INumberOfWaysToSplitArray
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int WaysToSplitArray(int[] nums)
+    {
+        long leftSum = nums[0];
+        long rightSum = 0;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            rightSum += nums[i];
+        }
+
+        var result = 0;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (leftSum >= rightSum)
+            {
+                result++;
+            }
+
+            leftSum += nums[i];
+            rightSum -= nums[i];
+        }
+
+        return result;
+    }
+}
