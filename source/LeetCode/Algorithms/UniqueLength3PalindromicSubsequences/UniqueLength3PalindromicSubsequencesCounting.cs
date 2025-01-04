19d5de4a38314b8e7c9f12e78613bd9c118281b9
eugene <eremeeveugene@gmail.com>
2025-01-04 12:59:38 +0200

    Unique Length-3 Palindromic Subsequences

---
+++ source/LeetCode/Algorithms/UniqueLength3PalindromicSubsequences/UniqueLength3PalindromicSubsequencesCounting.cs
@@ -0,0 +1,82 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Text;
+
+namespace LeetCode.Algorithms.UniqueLength3PalindromicSubsequences;
+
+/// <inheritdoc />
+public class UniqueLength3PalindromicSubsequencesCounting : IUniqueLength3PalindromicSubsequences
+{
+    private const int AlphabetLength = 'z' - 'a' + 1;
+
+    /// <summary>
+    ///     Time complexity - O(n * 26)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public int CountPalindromicSubsequence(string s)
+    {
+        var firstOccurrence = new int[AlphabetLength];
+        var lastOccurrence = new int[AlphabetLength];
+
+        Array.Fill(firstOccurrence, -1);
+        Array.Fill(lastOccurrence, -1);
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            var index = s[i] - 'a';
+
+            if (firstOccurrence[index] == -1)
+            {
+                firstOccurrence[index] = i;
+            }
+
+            lastOccurrence[index] = i;
+        }
+
+        var palindromesHashSet = new HashSet<string>();
+
+        for (var i = 0; i < AlphabetLength; i++)
+        {
+            if (firstOccurrence[i] == -1 || firstOccurrence[i] >= lastOccurrence[i])
+            {
+                continue;
+            }
+
+            var start = firstOccurrence[i];
+            var end = lastOccurrence[i];
+
+            var middleCharsHashSet = new HashSet<char>();
+
+            for (var j = start + 1; j < end; j++)
+            {
+                middleCharsHashSet.Add(s[j]);
+            }
+
+            foreach (var middleChar in middleCharsHashSet)
+            {
+                var palindromeStringBuilder = new StringBuilder();
+
+                palindromeStringBuilder.Append(i + 'a');
+                palindromeStringBuilder.Append(middleChar);
+                palindromeStringBuilder.Append(i + 'a');
+
+                var palindrome = palindromeStringBuilder.ToString();
+
+                palindromesHashSet.Add(palindrome);
+            }
+        }
+
+        return palindromesHashSet.Count;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/UniqueLength3PalindromicSubsequences/UniqueLength3PalindromicSubsequencesTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.UniqueLength3PalindromicSubsequences;
+
+namespace LeetCode.Tests.Algorithms.UniqueLength3PalindromicSubsequences;
+
+public abstract class UniqueLength3PalindromicSubsequencesTestsBase<T>
+    where T : IUniqueLength3PalindromicSubsequences, new()
+{
+    [TestMethod]
+    [DataRow("adc", 0)]
+    [DataRow("aabca", 3)]
+    [DataRow("bbcbaba", 4)]
+    public void CountPalindromicSubsequence_WithStringInput_ReturnsNumberOfUniquePalindromes(string s,
+        int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountPalindromicSubsequence(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
