b5caccd83cb4c6bb65ef69cb43a8411dce70d833
eugene <eremeeveugene@gmail.com>
2025-01-05 15:29:21 +0200

    Shifting Letters II

---
+++ source/LeetCode/Algorithms/ShiftingLetters2/IShiftingLetters2.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ShiftingLetters2;
+
+/// <summary>
+///     https://leetcode.com/problems/shifting-letters-ii/description/
+/// </summary>
+public interface IShiftingLetters2
+{
+    string ShiftingLetters(string s, int[][] shifts);
+}
