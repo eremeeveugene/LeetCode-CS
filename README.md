daeb850b5cb64643c70964a3889a53b9c919418d
eugene <eremeeveugene@gmail.com>
2025-01-08 13:59:44 +0200

    Transformed Array (#1031)

--- README.md
+++ README.md
@@ -508,6 +508,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [3254. Find the Power of K-Size Subarrays I](https://leetcode.com/problems/find-the-power-of-k-size-subarrays-i/description/)
 - [3264. Final Array State After K Multiplication Operations I](https://leetcode.com/problems/final-array-state-after-k-multiplication-operations-i/description/)
 - [3340. Check Balanced String](https://leetcode.com/problems/check-balanced-string/description/)
+- [3379. Transformed Array](https://leetcode.com/problems/transformed-array/description/)
 - [3386. Button with Longest Push Time](https://leetcode.com/problems/button-with-longest-push-time/description/)
 - [3396. Minimum Number of Operations to Make Elements in Array Distinct](https://leetcode.com/problems/minimum-number-of-operations-to-make-elements-in-array-distinct/description/)
 
---
+++ source/Tests/LeetCode.Tests/Algorithms/TransformedArray/TransformedArrayTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.TransformedArray;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.TransformedArray;
+
+public abstract class TransformedArrayTestsBase<T> where T : ITransformedArray, new()
+{
+    [TestMethod]
+    [DataRow("[3,-2,1,1]", "[1,1,1,3]")]
+    [DataRow("[-1,4,-1]", "[-1,-1,4]")]
+    [DataRow("[-10]", "[-10]")]
+    public void ConstructTransformedArray_WithInputArray_ReturnsTransformedArray(string numsJsonArray,
+        string expectedResultJsonArray)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+        var expectedResult = JsonHelper<int>.DeserializeToArray(expectedResultJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.ConstructTransformedArray(nums);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
