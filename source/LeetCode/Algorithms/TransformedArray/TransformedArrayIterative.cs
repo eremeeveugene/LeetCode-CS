03c368e505a9f1be20d89f99276582fee15a2fbd
eugene <eremeeveugene@gmail.com>
2025-01-08 13:56:42 +0200

    Transformed Array

---
+++ source/LeetCode/Algorithms/TransformedArray/TransformedArrayIterative.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.TransformedArray;
+
+/// <inheritdoc />
+public class TransformedArrayIterative : ITransformedArray
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int[] ConstructTransformedArray(int[] nums)
+    {
+        var result = new int[nums.Length];
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            result[i] = nums[(((i + nums[i]) % nums.Length) + nums.Length) % nums.Length];
+        }
+
+        return result;
+    }
+}
