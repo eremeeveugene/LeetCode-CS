ef4d0553bf00bde57d8d13e2403355db44de293e
eugene <eremeeveugene@gmail.com>
2025-01-09 12:15:40 +0200

    Counting Words With a Given Prefix (#1033)

---
+++ source/LeetCode/Algorithms/CountingWordsWithGivenPrefix/CountingWordsWithGivenPrefixIterative.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountingWordsWithGivenPrefix;
+
+/// <inheritdoc />
+public class CountingWordsWithGivenPrefixIterative : ICountingWordsWithGivenPrefix
+{
+    public int PrefixCount(string[] words, string pref)
+    {
+        return words.Count(word => word.StartsWith(pref));
+    }
+}
