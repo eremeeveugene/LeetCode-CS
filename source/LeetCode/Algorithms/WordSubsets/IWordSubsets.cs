3f1122275cdfa3f895730599af722de7ae861ce1
eugene <eremeeveugene@gmail.com>
2025-01-10 18:58:35 +0200

    Word Subsets (#1035)

---
+++ source/LeetCode/Algorithms/WordSubsets/IWordSubsets.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.WordSubsets;
+
+/// <summary>
+///     https://leetcode.com/problems/word-subsets/description/
+/// </summary>
+public interface IWordSubsets
+{
+    IList<string> WordSubsets(string[] words1, string[] words2);
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/WordSubsets/WordSubsetsTestsBase.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.WordSubsets;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.WordSubsets;
+
+public abstract class WordSubsetsTestsBase<T> where T : IWordSubsets, new()
+{
+    [TestMethod]
+    [DataRow("[\"amazon\",\"apple\",\"facebook\",\"google\",\"leetcode\"]", "[\"e\",\"o\"]",
+        "[\"facebook\",\"google\",\"leetcode\"]")]
+    [DataRow("[\"amazon\",\"apple\",\"facebook\",\"google\",\"leetcode\"]", "[\"l\",\"e\"]",
+        "[\"apple\",\"google\",\"leetcode\"]")]
+    public void WordSubsets_WithWords1AndWords2_ReturnsMatchingSubset(string words1JsonArray, string words2JsonArray,
+        string expectedResultJsonArray)
+    {
+        // Arrange
+        var words1 = JsonHelper<string>.DeserializeToArray(words1JsonArray);
+        var words2 = JsonHelper<string>.DeserializeToArray(words2JsonArray);
+        var expectedResult = JsonHelper<string>.DeserializeToArray(expectedResultJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.WordSubsets(words1, words2);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult.ToArray());
+    }
+}
