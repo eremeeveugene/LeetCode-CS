e23704841d80b219dd7cc02410c86c672ba5d66e
eugene <eremeeveugene@gmail.com>
2025-01-11 14:18:43 +0200

    Construct K Palindrome Strings

---
+++ source/LeetCode/Algorithms/ConstructKPalindromeStrings/ConstructKPalindromeStringsFrequencyArray.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ConstructKPalindromeStrings;
+
+/// <inheritdoc />
+public class ConstructKPalindromeStringsFrequencyArray : IConstructKPalindromeStrings
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public bool CanConstruct(string s, int k)
+    {
+        if (s.Length < k)
+        {
+            return false;
+        }
+
+        var frequencyArray = new int['z' - 'a' + 1];
+
+        foreach (var c in s)
+        {
+            frequencyArray[c - 'a']++;
+        }
+
+        return frequencyArray.Count(frequency => frequency % 2 == 1) <= k;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ConstructKPalindromeStrings/ConstructKPalindromeStringsTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ConstructKPalindromeStrings;
+
+namespace LeetCode.Tests.Algorithms.ConstructKPalindromeStrings;
+
+public abstract class ConstructKPalindromeStringsTestsBase<T> where T : IConstructKPalindromeStrings, new()
+{
+    [TestMethod]
+    [DataRow("annabelle", 2, true)]
+    [DataRow("leetcode", 3, false)]
+    [DataRow("true", 4, true)]
+    public void CanConstruct_GivenStringAndK_ReturnsIfKPalindromesCanBeFormed(string s, int k, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CanConstruct(s, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
