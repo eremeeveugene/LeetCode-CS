edde8c00af0ec6ce15eeefc1d658eb016ea0ea0c
eugene <eremeeveugene@gmail.com>
2025-01-14 13:42:47 +0200

    Find the Prefix Common Array of Two Arrays

---
+++ source/LeetCode/Algorithms/FindThePrefixCommonArrayOfTwoArrays/FindThePrefixCommonArrayOfTwoArraysFrequencyArray.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindThePrefixCommonArrayOfTwoArrays;
+
+/// <inheritdoc />
+public class FindThePrefixCommonArrayOfTwoArraysFrequencyArray : IFindThePrefixCommonArrayOfTwoArrays
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public int[] FindThePrefixCommonArray(int[] a, int[] b)
+    {
+        var result = new int[a.Length];
+        var frequencyArray = new int[a.Length];
+        var count = 0;
+
+        for (var i = 0; i < a.Length; i++)
+        {
+            frequencyArray[a[i] - 1]++;
+
+            if (frequencyArray[a[i] - 1] == 2)
+            {
+                count++;
+            }
+
+            frequencyArray[b[i] - 1]++;
+
+            if (frequencyArray[b[i] - 1] == 2)
+            {
+                count++;
+            }
+
+            result[i] = count;
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FindThePrefixCommonArrayOfTwoArrays/FindThePrefixCommonArrayOfTwoArraysTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindThePrefixCommonArrayOfTwoArrays;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.FindThePrefixCommonArrayOfTwoArrays;
+
+public abstract class FindThePrefixCommonArrayOfTwoArraysTestsBase<T>
+    where T : IFindThePrefixCommonArrayOfTwoArrays, new()
+{
+    [TestMethod]
+    [DataRow("[2,3,1]", "[3,1,2]", "[0,1,3]")]
+    [DataRow("[1,3,2,4]", "[3,1,2,4]", "[0,2,3,4]")]
+    public void Test(string aJsonArray, string bJsonArray, string expectedResultJsonArray)
+    {
+        // Arrange
+        var a = JsonHelper<int>.DeserializeToArray(aJsonArray);
+        var b = JsonHelper<int>.DeserializeToArray(bJsonArray);
+        var expectedResult = JsonHelper<int>.DeserializeToArray(expectedResultJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FindThePrefixCommonArray(a, b);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
