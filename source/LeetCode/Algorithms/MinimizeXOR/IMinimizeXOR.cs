b08885537592a1fd16fbca14e11c32dd678136b9
eugene <eremeeveugene@gmail.com>
2025-01-15 13:20:11 +0200

    Minimize XOR (#1045)

---
+++ source/LeetCode/Algorithms/MinimizeXOR/IMinimizeXOR.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimizeXOR;
+
+/// <summary>
+///     https://leetcode.com/problems/minimize-xor/description/
+/// </summary>
+public interface IMinimizeXOR
+{
+    int MinimizeXor(int num1, int num2);
+}
