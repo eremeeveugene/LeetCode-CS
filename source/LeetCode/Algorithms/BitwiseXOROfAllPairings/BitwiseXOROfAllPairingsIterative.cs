6ac7111e684c4df2325f4f6b610a77b9e80c2e8a
eugene <eremeeveugene@gmail.com>
2025-01-16 18:28:48 +0200

    Bitwise XOR of All Pairings (#1047)

---
+++ source/LeetCode/Algorithms/BitwiseXOROfAllPairings/BitwiseXOROfAllPairingsIterative.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.BitwiseXOROfAllPairings;
+
+/// <inheritdoc />
+public class BitwiseXOROfAllPairingsIterative : IBitwiseXOROfAllPairings
+{
+    /// <summary>
+    ///     Time complexity - O(n + m)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums1"></param>
+    /// <param name="nums2"></param>
+    /// <returns></returns>
+    public int XorAllNums(int[] nums1, int[] nums2)
+    {
+        var result = 0;
+
+        if (nums2.Length % 2 == 1)
+        {
+            foreach (var num1 in nums1)
+            {
+                result ^= num1;
+            }
+        }
+
+        if (nums1.Length % 2 == 1)
+        {
+            foreach (var num2 in nums2)
+            {
+                result ^= num2;
+            }
+        }
+
+        return result;
+    }
+}
