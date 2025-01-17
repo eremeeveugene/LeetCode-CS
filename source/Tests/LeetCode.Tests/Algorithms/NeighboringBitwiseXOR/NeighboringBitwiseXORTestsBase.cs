c6a376f389b10660e7c48d3e7cf6d71e0e9187fd
eugene <eremeeveugene@gmail.com>
2025-01-17 13:53:06 +0200

    Neighboring Bitwise XOR

---
+++ source/Tests/LeetCode.Tests/Algorithms/NeighboringBitwiseXOR/NeighboringBitwiseXORTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.NeighboringBitwiseXOR;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.NeighboringBitwiseXOR;
+
+public abstract class NeighboringBitwiseXORTestsBase<T> where T : INeighboringBitwiseXOR, new()
+{
+    [TestMethod]
+    [DataRow("[1,1,0]", true)]
+    [DataRow("[1,1]", true)]
+    [DataRow("[1,0]", false)]
+    public void DoesValidArrayExist_WithDerivedArray_ReturnsWhetherValidArrayExists(string derivedJsonArray,
+        bool expectedResult)
+    {
+        // Arrange
+        var derived = JsonHelper<int>.DeserializeToArray(derivedJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.DoesValidArrayExist(derived);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
