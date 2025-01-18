72a287f3dc98eae3e7b17b02cbfb97d2354c1d57
eugene <eremeeveugene@gmail.com>
2025-01-18 19:08:08 +0200

    Minimum Cost to Make at Least One Valid Path in a Grid

---
+++ source/LeetCode/Algorithms/MinimumCostToMakeAtLeastOneValidPathInGrid/MinimumCostToMakeAtLeastOneValidPathInGridBreadthFirstSearch.cs
@@ -0,0 +1,95 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumCostToMakeAtLeastOneValidPathInGrid;
+
+/// <inheritdoc />
+public class MinimumCostToMakeAtLeastOneValidPathInGridBreadthFirstSearch : IMinimumCostToMakeAtLeastOneValidPathInGrid
+{
+    private readonly (int X, int Y)[] _directions =
+    [
+        (0, 1),
+        (0, -1),
+        (1, 0),
+        (-1, 0)
+    ];
+
+    /// <summary>
+    ///     Time complexity - O(n * m)
+    ///     Space complexity - O(n * m)
+    /// </summary>
+    /// <param name="grid"></param>
+    /// <returns></returns>
+    public int MinCost(int[][] grid)
+    {
+        var m = grid.Length;
+        var n = grid[0].Length;
+
+        var cost = new int[m, n];
+
+        for (var i = 0; i < m; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                cost[i, j] = int.MaxValue;
+            }
+        }
+
+        cost[0, 0] = 0;
+
+        var linkedList = new LinkedList<(int X, int Y, int CurrentCost)>();
+
+        linkedList.AddFirst((0, 0, 0));
+
+        while (linkedList.First != null)
+        {
+            var (x, y, currentCost) = linkedList.First.Value;
+
+            linkedList.RemoveFirst();
+
+            if (currentCost > cost[x, y])
+            {
+                continue;
+            }
+
+            for (var d = 0; d < _directions.Length; d++)
+            {
+                var nx = x + _directions[d].X;
+                var ny = y + _directions[d].Y;
+
+                var newCost = currentCost;
+
+                if (grid[x][y] != d + 1)
+                {
+                    newCost++;
+                }
+
+                if (nx < 0 || ny < 0 || nx >= m || ny >= n || newCost >= cost[nx, ny])
+                {
+                    continue;
+                }
+
+                cost[nx, ny] = newCost;
+
+                if (grid[x][y] == d + 1)
+                {
+                    linkedList.AddFirst((nx, ny, newCost));
+                }
+                else
+                {
+                    linkedList.AddLast((nx, ny, newCost));
+                }
+            }
+        }
+
+        return cost[m - 1, n - 1];
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumCostToMakeAtLeastOneValidPathInGrid/MinimumCostToMakeAtLeastOneValidPathInGridTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumCostToMakeAtLeastOneValidPathInGrid;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MinimumCostToMakeAtLeastOneValidPathInGrid;
+
+public abstract class MinimumCostToMakeAtLeastOneValidPathInGridTestsBase<T>
+    where T : IMinimumCostToMakeAtLeastOneValidPathInGrid, new()
+{
+    [TestMethod]
+    [DataRow("[[1,1,1,1],[2,2,2,2],[1,1,1,1],[2,2,2,2]]", 3)]
+    [DataRow("[[1,1,3],[3,2,2],[1,1,4]]", 0)]
+    [DataRow("[[1,2],[4,3]]", 1)]
+    public void MinCost_WithGridJsonArray_ReturnsMinimumCost(string gridJsonArray, int expectedResult)
+    {
+        // Arrange
+        var grid = JsonHelper<int>.DeserializeToJaggedArray(gridJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinCost(grid);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
