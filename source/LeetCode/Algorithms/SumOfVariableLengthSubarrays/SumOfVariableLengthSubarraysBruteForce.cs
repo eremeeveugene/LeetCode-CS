b4b9b56431b9351efc55127574cc4c600c556b46
eugene <eremeeveugene@gmail.com>
2025-01-20 14:08:54 +0200

    Sum of Variable Length Subarrays

---
+++ source/LeetCode/Algorithms/SumOfVariableLengthSubarrays/SumOfVariableLengthSubarraysBruteForce.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SumOfVariableLengthSubarrays;
+
+/// <inheritdoc />
+public class SumOfVariableLengthSubarraysBruteForce : ISumOfVariableLengthSubarrays
+{
+    /// <summary>
+    ///     Time complexity - O(n^2)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int SubarraySum(int[] nums)
+    {
+        var sum = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var start = Math.Max(0, i - nums[i]);
+
+            for (var j = start; j <= i; j++)
+            {
+                sum += nums[j];
+            }
+        }
+
+        return sum;
+    }
+}
---
+++ source/LeetCode/Algorithms/SumOfVariableLengthSubarrays/SumOfVariableLengthSubarraysPrefixSum.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SumOfVariableLengthSubarrays;
+
+/// <inheritdoc />
+public class SumOfVariableLengthSubarraysPrefixSum : ISumOfVariableLengthSubarrays
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int SubarraySum(int[] nums)
+    {
+        var prefix = new int[nums.Length];
+
+        prefix[0] = nums[0];
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+
+        var sum = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var start = Math.Max(0, i - nums[i]);
+
+            sum += prefix[i] - (start > 0 ? prefix[start - 1] : 0);
+        }
+
+        return sum;
+    }
+}
