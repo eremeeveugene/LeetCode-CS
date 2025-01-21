8382f384c6f6c3d358dd37b17aa2b94d626e5d56
eugene <eremeeveugene@gmail.com>
2025-01-21 13:38:54 +0200

    Grid Game (#1057)

---
+++ source/LeetCode/Algorithms/GridGame/GridGamePrefixSum.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.GridGame;
+
+/// <inheritdoc />
+public class GridGamePrefixSum : IGridGame
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="grid"></param>
+    /// <returns></returns>
+    public long GridGame(int[][] grid)
+    {
+        var n = grid[0].Length;
+
+        var topPrefixSum = new long[n];
+        var bottomPrefixSum = new long[n];
+
+        topPrefixSum[0] = grid[0][0];
+        bottomPrefixSum[0] = grid[1][0];
+
+        for (var i = 1; i < n; i++)
+        {
+            topPrefixSum[i] = topPrefixSum[i - 1] + grid[0][i];
+            bottomPrefixSum[i] = bottomPrefixSum[i - 1] + grid[1][i];
+        }
+
+        var result = long.MaxValue;
+
+        for (var i = 0; i < n; i++)
+        {
+            var topRemaining = topPrefixSum[n - 1] - topPrefixSum[i];
+
+            long bottomCollected = 0;
+
+            if (i > 0)
+            {
+                bottomCollected = bottomPrefixSum[i - 1];
+            }
+
+            var secondRobotPoints = Math.Max(topRemaining, bottomCollected);
+
+            result = Math.Min(result, secondRobotPoints);
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/GridGame/GridGameTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.GridGame;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.GridGame;
+
+public abstract class GridGameTestsBase<T> where T : IGridGame, new()
+{
+    [TestMethod]
+    [DataRow("[[2,5,4],[1,5,1]]", 4)]
+    [DataRow("[[3,3,1],[8,5,2]]", 4)]
+    [DataRow("[[1,3,1,15],[1,3,3,1]]", 7)]
+    public void GridGame_WithInputGrid_ReturnsPointsCollectedBySecondRobot(string gridJsonArray,
+        long expectedResult)
+    {
+        // Arrange
+        var grid = JsonHelper<int>.DeserializeToJaggedArray(gridJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.GridGame(grid);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
