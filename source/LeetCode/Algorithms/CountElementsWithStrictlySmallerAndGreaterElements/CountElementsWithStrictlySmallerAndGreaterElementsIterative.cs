c6675036297541930c80b493d994434b0245df8f
eugene <eremeeveugene@gmail.com>
2025-01-24 14:12:08 +0200

    Count Elements With Strictly Smaller and Greater Elements (#1067)

---
+++ source/LeetCode/Algorithms/CountElementsWithStrictlySmallerAndGreaterElements/CountElementsWithStrictlySmallerAndGreaterElementsIterative.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountElementsWithStrictlySmallerAndGreaterElements;
+
+/// <inheritdoc />
+public class CountElementsWithStrictlySmallerAndGreaterElementsIterative :
+    ICountElementsWithStrictlySmallerAndGreaterElements
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int CountElements(int[] nums)
+    {
+        if (nums.Length < 3)
+        {
+            return 0;
+        }
+
+        var min = int.MaxValue;
+        var max = int.MinValue;
+
+        foreach (var num in nums)
+        {
+            min = Math.Min(min, num);
+            max = Math.Max(max, num);
+        }
+
+        return nums.Count(num => num > min && num < max);
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountElementsWithStrictlySmallerAndGreaterElements/CountElementsWithStrictlySmallerAndGreaterElementsTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountElementsWithStrictlySmallerAndGreaterElements;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.CountElementsWithStrictlySmallerAndGreaterElements;
+
+public abstract class CountElementsWithStrictlySmallerAndGreaterElementsTestsBase<T>
+    where T : ICountElementsWithStrictlySmallerAndGreaterElements, new()
+{
+    [TestMethod]
+    [DataRow("[11,7,2,15]", 2)]
+    [DataRow("[-3,3,3,90]", 2)]
+    public void CountElements_GivenArray_ReturnsCountOfElementsMatchingCriteria(string numsJsonArray,
+        int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountElements(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
