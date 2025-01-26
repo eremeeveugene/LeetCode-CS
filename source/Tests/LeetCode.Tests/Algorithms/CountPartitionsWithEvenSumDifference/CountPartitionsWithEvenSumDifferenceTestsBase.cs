8fb46c7a71093a4cc249a99d9fd9528def9c919c
eugene <eremeeveugene@gmail.com>
2025-01-26 13:13:18 +0200

    Count Partitions with Even Sum Difference

---
+++ source/Tests/LeetCode.Tests/Algorithms/CountPartitionsWithEvenSumDifference/CountPartitionsWithEvenSumDifferenceTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountPartitionsWithEvenSumDifference;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.CountPartitionsWithEvenSumDifference;
+
+public abstract class CountPartitionsWithEvenSumDifferenceTestsBase<T>
+    where T : ICountPartitionsWithEvenSumDifference, new()
+{
+    [TestMethod]
+    [DataRow("[1,2,2]", 0)]
+    [DataRow("[2,4,6,8]", 3)]
+    [DataRow("[10,10,3,7,6]", 4)]
+    public void CountPartitions_GivenNumsArray_ReturnsCorrectPartitionCount(string numsJsonArray, int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountPartitions(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
