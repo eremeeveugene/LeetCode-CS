bc7fc397d8077052be14b08eb7706c6243ccd1fc
eugene <eremeeveugene@gmail.com>
2025-01-26 13:30:57 +0200

    Count Partitions with Even Sum Difference (#1070)

--- README.md
+++ README.md
@@ -526,6 +526,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [3396. Minimum Number of Operations to Make Elements in Array Distinct](https://leetcode.com/problems/minimum-number-of-operations-to-make-elements-in-array-distinct/description/)
 - [3423. Maximum Difference Between Adjacent Elements in a Circular Array](https://leetcode.com/problems/maximum-difference-between-adjacent-elements-in-a-circular-array/description/)
 - [3427. Sum of Variable Length Subarrays](https://leetcode.com/problems/sum-of-variable-length-subarrays/description/)
+- [3432. Count Partitions with Even Sum Difference](https://leetcode.com/problems/count-partitions-with-even-sum-difference/description/)
 
   </p>
 </details>
---
+++ source/LeetCode/Algorithms/CountPartitionsWithEvenSumDifference/CountPartitionsWithEvenSumDifferencePrefixSum.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountPartitionsWithEvenSumDifference;
+
+/// <inheritdoc />
+public class CountPartitionsWithEvenSumDifferencePrefixSum : ICountPartitionsWithEvenSumDifference
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int CountPartitions(int[] nums)
+    {
+        var left = nums[0];
+        var right = nums.Sum() - nums[0];
+
+        var count = 0;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if ((left - right) % 2 == 0)
+            {
+                count++;
+            }
+
+            left += nums[i];
+            right -= nums[i];
+        }
+
+        return count;
+    }
+}
