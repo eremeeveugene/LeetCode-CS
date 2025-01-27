f04f3a32a5875155fdc8ef9cb504ccef75f12198
eugene <eremeeveugene@gmail.com>
2025-01-27 14:49:11 +0200

    Design Neighbor Sum Service (#1072)

---
+++ source/LeetCode/Algorithms/DesignNeighborSumService/DesignNeighborSumServiceDictionary.cs
@@ -0,0 +1,107 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.DesignNeighborSumService;
+
+/// <inheritdoc />
+public class DesignNeighborSumServiceDictionary : IDesignNeighborSumService
+{
+    private readonly int[][] _grid;
+    private readonly Dictionary<int, (int X, int Y)> _neighborsDictionary = [];
+
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="grid"></param>
+    public DesignNeighborSumServiceDictionary(int[][] grid)
+    {
+        _grid = grid;
+
+        for (var i = 0; i < grid.Length; i++)
+        {
+            for (var j = 0; j < grid[i].Length; j++)
+            {
+                _neighborsDictionary.Add(grid[i][j], (i, j));
+            }
+        }
+    }
+
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public int AdjacentSum(int value)
+    {
+        var (x, y) = _neighborsDictionary[value];
+
+        var sum = 0;
+
+        if (x > 0)
+        {
+            sum += _grid[x - 1][y];
+        }
+
+        if (x < _grid.Length - 1)
+        {
+            sum += _grid[x + 1][y];
+        }
+
+        if (y > 0)
+        {
+            sum += _grid[x][y - 1];
+        }
+
+        if (y < _grid[x].Length - 1)
+        {
+            sum += _grid[x][y + 1];
+        }
+
+        return sum;
+    }
+
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public int DiagonalSum(int value)
+    {
+        var (x, y) = _neighborsDictionary[value];
+
+        var sum = 0;
+
+        if (x > 0 && y > 0)
+        {
+            sum += _grid[x - 1][y - 1];
+        }
+
+        if (x > 0 && y < _grid[x].Length - 1)
+        {
+            sum += _grid[x - 1][y + 1];
+        }
+
+        if (x < _grid.Length - 1 && y > 0)
+        {
+            sum += _grid[x + 1][y - 1];
+        }
+
+        if (x < _grid.Length - 1 && y < _grid.Length - 1)
+        {
+            sum += _grid[x + 1][y + 1];
+        }
+
+        return sum;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/DesignNeighborSumService/DesignNeighborSumServiceTestsBase.cs
@@ -0,0 +1,63 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.DesignNeighborSumService;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.DesignNeighborSumService;
+
+public abstract class DesignNeighborSumServiceTestsBase<T>
+    where T : IDesignNeighborSumServiceFactory, new()
+{
+    private const string AdjacentSum = "adjacentSum";
+    private const string DiagonalSum = "diagonalSum";
+
+    [TestMethod]
+    [DataRow("[[0, 1, 2], [3, 4, 5], [6, 7, 8]]", "[\"adjacentSum\",\"adjacentSum\",\"diagonalSum\",\"diagonalSum\"]",
+        "[[1],[4],[4],[8]]", "[6,16,16,4]")]
+    public void NeighborSum_WithAdjacentAndDiagonalSum_ReturnsSums(string gridJsonArray,
+        string methodsJsonArray, string argsJsonArray, string expectedResultJsonArray)
+    {
+        // Arrange
+        var grid = JsonHelper<int>.DeserializeToJaggedArray(gridJsonArray);
+        var methods = JsonHelper<string>.DeserializeToArray(methodsJsonArray);
+        var args = JsonHelper<int>.DeserializeToJaggedArray(argsJsonArray);
+        var expectedResult = JsonHelper<int?>.DeserializeToArray(expectedResultJsonArray);
+
+        var solutionFactory = new T();
+        var solution = solutionFactory.Create(grid);
+
+        // Act
+        var actualResult = new List<int>();
+
+        for (var i = 0; i < methods.Length; i++)
+        {
+            var value = args[i][0];
+
+            switch (methods[i])
+            {
+                case AdjacentSum:
+                    var adjacentSum = solution.AdjacentSum(value);
+
+                    actualResult.Add(adjacentSum);
+                    break;
+                case DiagonalSum:
+                    var diagonalSum = solution.DiagonalSum(value);
+
+                    actualResult.Add(diagonalSum);
+                    break;
+            }
+        }
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
