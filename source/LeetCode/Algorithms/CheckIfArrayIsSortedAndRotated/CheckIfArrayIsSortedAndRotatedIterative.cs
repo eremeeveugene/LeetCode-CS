3eadc6a785b17a1d0ef05bce934540951a864d84
eugene <eremeeveugene@gmail.com>
2025-02-02 12:45:32 +0200

    Check if Array Is Sorted and Rotated

---
+++ source/LeetCode/Algorithms/CheckIfArrayIsSortedAndRotated/CheckIfArrayIsSortedAndRotatedIterative.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CheckIfArrayIsSortedAndRotated;
+
+/// <inheritdoc />
+public class CheckIfArrayIsSortedAndRotatedIterative : ICheckIfArrayIsSortedAndRotated
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public bool Check(int[] nums)
+    {
+        var isRotated = false;
+
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] <= nums[i + 1])
+            {
+                continue;
+            }
+
+            if (isRotated)
+            {
+                return false;
+            }
+
+            isRotated = true;
+        }
+
+        if (nums[^1] <= nums[0])
+        {
+            return true;
+        }
+
+        return !isRotated;
+    }
+}
---
+++ source/LeetCode/Algorithms/CheckIfArrayIsSortedAndRotated/ICheckIfArrayIsSortedAndRotated.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CheckIfArrayIsSortedAndRotated;
+
+/// <summary>
+///     https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/
+/// </summary>
+public interface ICheckIfArrayIsSortedAndRotated
+{
+    bool Check(int[] nums);
+}
