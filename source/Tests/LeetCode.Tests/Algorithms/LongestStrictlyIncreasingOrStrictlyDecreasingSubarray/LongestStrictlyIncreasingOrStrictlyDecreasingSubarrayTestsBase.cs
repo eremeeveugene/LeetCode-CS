a4fd1e1b9482e1a2efa1d151e0abb53563b5490b
eugene <eremeeveugene@gmail.com>
2025-02-03 14:09:55 +0200

    Longest Strictly Increasing or Strictly Decreasing Subarray

---
+++ source/Tests/LeetCode.Tests/Algorithms/LongestStrictlyIncreasingOrStrictlyDecreasingSubarray/LongestStrictlyIncreasingOrStrictlyDecreasingSubarrayTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.LongestStrictlyIncreasingOrStrictlyDecreasingSubarray;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.LongestStrictlyIncreasingOrStrictlyDecreasingSubarray;
+
+public abstract class LongestStrictlyIncreasingOrStrictlyDecreasingSubarrayTestsBase<T>
+    where T : ILongestStrictlyIncreasingOrStrictlyDecreasingSubarray, new()
+{
+    [TestMethod]
+    [DataRow("[1,4,3,3,2]", 2)]
+    [DataRow("[3,3,3,3]", 1)]
+    [DataRow("[3,2,1]", 3)]
+    public void LongestMonotonicSubarray_WithGivenNums_ReturnsLengthOfTheLongestMonotonicSubarray(string numsJsonArray,
+        int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.LongestMonotonicSubarray(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
