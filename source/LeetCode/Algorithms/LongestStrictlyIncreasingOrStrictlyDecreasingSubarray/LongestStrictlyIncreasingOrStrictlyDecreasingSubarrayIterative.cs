79a761939790e0499a89f59f423136c9227fde65
eugene <eremeeveugene@gmail.com>
2025-02-03 14:16:09 +0200

    Longest Strictly Increasing or Strictly Decreasing Subarray (#1086)

---
+++ source/LeetCode/Algorithms/LongestStrictlyIncreasingOrStrictlyDecreasingSubarray/LongestStrictlyIncreasingOrStrictlyDecreasingSubarrayIterative.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.LongestStrictlyIncreasingOrStrictlyDecreasingSubarray;
+
+/// <inheritdoc />
+public class LongestStrictlyIncreasingOrStrictlyDecreasingSubarrayIterative :
+    ILongestStrictlyIncreasingOrStrictlyDecreasingSubarray
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int LongestMonotonicSubarray(int[] nums)
+    {
+        var increasingLength = 1;
+        var decreasingLength = 1;
+        var maxLength = 1;
+
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] > nums[i + 1])
+            {
+                decreasingLength++;
+                increasingLength = 1;
+            }
+            else if (nums[i] < nums[i + 1])
+            {
+                increasingLength++;
+                decreasingLength = 1;
+            }
+            else
+            {
+                increasingLength = 1;
+                decreasingLength = 1;
+            }
+
+            maxLength = Math.Max(maxLength, Math.Max(increasingLength, decreasingLength));
+        }
+
+        return maxLength;
+    }
+}
