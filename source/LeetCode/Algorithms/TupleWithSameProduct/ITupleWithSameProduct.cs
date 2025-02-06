9057bdf220402b2285b73dded5752ccb4bd915bb
eugene <eremeeveugene@gmail.com>
2025-02-06 14:28:19 +0200

    Tuple with Same Product (#1092)

---
+++ source/LeetCode/Algorithms/TupleWithSameProduct/ITupleWithSameProduct.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.TupleWithSameProduct;
+
+/// <summary>
+///     https://leetcode.com/problems/tuple-with-same-product/description/
+/// </summary>
+public interface ITupleWithSameProduct
+{
+    int TupleSameProduct(int[] nums);
+}
---
+++ source/LeetCode/Algorithms/TupleWithSameProduct/TupleWithSameProductDictionary.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.TupleWithSameProduct;
+
+/// <inheritdoc />
+public class TupleWithSameProductDictionary : ITupleWithSameProduct
+{
+    /// <summary>
+    ///     Time complexity - O(n^2)
+    ///     Space complexity - O(n^2)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int TupleSameProduct(int[] nums)
+    {
+        var count = 0;
+
+        var frequencyDictionary = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                var product = nums[i] * nums[j];
+
+                if (frequencyDictionary.TryAdd(product, 1))
+                {
+                    continue;
+                }
+
+                count += frequencyDictionary[product] * 8;
+
+                frequencyDictionary[product]++;
+            }
+        }
+
+        return count;
+    }
+}
