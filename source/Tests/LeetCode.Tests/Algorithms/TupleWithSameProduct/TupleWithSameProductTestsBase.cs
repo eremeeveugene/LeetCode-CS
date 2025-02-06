7e59f576219ccc6f09d011f77b8eeae856effb50
eugene <eremeeveugene@gmail.com>
2025-02-06 13:26:42 +0200

    Tuple with Same Product

---
+++ source/Tests/LeetCode.Tests/Algorithms/TupleWithSameProduct/TupleWithSameProductTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.TupleWithSameProduct;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.TupleWithSameProduct;
+
+public abstract class TupleWithSameProductTestsBase<T> where T : ITupleWithSameProduct, new()
+{
+    [TestMethod]
+    [DataRow("[2,3,4,6]", 8)]
+    [DataRow("[1,2,4,5,10]", 16)]
+    [DataRow("[2,3,4,6,8,12]", 40)]
+    public void TupleSameProduct_GivenArrayOfNumbers_ReturnsTupleCount(string numsJsonArray, int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.TupleSameProduct(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
