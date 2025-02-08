c9a2d427bbcbd938ba10d2d98bd9201ce1d55090
eugene <eremeeveugene@gmail.com>
2025-02-08 19:52:45 +0200

    Design a Number Container System (#1096)

---
+++ source/LeetCode/Algorithms/DesignNumberContainerSystem/IDesignNumberContainerSystem.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.DesignNumberContainerSystem;
+
+/// <summary>
+///     https://leetcode.com/problems/design-a-number-container-system/description/
+/// </summary>
+public interface IDesignNumberContainerSystem
+{
+    void Change(int index, int number);
+
+    int Find(int number);
+}
