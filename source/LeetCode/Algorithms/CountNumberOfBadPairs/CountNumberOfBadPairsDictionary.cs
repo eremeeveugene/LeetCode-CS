282562a9e3d188248c43c85e66742d268caef5d5
eugene <eremeeveugene@gmail.com>
2025-02-09 14:00:06 +0200

    Count Number of Bad Pairs

---
+++ source/LeetCode/Algorithms/CountNumberOfBadPairs/CountNumberOfBadPairsDictionary.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountNumberOfBadPairs;
+
+/// <inheritdoc />
+public class CountNumberOfBadPairsDictionary : ICountNumberOfBadPairs
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public long CountBadPairs(int[] nums)
+    {
+        long badPairsCount = 0;
+
+        Dictionary<int, int> countDictionary = [];
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var difference = i - nums[i];
+
+            if (countDictionary.TryGetValue(difference, out var goodPairsCount))
+            {
+                badPairsCount += i - goodPairsCount;
+
+                countDictionary[difference]++;
+            }
+            else
+            {
+                badPairsCount += i;
+
+                countDictionary[difference] = 1;
+            }
+        }
+
+        return badPairsCount;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountNumberOfBadPairs/CountNumberOfBadPairsTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountNumberOfBadPairs;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.CountNumberOfBadPairs;
+
+public abstract class CountNumberOfBadPairsTestsBase<T> where T : ICountNumberOfBadPairs, new()
+{
+    [TestMethod]
+    [DataRow("[4,1,3,3]", 5)]
+    [DataRow("[1,2,3,4,5]", 0)]
+    public void CountBadPairs_GivenArray_ReturnsCountOfBadPairs(string numsJsonArray, int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountBadPairs(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
