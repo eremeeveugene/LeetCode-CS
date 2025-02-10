caf32b7e563d530950b88955eaa47da8c72665b7
eugene <eremeeveugene@gmail.com>
2025-02-10 12:49:31 +0200

    Second Largest Digit in a String

---
+++ source/LeetCode/Algorithms/SecondLargestDigitInString/SecondLargestDigitInStringIterative.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SecondLargestDigitInString;
+
+/// <inheritdoc />
+public class SecondLargestDigitInStringIterative : ISecondLargestDigitInString
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public int SecondHighest(string s)
+    {
+        var highest = -1;
+        var secondHighest = -1;
+
+        foreach (var c in s)
+        {
+            if (!char.IsDigit(c))
+            {
+                continue;
+            }
+
+            var num = c - '0';
+
+            if (num > highest)
+            {
+                secondHighest = highest;
+                highest = num;
+            }
+            else if (num > secondHighest && num < highest)
+            {
+                secondHighest = num;
+            }
+        }
+
+        return secondHighest;
+    }
+}
