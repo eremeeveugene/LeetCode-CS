dc61f4d9837d22d9b8f6c53184ab934ffdc774c5
eugene <eremeeveugene@gmail.com>
2025-02-13 14:57:34 +0200

    Minimum Operations to Exceed Threshold Value II

---
+++ source/LeetCode/Algorithms/MinimumOperationsToExceedThresholdValue2/MinimumOperationsToExceedThresholdValue2PriorityQueue.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumOperationsToExceedThresholdValue2;
+
+/// <inheritdoc />
+public class MinimumOperationsToExceedThresholdValue2PriorityQueue : IMinimumOperationsToExceedThresholdValue2
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int MinOperations(int[] nums, int k)
+    {
+        var minOperations = 0;
+
+        var priorityQueue = new PriorityQueue<long, long>();
+
+        foreach (var num in nums)
+        {
+            priorityQueue.Enqueue(num, num);
+        }
+
+        while (priorityQueue.Count > 1 && priorityQueue.Peek() < k)
+        {
+            var first = priorityQueue.Dequeue();
+            var second = priorityQueue.Dequeue();
+
+            var num = (first * 2) + second;
+
+            priorityQueue.Enqueue(num, num);
+
+            minOperations++;
+        }
+
+        return minOperations;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumOperationsToExceedThresholdValue2/MinimumOperationsToExceedThresholdValue2TestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumOperationsToExceedThresholdValue2;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MinimumOperationsToExceedThresholdValue2;
+
+public abstract class MinimumOperationsToExceedThresholdValue2TestsBase<T>
+    where T : IMinimumOperationsToExceedThresholdValue2, new()
+{
+    [TestMethod]
+    [DataRow("[2,11,10,1,3]", 10, 2)]
+    [DataRow("[1,1,2,4,9]", 20, 4)]
+    public void MinOperations_WithTargetSum_ReturnsMinimumOperations(string numsJsonArray, int k, int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinOperations(nums, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
