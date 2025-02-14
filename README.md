9f37fdd56fe325141ccd2110293c153e75d40451
eugene <eremeeveugene@gmail.com>
2025-02-14 14:41:43 +0200

    Product of the Last K Numbers (#1108)

--- README.md
+++ README.md
@@ -307,6 +307,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [1331. Rank Transform of an Array](https://leetcode.com/problems/rank-transform-of-an-array/description/)
 - [1342. Number of Steps to Reduce a Number to Zero](https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/)
 - [1346. Check If N and Its Double Exist](https://leetcode.com/problems/check-if-n-and-its-double-exist/description/)
+- [1352. Product of the Last K Numbers](https://leetcode.com/problems/product-of-the-last-k-numbers/description/)
 - [1365. How Many Numbers Are Smaller Than the Current Number](https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/description/)
 - [1367. Linked List in Binary Tree](https://leetcode.com/problems/linked-list-in-binary-tree/description/)
 - [1368. Minimum Cost to Make at Least One Valid Path in a Grid](https://leetcode.com/problems/minimum-cost-to-make-at-least-one-valid-path-in-a-grid/description/)
---
+++ source/LeetCode/Algorithms/ProductOfTheLastKNumbers/ProductOfTheLastKNumbersPrefixSum.cs
@@ -0,0 +1,63 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ProductOfTheLastKNumbers;
+
+/// <inheritdoc />
+public class ProductOfTheLastKNumbersPrefixSum : IProductOfTheLastKNumbers
+{
+    private readonly List<int> _products = [];
+
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="num"></param>
+    public void Add(int num)
+    {
+        if (num == 0)
+        {
+            _products.Clear();
+        }
+        else
+        {
+            if (_products.Count == 0)
+            {
+                _products.Add(num);
+            }
+            else
+            {
+                _products.Add(_products[^1] * num);
+            }
+        }
+    }
+
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int GetProduct(int k)
+    {
+        if (k > _products.Count)
+        {
+            return 0;
+        }
+
+        if (k == _products.Count)
+        {
+            return _products[^1];
+        }
+
+        return _products[^1] / _products[_products.Count - k - 1];
+    }
+}
