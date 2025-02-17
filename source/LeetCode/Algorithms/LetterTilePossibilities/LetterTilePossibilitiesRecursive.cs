6f1b24f93293c74d6b4da2c6a36cc1b7c952ff0f
eugene <eremeeveugene@gmail.com>
2025-02-17 12:24:05 +0200

    Letter Tile Possibilities

---
+++ source/LeetCode/Algorithms/LetterTilePossibilities/LetterTilePossibilitiesRecursive.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.LetterTilePossibilities;
+
+/// <inheritdoc />
+public class LetterTilePossibilitiesRecursive : ILetterTilePossibilities
+{
+    private const int Count = 'Z' - 'A' + 1;
+
+    /// <summary>
+    ///     Time complexity - O(n * n!)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="tiles"></param>
+    /// <returns></returns>
+    public int NumTilePossibilities(string tiles)
+    {
+        var charCount = new int[Count];
+
+        foreach (var tile in tiles)
+        {
+            charCount[tile - 'A']++;
+        }
+
+        return FindSequences(charCount);
+    }
+
+    private static int FindSequences(int[] charCount)
+    {
+        var totalCount = 0;
+
+        for (var i = 0; i < Count; i++)
+        {
+            if (charCount[i] == 0)
+            {
+                continue;
+            }
+
+            totalCount++;
+
+            charCount[i]--;
+
+            totalCount += FindSequences(charCount);
+
+            charCount[i]++;
+        }
+
+        return totalCount;
+    }
+}
