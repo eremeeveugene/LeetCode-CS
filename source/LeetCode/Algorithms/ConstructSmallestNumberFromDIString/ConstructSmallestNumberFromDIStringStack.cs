9b9c402c25ed9130bf5902f9654357ca1202b45e
eugene <eremeeveugene@gmail.com>
2025-02-18 14:11:09 +0200

    Construct Smallest Number From DI String

---
+++ source/LeetCode/Algorithms/ConstructSmallestNumberFromDIString/ConstructSmallestNumberFromDIStringStack.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Text;
+
+namespace LeetCode.Algorithms.ConstructSmallestNumberFromDIString;
+
+/// <inheritdoc />
+public class ConstructSmallestNumberFromDIStringStack : IConstructSmallestNumberFromDIString
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="pattern"></param>
+    /// <returns></returns>
+    public string SmallestNumber(string pattern)
+    {
+        var stringBuilder = new StringBuilder();
+        var stack = new Stack<int>();
+
+        for (var i = 0; i <= pattern.Length; i++)
+        {
+            stack.Push(i + 1);
+
+            if (i != pattern.Length && pattern[i] != 'I')
+            {
+                continue;
+            }
+
+            while (stack.Count > 0)
+            {
+                stringBuilder.Append(stack.Pop());
+            }
+        }
+
+        return stringBuilder.ToString();
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ConstructSmallestNumberFromDIString/ConstructSmallestNumberFromDIStringTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ConstructSmallestNumberFromDIString;
+
+namespace LeetCode.Tests.Algorithms.ConstructSmallestNumberFromDIString;
+
+public abstract class ConstructSmallestNumberFromDIStringTestsBase<T>
+    where T : IConstructSmallestNumberFromDIString, new()
+{
+    [TestMethod]
+    [DataRow("DDD", "4321")]
+    [DataRow("IIIDIDDD", "123549876")]
+    public void SmallestNumber_WithGivenPattern_ReturnsLexicographicallySmallestPermutation(string pattern,
+        string expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.SmallestNumber(pattern);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
