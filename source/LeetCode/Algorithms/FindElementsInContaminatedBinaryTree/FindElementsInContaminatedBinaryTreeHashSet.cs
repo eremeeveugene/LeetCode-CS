230ceb22aebce5a4a1d29fac00d489b79c56e63b
eugene <eremeeveugene@gmail.com>
2025-02-21 17:42:40 +0200

    Find Elements in a Contaminated Binary Tree

---
+++ source/LeetCode/Algorithms/FindElementsInContaminatedBinaryTree/FindElementsInContaminatedBinaryTreeHashSet.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.FindElementsInContaminatedBinaryTree;
+
+/// <inheritdoc />
+public class FindElementsInContaminatedBinaryTreeHashSet : IFindElementsInContaminatedBinaryTree
+{
+    private readonly HashSet<int> _hashSet = [];
+
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="root"></param>
+    public FindElementsInContaminatedBinaryTreeHashSet(TreeNode root)
+    {
+        var queue = new Queue<TreeNode>();
+
+        root.val = 0;
+
+        queue.Enqueue(root);
+
+        while (queue.Count > 0)
+        {
+            var treeNode = queue.Dequeue();
+
+            _hashSet.Add(treeNode.val);
+
+            if (treeNode.left != null)
+            {
+                treeNode.left.val = (2 * treeNode.val) + 1;
+
+                queue.Enqueue(treeNode.left);
+            }
+
+            if (treeNode.right != null)
+            {
+                treeNode.right.val = (2 * treeNode.val) + 2;
+
+                queue.Enqueue(treeNode.right);
+            }
+        }
+    }
+
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    public bool Find(int target)
+    {
+        return _hashSet.Contains(target);
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FindElementsInContaminatedBinaryTree/FindElementsInContaminatedBinaryTreeTestsBase.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindElementsInContaminatedBinaryTree;
+using LeetCode.Core.Exceptions;
+using LeetCode.Core.Helpers;
+using LeetCode.Core.Models;
+
+namespace LeetCode.Tests.Algorithms.FindElementsInContaminatedBinaryTree;
+
+public abstract class FindElementsInContaminatedBinaryTreeTestsBase<T>
+    where T : IFindElementsInContaminatedBinaryTreeFactory, new()
+{
+    private const string Find = "find";
+
+    [TestMethod]
+    [DataRow("[-1,null,-1]", "[\"find\",\"find\"]", "[[1],[2]]", "[false,true]")]
+    [DataRow("[-1,-1,-1,-1,-1]", "[\"find\",\"find\",\"find\"]", "[[1],[3],[5]]", "[true,true,false]")]
+    [DataRow("[-1,null,-1,-1,null,-1]", "[\"find\",\"find\",\"find\",\"find\"]", "[[2],[3],[4],[5]]", "[true,false,false,true]")]
+    public void Find_WithVariousTreeStructures_ReturnsIfElementsFound(string rootJsonArray, string methodsJsonArray,
+        string argumentsJsonArray, string expectedResultJsonArray)
+    {
+        // Arrange
+        var rootArray = JsonHelper<int?>.DeserializeToArray(rootJsonArray);
+        var root = TreeNode.ToTreeNode(rootArray) ?? throw new TreeNodeBuildException();
+
+        var methods = JsonHelper<string>.DeserializeToArray(methodsJsonArray);
+        var arguments = JsonHelper<int>.DeserializeToJaggedArray(argumentsJsonArray);
+        var expectedResult = JsonHelper<bool>.DeserializeToArray(expectedResultJsonArray);
+
+        var solutionFactory = new T();
+        var solution = solutionFactory.Create(root);
+
+        // Act
+        var actualResult = new List<object>();
+
+        for (var i = 0; i < methods.Length; i++)
+        {
+            switch (methods[i])
+            {
+                case Find:
+                    actualResult.Add(solution.Find(arguments[i][0]));
+
+                    break;
+            }
+        }
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
