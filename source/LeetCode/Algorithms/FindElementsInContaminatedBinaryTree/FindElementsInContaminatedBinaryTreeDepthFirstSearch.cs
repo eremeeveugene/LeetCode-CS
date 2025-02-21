9f04a9cd54c1015082e0bb80f1f150672f1b7d94
eugene <eremeeveugene@gmail.com>
2025-02-21 17:49:43 +0200

    Find Elements in a Contaminated Binary Tree (#1123)

---
+++ source/LeetCode/Algorithms/FindElementsInContaminatedBinaryTree/FindElementsInContaminatedBinaryTreeDepthFirstSearch.cs
@@ -0,0 +1,62 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.FindElementsInContaminatedBinaryTree;
+
+/// <inheritdoc />
+public class FindElementsInContaminatedBinaryTreeDepthFirstSearch : IFindElementsInContaminatedBinaryTree
+{
+    private readonly TreeNode _root;
+
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="root"></param>
+    public FindElementsInContaminatedBinaryTreeDepthFirstSearch(TreeNode root)
+    {
+        _root = root;
+        _root.val = 0;
+    }
+
+    /// <summary>
+    ///     Time complexity - O(log n)
+    ///     Space complexity - O(log n)
+    /// </summary>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    public bool Find(int target)
+    {
+        var node = _root;
+        var stack = new Stack<bool>();
+
+        while (target > 0)
+        {
+            stack.Push(target % 2 == 1);
+
+            target = (target - 1) / 2;
+        }
+
+        while (stack.Count > 0)
+        {
+            node = stack.Pop() ? node.left : node.right;
+
+            if (node == null)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ source/LeetCode/Algorithms/FindElementsInContaminatedBinaryTree/FindElementsInContaminatedBinaryTreeDepthFirstSearchFactory.cs
@@ -0,0 +1,22 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.FindElementsInContaminatedBinaryTree;
+
+public class FindElementsInContaminatedBinaryTreeDepthFirstSearchFactory : IFindElementsInContaminatedBinaryTreeFactory
+{
+    public IFindElementsInContaminatedBinaryTree Create(TreeNode root)
+    {
+        return new FindElementsInContaminatedBinaryTreeDepthFirstSearch(root);
+    }
+}
