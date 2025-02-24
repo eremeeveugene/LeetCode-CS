5686a952ba6d54743dce9aa14d1609fcc92b1748
eugene <eremeeveugene@gmail.com>
2025-02-24 13:36:45 +0200

    Check If Digits Are Equal in String After Operations I (#1129)

---
+++ source/LeetCode/Algorithms/CheckIfDigitsAreEqualInStringAfterOperations1/CheckIfDigitsAreEqualInStringAfterOperations1Simulation.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CheckIfDigitsAreEqualInStringAfterOperations1;
+
+/// <inheritdoc />
+public class CheckIfDigitsAreEqualInStringAfterOperations1Simulation : ICheckIfDigitsAreEqualInStringAfterOperations1
+{
+    /// <summary>
+    ///     Time complexity - O(n^2)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public bool HasSameDigits(string s)
+    {
+        while (s.Length > 2)
+        {
+            var newS = new char[s.Length - 1];
+
+            for (var i = 0; i < s.Length - 1; i++)
+            {
+                newS[i] = (char)(((s[i] - '0' + s[i + 1] - '0') % 10) + '0');
+            }
+
+            s = new string(newS);
+        }
+
+        return s[0] == s[1];
+    }
+}
