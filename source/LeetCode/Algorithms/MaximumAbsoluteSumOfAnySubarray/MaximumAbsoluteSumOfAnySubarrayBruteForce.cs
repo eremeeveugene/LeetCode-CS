00af1f4d6312bcfaee1d41940851c4a6ba9c0445
eugene <eremeeveugene@gmail.com>
2025-02-26 14:12:36 +0200

    Maximum Absolute Sum of Any Subarray (#1133)

---
+++ source/LeetCode/Algorithms/MaximumAbsoluteSumOfAnySubarray/MaximumAbsoluteSumOfAnySubarrayBruteForce.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumAbsoluteSumOfAnySubarray;
+
+/// <inheritdoc />
+public class MaximumAbsoluteSumOfAnySubarrayBruteForce : IMaximumAbsoluteSumOfAnySubarray
+{
+    /// <summary>
+    ///     Time complexity - O(n^2)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MaxAbsoluteSum(int[] nums)
+    {
+        var maxAbsoluteSum = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var currentSum = 0;
+
+            for (var j = i; j < nums.Length; j++)
+            {
+                currentSum += nums[j];
+
+                maxAbsoluteSum = Math.Max(maxAbsoluteSum, Math.Abs(currentSum));
+            }
+        }
+
+        return maxAbsoluteSum;
+    }
+}
