39a6793b1cb0099f86784b3b0cd353ad7dcb8eb6
eugene <eremeeveugene@gmail.com>
2025-02-28 12:37:35 +0200

    The Two Sneaky Numbers of Digitville (#1137)

---
+++ source/LeetCode/Algorithms/TheTwoSneakyNumbersOfDigitville/TheTwoSneakyNumbersOfDigitvilleArray.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.TheTwoSneakyNumbersOfDigitville;
+
+/// <inheritdoc />
+public class TheTwoSneakyNumbersOfDigitvilleArray : ITheTwoSneakyNumbersOfDigitville
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int[] GetSneakyNumbers(int[] nums)
+    {
+        var result = new int[2];
+        var resultIndex = 0;
+
+        var numbers = new bool[100];
+
+        foreach (var num in nums)
+        {
+            if (numbers[num])
+            {
+                result[resultIndex] = num;
+
+                resultIndex++;
+            }
+            else
+            {
+                numbers[num] = true;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ source/LeetCode/Algorithms/TheTwoSneakyNumbersOfDigitville/TheTwoSneakyNumbersOfDigitvilleHashSet.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.TheTwoSneakyNumbersOfDigitville;
+
+/// <inheritdoc />
+public class TheTwoSneakyNumbersOfDigitvilleHashSet : ITheTwoSneakyNumbersOfDigitville
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int[] GetSneakyNumbers(int[] nums)
+    {
+        var result = new int[2];
+        var resultIndex = 0;
+
+        var numbersHashSet = new HashSet<int>();
+
+        foreach (var num in nums)
+        {
+            if (numbersHashSet.Add(num))
+            {
+                continue;
+            }
+
+            result[resultIndex] = num;
+
+            resultIndex++;
+        }
+
+        return result;
+    }
+}
