e430d2de0adcb8feec3d0ab966fe0f75f888b64f
eugene <eremeeveugene@gmail.com>
2025-03-02 16:44:54 +0200

    Merge Two 2D Arrays by Summing Values (#1141)

---
+++ source/LeetCode/Algorithms/MergeTwo2DArraysBySummingValues/IMergeTwo2DArraysBySummingValues.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MergeTwo2DArraysBySummingValues;
+
+/// <summary>
+///     /https://leetcode.com/problems/merge-two-2d-arrays-by-summing-values/description/
+/// </summary>
+public interface IMergeTwo2DArraysBySummingValues
+{
+    int[][] MergeArrays(int[][] nums1, int[][] nums2);
+}
