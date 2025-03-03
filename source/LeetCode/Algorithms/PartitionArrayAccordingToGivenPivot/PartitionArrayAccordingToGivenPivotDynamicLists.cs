398ac48e4d02bf23d4e631a35279e4116e3d7f4b
eugene <eremeeveugene@gmail.com>
2025-03-03 14:33:46 +0200

    Partition Array According to Given Pivot (#1143)

---
+++ source/LeetCode/Algorithms/PartitionArrayAccordingToGivenPivot/PartitionArrayAccordingToGivenPivotDynamicLists.cs
@@ -0,0 +1,76 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.PartitionArrayAccordingToGivenPivot;
+
+/// <inheritdoc />
+public class PartitionArrayAccordingToGivenPivotDynamicLists : IPartitionArrayAccordingToGivenPivot
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="pivot"></param>
+    /// <returns></returns>
+    public int[] PivotArray(int[] nums, int pivot)
+    {
+        var lessThanPivotList = new List<int>();
+        var equalToPivotList = new List<int>();
+        var greaterThanPivotList = new List<int>();
+
+        foreach (var num in nums)
+        {
+            if (num < pivot)
+            {
+                lessThanPivotList.Add(num);
+            }
+            else if (num > pivot)
+            {
+                greaterThanPivotList.Add(num);
+            }
+            else
+            {
+                equalToPivotList.Add(num);
+            }
+        }
+
+        var lessThanPivotListIndex = 0;
+
+        while (lessThanPivotListIndex < lessThanPivotList.Count)
+        {
+            nums[lessThanPivotListIndex] = lessThanPivotList[lessThanPivotListIndex];
+
+            lessThanPivotListIndex++;
+        }
+
+        var equalToPivotListIndex = 0;
+
+        while (equalToPivotListIndex < equalToPivotList.Count)
+        {
+            nums[equalToPivotListIndex + lessThanPivotListIndex] = equalToPivotList[equalToPivotListIndex];
+
+            equalToPivotListIndex++;
+        }
+
+        var greaterThanPivotListIndex = 0;
+
+        while (greaterThanPivotListIndex < greaterThanPivotList.Count)
+        {
+            nums[greaterThanPivotListIndex + equalToPivotListIndex + lessThanPivotListIndex] =
+                greaterThanPivotList[greaterThanPivotListIndex];
+
+            greaterThanPivotListIndex++;
+        }
+
+        return nums;
+    }
+}
---
+++ source/LeetCode/Algorithms/PartitionArrayAccordingToGivenPivot/PartitionArrayAccordingToGivenPivotTwoPointers.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.PartitionArrayAccordingToGivenPivot;
+
+/// <inheritdoc />
+public class PartitionArrayAccordingToGivenPivotTwoPointers : IPartitionArrayAccordingToGivenPivot
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="pivot"></param>
+    /// <returns></returns>
+    public int[] PivotArray(int[] nums, int pivot)
+    {
+        var result = new int[nums.Length];
+        var leftIndex = 0;
+        var rightIndex = nums.Length - 1;
+
+        for (int i = 0, j = nums.Length - 1; i < nums.Length; i++, j--)
+        {
+            if (nums[i] < pivot)
+            {
+                result[leftIndex] = nums[i];
+                leftIndex++;
+            }
+
+            if (nums[j] > pivot)
+            {
+                result[rightIndex] = nums[j];
+                rightIndex--;
+            }
+        }
+
+        while (leftIndex <= rightIndex)
+        {
+            result[leftIndex] = pivot;
+            leftIndex++;
+        }
+
+        return result;
+    }
+}
