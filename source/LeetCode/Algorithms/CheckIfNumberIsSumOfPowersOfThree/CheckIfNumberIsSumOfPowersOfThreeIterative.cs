e311095d2b5e6088fba346f7366d70796a3d5e9a
eugene <eremeeveugene@gmail.com>
2025-03-04 13:44:23 +0200

    Check if Number is a Sum of Powers of Three

---
+++ source/LeetCode/Algorithms/CheckIfNumberIsSumOfPowersOfThree/CheckIfNumberIsSumOfPowersOfThreeIterative.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CheckIfNumberIsSumOfPowersOfThree;
+
+/// <inheritdoc />
+public class CheckIfNumberIsSumOfPowersOfThreeIterative : ICheckIfNumberIsSumOfPowersOfThree
+{
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public bool CheckPowersOfThree(int n)
+    {
+        for (var i = 14; i >= 0; i--)
+        {
+            var pow = (int)Math.Pow(3, i);
+
+            if (n - pow >= 0)
+            {
+                n -= pow;
+            }
+        }
+
+        return n == 0;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CheckIfNumberIsSumOfPowersOfThree/CheckIfNumberIsSumOfPowersOfThreeTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CheckIfNumberIsSumOfPowersOfThree;
+
+namespace LeetCode.Tests.Algorithms.CheckIfNumberIsSumOfPowersOfThree;
+
+public abstract class CheckIfNumberIsSumOfPowersOfThreeTestsBase<T> where T : ICheckIfNumberIsSumOfPowersOfThree, new()
+{
+    [TestMethod]
+    [DataRow(12, true)]
+    [DataRow(21, false)]
+    [DataRow(91, true)]
+    public void CheckPowersOfThree_WithGivenNumber_ReturnsWhetherItCanBeExpressedAsPowersOfThree(int n,
+        bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CheckPowersOfThree(n);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
