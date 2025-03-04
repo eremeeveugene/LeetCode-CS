3b1c9246322f23169f3f0671f7b4f0dcd22922c7
eugene <eremeeveugene@gmail.com>
2025-03-04 13:49:20 +0200

    Check if Number is a Sum of Powers of Three (#1145)

--- README.md
+++ README.md
@@ -379,6 +379,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [1760. Minimum Limit of Balls in a Bag](https://leetcode.com/problems/minimum-limit-of-balls-in-a-bag/description/)
 - [1768. Merge Strings Alternately](https://leetcode.com/problems/merge-strings-alternately/description/)
 - [1769. Minimum Number of Operations to Move All Balls to Each Box](https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/description/)
+- [1780. Check if Number is a Sum of Powers of Three](https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/description/)
 - [1790. Check if One String Swap Can Make Strings Equal](https://leetcode.com/problems/check-if-one-string-swap-can-make-strings-equal/description/)
 - [1791. Find Center of Star Graph](https://leetcode.com/problems/find-center-of-star-graph/description/)
 - [1792. Maximum Average Pass Ratio](https://leetcode.com/problems/maximum-average-pass-ratio/description/)
---
+++ source/LeetCode/Algorithms/CheckIfNumberIsSumOfPowersOfThree/CheckIfNumberIsSumOfPowersOfThreeBase3Check.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CheckIfNumberIsSumOfPowersOfThree;
+
+/// <inheritdoc />
+public class CheckIfNumberIsSumOfPowersOfThreeBase3Check : ICheckIfNumberIsSumOfPowersOfThree
+{
+    /// <summary>
+    ///     Time complexity - O(log3(n))
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public bool CheckPowersOfThree(int n)
+    {
+        while (n > 0)
+        {
+            if (n % 3 == 2)
+            {
+                return false;
+            }
+
+            n /= 3;
+        }
+
+        return true;
+    }
+}
