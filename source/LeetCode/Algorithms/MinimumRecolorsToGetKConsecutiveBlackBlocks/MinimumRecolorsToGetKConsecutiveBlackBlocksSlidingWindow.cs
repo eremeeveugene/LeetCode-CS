cc74d049ade20e236c2e252a058d2fa5206795ea
eugene <eremeeveugene@gmail.com>
2025-03-08 17:40:44 +0200

    Minimum Recolors to Get K Consecutive Black Blocks

---
+++ source/LeetCode/Algorithms/MinimumRecolorsToGetKConsecutiveBlackBlocks/MinimumRecolorsToGetKConsecutiveBlackBlocksSlidingWindow.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumRecolorsToGetKConsecutiveBlackBlocks;
+
+/// <inheritdoc />
+public class MinimumRecolorsToGetKConsecutiveBlackBlocksSlidingWindow : IMinimumRecolorsToGetKConsecutiveBlackBlocks
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="blocks"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int MinimumRecolors(string blocks, int k)
+    {
+        var blackCount = 0;
+
+        for (var i = 0; i < k; i++)
+        {
+            if (blocks[i] == 'B')
+            {
+                blackCount++;
+            }
+        }
+
+        var minimumRecolors = k - blackCount;
+
+        for (var i = k; i < blocks.Length; i++)
+        {
+            if (blocks[i] == 'B')
+            {
+                blackCount++;
+            }
+
+            if (blocks[i - k] == 'B')
+            {
+                blackCount--;
+            }
+
+            minimumRecolors = Math.Min(minimumRecolors, k - blackCount);
+
+            if (minimumRecolors == 0)
+            {
+                return 0;
+            }
+        }
+
+        return minimumRecolors;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumRecolorsToGetKConsecutiveBlackBlocks/MinimumRecolorsToGetKConsecutiveBlackBlocksTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumRecolorsToGetKConsecutiveBlackBlocks;
+
+namespace LeetCode.Tests.Algorithms.MinimumRecolorsToGetKConsecutiveBlackBlocks;
+
+public abstract class MinimumRecolorsToGetKConsecutiveBlackBlocksTestsBase<T>
+    where T : IMinimumRecolorsToGetKConsecutiveBlackBlocks, new()
+{
+    [TestMethod]
+    [DataRow("WBBWWBBWBW", 7, 3)]
+    [DataRow("WBWBBBW", 2, 0)]
+    [DataRow("BWWWBB", 6, 3)]
+    public void MinimumRecolors_WithBlocksAndK_ReturnsMinimumRecolorCount(string blocks, int k, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinimumRecolors(blocks, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
