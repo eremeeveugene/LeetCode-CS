2ff2072f1580614a36fe5e0427751f61cac8ff20
eugene <eremeeveugene@gmail.com>
2025-03-09 16:47:54 +0200

    Alternating Groups II (#1155)

---
+++ source/LeetCode/Algorithms/AlternatingGroups2/AlternatingGroups2BruteForce.cs
@@ -0,0 +1,55 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.AlternatingGroups2;
+
+/// <inheritdoc />
+public class AlternatingGroups2BruteForce : IAlternatingGroups2
+{
+    /// <summary>
+    ///     Time complexity - O(n * k)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="colors"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int NumberOfAlternatingGroups(int[] colors, int k)
+    {
+        var numberOfAlternatingGroups = 0;
+
+        for (var i = 0; i < colors.Length; i++)
+        {
+            var isAlternating = true;
+
+            for (var j = 0; j < k - 1; j++)
+            {
+                var curr = colors[(i + j) % colors.Length];
+                var next = colors[(i + j + 1) % colors.Length];
+
+                if (curr != next)
+                {
+                    continue;
+                }
+
+                isAlternating = false;
+
+                break;
+            }
+
+            if (isAlternating)
+            {
+                numberOfAlternatingGroups++;
+            }
+        }
+
+        return numberOfAlternatingGroups;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/AlternatingGroups2/AlternatingGroups2TestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.AlternatingGroups2;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.AlternatingGroups2;
+
+public abstract class AlternatingGroups2TestsBase<T> where T : IAlternatingGroups2, new()
+{
+    [TestMethod]
+    [DataRow("[0,1,0,1,0]", 3, 3)]
+    [DataRow("[0,1,0,0,1,0,1]", 6, 2)]
+    [DataRow("[1,1,0,1]", 4, 0)]
+    public void NumberOfAlternatingGroups_WithColorsAndK_ReturnsTheNumberOfAlternatingGroups(string colorsJsonArray,
+        int k, int expectedResult)
+    {
+        // Arrange
+        var colors = JsonHelper<int>.DeserializeToArray(colorsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.NumberOfAlternatingGroups(colors, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
