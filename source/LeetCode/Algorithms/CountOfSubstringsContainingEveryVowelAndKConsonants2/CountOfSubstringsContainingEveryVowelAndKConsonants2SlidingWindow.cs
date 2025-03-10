6c2db0f30c41a00e13679ff5c1d4e87826b4c8f3
eugene <eremeeveugene@gmail.com>
2025-03-10 13:57:17 +0200

    Count of Substrings Containing Every Vowel and K Consonants II

---
+++ source/LeetCode/Algorithms/CountOfSubstringsContainingEveryVowelAndKConsonants2/CountOfSubstringsContainingEveryVowelAndKConsonants2SlidingWindow.cs
@@ -0,0 +1,88 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountOfSubstringsContainingEveryVowelAndKConsonants2;
+
+/// <inheritdoc />
+public class CountOfSubstringsContainingEveryVowelAndKConsonants2SlidingWindow :
+    ICountOfSubstringsContainingEveryVowelAndKConsonants2
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="word"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public long CountOfSubstrings(string word, int k)
+    {
+        return AtLeastK(word, k) - AtLeastK(word, k + 1);
+    }
+
+    private static long AtLeastK(string word, int k)
+    {
+        long count = 0;
+
+        var start = 0;
+        var end = 0;
+
+        var vowelDictionary = new Dictionary<char, int>();
+        var consonantCount = 0;
+
+        while (end < word.Length)
+        {
+            var newLetter = word[end];
+
+            if (IsVowel(newLetter))
+            {
+                vowelDictionary.TryAdd(newLetter, 0);
+
+                vowelDictionary[newLetter]++;
+            }
+            else
+            {
+                consonantCount++;
+            }
+
+            while (vowelDictionary.Count == 5 && consonantCount >= k)
+            {
+                count += word.Length - end;
+
+                var startLetter = word[start];
+
+                if (IsVowel(startLetter))
+                {
+                    vowelDictionary[startLetter]--;
+
+                    if (vowelDictionary[startLetter] == 0)
+                    {
+                        vowelDictionary.Remove(startLetter);
+                    }
+                }
+                else
+                {
+                    consonantCount--;
+                }
+
+                start++;
+            }
+
+            end++;
+        }
+
+        return count;
+    }
+
+    private static bool IsVowel(char c)
+    {
+        return c is 'a' or 'e' or 'i' or 'o' or 'u';
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountOfSubstringsContainingEveryVowelAndKConsonants2/CountOfSubstringsContainingEveryVowelAndKConsonants2TestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountOfSubstringsContainingEveryVowelAndKConsonants2;
+
+namespace LeetCode.Tests.Algorithms.CountOfSubstringsContainingEveryVowelAndKConsonants2;
+
+public abstract class CountOfSubstringsContainingEveryVowelAndKConsonants2TestsBase<T> where T :
+    ICountOfSubstringsContainingEveryVowelAndKConsonants2, new()
+{
+    [TestMethod]
+    [DataRow("aeioqq", 1, 0)]
+    [DataRow("aeiou", 0, 1)]
+    [DataRow("ieaouqqieaouqq", 1, 3)]
+    [DataRow("iqeaouqi", 2, 3)]
+    public void CountOfSubstrings_WithWordAndK_ReturnsSubstringCount(string word, int k, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountOfSubstrings(word, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
