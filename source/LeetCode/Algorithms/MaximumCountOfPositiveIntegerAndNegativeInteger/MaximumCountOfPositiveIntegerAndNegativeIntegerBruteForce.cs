33e3acbae40138b09f9ea0b60c526f8e82cd0c68
eugene <eremeeveugene@gmail.com>
2025-03-12 17:38:26 +0200

    Maximum Count of Positive Integer and Negative Integer (#1160)

---
+++ source/LeetCode/Algorithms/MaximumCountOfPositiveIntegerAndNegativeInteger/MaximumCountOfPositiveIntegerAndNegativeIntegerBruteForce.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumCountOfPositiveIntegerAndNegativeInteger;
+
+/// <inheritdoc />
+public class MaximumCountOfPositiveIntegerAndNegativeIntegerBruteForce :
+    IMaximumCountOfPositiveIntegerAndNegativeInteger
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MaximumCount(int[] nums)
+    {
+        var negativeCount = 0;
+        var positiveCount = 0;
+
+        foreach (var num in nums)
+        {
+            switch (num)
+            {
+                case < 0:
+                    negativeCount++;
+                    break;
+                case > 0:
+                    positiveCount++;
+                    break;
+            }
+        }
+
+        return Math.Max(negativeCount, positiveCount);
+    }
+}
---
+++ source/LeetCode/Algorithms/MaximumCountOfPositiveIntegerAndNegativeInteger/MaximumCountOfPositiveIntegerAndNegativeIntegerCounting.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumCountOfPositiveIntegerAndNegativeInteger;
+
+/// <inheritdoc />
+public class MaximumCountOfPositiveIntegerAndNegativeIntegerCounting : IMaximumCountOfPositiveIntegerAndNegativeInteger
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MaximumCount(int[] nums)
+    {
+        var negativeCount = 0;
+
+        while (negativeCount < nums.Length && nums[negativeCount] < 0)
+        {
+            negativeCount++;
+        }
+
+        var zeroCount = 0;
+
+        while (zeroCount + negativeCount < nums.Length && nums[zeroCount + negativeCount] == 0)
+        {
+            zeroCount++;
+        }
+
+        var positiveCount = nums.Length - zeroCount - negativeCount;
+
+        return Math.Max(negativeCount, positiveCount);
+    }
+}
