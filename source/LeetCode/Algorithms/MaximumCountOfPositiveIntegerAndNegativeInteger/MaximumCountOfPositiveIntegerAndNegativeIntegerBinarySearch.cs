95138ce33aaa9330a74c4881a6dfd8f5b4486526
eugene <eremeeveugene@gmail.com>
2025-03-12 17:24:23 +0200

    Maximum Count of Positive Integer and Negative Integer

---
+++ source/LeetCode/Algorithms/MaximumCountOfPositiveIntegerAndNegativeInteger/MaximumCountOfPositiveIntegerAndNegativeIntegerBinarySearch.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumCountOfPositiveIntegerAndNegativeInteger;
+
+/// <inheritdoc />
+public class MaximumCountOfPositiveIntegerAndNegativeIntegerBinarySearch :
+    IMaximumCountOfPositiveIntegerAndNegativeInteger
+{
+    /// <summary>
+    ///     Time complexity - O(log n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MaximumCount(int[] nums)
+    {
+        var negativeCount = FindFirstIndex(nums, 0);
+        var positiveCount = nums.Length - FindFirstIndex(nums, 1);
+
+        return Math.Max(negativeCount, positiveCount);
+    }
+
+    private static int FindFirstIndex(int[] nums, int target)
+    {
+        var left = 0;
+        var right = nums.Length;
+
+        while (left < right)
+        {
+            var mid = left + ((right - left) / 2);
+
+            if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MaximumCountOfPositiveIntegerAndNegativeInteger/MaximumCountOfPositiveIntegerAndNegativeIntegerTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaximumCountOfPositiveIntegerAndNegativeInteger;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MaximumCountOfPositiveIntegerAndNegativeInteger;
+
+public abstract class MaximumCountOfPositiveIntegerAndNegativeIntegerTestsBase<T>
+    where T : IMaximumCountOfPositiveIntegerAndNegativeInteger, new()
+{
+    [TestMethod]
+    [DataRow("[-2,-1,-1,1,2,3]", 3)]
+    [DataRow("[-3,-2,-1,0,0,1,2]", 3)]
+    [DataRow("[5,20,66,1314]", 4)]
+    public void MaximumCount_GivenIntegerArray_ReturnsCountOfMaxPositiveOrNegativeNumbers(string numsJsonArray,
+        int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaximumCount(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
