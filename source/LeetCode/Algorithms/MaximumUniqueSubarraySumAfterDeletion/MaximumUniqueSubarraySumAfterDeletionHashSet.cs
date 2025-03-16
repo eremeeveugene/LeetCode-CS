8abef1f4e3a24856040b6f2bd44415b7486a6cc8
eugene <eremeeveugene@gmail.com>
2025-03-16 12:46:41 +0200

    Maximum Unique Subarray Sum After Deletion

---
+++ source/LeetCode/Algorithms/MaximumUniqueSubarraySumAfterDeletion/MaximumUniqueSubarraySumAfterDeletionHashSet.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumUniqueSubarraySumAfterDeletion;
+
+/// <inheritdoc />
+public class MaximumUniqueSubarraySumAfterDeletionHashSet : IMaximumUniqueSubarraySumAfterDeletion
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MaxSum(int[] nums)
+    {
+        var maxSum = 0;
+        var maxElement = int.MinValue;
+
+        var numsHashSet = new HashSet<int>();
+
+        foreach (var num in nums)
+        {
+            maxElement = Math.Max(maxElement, num);
+
+            if (num > 0 && numsHashSet.Add(num))
+            {
+                maxSum += num;
+            }
+        }
+
+        return maxSum == 0 ? maxElement : maxSum;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MaximumUniqueSubarraySumAfterDeletion/MaximumUniqueSubarraySumAfterDeletionTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaximumUniqueSubarraySumAfterDeletion;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MaximumUniqueSubarraySumAfterDeletion;
+
+public abstract class MaximumUniqueSubarraySumAfterDeletionTestsBase<T>
+    where T : IMaximumUniqueSubarraySumAfterDeletion, new()
+{
+    [TestMethod]
+    [DataRow("[1,2,3,4,5]", 15)]
+    [DataRow("[1,1,0,1,1]", 1)]
+    [DataRow("[1,2,-1,-2,1,0,-1]", 3)]
+    public void MaxSum_WithIntegerArray_ReturnsMaximumSubarraySum(string numsJsonArray, int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaxSum(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
