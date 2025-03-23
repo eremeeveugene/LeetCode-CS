972730e14cc40f6cf5a3a4f37a72fd59894a7805
eugene <eremeeveugene@gmail.com>
2025-03-23 17:24:41 +0200

    Minimum Operations to Make Binary Array Elements Equal to One I

---
+++ source/LeetCode/Algorithms/MinimumOperationsToMakeBinaryArrayElementsEqualToOne1/MinimumOperationsToMakeBinaryArrayElementsEqualToOne1Greedy.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumOperationsToMakeBinaryArrayElementsEqualToOne1;
+
+/// <inheritdoc />
+public class MinimumOperationsToMakeBinaryArrayElementsEqualToOne1Greedy :
+    IMinimumOperationsToMakeBinaryArrayElementsEqualToOne1
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MinOperations(int[] nums)
+    {
+        var minOperations = 0;
+
+        for (var i = 0; i < nums.Length - 2; i++)
+        {
+            if (nums[i] == 1)
+            {
+                continue;
+            }
+
+            nums[i] = 1;
+            nums[i + 1] ^= 1;
+            nums[i + 2] ^= 1;
+
+            minOperations++;
+        }
+
+        if (nums[^1] == 0 || nums[^2] == 0)
+        {
+            return -1;
+        }
+
+        return minOperations;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumOperationsToMakeBinaryArrayElementsEqualToOne1/MinimumOperationsToMakeBinaryArrayElementsEqualToOne1TestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumOperationsToMakeBinaryArrayElementsEqualToOne1;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MinimumOperationsToMakeBinaryArrayElementsEqualToOne1;
+
+public abstract class MinimumOperationsToMakeBinaryArrayElementsEqualToOne1TestsBase<T>
+    where T : IMinimumOperationsToMakeBinaryArrayElementsEqualToOne1, new()
+{
+    [TestMethod]
+    [DataRow("[0,1,1,1,0,0]", 3)]
+    [DataRow("[0,1,1,1]", -1)]
+    [DataRow("[1,0,0,1,1,0,1,1,1]", -1)]
+    public void MinOperations_WithBinaryArray_ReturnsMinimumOperationsOrMinusOne(string numsJsonArray,
+        int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinOperations(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
