4e17e9f343db6bfc821622c06f952e1cc5b0acaa
eugene <eremeeveugene@gmail.com>
2025-03-24 13:19:44 +0200

    Count Days Without Meeting

---
+++ source/Tests/LeetCode.Tests/Algorithms/CountDaysWithoutMeetings/CountDaysWithoutMeetingsTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountDaysWithoutMeetings;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.CountDaysWithoutMeetings;
+
+public abstract class CountDaysWithoutMeetingsTestsBase<T> where T : ICountDaysWithoutMeetings, new()
+{
+    [TestMethod]
+    [DataRow(10, "[[5,7],[1,3],[9,10]]", 2)]
+    [DataRow(5, "[[2,4],[1,3]]", 1)]
+    [DataRow(6, "[[1,6]]", 0)]
+    public void CountDays_WithTotalDaysAndMeetingRanges_ReturnsNumberOfFreeDays(int days, string meetingsJsonArray,
+        int expectedResult)
+    {
+        // Arrange
+        var meetings = JsonHelper<int>.DeserializeToJaggedArray(meetingsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountDays(days, meetings);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
