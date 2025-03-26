487195021416a3774af3cccdd0c24d2082259d30
eugene <eremeeveugene@gmail.com>
2025-03-26 14:06:48 +0200

    Minimum Operations to Make a Uni-Value Grid (#1187)

---
+++ source/LeetCode/Algorithms/MinimumOperationsToMakeUniValueGrid/MinimumOperationsToMakeUniValueGridSorting.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumOperationsToMakeUniValueGrid;
+
+/// <inheritdoc />
+public class MinimumOperationsToMakeUniValueGridSorting : IMinimumOperationsToMakeUniValueGrid
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(log n)
+    /// </summary>
+    /// <param name="grid"></param>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public int MinOperations(int[][] grid, int x)
+    {
+        var rows = grid.Length;
+        var cols = grid[0].Length;
+        var remainder = grid[0][0] % x;
+
+        var items = new List<int>(rows * cols);
+
+        foreach (var row in grid)
+        {
+            foreach (var cell in row)
+            {
+                if (cell % x != remainder)
+                {
+                    return -1;
+                }
+
+                items.Add(cell);
+            }
+        }
+
+        items.Sort();
+
+        var median = items[items.Count / 2];
+
+        return items.Sum(item => Math.Abs(item - median) / x);
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumOperationsToMakeUniValueGrid/MinimumOperationsToMakeUniValueGridTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumOperationsToMakeUniValueGrid;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MinimumOperationsToMakeUniValueGrid;
+
+public abstract class MinimumOperationsToMakeUniValueGridTestsBase<T>
+    where T : IMinimumOperationsToMakeUniValueGrid, new()
+{
+    [TestMethod]
+    [DataRow("[[2,4],[6,8]]", 2, 4)]
+    [DataRow("[[1,5],[2,3]]", 1, 5)]
+    [DataRow("[[1,2],[3,4]]", 2, -1)]
+    public void MinOperations_WithGridAndOperationValue_ReturnsMinimumOperationsToMakeUniValueGrid(string gridJsonArray,
+        int x, int expectedResult)
+    {
+        // Arrange
+        var grid = JsonHelper<int>.DeserializeToJaggedArray(gridJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinOperations(grid, x);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
