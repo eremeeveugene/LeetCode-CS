3f1aa99b7f85b0e77e8b3a5bf0da4b95bb6e0216
eugene <eremeeveugene@gmail.com>
2025-03-27 16:34:41 +0200

    Minimum Index of a Valid Split (#1189)

---
+++ source/LeetCode/Algorithms/MinimumIndexOfValidSplit/MinimumIndexOfValidSplitDictionary.cs
@@ -0,0 +1,62 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumIndexOfValidSplit;
+
+/// <inheritdoc />
+public class MinimumIndexOfValidSplitDictionary : IMinimumIndexOfValidSplit
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MinimumIndex(IList<int> nums)
+    {
+        var frequencyDictionary = new Dictionary<int, int>();
+        var dominant = nums[0];
+        var dominantFreq = 0;
+
+        foreach (var num in nums)
+        {
+            frequencyDictionary[num] = frequencyDictionary.GetValueOrDefault(num) + 1;
+
+            if (frequencyDictionary[num] <= dominantFreq)
+            {
+                continue;
+            }
+
+            dominant = num;
+            dominantFreq = frequencyDictionary[num];
+        }
+
+        var leftDominantCount = 0;
+
+        for (var i = 0; i < nums.Count - 1; i++)
+        {
+            if (nums[i] == dominant)
+            {
+                leftDominantCount++;
+            }
+
+            var leftIsDominant = leftDominantCount * 2 > i + 1;
+            var rightIsDominant = (dominantFreq - leftDominantCount) * 2 > nums.Count - i - 1;
+
+            if (leftIsDominant && rightIsDominant)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumIndexOfValidSplit/MinimumIndexOfValidSplitTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumIndexOfValidSplit;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MinimumIndexOfValidSplit;
+
+public abstract class MinimumIndexOfValidSplitTestsBase<T> where T : IMinimumIndexOfValidSplit, new()
+{
+    [TestMethod]
+    [DataRow("[1,2,2,2]", 2)]
+    [DataRow("[2,1,3,1,1,1,7,1,2,1]", 4)]
+    [DataRow("[3,3,3,3,7,2,2]", -1)]
+    public void MinimumIndex_WithMajorityElementInArray_ReturnsMinimumIndexOfValidSplit(string numsJsonArray,
+        int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinimumIndex(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
