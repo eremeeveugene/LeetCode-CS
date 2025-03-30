ac7ba45a15fac15ce28cf6805e6b92e01c9a2fb4
eugene <eremeeveugene@gmail.com>
2025-03-30 16:55:47 +0300

    Partition Labels

---
+++ source/LeetCode/Algorithms/PartitionLabels/IPartitionLabels.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.PartitionLabels;
+
+/// <summary>
+///     https://leetcode.com/problems/partition-labels/description/
+/// </summary>
+public interface IPartitionLabels
+{
+    IList<int> PartitionLabels(string s);
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/PartitionLabels/PartitionLabelsTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.PartitionLabels;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.PartitionLabels;
+
+public abstract class PartitionLabelsTestsBase<T> where T : IPartitionLabels, new()
+{
+    [TestMethod]
+    [DataRow("ababcbacadefegdehijhklij", "[9,7,8]")]
+    [DataRow("eccbbbbdec", "[10]")]
+    public void PartitionLabels_GivenString_ReturnsPartitionSizesWhereEachLetterAppearsOnce(string s,
+        string expectedResultJsonArray)
+    {
+        // Arrange
+        var expectedResult = JsonHelper<int>.DeserializeToArray(expectedResultJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.PartitionLabels(s);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult.ToArray());
+    }
+}
