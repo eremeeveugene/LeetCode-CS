3721aab1bc247e1a1a1922afb657f59e10765411
eugene <eremeeveugene@gmail.com>
2025-03-30 17:01:28 +0300

    Partition Labels (#1193)

--- README.md
+++ README.md
@@ -233,6 +233,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [733. Flood Fill](https://leetcode.com/problems/flood-fill/description/)
 - [746. Min Cost Climbing Stairs](https://leetcode.com/problems/min-cost-climbing-stairs/description/)
 - [752. Open the Lock](https://leetcode.com/problems/open-the-lock/description/)
+- [763. Partition Labels](https://leetcode.com/problems/partition-labels/description/)
 - [769. Max Chunks To Make Sorted](https://leetcode.com/problems/max-chunks-to-make-sorted/description/)
 - [771. Jewels and Stones](https://leetcode.com/problems/jewels-and-stones/description/)
 - [773. Sliding Puzzle](https://leetcode.com/problems/sliding-puzzle/description/)
---
+++ source/LeetCode/Algorithms/PartitionLabels/PartitionLabelsTwoPointers.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.PartitionLabels;
+
+/// <inheritdoc />
+public class PartitionLabelsTwoPointers : IPartitionLabels
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public IList<int> PartitionLabels(string s)
+    {
+        var lastIndex = new int[26];
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            lastIndex[s[i] - 'a'] = i;
+        }
+
+        var result = new List<int>();
+
+        var start = 0;
+        var end = 0;
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            end = Math.Max(end, lastIndex[s[i] - 'a']);
+
+            if (i != end)
+            {
+                continue;
+            }
+
+            result.Add(end - start + 1);
+
+            start = i + 1;
+        }
+
+        return result;
+    }
+}
