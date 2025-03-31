0ea137833f6c1a2be6354a6c6718b1ffccafa428
eugene <eremeeveugene@gmail.com>
2025-03-31 13:16:18 +0300

    Minimum Cost to Reach Every Position

---
+++ source/LeetCode/Algorithms/MinimumCostToReachEveryPosition/MinimumCostToReachEveryPositionGreedy.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumCostToReachEveryPosition;
+
+/// <inheritdoc />
+public class MinimumCostToReachEveryPositionGreedy : IMinimumCostToReachEveryPosition
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="cost"></param>
+    /// <returns></returns>
+    public int[] MinCosts(int[] cost)
+    {
+        var minCost = int.MaxValue;
+
+        for (var i = 0; i < cost.Length; i++)
+        {
+            minCost = Math.Min(minCost, cost[i]);
+
+            cost[i] = minCost;
+        }
+
+        return cost;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumCostToReachEveryPosition/MinimumCostToReachEveryPositionTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumCostToReachEveryPosition;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MinimumCostToReachEveryPosition;
+
+public abstract class MinimumCostToReachEveryPositionTestsBase<T> where T : IMinimumCostToReachEveryPosition, new()
+{
+    [TestMethod]
+    [DataRow("[5,3,4,1,3,2]", "[5,3,3,1,1,1]")]
+    [DataRow("[1,2,4,6,7]", "[1,1,1,1,1]")]
+    public void MinCosts_WithCostArray_ReturnsMinimumCostAtEachStep(string costJsonArray,
+        string expectedResultJsonArray)
+    {
+        // Arrange
+        var cost = JsonHelper<int>.DeserializeToArray(costJsonArray);
+        var expectedResult = JsonHelper<int>.DeserializeToArray(expectedResultJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinCosts(cost);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
