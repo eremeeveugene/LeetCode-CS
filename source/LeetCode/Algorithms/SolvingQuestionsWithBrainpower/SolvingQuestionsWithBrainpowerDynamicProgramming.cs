2ea877dc14d0554157790292a96be6462560dcbd
eugene <eremeeveugene@gmail.com>
2025-04-01 14:20:33 +0300

    Solving Questions With Brainpower (#1197)

---
+++ source/LeetCode/Algorithms/SolvingQuestionsWithBrainpower/SolvingQuestionsWithBrainpowerDynamicProgramming.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SolvingQuestionsWithBrainpower;
+
+/// <inheritdoc />
+public class SolvingQuestionsWithBrainpowerDynamicProgramming : ISolvingQuestionsWithBrainpower
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="questions"></param>
+    /// <returns></returns>
+    public long MostPoints(int[][] questions)
+    {
+        var maximumPoints = new long[questions.Length];
+
+        for (var currentQuestionIndex = questions.Length - 1; currentQuestionIndex >= 0; currentQuestionIndex--)
+        {
+            var currentQuestionPoints = questions[currentQuestionIndex][0];
+            var currentQuestionBrainpower = questions[currentQuestionIndex][1];
+            var nextSolvableQuestionIndex = currentQuestionIndex + currentQuestionBrainpower + 1;
+
+            long solvePoints = currentQuestionPoints;
+
+            if (nextSolvableQuestionIndex < questions.Length)
+            {
+                solvePoints += maximumPoints[nextSolvableQuestionIndex];
+            }
+
+            long skipPoints = 0;
+
+            var nextQuestionIndex = currentQuestionIndex + 1;
+
+            if (nextQuestionIndex < questions.Length)
+            {
+                skipPoints += maximumPoints[nextQuestionIndex];
+            }
+
+            maximumPoints[currentQuestionIndex] = Math.Max(solvePoints, skipPoints);
+        }
+
+        return maximumPoints[0];
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/SolvingQuestionsWithBrainpower/SolvingQuestionsWithBrainpowerTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SolvingQuestionsWithBrainpower;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.SolvingQuestionsWithBrainpower;
+
+public abstract class SolvingQuestionsWithBrainpowerTestsBase<T> where T : ISolvingQuestionsWithBrainpower, new()
+{
+    [TestMethod]
+    [DataRow("[[3,2],[4,3],[4,4],[2,5]]", 5)]
+    [DataRow("[[1,1],[2,2],[3,3],[4,4],[5,5]]", 7)]
+    public void MostPoints_GivenQuestionsArray_ReturnsMaximumPoints(string questionsJsonArray, long expectedResult)
+    {
+        // Arrange
+        var questions = JsonHelper<int>.DeserializeToJaggedArray(questionsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MostPoints(questions);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
