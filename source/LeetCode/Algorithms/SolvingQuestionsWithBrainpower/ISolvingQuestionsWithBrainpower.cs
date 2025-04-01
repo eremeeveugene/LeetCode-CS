18615aaf07177fa3299ee85ef47b29ecc25b99be
eugene <eremeeveugene@gmail.com>
2025-04-01 14:03:31 +0300

    Solving Questions With Brainpower

---
+++ source/LeetCode/Algorithms/SolvingQuestionsWithBrainpower/ISolvingQuestionsWithBrainpower.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SolvingQuestionsWithBrainpower;
+
+/// <summary>
+///     https://leetcode.com/problems/solving-questions-with-brainpower/description/
+/// </summary>
+public interface ISolvingQuestionsWithBrainpower
+{
+    long MostPoints(int[][] questions);
+}
---
+++ source/LeetCode/Algorithms/SolvingQuestionsWithBrainpower/SolvingQuestionsWithBrainpowerDynamicProgramming.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SolvingQuestionsWithBrainpower;
+
+/// <inheritdoc />
+public class SolvingQuestionsWithBrainpowerDynamicProgramming : ISolvingQuestionsWithBrainpower
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="questions"></param>
+    /// <returns></returns>
+    public long MostPoints(int[][] questions)
+    {
+        var maximumPoints = new long[questions.Length + 1];
+
+        for (var currentQuestionIndex = questions.Length - 1; currentQuestionIndex >= 0; currentQuestionIndex--)
+        {
+            var currentQuestionPoints = questions[currentQuestionIndex][0];
+            var currentQuestionBrainpower = questions[currentQuestionIndex][1];
+            var nextQuestionIndex = currentQuestionIndex + currentQuestionBrainpower + 1;
+
+            long solvePoints = currentQuestionPoints;
+
+            if (nextQuestionIndex < questions.Length)
+            {
+                solvePoints += maximumPoints[nextQuestionIndex];
+            }
+
+            var skipPoints = maximumPoints[currentQuestionIndex + 1];
+
+            maximumPoints[currentQuestionIndex] = Math.Max(solvePoints, skipPoints);
+        }
+
+        return maximumPoints[0];
+    }
+}
