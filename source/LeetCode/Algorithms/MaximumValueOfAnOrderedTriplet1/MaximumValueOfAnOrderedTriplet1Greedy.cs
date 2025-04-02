393b0862b98da8099fd0dd32e5aafab04fec0a40
eugene <eremeeveugene@gmail.com>
2025-04-02 23:05:31 +0300

    Maximum Value of an Ordered Triplet I (#1199)

---
+++ source/LeetCode/Algorithms/MaximumValueOfAnOrderedTriplet1/MaximumValueOfAnOrderedTriplet1Greedy.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumValueOfAnOrderedTriplet1;
+
+/// <inheritdoc />
+public class MaximumValueOfAnOrderedTriplet1Greedy : IMaximumValueOfAnOrderedTriplet1
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public long MaximumTripletValue(int[] nums)
+    {
+        long maximumTripletValue = 0;
+
+        var maxI = 0;
+        long maxDifference = 0;
+
+        foreach (var num in nums)
+        {
+            maximumTripletValue = Math.Max(maximumTripletValue, maxDifference * num);
+
+            maxDifference = Math.Max(maxDifference, maxI - num);
+            maxI = Math.Max(maxI, num);
+        }
+
+        return maximumTripletValue;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MaximumValueOfAnOrderedTriplet1/MaximumValueOfAnOrderedTriplet1TestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaximumValueOfAnOrderedTriplet1;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MaximumValueOfAnOrderedTriplet1;
+
+public abstract class MaximumValueOfAnOrderedTriplet1TestsBase<T> where T : IMaximumValueOfAnOrderedTriplet1, new()
+{
+    [TestMethod]
+    [DataRow("[12,6,1,2,7]", 77)]
+    [DataRow("[1,10,3,4,19]", 133)]
+    [DataRow("[1,2,3]", 0)]
+    [DataRow("[1000000,1,1000000]", 999999000000)]
+    public void MaximumTripletValue_WithIntegerArray_ReturnsMaximumTripletValue(string numsJsonArray,
+        long expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaximumTripletValue(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
