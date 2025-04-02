b336c0011d3fa5cb99c698a73be0735f01425a59
eugene <eremeeveugene@gmail.com>
2025-04-02 23:00:45 +0300

    Maximum Value of an Ordered Triplet I

---
+++ source/LeetCode/Algorithms/MaximumValueOfAnOrderedTriplet1/MaximumValueOfAnOrderedTriplet1BruteForce.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumValueOfAnOrderedTriplet1;
+
+/// <inheritdoc />
+public class MaximumValueOfAnOrderedTriplet1BruteForce : IMaximumValueOfAnOrderedTriplet1
+{
+    /// <summary>
+    ///     Time complexity - O(n^3)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public long MaximumTripletValue(int[] nums)
+    {
+        long maximumTripletValue = 0;
+
+        for (var i = 0; i < nums.Length - 2; i++)
+        {
+            for (var j = i + 1; j < nums.Length - 1; j++)
+            {
+                for (var k = j + 1; k < nums.Length; k++)
+                {
+                    maximumTripletValue = Math.Max(maximumTripletValue, (long)(nums[i] - nums[j]) * nums[k]);
+                }
+            }
+        }
+
+        return maximumTripletValue;
+    }
+}
