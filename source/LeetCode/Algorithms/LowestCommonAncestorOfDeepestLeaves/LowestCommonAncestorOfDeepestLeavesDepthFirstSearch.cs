8f8de2ba2e51adf09365105fe9d684e6f89a5283
eugene <eremeeveugene@gmail.com>
2025-04-04 19:16:56 +0300

    Lowest Common Ancestor of Deepest Leaves

---
+++ source/LeetCode/Algorithms/LowestCommonAncestorOfDeepestLeaves/LowestCommonAncestorOfDeepestLeavesDepthFirstSearch.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.LowestCommonAncestorOfDeepestLeaves;
+
+/// <inheritdoc />
+public class LowestCommonAncestorOfDeepestLeavesDepthFirstSearch : ILowestCommonAncestorOfDeepestLeaves
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="root"></param>
+    /// <returns></returns>
+    public TreeNode LcaDeepestLeaves(TreeNode root)
+    {
+        return FindLcaDeepestLeaves(root).TreeNode!;
+    }
+
+    private static (TreeNode? TreeNode, int Depth) FindLcaDeepestLeaves(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return (null, 0);
+        }
+
+        var left = FindLcaDeepestLeaves(root.left);
+        var right = FindLcaDeepestLeaves(root.right);
+
+        if (left.Depth > right.Depth)
+        {
+            return (left.TreeNode, left.Depth + 1);
+        }
+
+        return right.Depth > left.Depth ? (right.TreeNode, right.Depth + 1) : (root, left.Depth + 1);
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/LowestCommonAncestorOfDeepestLeaves/LowestCommonAncestorOfDeepestLeavesTestsBase.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.LowestCommonAncestorOfDeepestLeaves;
+using LeetCode.Core.Exceptions;
+using LeetCode.Core.Helpers;
+using LeetCode.Core.Models;
+using LeetCode.Tests.Base.Extensions;
+
+namespace LeetCode.Tests.Algorithms.LowestCommonAncestorOfDeepestLeaves;
+
+public abstract class LowestCommonAncestorOfDeepestLeavesTestsBase<T>
+    where T : ILowestCommonAncestorOfDeepestLeaves, new()
+{
+    [TestMethod]
+    [DataRow("[3,5,1,6,2,0,8,null,null,7,4]", "[2,7,4]")]
+    [DataRow("[1]", "[1]")]
+    [DataRow("[0,1,3,null,2]", "[2]")]
+    public void LcaDeepestLeaves_WithBinaryTree_ReturnsLcaOfDeepestLeaves(string rootJsonArray,
+        string expectedResultJsonArray)
+    {
+        // Arrange
+        var solution = new T();
+
+        var rootArray = JsonHelper<int?>.DeserializeToArray(rootJsonArray);
+        var root = TreeNode.ToTreeNode(rootArray) ?? throw new TreeNodeBuildException();
+        var expectedResultArray = JsonHelper<int?>.DeserializeToArray(expectedResultJsonArray);
+        var expectedResult = TreeNode.ToTreeNode(expectedResultArray) ?? throw new TreeNodeBuildException();
+
+        // Act
+        var actualResult = solution.LcaDeepestLeaves(root);
+
+        // Assert
+        TreeNodeAssert.AreEqual(expectedResult, actualResult);
+    }
+}
