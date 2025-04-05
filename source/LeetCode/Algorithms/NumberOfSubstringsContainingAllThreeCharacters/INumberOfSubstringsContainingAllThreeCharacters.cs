a731f8cb592aefe6dbb12d17c127f38aa35e00ac
eugene <eremeeveugene@gmail.com>
2025-04-05 16:30:47 +0300

    Number of Substrings Containing All Three Characters (#1206)

---
+++ source/LeetCode/Algorithms/NumberOfSubstringsContainingAllThreeCharacters/INumberOfSubstringsContainingAllThreeCharacters.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.NumberOfSubstringsContainingAllThreeCharacters;
+
+/// <summary>
+///     https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/description/
+/// </summary>
+public interface INumberOfSubstringsContainingAllThreeCharacters
+{
+    int NumberOfSubstrings(string s);
+}
