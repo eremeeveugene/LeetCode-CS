6362448202f294b60ad5441effccf46391e9618b
eugene <eremeeveugene@gmail.com>
2025-04-07 13:45:28 +0300

    Partition Equal Subset Sum

---
+++ source/LeetCode/Algorithms/PartitionEqualSubsetSum/PartitionEqualSubsetSumDynamicProgramming.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.PartitionEqualSubsetSum;
+
+/// <inheritdoc />
+public class PartitionEqualSubsetSumDynamicProgramming : IPartitionEqualSubsetSum
+{
+    /// <summary>
+    ///     Time complexity - O(n * target)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public bool CanPartition(int[] nums)
+    {
+        var total = nums.Sum();
+
+        if (total % 2 != 0)
+        {
+            return false;
+        }
+
+        var target = total / 2;
+
+        var dp = new bool[target + 1];
+
+        dp[0] = true;
+
+        foreach (var num in nums)
+        {
+            for (var i = target; i >= num; i--)
+            {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+
+        return dp[target];
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/PartitionEqualSubsetSum/PartitionEqualSubsetSumTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.PartitionEqualSubsetSum;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.PartitionEqualSubsetSum;
+
+public abstract class PartitionEqualSubsetSumTestsBase<T> where T : IPartitionEqualSubsetSum, new()
+{
+    [TestMethod]
+    [DataRow("[1,5,11,5]", true)]
+    [DataRow("[1,2,3,5]", false)]
+    public void CanPartition_WithGivenIntegerArray_ReturnsWhetherItCanBePartitionedIntoEqualSumSubsets(
+        string numsJsonArray, bool expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CanPartition(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
