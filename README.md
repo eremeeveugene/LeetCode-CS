af26b5753770655c53f8e3949a79448f67d24a87
eugene <eremeeveugene@gmail.com>
2025-04-07 13:50:39 +0300

    Partition Equal Subset Sum (#1210)

--- README.md
+++ README.md
@@ -170,6 +170,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [409. Longest Palindrome](https://leetcode.com/problems/longest-palindrome/description/)
 - [414. Third Maximum Number](https://leetcode.com/problems/third-maximum-number/description/)
 - [415. Add Strings](https://leetcode.com/problems/add-strings/description/)
+- [416. Partition Equal Subset Sum](https://leetcode.com/problems/partition-equal-subset-sum/description/)
 - [429. N-ary Tree Level Order Traversal](https://leetcode.com/problems/n-ary-tree-level-order-traversal/description/)
 - [432. All O`one Data Structure](https://leetcode.com/problems/all-oone-data-structure/description/)
 - [440. K-th Smallest in Lexicographical Order](https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/)
---
+++ source/LeetCode/Algorithms/PartitionEqualSubsetSum/IPartitionEqualSubsetSum.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.PartitionEqualSubsetSum;
+
+/// <summary>
+///     https://leetcode.com/problems/partition-equal-subset-sum/description/
+/// </summary>
+public interface IPartitionEqualSubsetSum
+{
+    bool CanPartition(int[] nums);
+}
