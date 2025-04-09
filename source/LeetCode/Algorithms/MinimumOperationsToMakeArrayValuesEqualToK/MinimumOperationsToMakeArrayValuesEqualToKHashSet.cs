ee3cc14d6a36e14ba2d9a81e69e09126c7e4496e
eugene <eremeeveugene@gmail.com>
2025-04-09 13:28:27 +0300

    Minimum Operations to Make Array Values Equal to K

---
+++ source/LeetCode/Algorithms/MinimumOperationsToMakeArrayValuesEqualToK/MinimumOperationsToMakeArrayValuesEqualToKHashSet.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumOperationsToMakeArrayValuesEqualToK;
+
+/// <inheritdoc />
+public class MinimumOperationsToMakeArrayValuesEqualToKHashSet : IMinimumOperationsToMakeArrayValuesEqualToK
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int MinOperations(int[] nums, int k)
+    {
+        var numsHashSet = new HashSet<int>();
+
+        foreach (var num in nums)
+        {
+            if (num < k)
+            {
+                return -1;
+            }
+
+            if (num > k)
+            {
+                numsHashSet.Add(num);
+            }
+        }
+
+        return numsHashSet.Count;
+    }
+}
