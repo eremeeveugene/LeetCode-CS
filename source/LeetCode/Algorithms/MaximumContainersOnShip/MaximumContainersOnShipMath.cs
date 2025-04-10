3889b0383461be244d38126c98ef70202ba509b4
eugene <eremeeveugene@gmail.com>
2025-04-10 12:45:38 +0300

    Maximum Containers on a Ship

---
+++ source/LeetCode/Algorithms/MaximumContainersOnShip/MaximumContainersOnShipMath.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumContainersOnShip;
+
+/// <inheritdoc />
+public class MaximumContainersOnShipMath : IMaximumContainersOnShip
+{
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="w"></param>
+    /// <param name="maxWeight"></param>
+    /// <returns></returns>
+    public int MaxContainers(int n, int w, int maxWeight)
+    {
+        return Math.Min(n * n, maxWeight / w);
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MaximumContainersOnShip/MaximumContainersOnShipTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaximumContainersOnShip;
+
+namespace LeetCode.Tests.Algorithms.MaximumContainersOnShip;
+
+public abstract class MaximumContainersOnShipTestsBase<T>
+    where T : IMaximumContainersOnShip, new()
+{
+    [TestMethod]
+    [DataRow(2, 3, 15, 4)]
+    [DataRow(3, 5, 20, 4)]
+    public void MaxContainers_WithNumberOfContainersWeightAndMaxCapacity_ReturnsMaxStackableCount(int n, int w,
+        int maxWeight, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaxContainers(n, w, maxWeight);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
