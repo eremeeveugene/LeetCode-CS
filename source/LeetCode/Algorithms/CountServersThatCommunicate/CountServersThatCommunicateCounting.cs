e3fe41397a1a854facdfed569af0e64a42a1ead3
eugene <eremeeveugene@gmail.com>
2025-04-12 14:26:45 +0300

    Count Servers that Communicate (#1219)

---
+++ source/LeetCode/Algorithms/CountServersThatCommunicate/CountServersThatCommunicateCounting.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountServersThatCommunicate;
+
+/// <inheritdoc />
+public class CountServersThatCommunicateCounting : ICountServersThatCommunicate
+{
+    /// <summary>
+    ///     Time complexity - O(m * n)
+    ///     Space complexity - O(m + n)
+    /// </summary>
+    /// <param name="grid"></param>
+    /// <returns></returns>
+    public int CountServers(int[][] grid)
+    {
+        var m = grid.Length;
+        var n = grid[0].Length;
+
+        var rowCount = new int[m];
+        var colCount = new int[n];
+
+        for (var i = 0; i < m; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                if (grid[i][j] == 0)
+                {
+                    continue;
+                }
+
+                rowCount[i]++;
+                colCount[j]++;
+            }
+        }
+
+        var result = 0;
+
+        for (var i = 0; i < m; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                if (grid[i][j] == 1 && (rowCount[i] > 1 || colCount[j] > 1))
+                {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountServersThatCommunicate/CountServersThatCommunicateTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountServersThatCommunicate;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.CountServersThatCommunicate;
+
+public abstract class CountServersThatCommunicateTestsBase<T> where T : ICountServersThatCommunicate, new()
+{
+    [TestMethod]
+    [DataRow("[[1,0],[0,1]]", 0)]
+    [DataRow("[[1,0],[1,1]]", 3)]
+    [DataRow("[[1,1,0,0],[0,0,1,0],[0,0,1,0],[0,0,0,1]]", 4)]
+    public void CountServers_WithGridInput_ReturnsNumberOfCommunicatingServers(string numsJsonArray, int expectedResult)
+    {
+        // Arrange
+        var grid = JsonHelper<int>.DeserializeToJaggedArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountServers(grid);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
