9d46d77bdbcfa9023b6905522094c89c20fcaf2f
eugene <eremeeveugene@gmail.com>
2025-04-12 13:16:16 +0300

    Find the Winning Player in Coin Game

---
+++ source/LeetCode/Algorithms/FindTheWinningPlayerInCoinGame/IFindTheWinningPlayerInCoinGame.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindTheWinningPlayerInCoinGame;
+
+/// <summary>
+///     https://leetcode.com/problems/find-the-winning-player-in-coin-game/description/
+/// </summary>
+public interface IFindTheWinningPlayerInCoinGame
+{
+    string WinningPlayer(int x, int y);
+}
