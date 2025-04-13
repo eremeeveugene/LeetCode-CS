9406d513a13db098b074876932c1324ad1320618
eugene <eremeeveugene@gmail.com>
2025-04-13 18:41:09 +0300

    Count Good Numbers (#1222)

---
+++ source/LeetCode/Algorithms/CountGoodNumbers/CountGoodNumbersFastExponentiation.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountGoodNumbers;
+
+/// <inheritdoc />
+public class CountGoodNumbersFastExponentiation : ICountGoodNumbers
+{
+    private const int Mod = 1_000_000_007;
+
+    /// <summary>
+    ///     Time complexity - O(log n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public int CountGoodNumbers(long n)
+    {
+        var evenPositions = n / 2;
+        var oddPositions = n - evenPositions;
+
+        return (int)(ModPow(5, oddPositions) * ModPow(4, evenPositions) % Mod);
+    }
+
+    private static long ModPow(long value, long exponent)
+    {
+        long result = 1;
+
+        while (exponent > 0)
+        {
+            if (exponent % 2 == 1)
+            {
+                result = result * value % Mod;
+            }
+
+            value = value * value % Mod;
+
+            exponent /= 2;
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountGoodNumbers/CountGoodNumbersTestsBase.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountGoodNumbers;
+
+namespace LeetCode.Tests.Algorithms.CountGoodNumbers;
+
+public abstract class CountGoodNumbersTestsBase<T> where T : ICountGoodNumbers, new()
+{
+    [TestMethod]
+    [DataRow(1, 5)]
+    [DataRow(2, 20)]
+    [DataRow(3, 100)]
+    [DataRow(4, 400)]
+    [DataRow(5, 2000)]
+    [DataRow(6, 8000)]
+    [DataRow(7, 40000)]
+    [DataRow(8, 160000)]
+    [DataRow(50, 564908303)]
+    public void CountGoodNumbers_WithInputLength_ReturnsTotalNumberOfGoodDigitStings(int n, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountGoodNumbers(n);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
