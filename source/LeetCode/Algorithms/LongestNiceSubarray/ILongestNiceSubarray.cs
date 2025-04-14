f1855a911708c3b8b58bc178e6c4c7cdec691295
eugene <eremeeveugene@gmail.com>
2025-04-14 23:24:59 +0300

    Longest Nice Subarray (#1226)

---
+++ source/LeetCode/Algorithms/LongestNiceSubarray/ILongestNiceSubarray.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.LongestNiceSubarray;
+
+/// <summary>
+///     https://leetcode.com/problems/longest-nice-subarray/description/
+/// </summary>
+public interface ILongestNiceSubarray
+{
+    int LongestNiceSubarray(int[] nums);
+}
