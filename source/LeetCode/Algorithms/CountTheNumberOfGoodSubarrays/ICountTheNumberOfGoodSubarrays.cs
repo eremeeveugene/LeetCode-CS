3a3892481cc3f80b2eceef6f796185c727767515
eugene <eremeeveugene@gmail.com>
2025-04-16 17:38:28 +0300

    Count the Number of Good Subarrays

---
+++ source/LeetCode/Algorithms/CountTheNumberOfGoodSubarrays/ICountTheNumberOfGoodSubarrays.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountTheNumberOfGoodSubarrays;
+
+/// <summary>
+///     https://leetcode.com/problems/count-the-number-of-good-subarrays/description/
+/// </summary>
+public interface ICountTheNumberOfGoodSubarrays
+{
+    long CountGood(int[] nums, int k);
+}
