b1344fa7fe29d7a8abcf58f694dd4edd1f7a92ea
eugene <eremeeveugene@gmail.com>
2025-04-16 17:44:31 +0300

    Count the Number of Good Subarrays (#1232)

---
+++ source/LeetCode/Algorithms/CountTheNumberOfGoodSubarrays/CountTheNumberOfGoodSubarraysTwoPointers.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountTheNumberOfGoodSubarrays;
+
+/// <inheritdoc />
+public class CountTheNumberOfGoodSubarraysTwoPointers : ICountTheNumberOfGoodSubarrays
+{
+    public long CountGood(int[] nums, int k)
+    {
+        long result = 0;
+
+        var frequencyDictionary = new Dictionary<int, int>();
+
+        var pairsCount = 0;
+        var left = 0;
+
+        for (var right = 0; right < nums.Length; right++)
+        {
+            if (!frequencyDictionary.TryAdd(nums[right], 1))
+            {
+                pairsCount += frequencyDictionary[nums[right]];
+
+                frequencyDictionary[nums[right]]++;
+            }
+
+            while (pairsCount >= k && left <= right)
+            {
+                result += nums.Length - right;
+
+                frequencyDictionary[nums[left]]--;
+
+                pairsCount -= frequencyDictionary[nums[left]];
+
+                left++;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountTheNumberOfGoodSubarrays/CountTheNumberOfGoodSubarraysTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountTheNumberOfGoodSubarrays;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.CountTheNumberOfGoodSubarrays;
+
+public abstract class CountTheNumberOfGoodSubarraysTestsBase<T> where T : ICountTheNumberOfGoodSubarrays, new()
+{
+    [TestMethod]
+    [DataRow("[1,1,1,1,1]", 10, 1)]
+    [DataRow("[3,1,4,3,2,2,4]", 2, 4)]
+    public void CountGood_WithIntegerArrayAndK_ReturnsNumberOfGoodPairs(string numsJsonArray, int k,
+        long expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountGood(nums, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
