9c492e58025ad7a9a36794e0a696e9e867e25f54
eugene <eremeeveugene@gmail.com>
2025-04-17 20:26:06 +0300

    Count Equal and Divisible Pairs in an Array

---
+++ source/LeetCode/Algorithms/CountEqualAndDivisiblePairsInAnArray/CountEqualAndDivisiblePairsInAnArrayBruteForce.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountEqualAndDivisiblePairsInAnArray;
+
+/// <inheritdoc />
+public class CountEqualAndDivisiblePairsInAnArrayBruteForce : ICountEqualAndDivisiblePairsInAnArray
+{
+    /// <summary>
+    ///     Time complexity - O(n^2)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int CountPairs(int[] nums, int k)
+    {
+        var pairsCount = 0;
+
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j] && i * j % k == 0)
+                {
+                    pairsCount++;
+                }
+            }
+        }
+
+        return pairsCount;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountEqualAndDivisiblePairsInAnArray/CountEqualAndDivisiblePairsInAnArrayTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountEqualAndDivisiblePairsInAnArray;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.CountEqualAndDivisiblePairsInAnArray;
+
+public abstract class CountEqualAndDivisiblePairsInAnArrayTestsBase<T>
+    where T : ICountEqualAndDivisiblePairsInAnArray, new()
+{
+    [TestMethod]
+    [DataRow("[3,1,2,2,2,1,3]", 2, 4)]
+    [DataRow("[1,2,3,4]", 1, 0)]
+    public void CountPairs_WithEqualElementsAndIndexProductDivisibleByK_ReturnsTheNumberOfPairs(string numsJsonArray,
+        int k, int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountPairs(nums, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
