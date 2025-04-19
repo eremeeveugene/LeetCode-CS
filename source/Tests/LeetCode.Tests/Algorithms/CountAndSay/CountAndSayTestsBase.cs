8e97ef70aa67cbe60b850f6fe1166a48627fc39d
eugene <eremeeveugene@gmail.com>
2025-04-19 22:00:31 +0300

    Count and Say (#1236)

---
+++ source/Tests/LeetCode.Tests/Algorithms/CountAndSay/CountAndSayTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountAndSay;
+
+namespace LeetCode.Tests.Algorithms.CountAndSay;
+
+public abstract class CountAndSayTestsBase<T> where T : ICountAndSay, new()
+{
+    [TestMethod]
+    [DataRow(1, "1")]
+    [DataRow(2, "11")]
+    [DataRow(3, "21")]
+    [DataRow(4, "1211")]
+    public void CountAndSay_WithPositiveIntegerInput_ReturnsNthElementOfTheSequence(int n, string expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountAndSay(n);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
