5156031b8f4b071f0b593a3dd05737c9a3257090
eugene <eremeeveugene@gmail.com>
2025-04-20 19:09:29 +0300

    First Completely Painted Row or Column

---
+++ source/LeetCode/Algorithms/FirstCompletelyPaintedRowOrColumn/FirstCompletelyPaintedRowOrColumnDictionary.cs
@@ -0,0 +1,63 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FirstCompletelyPaintedRowOrColumn;
+
+/// <inheritdoc />
+public class FirstCompletelyPaintedRowOrColumnDictionary : IFirstCompletelyPaintedRowOrColumn
+{
+    /// <summary>
+    ///     Time complexity - O(m * n)
+    ///     Space complexity - O(m * n)
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <param name="mat"></param>
+    /// <returns></returns>
+    public int FirstCompleteIndex(int[] arr, int[][] mat)
+    {
+        var numsDictionary = new Dictionary<int, int>();
+
+        for (var i = 0; i < arr.Length; i++)
+        {
+            numsDictionary[arr[i]] = i;
+        }
+
+        var result = int.MaxValue;
+        var m = mat.Length;
+        var n = mat[0].Length;
+
+        for (var i = 0; i < m; i++)
+        {
+            var index = int.MinValue;
+
+            for (var j = 0; j < n; j++)
+            {
+                index = Math.Max(index, numsDictionary[mat[i][j]]);
+            }
+
+            result = Math.Min(result, index);
+        }
+
+        for (var i = 0; i < n; i++)
+        {
+            var index = int.MinValue;
+
+            for (var j = 0; j < m; j++)
+            {
+                index = Math.Max(index, numsDictionary[mat[j][i]]);
+            }
+
+            result = Math.Min(result, index);
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FirstCompletelyPaintedRowOrColumn/FirstCompletelyPaintedRowOrColumnTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FirstCompletelyPaintedRowOrColumn;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.FirstCompletelyPaintedRowOrColumn;
+
+public abstract class FirstCompletelyPaintedRowOrColumnTestsBase<T> where T : IFirstCompletelyPaintedRowOrColumn, new()
+{
+    [TestMethod]
+    [DataRow("[1,3,4,2]", "[[1,4],[2,3]]", 2)]
+    [DataRow("[2,8,7,4,1,3,5,6,9]", "[[3,2,5],[1,4,6],[8,7,9]]", 3)]
+    public void FirstCompleteIndex_WithSequenceAndMatrix_ReturnsFirstCompletedRowOrColumnIndex(string arrJsonArray,
+        string matJsonArray, int expectedResult)
+    {
+        // Arrange
+        var arr = JsonHelper<int>.DeserializeToArray(arrJsonArray);
+        var mat = JsonHelper<int>.DeserializeToJaggedArray(matJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FirstCompleteIndex(arr, mat);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
