291100e43174956d672b533f6fffaa2634d4dadc
eugene <eremeeveugene@gmail.com>
2025-04-20 18:07:13 +0300

    Count the Number of Fair Pair

---
+++ source/LeetCode/Algorithms/CountTheNumberOfFairPairs/CountTheNumberOfFairPairsBruteForce.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountTheNumberOfFairPairs;
+
+/// <inheritdoc />
+public class CountTheNumberOfFairPairsBruteForce : ICountTheNumberOfFairPairs
+{
+    /// <summary>
+    ///     Time complexity - O(n^2)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="lower"></param>
+    /// <param name="upper"></param>
+    /// <returns></returns>
+    public long CountFairPairs(int[] nums, int lower, int upper)
+    {
+        var count = 0;
+
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                var sum = nums[i] + nums[j];
+
+                if (lower <= sum && sum <= upper)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
---
+++ source/LeetCode/Algorithms/CountTheNumberOfFairPairs/CountTheNumberOfFairPairsTwoPointers.cs
@@ -0,0 +1,55 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountTheNumberOfFairPairs;
+
+/// <inheritdoc />
+public class CountTheNumberOfFairPairsTwoPointers : ICountTheNumberOfFairPairs
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(log n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="lower"></param>
+    /// <param name="upper"></param>
+    /// <returns></returns>
+    public long CountFairPairs(int[] nums, int lower, int upper)
+    {
+        Array.Sort(nums);
+
+        return CountPairsAtMost(nums, upper) - CountPairsAtMost(nums, lower - 1);
+    }
+
+    private static long CountPairsAtMost(int[] nums, int threshold)
+    {
+        long count = 0;
+
+        var left = 0;
+        var right = nums.Length - 1;
+
+        while (left < right)
+        {
+            if (nums[left] + nums[right] > threshold)
+            {
+                right--;
+            }
+            else
+            {
+                count += right - left;
+
+                left++;
+            }
+        }
+
+        return count;
+    }
+}
