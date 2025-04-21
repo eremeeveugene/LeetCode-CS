ef15c973b246fb4d225e1068563b77962ddbb11a
eugene <eremeeveugene@gmail.com>
2025-04-21 14:41:08 +0300

    Count the Hidden Sequences

---
+++ source/Tests/LeetCode.Tests/Algorithms/CountTheHiddenSequences/CountTheHiddenSequencesTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountTheHiddenSequences;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.CountTheHiddenSequences;
+
+public abstract class CountTheHiddenSequencesTestsBase<T> where T : ICountTheHiddenSequences, new()
+{
+    [TestMethod]
+    [DataRow("[1,-3,4]", 1, 6, 2)]
+    [DataRow("[3,-4,5,1,-2]", -4, 5, 4)]
+    [DataRow("[4,-7,2]", 3, 6, 0)]
+    public void NumberOfArrays_WithDifferencesAndBounds_ReturnsNumberOfPossibleArrays(string differencesJsonArray,
+        int lower, int upper, int expectedResult)
+    {
+        // Arrange
+        var differences = JsonHelper<int>.DeserializeToArray(differencesJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.NumberOfArrays(differences, lower, upper);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
