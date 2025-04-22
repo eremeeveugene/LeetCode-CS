d8e2b3a30e8c516c19612f8e2d6263b2a08cf83a
eugene <eremeeveugene@gmail.com>
2025-04-22 13:29:13 +0300

    Count Subarrays of Length Three With a Condition

---
+++ source/LeetCode/Algorithms/CountSubarraysOfLengthThreeWithCondition/ICountSubarraysOfLengthThreeWithCondition.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountSubarraysOfLengthThreeWithCondition;
+
+/// <summary>
+///     https://leetcode.com/problems/count-subarrays-of-length-three-with-a-condition/description/
+/// </summary>
+public interface ICountSubarraysOfLengthThreeWithCondition
+{
+    int CountSubarrays(int[] nums);
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountSubarraysOfLengthThreeWithCondition/CountSubarraysOfLengthThreeWithConditionTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountSubarraysOfLengthThreeWithCondition;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.CountSubarraysOfLengthThreeWithCondition;
+
+public abstract class CountSubarraysOfLengthThreeWithConditionTestsBase<T>
+    where T : ICountSubarraysOfLengthThreeWithCondition, new()
+{
+    [TestMethod]
+    [DataRow("[1,2,1,4,1]", 1)]
+    [DataRow("[1,1,1]", 0)]
+    [DataRow("[-1,-4,-1,4]", 1)]
+    public void CountSubarrays_WithArrayContainingRepeatedElements_ReturnsNumberOfSubarraysLengthThree(
+        string numsJsonArray, int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountSubarrays(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
