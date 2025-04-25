bbf4c1040690b90dd050b46b4c2bd35c0fa18be6
eugene <eremeeveugene@gmail.com>
2025-04-25 16:09:13 +0300

    Count of Interesting Subarrays

---
+++ source/LeetCode/Algorithms/CountOfInterestingSubarrays/CountOfInterestingSubarraysPrefixSum.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountOfInterestingSubarrays;
+
+/// <inheritdoc />
+public class CountOfInterestingSubarraysPrefixSum : ICountOfInterestingSubarrays
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(modulo)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="modulo"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public long CountInterestingSubarrays(IList<int> nums, int modulo, int k)
+    {
+        long result = 0;
+
+        var prefixModDictionary = new Dictionary<int, int>
+        {
+            [0] = 1
+        };
+
+        var prefix = 0;
+
+        foreach (var num in nums)
+        {
+            if (num % modulo == k)
+            {
+                prefix++;
+            }
+
+            var currentMod = prefix % modulo;
+            var target = (currentMod - k + modulo) % modulo;
+
+            if (prefixModDictionary.TryGetValue(target, out var count))
+            {
+                result += count;
+            }
+
+            if (!prefixModDictionary.TryAdd(currentMod, 1))
+            {
+                prefixModDictionary[currentMod]++;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountOfInterestingSubarrays/CountOfInterestingSubarraysTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountOfInterestingSubarrays;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.CountOfInterestingSubarrays;
+
+public abstract class CountOfInterestingSubarraysTestsBase<T> where T : ICountOfInterestingSubarrays, new()
+{
+    [TestMethod]
+    [DataRow("[3,2,4]", 2, 1, 3)]
+    [DataRow("[3,1,9,6]", 3, 0, 2)]
+    [DataRow("[11,12,21,31]", 10, 1, 5)]
+    public void CountInterestingSubarrays_WithModuloAndTargetRemainder_ReturnsMatchingSubarrayCount(
+        string numsJsonArray, int modulo, int k, long expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountInterestingSubarrays(nums, modulo, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
