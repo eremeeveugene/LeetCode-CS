ec494ca9ab699c8b860bf71e26c8c6d06f26dd80
eugene <eremeeveugene@gmail.com>
2025-04-27 16:32:59 +0300

    Count Unguarded Cells in the Grid (#1253)

---
+++ source/LeetCode/Algorithms/CountUnguardedCellsInTheGrid/ICountUnguardedCellsInTheGrid.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountUnguardedCellsInTheGrid;
+
+/// <summary>
+///     https://leetcode.com/problems/count-unguarded-cells-in-the-grid/description/
+/// </summary>
+public interface ICountUnguardedCellsInTheGrid
+{
+    int CountUnguarded(int m, int n, int[][] guards, int[][] walls);
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountUnguardedCellsInTheGrid/CountUnguardedCellsInTheGridTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountUnguardedCellsInTheGrid;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.CountUnguardedCellsInTheGrid;
+
+public abstract class CountUnguardedCellsInTheGridTestsBase<T> where T : ICountUnguardedCellsInTheGrid, new()
+{
+    [TestMethod]
+    [DataRow(4, 6, "[[0,0],[1,1],[2,3]]", "[[0,1],[2,2],[1,4]]", 7)]
+    [DataRow(3, 3, "[[1,1]]", "[[0,1],[1,0],[2,1],[1,2]]", 4)]
+    public void CountUnguarded_WithGridSizeGuardsAndWalls_ReturnsNumberOfUnguardedCells(int m, int n,
+        string guardsJsonArray, string wordsJsonArray, int expectedResult)
+    {
+        // Arrange
+        var guards = JsonHelper<int>.DeserializeToJaggedArray(guardsJsonArray);
+        var words = JsonHelper<int>.DeserializeToJaggedArray(wordsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountUnguarded(m, n, guards, words);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
