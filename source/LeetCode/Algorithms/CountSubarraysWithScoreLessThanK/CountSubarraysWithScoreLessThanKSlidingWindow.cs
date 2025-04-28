2ec5facbeed1fe7667f33e60037d1fb12e694733
eugene <eremeeveugene@gmail.com>
2025-04-28 13:52:41 +0300

    Count Subarrays With Score Less Than K (#1255)

---
+++ source/LeetCode/Algorithms/CountSubarraysWithScoreLessThanK/CountSubarraysWithScoreLessThanKSlidingWindow.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountSubarraysWithScoreLessThanK;
+
+/// <inheritdoc />
+public class CountSubarraysWithScoreLessThanKSlidingWindow : ICountSubarraysWithScoreLessThanK
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public long CountSubarrays(int[] nums, long k)
+    {
+        long result = 0;
+
+        var left = 0;
+
+        long sum = 0;
+
+        for (var right = left; right < nums.Length; right++)
+        {
+            sum += nums[right];
+
+            while (left <= right && sum * (right - left + 1) >= k)
+            {
+                sum -= nums[left];
+
+                left++;
+            }
+
+            result += right - left + 1;
+        }
+
+        return result;
+    }
+}
