11cf4945ae47a37f916aacc3cff14925c07ab838
eugene <eremeeveugene@gmail.com>
2025-04-30 19:40:58 +0300

    Find Numbers with Even Number of Digits

---
+++ source/LeetCode/Algorithms/FindNumbersWithEvenNumberOfDigits/FindNumbersWithEvenNumberOfDigitsGreedy.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindNumbersWithEvenNumberOfDigits;
+
+/// <inheritdoc />
+public class FindNumbersWithEvenNumberOfDigitsGreedy : IFindNumbersWithEvenNumberOfDigits
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int FindNumbers(int[] nums)
+    {
+        var count = 0;
+
+        foreach (var num in nums)
+        {
+            if (num is >= 10 and < 100 or >= 1000 and < 10000 or >= 100000 and < 1000000 or >= 10000000 and < 100000000
+                or >= 1000000000)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FindNumbersWithEvenNumberOfDigits/FindNumbersWithEvenNumberOfDigitsTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindNumbersWithEvenNumberOfDigits;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.FindNumbersWithEvenNumberOfDigits;
+
+public abstract class FindNumbersWithEvenNumberOfDigitsTestsBase<T> where T : IFindNumbersWithEvenNumberOfDigits, new()
+{
+    [TestMethod]
+    [DataRow("[555,901,482,1771]", 1)]
+    [DataRow("[12,345,2,6,7896]", 2)]
+    public void FindNumbers_WithArrayOfIntegers_ReturnsCountOfNumbersWithEvenNumberOfDigits(string numsJsonArray,
+        int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FindNumbers(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
