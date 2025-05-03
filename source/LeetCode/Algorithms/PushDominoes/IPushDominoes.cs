43bc429a8c10a0a8d6b55a75253a9a196adac385
eugene <eremeeveugene@gmail.com>
2025-05-03 15:18:10 +0300

    Push Dominoes

---
+++ source/LeetCode/Algorithms/PushDominoes/IPushDominoes.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.PushDominoes;
+
+/// <summary>
+///     https://leetcode.com/problems/push-dominoes/description/
+/// </summary>
+public interface IPushDominoes
+{
+    string PushDominoes(string dominoes);
+}
---
+++ source/LeetCode/Algorithms/PushDominoes/PushDominoesTwoPointers.cs
@@ -0,0 +1,71 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.PushDominoes;
+
+/// <inheritdoc />
+public class PushDominoesTwoPointers : IPushDominoes
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="dominoes"></param>
+    /// <returns></returns>
+    public string PushDominoes(string dominoes)
+    {
+        var dominoesCharArray = dominoes.ToCharArray();
+
+        var left = -1;
+
+        for (var right = 0; right <= dominoesCharArray.Length; right++)
+        {
+            var current = right < dominoesCharArray.Length ? dominoesCharArray[right] : 'R';
+            var previous = left >= 0 ? dominoesCharArray[left] : 'L';
+
+            if (current == '.')
+            {
+                continue;
+            }
+
+            if (right - left > 1)
+            {
+                var currentLeft = left + 1;
+                var currentRight = right - 1;
+
+                if (previous == current)
+                {
+                    while (currentLeft <= currentRight)
+                    {
+                        dominoesCharArray[currentLeft] = current;
+
+                        currentLeft++;
+                    }
+                }
+                else if (previous == 'R' && current == 'L')
+                {
+                    while (currentLeft < currentRight)
+                    {
+                        dominoesCharArray[currentLeft] = 'R';
+                        dominoesCharArray[currentRight] = 'L';
+
+                        currentLeft++;
+                        currentRight--;
+                    }
+                }
+            }
+
+            left = right;
+        }
+
+        return new string(dominoesCharArray);
+    }
+}
