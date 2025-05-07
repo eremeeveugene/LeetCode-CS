62e01994bd26134a925631257c3ddb30348e18a0
eugene <eremeeveugene@gmail.com>
2025-05-07 21:09:40 +0300

    Find Minimum Time to Reach Last Room I (#1271)

---
+++ source/LeetCode/Algorithms/FindMinimumTimeToReachLastRoom1/FindMinimumTimeToReachLastRoom1PriorityQueue.cs
@@ -0,0 +1,80 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindMinimumTimeToReachLastRoom1;
+
+/// <inheritdoc />
+public class FindMinimumTimeToReachLastRoom1PriorityQueue : IFindMinimumTimeToReachLastRoom1
+{
+    private static readonly (int X, int Y)[] Directions = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+
+    /// <summary>
+    ///     Time complexity - O(n * m * log(n * m))
+    ///     Space complexity - O(n * m)
+    /// </summary>
+    /// <param name="moveTime"></param>
+    /// <returns></returns>
+    public int MinTimeToReach(int[][] moveTime)
+    {
+        var n = moveTime.Length;
+        var m = moveTime[0].Length;
+
+        var visited = new int[n, m];
+
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < m; j++)
+            {
+                visited[i, j] = int.MaxValue;
+            }
+        }
+
+        visited[0, 0] = 0;
+
+        var priorityQueue = new PriorityQueue<(int X, int Y, int Time), int>();
+
+        priorityQueue.Enqueue((0, 0, 0), 0);
+
+        while (priorityQueue.Count > 0)
+        {
+            var item = priorityQueue.Dequeue();
+
+            if (item.X == n - 1 && item.Y == m - 1)
+            {
+                return item.Time;
+            }
+
+            foreach (var direction in Directions)
+            {
+                var targetX = item.X + direction.X;
+                var targetY = item.Y + direction.Y;
+
+                if (targetX < 0 || targetX >= n || targetY < 0 || targetY >= m)
+                {
+                    continue;
+                }
+
+                var targetTime = Math.Max(item.Time + 1, moveTime[targetX][targetY] + 1);
+
+                if (targetTime >= visited[targetX, targetY])
+                {
+                    continue;
+                }
+
+                visited[targetX, targetY] = targetTime;
+
+                priorityQueue.Enqueue((targetX, targetY, targetTime), targetTime);
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FindMinimumTimeToReachLastRoom1/FindMinimumTimeToReachLastRoom1TestsBase.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindMinimumTimeToReachLastRoom1;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.FindMinimumTimeToReachLastRoom1;
+
+public abstract class FindMinimumTimeToReachLastRoom1TestsBase<T> where T : IFindMinimumTimeToReachLastRoom1, new()
+{
+    [TestMethod]
+    [DataRow("[[0,4],[4,4]]", 6)]
+    [DataRow("[[0,1],[1,2]]", 3)]
+    [DataRow("[[0,0,0],[0,0,0]]", 3)]
+    [DataRow("[[0,2,4],[3,1,5],[6,3,8]]", 9)]
+    [DataRow("[[15,58],[67,4]]", 60)]
+    [DataRow("[[17,56],[97,80]]", 81)]
+    [DataRow("[[94,79,62,27,69,84],[6,32,11,82,42,30]]", 72)]
+    [DataRow(
+        "[[63,102,19,11,110,26,89,101,19],[7,17,119,94,44,3,30,111,54],[20,66,72,19,76,6,16,0,97],[7,48,113,22,14,100,99,91,60]]",
+        93)]
+    public void MinTimeToReach_WithJaggedArrayOfMoveTimes_ReturnsMinimumTotalTime(string moveTimeJsonArray,
+        int expectedResult)
+    {
+        // Arrange
+        var moveTime = JsonHelper<int>.DeserializeToJaggedArray(moveTimeJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinTimeToReach(moveTime);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
