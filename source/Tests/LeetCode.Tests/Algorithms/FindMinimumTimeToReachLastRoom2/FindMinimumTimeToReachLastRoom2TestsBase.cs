a9198afb2b0dc06bdda4251d161f8b792ce6853a
eugene <eremeeveugene@gmail.com>
2025-05-08 14:58:56 +0300

    Find Minimum Time to Reach Last Room II (#1274)

---
+++ source/Tests/LeetCode.Tests/Algorithms/FindMinimumTimeToReachLastRoom2/FindMinimumTimeToReachLastRoom2TestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindMinimumTimeToReachLastRoom2;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.FindMinimumTimeToReachLastRoom2;
+
+public abstract class FindMinimumTimeToReachLastRoom2TestsBase<T> where T : IFindMinimumTimeToReachLastRoom2, new()
+{
+    [TestMethod]
+    [DataRow("[[0,1],[1,2]]", 4)]
+    [DataRow("[[0,4],[4,4]]", 7)]
+    [DataRow("[[0,0,0,0],[0,0,0,0]]", 6)]
+    public void MinTimeToReach_GivenMoveTimeMatrix_ReturnsMinimumTotalTime(string moveTimeJsonArray, int expectedResult)
+    {
+        // Arrange
+        var moveTime = JsonHelper<int>.DeserializeToJaggedArray(moveTimeJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinTimeToReach(moveTime);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
