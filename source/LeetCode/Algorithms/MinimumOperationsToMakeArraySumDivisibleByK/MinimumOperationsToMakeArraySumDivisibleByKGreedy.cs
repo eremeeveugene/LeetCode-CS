6f6ae0ecb5a1ecade2de997331d30cbf989630c8
eugene <eremeeveugene@gmail.com>
2025-05-09 13:43:47 +0300

    Minimum Operations to Make Array Sum Divisible by K (#1276)

---
+++ source/LeetCode/Algorithms/MinimumOperationsToMakeArraySumDivisibleByK/MinimumOperationsToMakeArraySumDivisibleByKGreedy.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumOperationsToMakeArraySumDivisibleByK;
+
+/// <inheritdoc />
+public class MinimumOperationsToMakeArraySumDivisibleByKGreedy : IMinimumOperationsToMakeArraySumDivisibleByK
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int MinOperations(int[] nums, int k)
+    {
+        var sum = 0;
+
+        foreach (var i in nums)
+        {
+            sum += i;
+        }
+
+        return sum % k;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumOperationsToMakeArraySumDivisibleByK/MinimumOperationsToMakeArraySumDivisibleByKTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumOperationsToMakeArraySumDivisibleByK;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MinimumOperationsToMakeArraySumDivisibleByK;
+
+public abstract class MinimumOperationsToMakeArraySumDivisibleByKTestsBase<T>
+    where T : IMinimumOperationsToMakeArraySumDivisibleByK, new()
+{
+    [TestMethod]
+    [DataRow("[3,9,7]", 5, 4)]
+    [DataRow("[4,1,3]", 4, 0)]
+    [DataRow("[3,2]", 6, 5)]
+    public void MinOperations_WithNumArrayAndTargetValueK_ReturnsMinimumOperationCount(string numsJsonArray, int k,
+        int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int>.DeserializeToArray(numsJsonArray);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinOperations(nums, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
