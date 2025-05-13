5ea6212f0ec1a34be6838149e32655e47f815a97
eugene <eremeeveugene@gmail.com>
2025-05-13 22:19:33 +0300

    Shortest Subarray with Sum at Least K

---
+++ source/LeetCode/Algorithms/ShortestSubarrayWithSumAtLeastK/ShortestSubarrayWithSumAtLeastKLinkedList.cs
@@ -0,0 +1,57 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ShortestSubarrayWithSumAtLeastK;
+
+/// <inheritdoc />
+public class ShortestSubarrayWithSumAtLeastKLinkedList : IShortestSubarrayWithSumAtLeastK
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int ShortestSubarray(int[] nums, int k)
+    {
+        var prefixSum = new long[nums.Length + 1];
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+
+        var linkedList = new LinkedList<int>();
+
+        var minLength = nums.Length + 1;
+
+        for (var i = 0; i <= nums.Length; i++)
+        {
+            while (linkedList is { Count: > 0, First: not null } &&
+                   prefixSum[i] - prefixSum[linkedList.First.Value] >= k)
+            {
+                minLength = Math.Min(minLength, i - linkedList.First.Value);
+
+                linkedList.RemoveFirst();
+            }
+
+            while (linkedList is { Count: > 0, Last: not null } && prefixSum[i] <= prefixSum[linkedList.Last.Value])
+            {
+                linkedList.RemoveLast();
+            }
+
+            linkedList.AddLast(i);
+        }
+
+        return minLength <= nums.Length ? minLength : -1;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ShortestSubarrayWithSumAtLeastK/ShortestSubarrayWithSumAtLeastKTestsBase.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ShortestSubarrayWithSumAtLeastK;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.ShortestSubarrayWithSumAtLeastK;
+
+public abstract class ShortestSubarrayWithSumAtLeastKTestsBase<T> where T : IShortestSubarrayWithSumAtLeastK, new()
+{
+    [TestMethod]
+    [DataRow("[1]", 1, 1)]
+    [DataRow("[1,2]", 4, -1)]
+    [DataRow("[2,-1,2]", 3, 3)]
+    [DataRow("[84,-37,32,40,95]", 167, 3)]
+    [DataRow("[-28,81,-20,28,-29]", 89, 3)]
+    public void ShortestSubarray_WithGivenArrayAndTargetSum_ReturnsLengthOfShortestValidSubarrayOrMinusOne(
+        string numsJson, int k, int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int[]>.Parse(numsJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.ShortestSubarray(nums, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
