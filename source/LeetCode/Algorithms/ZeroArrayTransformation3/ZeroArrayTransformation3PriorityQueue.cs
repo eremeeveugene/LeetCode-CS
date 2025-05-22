bdbfe3297a815a34d0f4bc857c8bbdc0c1962841
eugene <eremeeveugene@gmail.com>
2025-05-22 19:37:36 +0300

    Zero Array Transformation III

---
+++ source/LeetCode/Algorithms/ZeroArrayTransformation3/ZeroArrayTransformation3PriorityQueue.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ZeroArrayTransformation3;
+
+/// <inheritdoc />
+public class ZeroArrayTransformation3PriorityQueue : IZeroArrayTransformation3
+{
+    /// <summary>
+    ///     Time complexity - O((n + q) log q)
+    ///     sPACE complexity - O(n + q)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="queries"></param>
+    /// <returns></returns>
+    public int MaxRemoval(int[] nums, int[][] queries)
+    {
+        Array.Sort(queries, (a, b) => a[0] - b[0]);
+
+        var prefixSum = new int[nums.Length + 1];
+
+        var priorityQueue = new PriorityQueue<int, int>();
+
+        var count = 0;
+
+        for (int i = 0, j = 0; i < nums.Length; i++)
+        {
+            count += prefixSum[i];
+
+            while (j < queries.Length && queries[j][0] == i)
+            {
+                priorityQueue.Enqueue(queries[j][1], -queries[j][1]);
+
+                j++;
+            }
+
+            while (count < nums[i] && priorityQueue.Count > 0 && priorityQueue.Peek() >= i)
+            {
+                count += 1;
+
+                prefixSum[priorityQueue.Dequeue() + 1]--;
+            }
+
+            if (count < nums[i])
+            {
+                return -1;
+            }
+        }
+
+        return priorityQueue.Count;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ZeroArrayTransformation3/ZeroArrayTransformation3TestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ZeroArrayTransformation3;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.ZeroArrayTransformation3;
+
+public abstract class ZeroArrayTransformation3TestsBase<T> where T : IZeroArrayTransformation3, new()
+{
+    [TestMethod]
+    [DataRow("[2,0,2]", "[[0,2],[0,2],[1,1]]", 1)]
+    [DataRow("[1,1,1,1]", "[[1,3],[0,2],[1,3],[1,2]]", 2)]
+    [DataRow("[1,2,3,4]", "[[0,3]]", -1)]
+    public void MaxRemoval_WithGivenNumsAndQueries_ReturnsMaximumRemovablePrefixLength(string numsJson,
+        string queriesJson, int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int[]>.Parse(numsJson);
+        var queries = JsonHelper<int[][]>.Parse(queriesJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaxRemoval(nums, queries);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
