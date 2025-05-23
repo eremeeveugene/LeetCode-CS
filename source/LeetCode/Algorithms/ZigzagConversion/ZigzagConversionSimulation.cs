32a4c6155283f681258902591880290e98bd2ada
eugene <eremeeveugene@gmail.com>
2025-05-23 23:00:11 +0300

    Zigzag Conversion

---
+++ source/LeetCode/Algorithms/ZigzagConversion/ZigzagConversionSimulation.cs
@@ -0,0 +1,65 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Text;
+
+namespace LeetCode.Algorithms.ZigzagConversion;
+
+/// <inheritdoc />
+public class ZigzagConversionSimulation : IZigzagConversion
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="numRows"></param>
+    /// <returns></returns>
+    public string Convert(string s, int numRows)
+    {
+        if (numRows == 1 || s.Length <= numRows)
+        {
+            return s;
+        }
+
+        var stringBuilders = new StringBuilder[numRows];
+
+        for (var i = 0; i < stringBuilders.Length; i++)
+        {
+            stringBuilders[i] = new StringBuilder();
+        }
+
+        var index = 0;
+
+        var goingDown = false;
+
+        foreach (var c in s)
+        {
+            stringBuilders[index].Append(c);
+
+            if (index == 0 || index == numRows - 1)
+            {
+                goingDown = !goingDown;
+            }
+
+            index += goingDown ? 1 : -1;
+        }
+
+        var resultStringBuilder = new StringBuilder();
+
+        foreach (var stringBuilder in stringBuilders)
+        {
+            resultStringBuilder.Append(stringBuilder);
+        }
+
+        return resultStringBuilder.ToString();
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ZigzagConversion/ZigzagConversionTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ZigzagConversion;
+
+namespace LeetCode.Tests.Algorithms.ZigzagConversion;
+
+public abstract class ZigzagConversionTestsBase<T> where T : IZigzagConversion, new()
+{
+    [TestMethod]
+    [DataRow("A", 1, "A")]
+    [DataRow("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR")]
+    [DataRow("PAYPALISHIRING", 4, "PINALSIGYAHRPI")]
+    public void Convert_WithInputStringAndNumRows_ReturnsZigzagConvertedString(string s, int numRows,
+        string expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.Convert(s, numRows);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
