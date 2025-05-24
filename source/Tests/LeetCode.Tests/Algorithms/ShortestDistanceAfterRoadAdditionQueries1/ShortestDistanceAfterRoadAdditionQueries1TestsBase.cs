90abf5cf329c9fed9d2d382ae857245ec7eebe5c
eugene <eremeeveugene@gmail.com>
2025-05-24 14:24:08 +0300

    Shortest Distance After Road Addition Queries I (#1306)

---
+++ source/Tests/LeetCode.Tests/Algorithms/ShortestDistanceAfterRoadAdditionQueries1/ShortestDistanceAfterRoadAdditionQueries1TestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ShortestDistanceAfterRoadAdditionQueries1;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.ShortestDistanceAfterRoadAdditionQueries1;
+
+public abstract class ShortestDistanceAfterRoadAdditionQueries1TestsBase<T>
+    where T : IShortestDistanceAfterRoadAdditionQueries1, new()
+{
+    [TestMethod]
+    [DataRow(5, "[[2,4],[0,2],[0,4]]", "[3,2,1]")]
+    [DataRow(4, "[[0,3],[0,2]]", "[1,1]")]
+    public void ShortestDistanceAfterQueries_WithNumberOfNodesAndQueryRanges_ReturnsShortestDistances(int n,
+        string queriesJson, string expectedResultJson)
+    {
+        // Arrange
+        var queries = JsonHelper<int[][]>.Parse(queriesJson);
+        var expectedResult = JsonHelper<int[]>.Parse(expectedResultJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.ShortestDistanceAfterQueries(n, queries);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
