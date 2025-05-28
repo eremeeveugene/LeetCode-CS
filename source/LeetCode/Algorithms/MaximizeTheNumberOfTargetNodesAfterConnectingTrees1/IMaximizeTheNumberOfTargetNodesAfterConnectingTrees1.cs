ea6f4bdbb33118cc047abb321b6b009da38ee544
eugene <eremeeveugene@gmail.com>
2025-05-28 19:40:38 +0300

    Maximize the Number of Target Nodes After Connecting Trees I (#1312)

---
+++ source/LeetCode/Algorithms/MaximizeTheNumberOfTargetNodesAfterConnectingTrees1/IMaximizeTheNumberOfTargetNodesAfterConnectingTrees1.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximizeTheNumberOfTargetNodesAfterConnectingTrees1;
+
+/// <summary>
+///     https://leetcode.com/problems/maximize-the-number-of-target-nodes-after-connecting-trees-i/description/
+/// </summary>
+public interface IMaximizeTheNumberOfTargetNodesAfterConnectingTrees1
+{
+    int[] MaxTargetNodes(int[][] edges1, int[][] edges2, int k);
+}
