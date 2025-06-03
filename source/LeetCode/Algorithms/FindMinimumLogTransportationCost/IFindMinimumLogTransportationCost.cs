be80fa8870f9dca652097d924077e220486baf8a
eugene <eremeeveugene@gmail.com>
2025-06-03 16:57:03 +0300

    Find Minimum Log Transportation Cost

---
+++ source/LeetCode/Algorithms/FindMinimumLogTransportationCost/IFindMinimumLogTransportationCost.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindMinimumLogTransportationCost;
+
+/// <summary>
+///     https://leetcode.com/problems/find-minimum-log-transportation-cost/description/
+/// </summary>
+public interface IFindMinimumLogTransportationCost
+{
+    long MinCuttingCost(int n, int m, int k);
+}
