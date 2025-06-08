505c5e452ca363b145ff63eab24beca6cf5578cf
eugene <eremeeveugene@gmail.com>
2025-06-08 19:41:41 +0300

    solution

---
+++ source/Tests/LeetCode.Tests/Algorithms/FindTheLexicographicallyLargestStringFromTheBox1/FindTheLexicographicallyLargestStringFromTheBox1TempTests.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindTheLexicographicallyLargestStringFromTheBox1;
+
+namespace LeetCode.Tests.Algorithms.FindTheLexicographicallyLargestStringFromTheBox1;
+
+[TestClass]
+public class FindTheLexicographicallyLargestStringFromTheBox1TempTests :
+    FindTheLexicographicallyLargestStringFromTheBox1TestsBase<FindTheLexicographicallyLargestStringFromTheBox1Temp>;
---
+++ source/Tests/LeetCode.Tests/Algorithms/FindTheLexicographicallyLargestStringFromTheBox1/FindTheLexicographicallyLargestStringFromTheBox1TestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindTheLexicographicallyLargestStringFromTheBox1;
+
+namespace LeetCode.Tests.Algorithms.FindTheLexicographicallyLargestStringFromTheBox1;
+
+public abstract class FindTheLexicographicallyLargestStringFromTheBox1TestsBase<T>
+    where T : IFindTheLexicographicallyLargestStringFromTheBox1, new()
+{
+    [TestMethod]
+    [DataRow("dbca", 2, "dbc")]
+    [DataRow("gggg", 4, "g")]
+    public void AnswerString_WithWordAndNumFriends_ReturnsLexicographicallySmallestSubsequence(string word,
+        int numFriends, string expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.AnswerString(word, numFriends);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
