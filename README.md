acff3cdbc90ad409562c2ea093f9b8c740b72b19
Eugene <eremeeveugene@gmail.com>
2025-06-12 14:55:26 +0300

    Minimum Deletions for At Most K Distinct Characters (#1323)

--- README.md
+++ README.md
@@ -647,6 +647,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [3512. Minimum Operations to Make Array Sum Divisible by K](https://leetcode.com/problems/minimum-operations-to-make-array-sum-divisible-by-k/description/)
 - [3516. Find Closest Person](https://leetcode.com/problems/find-closest-person/description/)
 - [3536. Maximum Product of Two Digits](https://leetcode.com/problems/maximum-product-of-two-digits/description/)
+- [3545. Minimum Deletions for At Most K Distinct Characters](https://leetcode.com/problems/maximum-product-of-two-digits/description/)
 - [3550. Smallest Index With Digit Sum Equal to Index](https://leetcode.com/problems/smallest-index-with-digit-sum-equal-to-index/description/)
 - [3560. Find Minimum Log Transportation Cost](https://leetcode.com/problems/find-minimum-log-transportation-cost/description/)
 
---
+++ source/LeetCode/Algorithms/MinimumDeletionsForKMostKDistinctCharacters/MinimumDeletionsForKMostKDistinctCharactersFrequencyDictionaryBucketSort.cs
@@ -0,0 +1,69 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumDeletionsForKMostKDistinctCharacters;
+
+/// <inheritdoc />
+public class MinimumDeletionsForKMostKDistinctCharactersFrequencyDictionaryBucketSort :
+    IMinimumDeletionsForKMostKDistinctCharacters
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int MinDeletion(string s, int k)
+    {
+        var frequencyDictionary = new Dictionary<char, int>();
+
+        foreach (var c in s.Where(c => !frequencyDictionary.TryAdd(c, 1)))
+        {
+            frequencyDictionary[c]++;
+        }
+
+        if (frequencyDictionary.Count <= k)
+        {
+            return 0;
+        }
+
+        var countToRemove = frequencyDictionary.Values.Count(frequency => frequency > 0) - k;
+
+        if (countToRemove <= 0)
+        {
+            return 0;
+        }
+
+        var buckets = new int[s.Length];
+
+        foreach (var frequency in frequencyDictionary)
+        {
+            buckets[frequency.Value - 1]++;
+        }
+
+        var result = 0;
+
+        for (var i = 0; i < s.Length && countToRemove > 0; i++)
+        {
+            while (buckets[i] > 0 && countToRemove > 0)
+            {
+                result += i + 1;
+
+                buckets[i]--;
+
+                countToRemove--;
+            }
+        }
+
+        return result;
+    }
+}
