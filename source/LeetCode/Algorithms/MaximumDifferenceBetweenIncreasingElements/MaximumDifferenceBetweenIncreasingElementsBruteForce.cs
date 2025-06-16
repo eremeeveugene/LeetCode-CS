96911bdd4bc44f1d90c02eb9c049f90efcb639f0
Eugene <eremeeveugene@gmail.com>
2025-06-16 19:19:59 +0300

    Maximum Difference Between Increasing Elements (#1335)

---
+++ source/LeetCode/Algorithms/MaximumDifferenceBetweenIncreasingElements/MaximumDifferenceBetweenIncreasingElementsBruteForce.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumDifferenceBetweenIncreasingElements;
+
+/// <inheritdoc />
+public class MaximumDifferenceBetweenIncreasingElementsBruteForce : IMaximumDifferenceBetweenIncreasingElements
+{
+    /// <summary>
+    ///     Time complexity - O(n^2)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MaximumDifference(int[] nums)
+    {
+        var maximumDifference = -1;
+
+        for (var i = 0; i < nums.Length - 1; i++)
+        {
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] >= nums[j])
+                {
+                    continue;
+                }
+
+                var difference = nums[j] - nums[i];
+
+                maximumDifference = Math.Max(maximumDifference, difference);
+            }
+        }
+
+        return maximumDifference;
+    }
+}
