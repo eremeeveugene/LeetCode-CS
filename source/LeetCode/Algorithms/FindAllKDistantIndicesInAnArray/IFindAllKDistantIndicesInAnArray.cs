3cf5a489036cfcff02ae9c04e6c43b211bad7fd3
Eugene <eremeeveugene@gmail.com>
2025-06-24 13:15:00 +0300

    Find All K-Distant Indices in an Array (#1345)

---
+++ source/LeetCode/Algorithms/FindAllKDistantIndicesInAnArray/IFindAllKDistantIndicesInAnArray.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindAllKDistantIndicesInAnArray;
+
+/// <summary>
+///     https://leetcode.com/problems/find-all-k-distant-indices-in-an-array/description/
+/// </summary>
+public interface IFindAllKDistantIndicesInAnArray
+{
+    IList<int> FindKDistantIndices(int[] nums, int key, int k);
+}
