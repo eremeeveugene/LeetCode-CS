bfc0f615ac83531f55c675d93d2adc2d60fb77ce
Eugene <eremeeveugene@gmail.com>
2025-06-26 14:31:09 +0300

    Longest Binary Subsequence Less Than or Equal to K (#1348)

---
+++ source/LeetCode/Algorithms/LongestBinarySubsequenceLessThanOrEqualToK/LongestBinarySubsequenceLessThanOrEqualToKGreedy.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.LongestBinarySubsequenceLessThanOrEqualToK;
+
+/// <inheritdoc />
+public class LongestBinarySubsequenceLessThanOrEqualToKGreedy : ILongestBinarySubsequenceLessThanOrEqualToK
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int LongestSubsequence(string s, int k)
+    {
+        var count = 0;
+        var value = 0;
+        var power = 1;
+
+        for (var i = s.Length - 1; i >= 0; i--)
+        {
+            if (s[i] == '1' && value + power <= k)
+            {
+                value += power;
+
+                count++;
+            }
+
+            if (power > k / 2)
+            {
+                break;
+            }
+
+            power <<= 1;
+        }
+
+        return count + s.Count(c => c == '0');
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/LongestBinarySubsequenceLessThanOrEqualToK/LongestBinarySubsequenceLessThanOrEqualToKTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.LongestBinarySubsequenceLessThanOrEqualToK;
+
+namespace LeetCode.Tests.Algorithms.LongestBinarySubsequenceLessThanOrEqualToK;
+
+public abstract class LongestBinarySubsequenceLessThanOrEqualToKTestsBase<T>
+    where T : ILongestBinarySubsequenceLessThanOrEqualToK, new()
+{
+    [TestMethod]
+    [DataRow("1001010", 5, 5)]
+    [DataRow("00101001", 1, 6)]
+    public void LongestSubsequence_WithBinaryStringAndLimitK_ReturnsMaxValidSubsequenceLength(string s, int k,
+        int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.LongestSubsequence(s, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
