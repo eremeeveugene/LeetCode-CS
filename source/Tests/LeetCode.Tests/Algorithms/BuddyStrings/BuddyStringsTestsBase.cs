8ce489d2e2003ed860d7f0eedc0a94aff162fced
Eugene <eremeeveugene@gmail.com>
2025-06-27 13:31:06 +0300

    Buddy Strings (#1349)

---
+++ source/Tests/LeetCode.Tests/Algorithms/BuddyStrings/BuddyStringsTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.BuddyStrings;
+
+namespace LeetCode.Tests.Algorithms.BuddyStrings;
+
+public abstract class BuddyStringsTestsBase<T> where T : IBuddyStrings, new()
+{
+    [TestMethod]
+    [DataRow("ab", "ba", true)]
+    [DataRow("ab", "ab", false)]
+    [DataRow("aa", "aa", true)]
+    [DataRow("abcaa", "abcbb", false)]
+    [DataRow("abcd", "abcde", false)]
+    public void BuddyStrings_WithGivenStrings_ReturnsTrueIfSingleSwapMakesEqualOtherwiseFalse(string s, string goal,
+        bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.BuddyStrings(s, goal);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
