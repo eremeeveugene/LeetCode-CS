fcd6c321ecc3548d3d6fe572ba464b20401c327c
Eugene <eremeeveugene@gmail.com>
2025-06-28 13:45:27 +0300

    Find Subsequence of Length K With the Largest Sum

---
+++ source/LeetCode/Algorithms/FindSubsequenceOfLengthKWithTheLargestSum/FindSubsequenceOfLengthKWithTheLargestSumSorting.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindSubsequenceOfLengthKWithTheLargestSum;
+
+/// <inheritdoc />
+public class FindSubsequenceOfLengthKWithTheLargestSumSorting : IFindSubsequenceOfLengthKWithTheLargestSum
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int[] MaxSubsequence(int[] nums, int k)
+    {
+        var indexedNums = new (int Index, int Value)[nums.Length];
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            indexedNums[i] = (i, nums[i]);
+        }
+
+        Array.Sort(indexedNums, (a, b) => b.Value.CompareTo(a.Value));
+
+        var kIndexedNums = new (int Index, int Value)[k];
+
+        for (var i = 0; i < k; i++)
+        {
+            kIndexedNums[i] = indexedNums[i];
+        }
+
+        Array.Sort(kIndexedNums, (a, b) => a.Index.CompareTo(b.Index));
+
+        var result = new int[k];
+
+        for (var i = 0; i < k; i++)
+        {
+            result[i] = kIndexedNums[i].Value;
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FindSubsequenceOfLengthKWithTheLargestSum/FindSubsequenceOfLengthKWithTheLargestSumTestsBase.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindSubsequenceOfLengthKWithTheLargestSum;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.FindSubsequenceOfLengthKWithTheLargestSum;
+
+public abstract class FindSubsequenceOfLengthKWithTheLargestSumTestsBase<T>
+    where T : IFindSubsequenceOfLengthKWithTheLargestSum, new()
+{
+    [TestMethod]
+    [DataRow("[2,1,3,3]", 2, "[3,3]")]
+    [DataRow("[-1,-2,3,4]", 3, "[-1,3,4]")]
+    [DataRow("[3,4,3,3]", 2, "[3,4]")]
+    public void MaxSubsequence_WithGivenArrayAndK_ReturnsSubsequenceWithLargestSum(string numsJson, int k,
+        string expectedResultJson)
+    {
+        // Arrange
+        var nums = JsonHelper<int[]>.Parse(numsJson);
+        var expectedResult = JsonHelper<int[]>.Parse(expectedResultJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaxSubsequence(nums, k);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
