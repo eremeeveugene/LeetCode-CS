c2f6076d23ccdf04a3fdd050897422329c17507c
Eugene <eremeeveugene@gmail.com>
2025-06-30 12:21:17 +0300

    Longest Harmonious Subsequence (#1355)

---
+++ source/Tests/LeetCode.Tests/Algorithms/LongestHarmoniousSubsequence/LongestHarmoniousSubsequenceTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.LongestHarmoniousSubsequence;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.LongestHarmoniousSubsequence;
+
+public abstract class LongestHarmoniousSubsequenceTestsBase<T> where T : ILongestHarmoniousSubsequence, new()
+{
+    [TestMethod]
+    [DataRow("[1,3,2,2,5,2,3,7]", 5)]
+    [DataRow("[1,2,3,4]", 2)]
+    [DataRow("[1,1,1,1]", 0)]
+    public void FindLHS_WithNumsArray_ReturnsLongestHarmoniousSubsequenceLength(string numsJson, int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int[]>.Parse(numsJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FindLHS(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
