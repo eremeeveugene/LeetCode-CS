b273c8506e79d250e30ba3b08626506338261d55
Eugene <eremeeveugene@gmail.com>
2025-07-05 16:54:36 +0300

    Find Lucky Integer in an Array

---
+++ source/LeetCode/Algorithms/FindLuckyIntegerInAnArray/FindLuckyIntegerInAnArrayFrequencyDictionary.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindLuckyIntegerInAnArray;
+
+/// <inheritdoc />
+public class FindLuckyIntegerInAnArrayFrequencyDictionary : IFindLuckyIntegerInAnArray
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <returns></returns>
+    public int FindLucky(int[] arr)
+    {
+        var frequencyDictionary = new Dictionary<int, int>();
+
+        foreach (var number in arr)
+        {
+            if (!frequencyDictionary.TryAdd(number, 1))
+            {
+                frequencyDictionary[number]++;
+            }
+        }
+
+        var result = -1;
+
+        foreach (var frequency in frequencyDictionary.Where(frequency =>
+                     frequency.Key == frequency.Value && frequency.Key > result))
+        {
+            result = frequency.Key;
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FindLuckyIntegerInAnArray/FindLuckyIntegerInAnArrayTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindLuckyIntegerInAnArray;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.FindLuckyIntegerInAnArray;
+
+public abstract class FindLuckyIntegerInAnArrayTestsBase<T>
+    where T : IFindLuckyIntegerInAnArray, new()
+{
+    [TestMethod]
+    [DataRow("[2,2,3,4]", 2)]
+    [DataRow("[1,2,2,3,3,3]", 3)]
+    [DataRow("[2,2,2,3,3]", -1)]
+    public void FindLucky_WithIntegersArray_ReturnsLargestLuckyIntegerOrMinusOne(string arrJson, int expectedResult)
+    {
+        // Arrange
+        var arr = JsonHelper<int[]>.Parse(arrJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FindLucky(arr);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
