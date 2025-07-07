51784cc1640f303d1d3c7456a7007bc29d0582be
Eugene <eremeeveugene@gmail.com>
2025-07-07 22:15:58 +0300

    Maximum Number of Events That Can Be Attended (#1365)

---
+++ source/LeetCode/Algorithms/MaximumNumberOfEventsThatCanBeAttended/MaximumNumberOfEventsThatCanBeAttendedPriorityQueue.cs
@@ -0,0 +1,61 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumNumberOfEventsThatCanBeAttended;
+
+/// <inheritdoc />
+public class MaximumNumberOfEventsThatCanBeAttendedPriorityQueue : IMaximumNumberOfEventsThatCanBeAttended
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="events"></param>
+    /// <returns></returns>
+    public int MaxEvents(int[][] events)
+    {
+        Array.Sort(events, (event1, event2) => event1[0] - event2[0]);
+
+        var maxEvents = 0;
+
+        var priorityQueue = new PriorityQueue<int, int>();
+
+        var eventIndex = 0;
+
+        var day = 0;
+
+        while (eventIndex < events.Length || priorityQueue.Count > 0)
+        {
+            while (eventIndex < events.Length && events[eventIndex][0] <= day)
+            {
+                priorityQueue.Enqueue(events[eventIndex][1], events[eventIndex][1]);
+
+                eventIndex++;
+            }
+
+            while (priorityQueue.Count > 0 && priorityQueue.Peek() < day)
+            {
+                priorityQueue.Dequeue();
+            }
+
+            if (priorityQueue.Count > 0)
+            {
+                priorityQueue.Dequeue();
+
+                maxEvents++;
+            }
+
+            day++;
+        }
+
+        return maxEvents;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MaximumNumberOfEventsThatCanBeAttended/MaximumNumberOfEventsThatCanBeAttendedTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaximumNumberOfEventsThatCanBeAttended;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MaximumNumberOfEventsThatCanBeAttended;
+
+public abstract class MaximumNumberOfEventsThatCanBeAttendedTestsBase<T>
+    where T : IMaximumNumberOfEventsThatCanBeAttended, new()
+{
+    [TestMethod]
+    [DataRow("[[1,2],[2,3],[3,4]]", 3)]
+    [DataRow("[[1,2],[2,3],[3,4],[1,2]]", 4)]
+    public void MaxEvents_WithMultipleIntervals_ReturnsMaximumAttendableEvents(string eventsJson, int expectedResult)
+    {
+        // Arrange
+        var events = JsonHelper<int[][]>.Parse(eventsJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaxEvents(events);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
