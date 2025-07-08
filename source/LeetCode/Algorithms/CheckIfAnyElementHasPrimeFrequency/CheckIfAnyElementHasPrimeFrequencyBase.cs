064228fc049a5de465ad629eeeebf4549d83d588
Eugene <eremeeveugene@gmail.com>
2025-07-08 12:48:24 +0300

    Check if Any Element Has Prime Frequency (#1367)

---
+++ source/LeetCode/Algorithms/CheckIfAnyElementHasPrimeFrequency/CheckIfAnyElementHasPrimeFrequencyBase.cs
@@ -0,0 +1,76 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CheckIfAnyElementHasPrimeFrequency;
+
+public abstract class CheckIfAnyElementHasPrimeFrequencyBase : ICheckIfAnyElementHasPrimeFrequency
+{
+    protected const int Count = 101;
+
+    private static readonly bool[] Primes = GeneratePrimes(Count);
+
+    public abstract bool CheckPrimeFrequency(int[] nums);
+
+    protected static bool IsPrime(int number)
+    {
+        return Primes[number];
+    }
+
+    private static bool[] GeneratePrimes(int max)
+    {
+        var isPrime = new bool[max + 1];
+
+        if (max >= 2)
+        {
+            isPrime[2] = true;
+        }
+
+        for (var i = 3; i <= max; i += 2)
+        {
+            isPrime[i] = true;
+        }
+
+        for (var p = 3; p * p <= max; p += 2)
+        {
+            if (!isPrime[p])
+            {
+                continue;
+            }
+
+            for (var multiple = p * p; multiple <= max; multiple += p * 2)
+            {
+                isPrime[multiple] = false;
+            }
+        }
+
+        if (max >= 2)
+        {
+            isPrime[2] = true;
+        }
+
+        if (max >= 3)
+        {
+            isPrime[3] = true;
+        }
+
+        if (max >= 0)
+        {
+            isPrime[0] = false;
+        }
+
+        if (max >= 1)
+        {
+            isPrime[1] = false;
+        }
+
+        return isPrime;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CheckIfAnyElementHasPrimeFrequency/CheckIfAnyElementHasPrimeFrequencyTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CheckIfAnyElementHasPrimeFrequency;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.CheckIfAnyElementHasPrimeFrequency;
+
+public abstract class CheckIfAnyElementHasPrimeFrequencyTestsBase<T>
+    where T : ICheckIfAnyElementHasPrimeFrequency, new()
+{
+    [TestMethod]
+    [DataRow("[1,2,3,4,5,4]", true)]
+    [DataRow("[1,2,3,4,5]", false)]
+    [DataRow("[2,2,2,4,4]", true)]
+    public void CheckPrimeFrequency_WithIntegerArray_ReturnsTrueIfAnyElementHasPrimeFrequency(string numsJson,
+        bool expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int[]>.Parse(numsJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CheckPrimeFrequency(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
