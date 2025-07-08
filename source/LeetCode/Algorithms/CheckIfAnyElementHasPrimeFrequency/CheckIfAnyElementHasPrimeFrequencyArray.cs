0baab6955faffcf8ef67ab2511ceba510063e30d
Eugene <eremeeveugene@gmail.com>
2025-07-08 12:40:37 +0300

    Check if Any Element Has Prime Frequency

---
+++ source/LeetCode/Algorithms/CheckIfAnyElementHasPrimeFrequency/CheckIfAnyElementHasPrimeFrequencyArray.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CheckIfAnyElementHasPrimeFrequency;
+
+/// <inheritdoc />
+public class CheckIfAnyElementHasPrimeFrequencyArray : CheckIfAnyElementHasPrimeFrequencyBase
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public override bool CheckPrimeFrequency(int[] nums)
+    {
+        var frequencyArray = new int[101];
+
+        foreach (var num in nums)
+        {
+            frequencyArray[num]++;
+        }
+
+        return frequencyArray.Any(IsPrime);
+    }
+}
