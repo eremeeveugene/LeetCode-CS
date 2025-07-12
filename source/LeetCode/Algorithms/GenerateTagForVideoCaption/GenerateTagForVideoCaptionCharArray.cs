6d8e285323ea520d67ac08dcdd5a3490dba56f67
Eugene <eremeeveugene@gmail.com>
2025-07-12 18:36:06 +0300

    Generate Tag for Video Caption

---
+++ source/LeetCode/Algorithms/GenerateTagForVideoCaption/GenerateTagForVideoCaptionCharArray.cs
@@ -0,0 +1,87 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.GenerateTagForVideoCaption;
+
+/// <inheritdoc />
+public class GenerateTagForVideoCaptionCharArray : IGenerateTagForVideoCaption
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="caption"></param>
+    /// <returns></returns>
+    public string GenerateTag(string caption)
+    {
+        if (string.IsNullOrWhiteSpace(caption))
+        {
+            return "#";
+        }
+
+        var chars = new char[100];
+
+        chars[0] = '#';
+
+        var isFirstWord = true;
+        var isWord = false;
+
+        var length = 1;
+
+        foreach (var @char in caption)
+        {
+            if (@char == ' ')
+            {
+                isWord = false;
+
+                continue;
+            }
+
+            char charToAppend;
+
+            if (isWord)
+            {
+                charToAppend = char.ToLowerInvariant(@char);
+            }
+            else
+            {
+                isWord = true;
+
+                charToAppend = isFirstWord
+                    ? char.ToLowerInvariant(@char)
+                    : char.ToUpperInvariant(@char);
+
+                isFirstWord = false;
+            }
+
+            if (TryAppend(chars, ref length, charToAppend))
+            {
+                break;
+            }
+        }
+
+        return new string(chars, 0, length);
+    }
+
+    private static bool TryAppend(char[] chars, ref int length, char @char)
+    {
+        if (length == 100)
+        {
+            return true;
+        }
+
+        chars[length] = @char;
+
+        length++;
+
+        return false;
+    }
+}
