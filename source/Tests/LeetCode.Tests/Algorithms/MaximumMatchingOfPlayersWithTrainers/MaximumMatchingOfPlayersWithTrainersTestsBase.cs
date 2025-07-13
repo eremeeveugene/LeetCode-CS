d489f31a6f9a3899726e0ed495c0ffdcfd8b5fa4
Eugene <eremeeveugene@gmail.com>
2025-07-13 14:24:05 +0300

    Maximum Matching of Players With Trainers

---
+++ source/Tests/LeetCode.Tests/Algorithms/MaximumMatchingOfPlayersWithTrainers/MaximumMatchingOfPlayersWithTrainersTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaximumMatchingOfPlayersWithTrainers;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MaximumMatchingOfPlayersWithTrainers;
+
+public abstract class MaximumMatchingOfPlayersWithTrainersTestsBase<T>
+    where T : IMaximumMatchingOfPlayersWithTrainers, new()
+{
+    [TestMethod]
+    [DataRow("[4,7,9]", "[8,2,5,8]", 2)]
+    [DataRow("[1,1,1]", "[10]", 1)]
+    public void MatchPlayersAndTrainers_WithPlayerAbilitiesAndTrainerCapacities_ReturnsMaximumMatchingCount(
+        string playersJson, string trainersJson, long expectedResult)
+    {
+        // Arrange
+        var players = JsonHelper<int[]>.Parse(playersJson);
+        var trainers = JsonHelper<int[]>.Parse(trainersJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MatchPlayersAndTrainers(players, trainers);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
