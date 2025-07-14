db1b14ea2b80ec51b7e5e6820000812d1ab2735b
Eugene <eremeeveugene@gmail.com>
2025-07-14 17:15:36 +0300

    Convert Binary Number in a Linked List to Integer (#1379)

---
+++ source/LeetCode/Algorithms/ConvertBinaryNumberInLinkedListToInteger/ConvertBinaryNumberInLinkedListToIntegerIterative.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Core.Models;
+
+namespace LeetCode.Algorithms.ConvertBinaryNumberInLinkedListToInteger;
+
+/// <inheritdoc />
+public class ConvertBinaryNumberInLinkedListToIntegerIterative : IConvertBinaryNumberInLinkedListToInteger
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="head"></param>
+    /// <returns></returns>
+    public int GetDecimalValue(ListNode head)
+    {
+        var result = 0;
+
+        var current = head;
+
+        while (current != null)
+        {
+            result *= 2;
+            result += current.val;
+
+            current = current.next;
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ConvertBinaryNumberInLinkedListToInteger/ConvertBinaryNumberInLinkedListToIntegerTestsBase.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ConvertBinaryNumberInLinkedListToInteger;
+using LeetCode.Core.Exceptions;
+using LeetCode.Core.Helpers;
+using LeetCode.Core.Models;
+
+namespace LeetCode.Tests.Algorithms.ConvertBinaryNumberInLinkedListToInteger;
+
+public abstract class ConvertBinaryNumberInLinkedListToIntegerTestsBase<T>
+    where T : IConvertBinaryNumberInLinkedListToInteger, new()
+{
+    [TestMethod]
+    [DataRow("[0]", 0)]
+    [DataRow("[1,0,1]", 5)]
+    public void GetDecimalValue_WithBinaryLinkedList_ReturnsDecimalValue(string headJson, int expectedResult)
+    {
+        // Arrange
+        var headArray = JsonHelper<int[]>.Parse(headJson);
+
+        var head = ListNode.ToListNode(headArray) ?? throw new ListNodeBuildException();
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.GetDecimalValue(head);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
