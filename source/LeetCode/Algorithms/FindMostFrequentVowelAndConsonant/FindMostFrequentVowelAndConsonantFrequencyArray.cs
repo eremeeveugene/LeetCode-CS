a67fbe58af2a8be9fea27bd7f1d23bd423db32de
Eugene <eremeeveugene@gmail.com>
2025-07-18 16:29:16 +0300

    Find Most Frequent Vowel and Consonant

---
+++ source/LeetCode/Algorithms/FindMostFrequentVowelAndConsonant/FindMostFrequentVowelAndConsonantFrequencyArray.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindMostFrequentVowelAndConsonant;
+
+/// <inheritdoc />
+public class FindMostFrequentVowelAndConsonantFrequencyArray : IFindMostFrequentVowelAndConsonant
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public int MaxFreqSum(string s)
+    {
+        var frequencyArray = new char['z' - 'a' + 1];
+
+        var maxVowel = 0;
+        var maxConsonant = 0;
+
+        foreach (var c in s)
+        {
+            var index = c - 'a';
+
+            frequencyArray[index]++;
+
+            if (IsVowel(c))
+            {
+                maxVowel = Math.Max(maxVowel, frequencyArray[index]);
+            }
+            else
+            {
+                maxConsonant = Math.Max(maxConsonant, frequencyArray[index]);
+            }
+        }
+
+        return maxVowel + maxConsonant;
+    }
+
+    private static bool IsVowel(char c)
+    {
+        return c is 'a' or 'e' or 'i' or 'o' or 'u';
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FindMostFrequentVowelAndConsonant/FindMostFrequentVowelAndConsonantTestsBase.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindMostFrequentVowelAndConsonant;
+
+namespace LeetCode.Tests.Algorithms.FindMostFrequentVowelAndConsonant;
+
+public abstract class FindMostFrequentVowelAndConsonantTestsBase<T> where T : IFindMostFrequentVowelAndConsonant, new()
+{
+    [TestMethod]
+    [DataRow("successes", 6)]
+    [DataRow("aeiaeia", 3)]
+    public void MaxFreqSum_WithMixedVowelsAndConsonants_ReturnsSumOfMostFrequentCounts(string s, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaxFreqSum(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
