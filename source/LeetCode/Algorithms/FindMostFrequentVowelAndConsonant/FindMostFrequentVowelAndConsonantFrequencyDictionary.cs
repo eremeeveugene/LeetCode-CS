41550b9ee2be514b0cd8c9e5ae00f3726775ba3c
Eugene <eremeeveugene@gmail.com>
2025-07-18 16:34:02 +0300

    Find Most Frequent Vowel and Consonant (#1385)

---
+++ source/LeetCode/Algorithms/FindMostFrequentVowelAndConsonant/FindMostFrequentVowelAndConsonantFrequencyDictionary.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindMostFrequentVowelAndConsonant;
+
+/// <inheritdoc />
+public class FindMostFrequentVowelAndConsonantFrequencyDictionary : IFindMostFrequentVowelAndConsonant
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public int MaxFreqSum(string s)
+    {
+        var frequencyDictionary = new Dictionary<char, int>();
+
+        var maxVowel = 0;
+        var maxConsonant = 0;
+
+        foreach (var c in s)
+        {
+            if (!frequencyDictionary.TryAdd(c, 1))
+            {
+                frequencyDictionary[c]++;
+            }
+
+            if (IsVowel(c))
+            {
+                maxVowel = Math.Max(maxVowel, frequencyDictionary[c]);
+            }
+            else
+            {
+                maxConsonant = Math.Max(maxConsonant, frequencyDictionary[c]);
+            }
+        }
+
+        return maxVowel + maxConsonant;
+    }
+
+    private static bool IsVowel(char c)
+    {
+        return c is 'a' or 'e' or 'i' or 'o' or 'u';
+    }
+}
