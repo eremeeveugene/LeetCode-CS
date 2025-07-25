753c6aaf7e1fc7d0867b5c8cfeca3737ad800d8f
Eugene <eremeeveugene@gmail.com>
2025-07-25 11:39:59 +0300

    Design HashMap (#1395)

---
+++ source/LeetCode/Algorithms/DesignHashMap/IDesignHashMap.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.DesignHashMap;
+
+/// <summary>
+///     https://leetcode.com/problems/design-hashmap/description/
+/// </summary>
+public interface IDesignHashMap
+{
+    void Put(int key, int value);
+
+    int Get(int key);
+
+    void Remove(int key);
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/DesignHashMap/DesignHashMapTestsBase.cs
@@ -0,0 +1,65 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.DesignHashMap;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.DesignHashMap;
+
+public abstract class DesignHashMapTestsBase<T> where T : IDesignHashMap, new()
+{
+    private const string Put = "put";
+    private const string Get = "get";
+    private const string Remove = "remove";
+
+    [TestMethod]
+    [DataRow(
+        "[\"put\", \"put\", \"get\", \"get\", \"put\", \"get\", \"remove\", \"get\"]",
+        "[[1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]",
+        "[1, -1, 1, -1]")]
+    public void DesignHashMap_WithMixedOperations_ProcessesOperationsAccordingToSpecification(string operationsJson,
+        string argumentsJson, string expectedResultJson)
+    {
+        // Arrange
+        var operations = JsonHelper<string[]>.Parse(operationsJson);
+        var arguments = JsonHelper<int[][]>.Parse(argumentsJson);
+        var expectedResult = JsonHelper<int[]>.Parse(expectedResultJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = new List<object>();
+
+        for (var i = 0; i < operations.Length; i++)
+        {
+            switch (operations[i])
+            {
+                case Put:
+                    solution.Put(arguments[i][0], arguments[i][1]);
+
+                    break;
+                case Get:
+                    actualResult.Add(solution.Get(arguments[i][0]));
+
+                    break;
+                case Remove:
+                    solution.Remove(arguments[i][0]);
+
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException($"Unexpected operation '{operations[i]}' at index {i}.");
+            }
+        }
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
