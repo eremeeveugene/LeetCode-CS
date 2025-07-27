884b4bdd96cb85f33ecbf8242d7a2a310c7a0196
Eugene <eremeeveugene@gmail.com>
2025-07-27 18:14:26 +0300

    Set Mismatch (#1397)

---
+++ source/LeetCode/Algorithms/SetMismatch/SetMismatchHashSet.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SetMismatch;
+
+/// <inheritdoc />
+public class SetMismatchHashSet : ISetMismatch
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int[] FindErrorNums(int[] nums)
+    {
+        var duplicate = 0;
+        var missing = 0;
+
+        var duplicatesHashSet = new HashSet<int>();
+        var numsHashSet = new HashSet<int>(nums);
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (!duplicatesHashSet.Add(nums[i]))
+            {
+                duplicate = nums[i];
+            }
+
+            var number = i + 1;
+
+            if (!numsHashSet.Contains(number))
+            {
+                missing = number;
+            }
+        }
+
+        return [duplicate, missing];
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/SetMismatch/SetMismatchTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SetMismatch;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.SetMismatch;
+
+public abstract class SetMismatchTestsBase<T> where T : ISetMismatch, new()
+{
+    [TestMethod]
+    [DataRow("[1,2,2,4]", "[2,3]")]
+    [DataRow("[1,1]", "[1,2]")]
+    public void FindErrorNums_WithDuplicateAndMissingNumberInArray_ReturnsDuplicatedAndMissingNumbers(string numsJson,
+        string expectedResultJson)
+    {
+        // Arrange
+        var nums = JsonHelper<int[]>.Parse(numsJson);
+        var expectedResult = JsonHelper<int[]>.Parse(expectedResultJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FindErrorNums(nums);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
