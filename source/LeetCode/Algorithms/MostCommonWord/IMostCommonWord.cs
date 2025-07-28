9f2d04fe615176ac687b007f6c00963bd1d3e001
Eugene <eremeeveugene@gmail.com>
2025-07-28 10:54:39 +0300

    Most Common Word (#1401)

---
+++ source/LeetCode/Algorithms/MostCommonWord/IMostCommonWord.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MostCommonWord;
+
+/// <summary>
+///     https://leetcode.com/problems/most-common-word/description/
+/// </summary>
+public interface IMostCommonWord
+{
+    string MostCommonWord(string paragraph, string[] banned);
+}
