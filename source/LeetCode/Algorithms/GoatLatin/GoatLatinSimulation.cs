2a3a8dbbdca07cefa32e1131d7b7e5353828e526
Eugene <eremeeveugene@gmail.com>
2025-07-29 12:25:43 +0300

    Goat Latin (#1403)

---
+++ source/LeetCode/Algorithms/GoatLatin/GoatLatinSimulation.cs
@@ -0,0 +1,82 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Text;
+
+namespace LeetCode.Algorithms.GoatLatin;
+
+/// <inheritdoc />
+public class GoatLatinSimulation : IGoatLatin
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="sentence"></param>
+    /// <returns></returns>
+    public string ToGoatLatin(string sentence)
+    {
+        var resultStringBuilder = new StringBuilder(sentence.Length * 2);
+        var isFirstLetter = true;
+        var firstLetter = '\0';
+        var wordIndex = 0;
+
+        foreach (var character in sentence)
+        {
+            if (isFirstLetter)
+            {
+                firstLetter = character;
+
+                isFirstLetter = false;
+
+                if (IsVowel(firstLetter))
+                {
+                    resultStringBuilder.Append(firstLetter);
+                }
+            }
+            else if (character == ' ')
+            {
+                CloseWord(resultStringBuilder, firstLetter, ref wordIndex);
+
+                resultStringBuilder.Append(' ');
+
+                isFirstLetter = true;
+            }
+            else
+            {
+                resultStringBuilder.Append(character);
+            }
+        }
+
+        CloseWord(resultStringBuilder, firstLetter, ref wordIndex);
+
+        return resultStringBuilder.ToString();
+    }
+
+    private static bool IsVowel(char c)
+    {
+        return char.ToLowerInvariant(c) is 'a' or 'e' or 'i' or 'o' or 'u';
+    }
+
+    private static void CloseWord(StringBuilder stringBuilder, char firstLetter, ref int wordIndex)
+    {
+        if (!IsVowel(firstLetter))
+        {
+            stringBuilder.Append(firstLetter);
+        }
+
+        stringBuilder.Append("ma");
+
+        wordIndex++;
+
+        stringBuilder.Append('a', wordIndex);
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/GoatLatin/GoatLatinTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.GoatLatin;
+
+namespace LeetCode.Tests.Algorithms.GoatLatin;
+
+public abstract class GoatLatinTestsBase<T> where T : IGoatLatin, new()
+{
+    [TestMethod]
+    [DataRow("I speak Goat Latin", "Imaa peaksmaaa oatGmaaaa atinLmaaaaa")]
+    [DataRow("The quick brown fox jumped over the lazy dog",
+        "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa")]
+    public void ToGoatLatin_WithSentenceContainingWords_ReturnsConvertedGoatLatinSentence(string sentence,
+        string expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.ToGoatLatin(sentence);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
