2a99122d34899673d22a013e70b25434d200194b
Eugene <eremeeveugene@gmail.com>
2025-08-03 14:43:27 +0300

    Flipping an Image

---
+++ source/LeetCode/Algorithms/FlippingAnImage/FlippingAnImageReverse.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FlippingAnImage;
+
+/// <inheritdoc />
+public class FlippingAnImageReverse : IFlippingAnImage
+{
+    /// <summary>
+    ///     Time complexity - O(n * m)
+    ///     Space complexity - O(n * m)
+    /// </summary>
+    /// <param name="image"></param>
+    /// <returns></returns>
+    public int[][] FlipAndInvertImage(int[][] image)
+    {
+        for (var i = 0; i < image.Length; i++)
+        {
+            image[i] = ReverseRow(image[i]);
+        }
+
+        return image;
+    }
+
+    private static int[] ReverseRow(int[] row)
+    {
+        return row.Reverse().Select(cell => cell ^ 1).ToArray();
+    }
+}
