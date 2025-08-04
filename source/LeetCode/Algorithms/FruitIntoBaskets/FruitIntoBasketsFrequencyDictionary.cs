fe14be8625d948bd0fe8ee3afcb1a2b748809fda
Eugene <eremeeveugene@gmail.com>
2025-08-04 14:57:31 +0300

    Fruit Into Baskets

---
+++ source/LeetCode/Algorithms/FruitIntoBaskets/FruitIntoBasketsFrequencyDictionary.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FruitIntoBaskets;
+
+/// <inheritdoc />
+public class FruitIntoBasketsFrequencyDictionary : IFruitIntoBaskets
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="fruits"></param>
+    /// <returns></returns>
+    public int TotalFruit(int[] fruits)
+    {
+        var maximumNumberOfFruits = 0;
+
+        var fruitsFrequencyDictionary = new Dictionary<int, int>();
+
+        var left = 0;
+
+        for (var right = 0; right < fruits.Length; right++)
+        {
+            fruitsFrequencyDictionary[fruits[right]] = fruitsFrequencyDictionary.GetValueOrDefault(fruits[right]) + 1;
+
+            while (fruitsFrequencyDictionary.Count > 2)
+            {
+                if (fruitsFrequencyDictionary[fruits[left]] == 1)
+                {
+                    fruitsFrequencyDictionary.Remove(fruits[left]);
+                }
+                else
+                {
+                    fruitsFrequencyDictionary[fruits[left]]--;
+                }
+
+                left++;
+            }
+
+            maximumNumberOfFruits = Math.Max(maximumNumberOfFruits, right - left + 1);
+        }
+
+        return maximumNumberOfFruits;
+    }
+}
