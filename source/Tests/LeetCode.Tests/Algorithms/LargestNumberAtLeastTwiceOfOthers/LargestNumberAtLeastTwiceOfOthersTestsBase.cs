40fee985d600055b89a6b608af797fc74cc7f5a6
Eugene <eremeeveugene@gmail.com>
2025-08-09 15:02:11 +0300

    Largest Number At Least Twice of Others

---
+++ source/Tests/LeetCode.Tests/Algorithms/LargestNumberAtLeastTwiceOfOthers/LargestNumberAtLeastTwiceOfOthersTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.LargestNumberAtLeastTwiceOfOthers;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.LargestNumberAtLeastTwiceOfOthers;
+
+public abstract class LargestNumberAtLeastTwiceOfOthersTestsBase<T> where T : ILargestNumberAtLeastTwiceOfOthers, new()
+{
+    [TestMethod]
+    [DataRow("[3,6,1,0]", 1)]
+    [DataRow("[1,2,3,4]", -1)]
+    public void DominantIndex_WithIntegerArrayContainingUniqueLargest_ReturnsIndexOfLargestElementOrMinusOne(
+        string numsJson, int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int[]>.Parse(numsJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.DominantIndex(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
