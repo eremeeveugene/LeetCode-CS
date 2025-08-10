517665baa0fd5554da18deb1fabc82d9b434e3cd
Eugene <eremeeveugene@gmail.com>
2025-08-10 16:41:29 +0300

    Reordered Power of 2 (#1415)

---
+++ source/LeetCode/Algorithms/ReorderedPowerOfTwo/ReorderedPowerOfTwoFrequencyArray.cs
@@ -0,0 +1,66 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ReorderedPowerOfTwo;
+
+/// <inheritdoc />
+public class ReorderedPowerOfTwoFrequencyArray : IReorderedPowerOfTwo
+{
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public bool ReorderedPowerOf2(int n)
+    {
+        var targetDigitsFrequency = GetDigitsFrequency(n);
+
+        for (var i = 0; i < 31; i++)
+        {
+            var currentDigitsFrequency = GetDigitsFrequency(1 << i);
+
+            if (AreEqual(targetDigitsFrequency, currentDigitsFrequency))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static int[] GetDigitsFrequency(int num)
+    {
+        var digitsFrequency = new int[10];
+
+        while (num > 0)
+        {
+            digitsFrequency[num % 10]++;
+
+            num /= 10;
+        }
+
+        return digitsFrequency;
+    }
+
+    private static bool AreEqual(int[] a, int[] b)
+    {
+        for (var i = 0; i < 10; i++)
+        {
+            if (a[i] != b[i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
