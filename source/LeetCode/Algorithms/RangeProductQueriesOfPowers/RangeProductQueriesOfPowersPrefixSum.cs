6377f64105ceb28eaf0f92bb8f686d831917b729
Eugene <eremeeveugene@gmail.com>
2025-08-11 18:03:04 +0300

    Range Product Queries of Powers (#1417)

---
+++ source/LeetCode/Algorithms/RangeProductQueriesOfPowers/RangeProductQueriesOfPowersPrefixSum.cs
@@ -0,0 +1,73 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Numerics;
+
+namespace LeetCode.Algorithms.RangeProductQueriesOfPowers;
+
+/// <inheritdoc />
+public class RangeProductQueriesOfPowersPrefixSum : IRangeProductQueriesOfPowers
+{
+    private const int Mod = 1_000_000_007;
+
+    /// <summary>
+    ///     Time complexity - O(q), where q is queries.Length
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="queries"></param>
+    /// <returns></returns>
+    public int[] ProductQueries(int n, int[][] queries)
+    {
+        var exponents = new List<int>(BitOperations.PopCount((uint)n));
+
+        for (var bit = 0; bit < 31; bit++)
+        {
+            if (((n >> bit) & 1) == 0)
+            {
+                continue;
+            }
+
+            exponents.Add(bit);
+        }
+
+        var exponentsPrefixSum = new int[exponents.Count + 1];
+
+        for (var i = 0; i < exponents.Count; i++)
+        {
+            exponentsPrefixSum[i + 1] = exponentsPrefixSum[i] + exponents[i];
+        }
+
+        var maxExponent = exponentsPrefixSum[^1];
+
+        var powersOfTwo = new int[maxExponent + 1];
+
+        powersOfTwo[0] = 1;
+
+        for (var i = 1; i <= maxExponent; i++)
+        {
+            powersOfTwo[i] = powersOfTwo[i - 1] * 2 % Mod;
+        }
+
+        var result = new int[queries.Length];
+
+        for (var i = 0; i < queries.Length; i++)
+        {
+            var left = queries[i][0];
+            var right = queries[i][1];
+            var exponent = exponentsPrefixSum[right + 1] - exponentsPrefixSum[left];
+
+            result[i] = powersOfTwo[exponent];
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/RangeProductQueriesOfPowers/RangeProductQueriesOfPowersTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.RangeProductQueriesOfPowers;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.RangeProductQueriesOfPowers;
+
+public abstract class RangeProductQueriesOfPowersTestsBase<T> where T : IRangeProductQueriesOfPowers, new()
+{
+    [TestMethod]
+    [DataRow(2, "[[0,0]]", "[2]")]
+    [DataRow(15, "[[0,1],[2,2],[0,3]]", "[2,4,64]")]
+    public void ProductQueries_WithPowersOfTwoDecompositionAndRangeQueries_ReturnsModuloProductArray(int n,
+        string queriesJson, string expectedResultJson)
+    {
+        // Arrange
+        var queries = JsonHelper<int[][]>.Parse(queriesJson);
+        var expectedResult = JsonHelper<int[]>.Parse(expectedResultJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.ProductQueries(n, queries);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
