4296fbf53772eec1c27841eef81150db0cc7f194
Eugene <eremeeveugene@gmail.com>
2025-08-12 19:46:07 +0300

    Ways to Express an Integer as Sum of Powers (#1420)

--- README.md
+++ README.md
@@ -584,6 +584,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [2769. Find the Maximum Achievable Number](https://leetcode.com/problems/find-the-maximum-achievable-number/description/)
 - [2779. Maximum Beauty of an Array After Applying Operation](https://leetcode.com/problems/maximum-beauty-of-an-array-after-applying-operation/description/)
 - [2780. Minimum Index of a Valid Split](https://leetcode.com/problems/minimum-index-of-a-valid-split/description/)
+- [2787. Ways to Express an Integer as Sum of Powers](https://leetcode.com/problems/ways-to-express-an-integer-as-sum-of-powers/description/)
 - [2799. Count Complete Subarrays in an Array](https://leetcode.com/problems/count-complete-subarrays-in-an-array/description/)
 - [2807. Insert Greatest Common Divisors in Linked List](https://leetcode.com/problems/insert-greatest-common-divisors-in-linked-list/description/)
 - [2810. Faulty Keyboard](https://leetcode.com/problems/faulty-keyboard/description/)
---
+++ source/LeetCode/Algorithms/WaysToExpressAnIntegerAsSumOfPowers/WaysToExpressAnIntegerAsSumOfPowersDynamicProgramming.cs
@@ -0,0 +1,61 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.WaysToExpressAnIntegerAsSumOfPowers;
+
+/// <inheritdoc />
+public class WaysToExpressAnIntegerAsSumOfPowersDynamicProgramming : IWaysToExpressAnIntegerAsSumOfPowers
+{
+    private const int Mod = 1_000_000_007;
+
+    /// <summary>
+    ///     Time complexity - O(n^(1 + 1/x))
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public int NumberOfWays(int n, int x)
+    {
+        var dp = new long[n + 1];
+
+        dp[0] = 1;
+
+        for (var i = 1; i <= n; i++)
+        {
+            var power = Pow(i, x);
+
+            if (power > n)
+            {
+                break;
+            }
+
+            for (var j = n; j >= power; j--)
+            {
+                dp[j] = (dp[j] + dp[j - power]) % Mod;
+            }
+        }
+
+        return (int)dp[n];
+    }
+
+    private static int Pow(int value, int exponent)
+    {
+        var result = 1;
+
+        for (var i = 0; i < exponent; i++)
+        {
+            result *= value;
+        }
+
+        return result;
+    }
+}
