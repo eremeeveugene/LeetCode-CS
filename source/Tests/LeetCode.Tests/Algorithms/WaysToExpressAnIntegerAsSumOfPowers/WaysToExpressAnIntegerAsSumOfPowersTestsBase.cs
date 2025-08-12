5836fa0a3535fd92b1d55e66dbacad76e5f412a4
Eugene <eremeeveugene@gmail.com>
2025-08-12 19:42:26 +0300

    Ways to Express an Integer as Sum of Powers

---
+++ source/Tests/LeetCode.Tests/Algorithms/WaysToExpressAnIntegerAsSumOfPowers/WaysToExpressAnIntegerAsSumOfPowersTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.WaysToExpressAnIntegerAsSumOfPowers;
+
+namespace LeetCode.Tests.Algorithms.WaysToExpressAnIntegerAsSumOfPowers;
+
+public abstract class WaysToExpressAnIntegerAsSumOfPowersTestsBase<T>
+    where T : IWaysToExpressAnIntegerAsSumOfPowers, new()
+{
+    [TestMethod]
+    [DataRow(10, 2, 1)]
+    [DataRow(4, 1, 2)]
+    public void NumberOfWays_WithPositiveNAndExponent_ReturnsCountOfUniquePowerSumDecompositions(
+        int n, int x, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.NumberOfWays(n, x);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
