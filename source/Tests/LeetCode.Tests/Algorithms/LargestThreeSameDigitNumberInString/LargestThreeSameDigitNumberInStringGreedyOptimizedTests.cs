42dc161ca8e295bb4dc77f49276c31fb7d83c12f
Eugene <eremeeveugene@gmail.com>
2025-08-14 13:07:14 +0300

    Improve

---
+++ source/Tests/LeetCode.Tests/Algorithms/LargestThreeSameDigitNumberInString/LargestThreeSameDigitNumberInStringGreedyOptimizedTests.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.LargestThreeSameDigitNumberInString;
+
+namespace LeetCode.Tests.Algorithms.LargestThreeSameDigitNumberInString;
+
+[TestClass]
+public class LargestThreeSameDigitNumberInStringGreedyOptimizedTests :
+    LargestThreeSameDigitNumberInStringTestsBase<LargestThreeSameDigitNumberInStringGreedyOptimized>;
