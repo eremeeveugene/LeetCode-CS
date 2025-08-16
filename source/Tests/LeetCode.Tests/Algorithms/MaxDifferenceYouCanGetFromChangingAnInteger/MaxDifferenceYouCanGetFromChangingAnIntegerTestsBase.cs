ad9aa024490d290160b8dff2c8c8b4601c2e9483
Eugene <eremeeveugene@gmail.com>
2025-08-16 17:43:02 +0300

    Max Difference You Can Get From Changing an Integer

---
+++ source/Tests/LeetCode.Tests/Algorithms/MaxDifferenceYouCanGetFromChangingAnInteger/MaxDifferenceYouCanGetFromChangingAnIntegerTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaxDifferenceYouCanGetFromChangingAnInteger;
+
+namespace LeetCode.Tests.Algorithms.MaxDifferenceYouCanGetFromChangingAnInteger;
+
+public abstract class MaxDifferenceYouCanGetFromChangingAnIntegerTestsBase<T>
+    where T : IMaxDifferenceYouCanGetFromChangingAnInteger, new()
+{
+    [TestMethod]
+    [DataRow(9, 8)]
+    [DataRow(555, 888)]
+    [DataRow(10000, 80000)]
+    [DataRow(123456, 820000)]
+    public void MaxDiff_WithDigitReplacementsAppliedTwice_ReturnsMaximumDifferenceBetweenResults(int num,
+        int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaxDiff(num);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
