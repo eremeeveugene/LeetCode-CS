2a4c0e05cb6ea23bf8b45bd3505ff16389c47749
Eugene <eremeeveugene@gmail.com>
2025-08-17 15:35:05 +0300

    New 21 Game

---
+++ source/Tests/LeetCode.Tests/Algorithms/New21Game/New21GameDictionaryPriorityQueueTests.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.New21Game;
+
+namespace LeetCode.Tests.Algorithms.New21Game;
+
+[TestClass]
+public class New21GameDictionaryPriorityQueueTests : New21GameTestsBase<New21GameDictionaryPriorityQueue>;
