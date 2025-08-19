3ce481f7d3f664b4e0ee6dec6a3b7d4ef12f1024
Eugene <eremeeveugene@gmail.com>
2025-08-19 12:21:13 +0300

    Number of Zero-Filled Subarrays (#1429)

---
+++ source/LeetCode/Algorithms/NumberOfZeroFilledSubarrays/NumberOfZeroFilledSubarraysCounting.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.NumberOfZeroFilledSubarrays;
+
+/// <inheritdoc />
+public class NumberOfZeroFilledSubarraysCounting : INumberOfZeroFilledSubarrays
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public long ZeroFilledSubarray(int[] nums)
+    {
+        long count = 0;
+        var zeros = 0;
+
+        foreach (var num in nums)
+        {
+            if (num == 0)
+            {
+                zeros++;
+
+                count += zeros;
+            }
+            else
+            {
+                zeros = 0;
+            }
+        }
+
+        return count;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/NumberOfZeroFilledSubarrays/NumberOfZeroFilledSubarraysTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.NumberOfZeroFilledSubarrays;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.NumberOfZeroFilledSubarrays;
+
+public abstract class NumberOfZeroFilledSubarraysTestsBase<T> where T : INumberOfZeroFilledSubarrays, new()
+{
+    [TestMethod]
+    [DataRow("[1,3,0,0,2,0,0,4]", 6)]
+    [DataRow("[0,0,0,2,0,0]", 9)]
+    [DataRow("[2,10,2019]", 0)]
+    [DataRow("[0,0,0,0,0]", 15)]
+    public void ZeroFilledSubarray_WithNumsArray_ReturnsCountOfZeroFilledSubarrays(string numsJson,
+        long expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int[]>.Parse(numsJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.ZeroFilledSubarray(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
