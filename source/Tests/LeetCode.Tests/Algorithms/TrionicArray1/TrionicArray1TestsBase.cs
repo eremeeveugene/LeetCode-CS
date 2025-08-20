db2b67e74ca845d2f7fde405b06a35825d6d9e05
Eugene <eremeeveugene@gmail.com>
2025-08-20 12:34:04 +0300

    Trionic Array I (#1433)

---
+++ source/Tests/LeetCode.Tests/Algorithms/TrionicArray1/TrionicArray1TestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.TrionicArray1;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.TrionicArray1;
+
+public abstract class TrionicArray1TestsBase<T> where T : ITrionicArray1, new()
+{
+    [TestMethod]
+    [DataRow("[1,3,5,4,2,6]", true)]
+    [DataRow("[2,1,3]", false)]
+    public void IsTrionic_WithNumsArray_ReturnsTrueIfTrionic(string numsJson, bool expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int[]>.Parse(numsJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsTrionic(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
