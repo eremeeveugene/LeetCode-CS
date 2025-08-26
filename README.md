d7cdf9d8d45128d0e5ac7140a1601a6fd675414d
Eugene <eremeeveugene@gmail.com>
2025-08-26 15:40:11 +0300

    Maximum Area of Longest Diagonal Rectangle

--- README.md
+++ README.md
@@ -624,6 +624,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [2974. Minimum Number Game](https://leetcode.com/problems/minimum-number-game/description/)
 - [2981. Find Longest Special Substring That Occurs Thrice I](https://leetcode.com/problems/find-longest-special-substring-that-occurs-thrice-i/description/)
 - [2997. Minimum Number of Operations to Make Array XOR Equal to K](https://leetcode.com/problems/minimum-number-of-operations-to-make-array-xor-equal-to-k/description/)
+- [3000. Maximum Area of Longest Diagonal Rectangle](https://leetcode.com/problems/maximum-area-of-longest-diagonal-rectangle/description/)
 - [3005. Count Elements With Maximum Frequency](https://leetcode.com/problems/count-elements-with-maximum-frequency/description/)
 - [3011. Find if Array Can Be Sorted](https://leetcode.com/problems/find-if-array-can-be-sorted/description/)
 - [3024. Type of Triangle](https://leetcode.com/problems/type-of-triangle/description/)
---
+++ source/Tests/LeetCode.Tests/Algorithms/MaximumAreaOfLongestDiagonalRectangle/MaximumAreaOfLongestDiagonalRectangleTestsBase.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaximumAreaOfLongestDiagonalRectangle;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MaximumAreaOfLongestDiagonalRectangle;
+
+public abstract class MaximumAreaOfLongestDiagonalRectangleTestsBase<T>
+    where T : IMaximumAreaOfLongestDiagonalRectangle, new()
+{
+    [TestMethod]
+    [DataRow("[[9,3],[8,6]]", 48)]
+    [DataRow("[[3,4],[4,3]]", 12)]
+    [DataRow("[[2,6],[5,1],[3,10],[8,4]]", 30)]
+    [DataRow("[[6,5],[8,6],[2,10],[8,1],[9,2],[3,5],[3,5]]", 20)]
+    public void AreaOfMaxDiagonal_WithDimensionsArray_ReturnsAreaOfRectangleHavingTheLongestDiagonal(
+        string dimensionsJson, double expectedResult)
+    {
+        // Arrange
+        var dimensions = JsonHelper<int[][]>.Parse(dimensionsJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.AreaOfMaxDiagonal(dimensions);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
