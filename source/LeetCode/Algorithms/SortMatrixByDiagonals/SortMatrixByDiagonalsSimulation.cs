81dbc858b50fc811af4fec907bfa52a4491b0c92
Eugene <eremeeveugene@gmail.com>
2025-08-28 19:34:41 +0300

    Sort Matrix by Diagonals (#1448)

---
+++ source/LeetCode/Algorithms/SortMatrixByDiagonals/SortMatrixByDiagonalsSimulation.cs
@@ -0,0 +1,74 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SortMatrixByDiagonals;
+
+/// <inheritdoc />
+public class SortMatrixByDiagonalsSimulation : ISortMatrixByDiagonals
+{
+    /// <summary>
+    ///     Time complexity - O(n^2 log n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="grid"></param>
+    /// <returns></returns>
+    public int[][] SortMatrix(int[][] grid)
+    {
+        var n = grid.Length;
+
+        if (n == 1)
+        {
+            return grid;
+        }
+
+        Span<int> items = stackalloc int[n * n];
+
+        for (var i = 0; i < n - 1; i++)
+        {
+            var index = 0;
+
+            for (var j = 0; i + j < n; j++)
+            {
+                items[index] = grid[i + j][j];
+
+                index++;
+            }
+
+            items[..index].Sort((a, b) => b.CompareTo(a));
+
+            for (var j = 0; j < index; j++)
+            {
+                grid[i + j][j] = items[j];
+            }
+        }
+
+        for (var j = 1; j < n - 1; j++)
+        {
+            var index = 0;
+
+            for (var i = 0; j + i < n; i++)
+            {
+                items[index] = grid[i][j + i];
+
+                index++;
+            }
+
+            items[..index].Sort();
+
+            for (var i = 0; i < index; i++)
+            {
+                grid[i][j + i] = items[i];
+            }
+        }
+
+        return grid;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/SortMatrixByDiagonals/SortMatrixByDiagonalsTestsBase.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SortMatrixByDiagonals;
+using LeetCode.Core.Helpers;
+using LeetCode.Tests.Base.Extensions;
+
+namespace LeetCode.Tests.Algorithms.SortMatrixByDiagonals;
+
+public abstract class SortMatrixByDiagonalsTestsBase<T> where T : ISortMatrixByDiagonals, new()
+{
+    [TestMethod]
+    [DataRow("[[1]]", "[[1]]")]
+    [DataRow("[[0,1],[1,2]]", "[[2,1],[1,0]]")]
+    [DataRow("[[1,7,3],[9,8,2],[4,5,6]]", "[[8,2,3],[9,6,7],[4,5,1]]")]
+    [DataRow("[[2,-4,0],[2,0,-4],[-4,2,0]]", "[[2,-4,0],[2,0,-4],[-4,2,0]]")]
+    public void SortMatrix_WithSquareMatrix_SortsBottomLeftDiagonalsDescendingAndTopRightDiagonalsAscending(
+        string gridJson, string expectedResultJson)
+    {
+        // Arrange
+        var grid = JsonHelper<int[][]>.Parse(gridJson);
+        var expectedResult = JsonHelper<int[][]>.Parse(expectedResultJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.SortMatrix(grid);
+
+        // Assert
+        NestedCollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
