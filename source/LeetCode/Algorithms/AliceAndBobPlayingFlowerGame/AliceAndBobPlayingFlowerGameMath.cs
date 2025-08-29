366348c4525fd8562c445610b7e5d0119a01fe97
Eugene <eremeeveugene@gmail.com>
2025-08-29 13:56:22 +0300

    Alice and Bob Playing Flower Game

---
+++ source/LeetCode/Algorithms/AliceAndBobPlayingFlowerGame/AliceAndBobPlayingFlowerGameMath.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.AliceAndBobPlayingFlowerGame;
+
+/// <inheritdoc />
+public class AliceAndBobPlayingFlowerGameMath : IAliceAndBobPlayingFlowerGame
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="m"></param>
+    /// <returns></returns>
+    public long FlowerGame(int n, int m)
+    {
+        return (long)n * m / 2;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/AliceAndBobPlayingFlowerGame/AliceAndBobPlayingFlowerGameTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.AliceAndBobPlayingFlowerGame;
+
+namespace LeetCode.Tests.Algorithms.AliceAndBobPlayingFlowerGame;
+
+public abstract class AliceAndBobPlayingFlowerGameTestsBase<T> where T : IAliceAndBobPlayingFlowerGame, new()
+{
+    [TestMethod]
+    [DataRow(3, 2, 3)]
+    [DataRow(1, 1, 0)]
+    public void FlowerGame_WithFirstAndSecondLaneFlowerCounts_ReturnsTotalWinningPairsForAlice(int n, int m,
+        long expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FlowerGame(n, m);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
