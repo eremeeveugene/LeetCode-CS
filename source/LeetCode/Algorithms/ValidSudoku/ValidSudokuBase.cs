0e0a8e7926516d3ccd68fb9d6eb5d36ab1523fee
Eugene <eremeeveugene@gmail.com>
2025-08-30 18:14:08 +0300

    Valid Sudoku (#1453)

---
+++ source/LeetCode/Algorithms/ValidSudoku/ValidSudokuBase.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ValidSudoku;
+
+/// <inheritdoc />
+public abstract class ValidSudokuBase : IValidSudoku
+{
+    protected const char EmptyCell = '.';
+    protected const int N = 9;
+
+    public abstract bool IsValidSudoku(char[][] board);
+
+    protected static int GetCellIndex(int cell)
+    {
+        return cell - '1';
+    }
+
+    protected static int GetBoxIndex(int rowIndex, int columnIndex)
+    {
+        return (rowIndex / 3 * 3) + (columnIndex / 3);
+    }
+}
---
+++ source/LeetCode/Algorithms/ValidSudoku/ValidSudokuBoolTables.cs
@@ -0,0 +1,62 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ValidSudoku;
+
+/// <inheritdoc />
+public class ValidSudokuBoolTables : ValidSudokuBase
+{
+    /// <summary>
+    ///     Time complexity - O(n^2)
+    ///     Space complexity - O(n^2)
+    /// </summary>
+    /// <param name="board"></param>
+    /// <returns></returns>
+    public override bool IsValidSudoku(char[][] board)
+    {
+        Span<bool> rows = stackalloc bool[N * N];
+        Span<bool> columns = stackalloc bool[N * N];
+        Span<bool> boxes = stackalloc bool[N * N];
+
+        for (var rowIndex = 0; rowIndex < N; rowIndex++)
+        {
+            var row = board[rowIndex];
+
+            for (var columnIndex = 0; columnIndex < N; columnIndex++)
+            {
+                int cell = row[columnIndex];
+
+                if (cell == EmptyCell)
+                {
+                    continue;
+                }
+
+                var cellIndex = GetCellIndex(cell);
+                var boxIndex = GetBoxIndex(rowIndex, columnIndex);
+
+                var rowSlot = (rowIndex * N) + cellIndex;
+                var columnSlot = (columnIndex * N) + cellIndex;
+                var boxSlot = (boxIndex * N) + cellIndex;
+
+                if (rows[rowSlot] || columns[columnSlot] || boxes[boxSlot])
+                {
+                    return false;
+                }
+
+                rows[rowSlot] = true;
+                columns[columnSlot] = true;
+                boxes[boxSlot] = true;
+            }
+        }
+
+        return true;
+    }
+}
