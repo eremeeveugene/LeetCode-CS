b32f18f460836eff30aff4de8766d862793321f7
Eugene <eremeeveugene@gmail.com>
2025-08-31 18:36:21 +0300

    Using a Robot to Print the Lexicographically Smallest String

---
+++ source/Tests/LeetCode.Tests/Algorithms/UsingRobotToPrintTheLexicographicallySmallestString/UsingRobotToPrintTheLexicographicallySmallestStringTestsBase.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.UsingRobotToPrintTheLexicographicallySmallestString;
+
+namespace LeetCode.Tests.Algorithms.UsingRobotToPrintTheLexicographicallySmallestString;
+
+public abstract class UsingRobotToPrintTheLexicographicallySmallestStringTestsBase<T> where T :
+    IUsingRobotToPrintTheLexicographicallySmallestString, new()
+{
+    [TestMethod]
+    [DataRow("zza", "azz")]
+    [DataRow("bac", "abc")]
+    [DataRow("bdda", "addb")]
+    [DataRow("bydizfve", "bdevfziy")]
+    [DataRow("bzeyxf", "befxyz")]
+    [DataRow("vzhofnpo", "fnohopzv")]
+    [DataRow("mmuqezwmomeplrtskz", "eekstrlpmomwzqummz")]
+    public void RobotWithString_WithGivenString_ReturnsLexicographicallySmallestWrittenString(string s,
+        string expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.RobotWithString(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
