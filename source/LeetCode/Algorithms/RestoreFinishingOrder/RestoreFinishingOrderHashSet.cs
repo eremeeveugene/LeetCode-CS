f6091464e84767ef2c98b6c7eea74bd0443697c1
Eugene <eremeeveugene@gmail.com>
2025-09-01 14:42:06 +0300

    Restore Finishing Order (#1456)

---
+++ source/LeetCode/Algorithms/RestoreFinishingOrder/RestoreFinishingOrderHashSet.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.RestoreFinishingOrder;
+
+/// <inheritdoc />
+public class RestoreFinishingOrderHashSet : IRestoreFinishingOrder
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="orders"></param>
+    /// <param name="friends"></param>
+    /// <returns></returns>
+    public int[] RecoverOrder(int[] orders, int[] friends)
+    {
+        var friendsHashSet = new HashSet<int>(friends);
+
+        var friendsIndex = 0;
+
+        foreach (var order in orders)
+        {
+            if (!friendsHashSet.Contains(order))
+            {
+                continue;
+            }
+
+            friends[friendsIndex++] = order;
+
+            if (friendsIndex == friends.Length)
+            {
+                break;
+            }
+        }
+
+        return friends;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/RestoreFinishingOrder/RestoreFinishingOrderTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.RestoreFinishingOrder;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.RestoreFinishingOrder;
+
+public abstract class RestoreFinishingOrderTestsBase<T> where T : IRestoreFinishingOrder, new()
+{
+    [TestMethod]
+    [DataRow("[3,1,2,5,4]", "[1,3,4]", "[3,1,4]")]
+    [DataRow("[1,4,5,3,2]", "[2,5]", "[5,2]")]
+    public void RecoverOrder_WithFriendIdsInOrderArray_ReturnsFriendsInFinishingOrder(string ordersJson,
+        string friendsJson, string expectedResultJson)
+    {
+        // Arrange
+        var orders = JsonHelper<int[]>.Parse(ordersJson);
+        var friends = JsonHelper<int[]>.Parse(friendsJson);
+        var expectedResult = JsonHelper<int[]>.Parse(expectedResultJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.RecoverOrder(orders, friends);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
