9cf29f260648186b450ee4f25f6d0e3c3f82c503
Eugene <eremeeveugene@gmail.com>
2025-09-02 18:35:48 +0300

    Find the Number of Ways to Place People I (#1458)

---
+++ source/LeetCode/Algorithms/FindTheNumberOfWaysToPlacePeople1/IFindTheNumberOfWaysToPlacePeople1.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindTheNumberOfWaysToPlacePeople1;
+
+/// <summary>
+///     https://leetcode.com/problems/find-the-number-of-ways-to-place-people-i/description/
+/// </summary>
+public interface IFindTheNumberOfWaysToPlacePeople1
+{
+    int NumberOfPairs(int[][] points);
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FindTheNumberOfWaysToPlacePeople1/FindTheNumberOfWaysToPlacePeople1TestsBase.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindTheNumberOfWaysToPlacePeople1;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.FindTheNumberOfWaysToPlacePeople1;
+
+public abstract class FindTheNumberOfWaysToPlacePeople1TestsBase<T> where T : IFindTheNumberOfWaysToPlacePeople1, new()
+{
+    [TestMethod]
+    [DataRow("[[1,1],[2,2],[3,3]]", 0)]
+    [DataRow("[[3,1],[1,3],[1,1]]", 2)]
+    [DataRow("[[6,2],[4,4],[2,6]]", 2)]
+    [DataRow("[[6,2],[4,4],[2,6],[4,8]]", 3)]
+    [DataRow("[[6,2],[4,4],[2,6],[4,8],[1,4]]", 4)]
+    [DataRow("[[6,2],[4,4],[2,6],[4,8],[1,4],[2,2]]", 7)]
+    public void NumberOfPairs_With2DPointsArray_ReturnsCountOfValidPairs(string pointsJson,
+        int expectedResult)
+    {
+        // Arrange
+        var points = JsonHelper<int[][]>.Parse(pointsJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.NumberOfPairs(points);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
