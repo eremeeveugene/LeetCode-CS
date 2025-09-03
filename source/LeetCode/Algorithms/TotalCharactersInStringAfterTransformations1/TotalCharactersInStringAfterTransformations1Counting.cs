3ee2c228c4befad6517eb72d2bc0bcb6a456df05
Eugene <eremeeveugene@gmail.com>
2025-09-03 17:24:56 +0300

    Total Characters in String After Transformations I

---
+++ source/LeetCode/Algorithms/TotalCharactersInStringAfterTransformations1/TotalCharactersInStringAfterTransformations1Counting.cs
@@ -0,0 +1,65 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.TotalCharactersInStringAfterTransformations1;
+
+/// <inheritdoc />
+public class TotalCharactersInStringAfterTransformations1Counting : ITotalCharactersInStringAfterTransformations1
+{
+    private const int Modulo = 1_000_000_007;
+    private const byte AlphabetLength = 'z' - 'a' + 1;
+
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="transformationsCount"></param>
+    /// <returns></returns>
+    public int LengthAfterTransformations(string input, int transformationsCount)
+    {
+        Span<long> frequencies = stackalloc long[AlphabetLength];
+
+        foreach (var c in input)
+        {
+            frequencies[c - 'a']++;
+        }
+
+        while (transformationsCount > 0)
+        {
+            for (var i = AlphabetLength - 1; i >= 0 && transformationsCount > 0; i--)
+            {
+                if (frequencies[i] > 0)
+                {
+                    var nextIndex = i + 1;
+
+                    if (nextIndex >= AlphabetLength)
+                    {
+                        nextIndex = 0;
+                    }
+
+                    frequencies[nextIndex] = (frequencies[nextIndex] + frequencies[i]) % Modulo;
+                }
+
+                transformationsCount--;
+            }
+        }
+
+        long result = 0;
+
+        foreach (var frequency in frequencies)
+        {
+            result = (result + frequency) % Modulo;
+        }
+
+        return (int)result;
+    }
+}
