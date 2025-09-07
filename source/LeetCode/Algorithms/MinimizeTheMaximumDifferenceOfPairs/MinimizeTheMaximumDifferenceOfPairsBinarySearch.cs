6a737ce40241129f9486bca3bf917f694458d8f9
Eugene <eremeeveugene@gmail.com>
2025-09-07 16:50:49 +0300

    Minimize the Maximum Difference of Pairs

---
+++ source/LeetCode/Algorithms/MinimizeTheMaximumDifferenceOfPairs/MinimizeTheMaximumDifferenceOfPairsBinarySearch.cs
@@ -0,0 +1,78 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimizeTheMaximumDifferenceOfPairs;
+
+/// <inheritdoc />
+public class MinimizeTheMaximumDifferenceOfPairsBinarySearch : IMinimizeTheMaximumDifferenceOfPairs
+{
+    /// <summary>
+    ///     Time complexity - O(n log n + n log R), where R = max(nums) - min(nums)
+    ///     Space complexity - O(log n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="requiredPairsCount"></param>
+    /// <returns></returns>
+    public int MinimizeMax(int[] nums, int requiredPairsCount)
+    {
+        if (requiredPairsCount == 0)
+        {
+            return 0;
+        }
+
+        Array.Sort(nums);
+
+        var low = 0;
+        var high = nums[^1] - nums[0];
+
+        while (low < high)
+        {
+            var mid = low + ((high - low) / 2);
+
+            if (CanFormEnoughPairs(nums, mid, requiredPairsCount))
+            {
+                high = mid;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    private static bool CanFormEnoughPairs(int[] nums, int x, int requiredPairsCount)
+    {
+        var numsLength = nums.Length;
+
+        var pairsCount = 0;
+
+        for (var i = 0; i < numsLength - 1; i++)
+        {
+            if (nums[i + 1] - nums[i] > x)
+            {
+                continue;
+            }
+
+            pairsCount++;
+
+            if (pairsCount == requiredPairsCount)
+            {
+                return true;
+            }
+
+            i++;
+        }
+
+        return false;
+    }
+}
