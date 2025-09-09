75236bacaa0825e462071341c0772c183a328c93
Eugene <eremeeveugene@gmail.com>
2025-09-09 20:14:57 +0300

    Number of People Aware of a Secret (#1470)

---
+++ source/LeetCode/Algorithms/NumberOfPeopleAwareOfSecret/INumberOfPeopleAwareOfSecret.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.NumberOfPeopleAwareOfSecret;
+
+/// <summary>
+///     https://leetcode.com/problems/number-of-people-aware-of-a-secret/description/
+/// </summary>
+public interface INumberOfPeopleAwareOfSecret
+{
+    int PeopleAwareOfSecret(int n, int delay, int forget);
+}
