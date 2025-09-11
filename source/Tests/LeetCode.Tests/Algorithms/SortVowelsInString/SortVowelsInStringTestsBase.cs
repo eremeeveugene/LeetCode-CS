aad7b5d9ef5457f2568e5379d9fbe9c8b1bd2b8a
Eugene <eremeeveugene@gmail.com>
2025-09-11 12:20:50 +0300

    Sort Vowels in a String

---
+++ source/Tests/LeetCode.Tests/Algorithms/SortVowelsInString/SortVowelsInStringTestsBase.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SortVowelsInString;
+
+namespace LeetCode.Tests.Algorithms.SortVowelsInString;
+
+public abstract class SortVowelsInStringTestsBase<T> where T : ISortVowelsInString, new()
+{
+    [TestMethod]
+    [DataRow("lEetcOde", "lEOtcede")]
+    [DataRow("lYmpH", "lYmpH")]
+    public void SortVowels_WithString_SortsVowelsByAsciiOrder(string s, string expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.SortVowels(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
