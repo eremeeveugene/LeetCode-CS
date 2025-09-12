59e6d4c1128a3d26a5e4376d7541d22d55235e88
Eugene <eremeeveugene@gmail.com>
2025-09-12 17:32:29 +0300

    Vowels Game in a String (#1477)

---
+++ source/LeetCode/Algorithms/VowelsGameInString/VowelsGameInStringMath.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.VowelsGameInString;
+
+/// <inheritdoc />
+public class VowelsGameInStringMath : IVowelsGameInString
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public bool DoesAliceWin(string s)
+    {
+        foreach (var c in s)
+        {
+            if (IsVowel(c))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private static bool IsVowel(char c)
+    {
+        return c is 'a' or 'e' or 'i' or 'o' or 'u';
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/VowelsGameInString/VowelsGameInStringTestsBase.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.VowelsGameInString;
+
+namespace LeetCode.Tests.Algorithms.VowelsGameInString;
+
+public abstract class VowelsGameInStringTestsBase<T> where T : IVowelsGameInString, new()
+{
+    [TestMethod]
+    [DataRow("leetcoder", true)]
+    [DataRow("bbcd", false)]
+    public void DoesAliceWin_WithGivenString_ReturnsTrueIfAliceCanForceWin(string s, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.DoesAliceWin(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
