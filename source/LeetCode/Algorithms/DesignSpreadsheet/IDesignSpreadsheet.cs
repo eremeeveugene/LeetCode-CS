d7d4233f4ca4b39e350cc57d57720951caddf9f8
Eugene <eremeeveugene@gmail.com>
2025-09-19 17:00:39 +0300

    Design Spreadsheet

---
+++ source/LeetCode/Algorithms/DesignSpreadsheet/IDesignSpreadsheet.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.DesignSpreadsheet;
+
+/// <summary>
+///     https://leetcode.com/problems/design-spreadsheet/description/
+/// </summary>
+public interface IDesignSpreadsheet
+{
+    void SetCell(string cell, int value);
+
+    void ResetCell(string cell);
+
+    int GetValue(string formula);
+}
