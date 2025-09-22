58b7e6d4347a244ba6f189f8f3bffc22dba8051c
Eugene <eremeeveugene@gmail.com>
2025-09-22 15:54:29 +0300

    Design Movie Rental System

---
+++ source/LeetCode/Algorithms/DesignMovieRentalSystem/DesignMovieRentalSystemDictionary.cs
@@ -0,0 +1,197 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.DesignMovieRentalSystem;
+
+/// <inheritdoc />
+public class DesignMovieRentalSystemDictionary : IDesignMovieRentalSystem
+{
+    private const int ResultsCount = 5;
+    private readonly Dictionary<int, SortedSet<MovieOffer>> _movieToMovieOffersDictionary;
+    private readonly SortedSet<RentalRecord> _rentalRecordsSortedSet = [];
+    private readonly Dictionary<(int Shop, int Movie), int> _shopMovieToPriceDictionary;
+
+    public DesignMovieRentalSystemDictionary(int n, int[][] entries)
+    {
+        var entriesLength = entries.Length;
+
+        _movieToMovieOffersDictionary = new Dictionary<int, SortedSet<MovieOffer>>(entriesLength);
+        _shopMovieToPriceDictionary = new Dictionary<(int Shop, int Movie), int>(entriesLength);
+
+        for (var i = 0; i < entriesLength; i++)
+        {
+            var entry = entries[i];
+            var shop = entry[0];
+            var movie = entry[1];
+            var price = entry[2];
+
+            _shopMovieToPriceDictionary.Add((shop, movie), price);
+
+            if (!_movieToMovieOffersDictionary.TryGetValue(movie, out var movieOffers))
+            {
+                movieOffers = [];
+
+                _movieToMovieOffersDictionary[movie] = movieOffers;
+            }
+
+            movieOffers.Add(new MovieOffer(price, shop));
+        }
+    }
+
+    public IList<int> Search(int movie)
+    {
+        if (!_movieToMovieOffersDictionary.TryGetValue(movie, out var movieOffers))
+        {
+            return [];
+        }
+
+        var movieOffersCount = movieOffers.Count;
+
+        if (movieOffersCount == 0)
+        {
+            return [];
+        }
+
+        var searchResultsCount = Math.Min(movieOffersCount, ResultsCount);
+
+        var searchResults = new int[searchResultsCount];
+
+        var searchResultsIndex = 0;
+
+        foreach (var movieOffer in movieOffers)
+        {
+            searchResults[searchResultsIndex] = movieOffer.Shop;
+
+            searchResultsIndex++;
+
+            if (searchResultsIndex == searchResultsCount)
+            {
+                break;
+            }
+        }
+
+        return searchResults;
+    }
+
+    public IList<IList<int>> Report()
+    {
+        var rentalItemsCount = _rentalRecordsSortedSet.Count;
+
+        if (rentalItemsCount == 0)
+        {
+            return [];
+        }
+
+        var reportResultsCount = Math.Min(rentalItemsCount, ResultsCount);
+
+        var reportResults = new IList<int>[reportResultsCount];
+
+        var reportResultsIndex = 0;
+
+        foreach (var rentalItem in _rentalRecordsSortedSet)
+        {
+            reportResults[reportResultsIndex] = rentalItem.GetReport();
+
+            reportResultsIndex++;
+
+            if (reportResultsIndex == reportResultsCount)
+            {
+                break;
+            }
+        }
+
+        return reportResults;
+    }
+
+    public void Rent(int shop, int movie)
+    {
+        var price = _shopMovieToPriceDictionary[(shop, movie)];
+
+        _rentalRecordsSortedSet.Add(new RentalRecord(shop, movie, price));
+
+        var movieOffers = _movieToMovieOffersDictionary[movie];
+
+        movieOffers.Remove(new MovieOffer(price, shop));
+    }
+
+    public void Drop(int shop, int movie)
+    {
+        var price = _shopMovieToPriceDictionary[(shop, movie)];
+
+        _rentalRecordsSortedSet.Remove(new RentalRecord(shop, movie, price));
+
+        var movieOffers = _movieToMovieOffersDictionary[movie];
+
+        movieOffers.Add(new MovieOffer(price, shop));
+    }
+
+    public readonly record struct MovieOffer(int Price, int Shop) : IComparable<MovieOffer>
+    {
+        public int CompareTo(MovieOffer movieOffer)
+        {
+            var priceComparison = CompareToPrice(movieOffer.Price);
+
+            return priceComparison != 0 ? priceComparison : CompareToShop(movieOffer.Shop);
+        }
+
+        private int CompareToPrice(int price)
+        {
+            return Price.CompareTo(price);
+        }
+
+        private int CompareToShop(int shop)
+        {
+            return Shop.CompareTo(shop);
+        }
+    }
+
+    private readonly record struct RentalRecord(int Shop, int Movie, int Price) : IComparable<RentalRecord>
+    {
+        private readonly int[] _report = new int[2];
+
+        public int CompareTo(RentalRecord rentalRecord)
+        {
+            var priceComparison = CompareToPrice(rentalRecord.Price);
+
+            if (priceComparison != 0)
+            {
+                return priceComparison;
+            }
+
+            var shopComparison = CompareToShop(rentalRecord.Shop);
+
+            return shopComparison != 0 ? shopComparison : CompareToMovie(rentalRecord.Movie);
+        }
+
+        public int[] GetReport()
+        {
+            _report[0] = Shop;
+            _report[1] = Movie;
+
+            return _report;
+        }
+
+        private int CompareToPrice(int price)
+        {
+            return Price.CompareTo(price);
+        }
+
+        private int CompareToShop(int shop)
+        {
+            return Shop.CompareTo(shop);
+        }
+
+        private int CompareToMovie(int movie)
+        {
+            return Movie.CompareTo(movie);
+        }
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/DesignMovieRentalSystem/DesignMovieRentalSystemTestsBase.cs
@@ -0,0 +1,71 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.DesignMovieRentalSystem;
+using LeetCode.Core.Helpers;
+using LeetCode.Tests.Base.Exceptions;
+
+namespace LeetCode.Tests.Algorithms.DesignMovieRentalSystem;
+
+public abstract class DesignMovieRentalSystemTestsBase
+{
+    private const string Search = "search";
+    private const string Rent = "rent";
+    private const string Drop = "drop";
+    private const string Report = "report";
+
+    [TestMethod]
+    [DataRow(3,
+        "[[0, 1, 5], [0, 2, 6], [0, 3, 7], [1, 1, 4], [1, 2, 7], [2, 1, 5]]",
+        "[\"search\", \"rent\", \"rent\", \"report\", \"drop\", \"search\"]",
+        "[[1], [0, 1], [1, 2], [], [1, 2], [2]]",
+        "[[1, 0, 2], [[0, 1], [1, 2]], [0, 1]]")]
+    public void DesignMovieRentalSystem_WithMixedOperations_ProcessesOperationsAccordingToSpecification(
+        int n, string entriesJson, string methodsJson, string argumentsJson, string expectedResultJson)
+    {
+        // Arrange
+        var entries = JsonHelper<int[][]>.Parse(entriesJson);
+        var methods = JsonHelper<string[]>.Parse(methodsJson);
+        var arguments = JsonHelper<object[][]>.Parse(argumentsJson);
+        var expectedResult = JsonHelper<object[]>.Parse(expectedResultJson);
+
+        var solution = GetSolution(n, entries);
+
+        // Act
+        var actualResult = new List<object>();
+
+        for (var i = 0; i < methods.Length; i++)
+        {
+            switch (methods[i])
+            {
+                case Search:
+                    actualResult.Add(solution.Search((int)arguments[i][0]));
+                    break;
+                case Rent:
+                    solution.Rent((int)arguments[i][0], (int)arguments[i][1]);
+                    break;
+                case Drop:
+                    solution.Drop((int)arguments[i][0], (int)arguments[i][1]);
+                    break;
+                case Report:
+                    actualResult.Add(solution.Report());
+                    break;
+                default:
+                    throw new UnexpectedMethodException(methods[i]);
+            }
+        }
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+
+    protected abstract IDesignMovieRentalSystem GetSolution(int n, int[][] entries);
+}
