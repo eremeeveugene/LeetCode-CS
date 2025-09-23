0e73d08f6a427bf52de5410a8b681591848f8654
Eugene <eremeeveugene@gmail.com>
2025-09-23 16:27:15 +0300

    Valid Arrangement of Pairs (#1498)

---
+++ source/LeetCode/Algorithms/ValidArrangementOfPairs/IValidArrangementOfPairs.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetCode.Algorithms.ValidArrangementOfPairs;
+
+/// <summary>
+///     https://leetcode.com/problems/valid-arrangement-of-pairs/description/
+/// </summary>
+public interface IValidArrangementOfPairs
+{
+    int[][] ValidArrangement(int[][] pairs);
+}
