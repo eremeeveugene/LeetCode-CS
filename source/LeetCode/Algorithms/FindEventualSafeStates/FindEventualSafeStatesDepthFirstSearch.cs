a527ec7dbad402d295f0bff6487121b4fb2c5eae
Eugene <eremeeveugene@gmail.com>
2025-09-23 20:33:04 +0300

    Find Eventual Safe States

---
+++ source/LeetCode/Algorithms/FindEventualSafeStates/FindEventualSafeStatesDepthFirstSearch.cs
@@ -0,0 +1,62 @@
+﻿namespace LeetCode.Algorithms.FindEventualSafeStates;
+
+/// <inheritdoc />
+public class FindEventualSafeStatesDepthFirstSearch : IFindEventualSafeStates
+{
+    /// <summary>
+    ///     Time complexity - O(n + E), where n is a number of nodes, E is a number of edges
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="graph"></param>
+    /// <returns></returns>
+    public IList<int> EventualSafeNodes(int[][] graph)
+    {
+        var n = graph.Length;
+
+        Span<int> states = stackalloc int[n];
+
+        var result = new List<int>();
+
+        for (var node = 0; node < n; node++)
+        {
+            if (!IsSafeNode(node, graph, states))
+            {
+                continue;
+            }
+
+            result.Add(node);
+        }
+
+        return result;
+    }
+
+    private static bool IsSafeNode(int node, int[][] graph, Span<int> states)
+    {
+        switch (states[node])
+        {
+            case 1: return true;
+            case 2: return false;
+        }
+
+        var adjacentNodes = graph[node];
+        var adjacentNodesLength = adjacentNodes.Length;
+
+        states[node] = 2;
+
+        for (var i = 0; i < adjacentNodesLength; i++)
+        {
+            var adjacentNode = graph[node][i];
+
+            if (IsSafeNode(adjacentNode, graph, states))
+            {
+                continue;
+            }
+
+            return false;
+        }
+
+        states[node] = 1;
+
+        return true;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FindEventualSafeStates/FindEventualSafeStatesDepthFirstSearchTests.cs
@@ -0,0 +1,7 @@
+﻿using LeetCode.Algorithms.FindEventualSafeStates;
+
+namespace LeetCode.Tests.Algorithms.FindEventualSafeStates;
+
+[TestClass]
+public class FindEventualSafeStatesDepthFirstSearchTests :
+    FindEventualSafeStatesTestsBase<FindEventualSafeStatesDepthFirstSearch>;
