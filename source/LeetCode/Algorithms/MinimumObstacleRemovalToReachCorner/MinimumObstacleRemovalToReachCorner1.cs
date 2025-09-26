074ce2fb5a09bd0972875245a67b729360898496
Eugene <eremeeveugene@gmail.com>
2025-09-26 20:29:28 +0300

    add tests

---
+++ source/LeetCode/Algorithms/MinimumObstacleRemovalToReachCorner/MinimumObstacleRemovalToReachCorner1.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumObstacleRemovalToReachCorner;
+
+/// <inheritdoc />
+public class MinimumObstacleRemovalToReachCorner1 : IMinimumObstacleRemovalToReachCorner
+{
+    public int MinimumObstacles(int[][] grid)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumObstacleRemovalToReachCorner/MinimumObstacleRemovalToReachCornerTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumObstacleRemovalToReachCorner;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MinimumObstacleRemovalToReachCorner;
+
+public abstract class MinimumObstacleRemovalToReachCornerTestsBase<T>
+    where T : IMinimumObstacleRemovalToReachCorner, new()
+{
+    [TestMethod]
+    [DataRow("[[0,1,1],[1,1,0],[1,1,0]]", 2)]
+    [DataRow("[[0,1,0,0,0],[0,1,0,1,0],[0,0,0,1,0]]", 0)]
+    public void MinimumObstacles_WithStartToEndPath_ReturnsMinimumObstaclesToRemove(string gridJson,
+        int expectedResult)
+    {
+        // Arrange
+        var grid = JsonHelper<int[][]>.Parse(gridJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinimumObstacles(grid);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
