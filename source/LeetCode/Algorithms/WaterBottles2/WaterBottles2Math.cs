749229281c03c7d970c8dd10f2371f6532373a1a
Eugene <eremeeveugene@gmail.com>
2025-10-02 19:35:12 +0300

    Water Bottles II

---
+++ source/LeetCode/Algorithms/WaterBottles2/WaterBottles2Math.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.WaterBottles2;
+
+/// <inheritdoc />
+public class WaterBottles2Math : IWaterBottles2
+{
+    private const int QuadraticA = 1;
+    private const int LinearMultiplier = 2;
+    private const int LinearOffset = 3;
+    private const int ConstantMultiplier = -2;
+    private const double Two = 2.0;
+    private const double Four = 4.0;
+
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="numBottles"></param>
+    /// <param name="numExchange"></param>
+    /// <returns></returns>
+    public int MaxBottlesDrunk(int numBottles, int numExchange)
+    {
+        var quadraticB = (LinearMultiplier * numExchange) - LinearOffset;
+        var quadraticC = ConstantMultiplier * numBottles;
+
+        var discriminant = ((double)quadraticB * quadraticB) - (Four * QuadraticA * quadraticC);
+
+        var root = (-quadraticB + Math.Sqrt(discriminant)) / (Two * QuadraticA);
+
+        var maxExchanges = (int)Math.Ceiling(root);
+
+        return numBottles + maxExchanges - 1;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/WaterBottles2/WaterBottles2TestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.WaterBottles2;
+
+namespace LeetCode.Tests.Algorithms.WaterBottles2;
+
+public abstract class WaterBottles2TestsBase<T> where T : IWaterBottles2, new()
+{
+    [TestMethod]
+    [DataRow(13, 6, 15)]
+    [DataRow(10, 3, 13)]
+    public void MaxBottlesDrunk_WithFullBottlesAndExchangeRate_ReturnsMaximumNumberOfBottlesDrunk(
+        int numBottles, int numExchange, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaxBottlesDrunk(numBottles, numExchange);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
