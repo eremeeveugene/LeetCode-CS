46a35f2629988bfbd4482ae7916ce21ed40edac6
Eugene <eremeeveugene@gmail.com>
2025-10-09 18:46:14 +0300

    Find the Minimum Amount of Time to Brew Potions

--- README.md
+++ README.md
@@ -729,6 +729,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [3484. Design Spreadsheet](https://leetcode.com/problems/design-spreadsheet/description/)
 - [3487. Maximum Unique Subarray Sum After Deletion](https://leetcode.com/problems/maximum-unique-subarray-sum-after-deletion/description/)
 - [3492. Maximum Containers on a Ship](https://leetcode.com/problems/maximum-containers-on-a-ship/description/)
+- [3494. Find the Minimum Amount of Time to Brew Potions](https://leetcode.com/problems/find-the-minimum-amount-of-time-to-brew-potions/description/)
 - [3498. Reverse Degree of a String](https://leetcode.com/problems/reverse-degree-of-a-string/description/)
 - [3502. Minimum Cost to Reach Every Position](https://leetcode.com/problems/minimum-cost-to-reach-every-position/description/)
 - [3507. Minimum Pair Removal to Sort Array I](https://leetcode.com/problems/minimum-pair-removal-to-sort-array-i/description/)
---
+++ source/LeetCode/Algorithms/FindTheMinimumAmountOfTimeToBrewPotions/FindTheMinimumAmountOfTimeToBrewPotionsDynamicProgramming.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindTheMinimumAmountOfTimeToBrewPotions;
+
+/// <inheritdoc />
+public class FindTheMinimumAmountOfTimeToBrewPotionsDynamicProgramming : IFindTheMinimumAmountOfTimeToBrewPotions
+{
+    /// <summary>
+    ///     Time complexity - O(n * m), where n is the length of skill and m is the length of mana
+    ///     Space complexity - O(n), where n is the length of skill
+    /// </summary>
+    /// <param name="skill"></param>
+    /// <param name="mana"></param>
+    /// <returns></returns>
+    public long MinTime(int[] skill, int[] mana)
+    {
+        var n = skill.Length;
+        var m = mana.Length;
+
+        Span<long> times = stackalloc long[n];
+
+        for (var j = 0; j < m; j++)
+        {
+            long currentTime = 0;
+
+            for (var i = 0; i < n; i++)
+            {
+                currentTime = Math.Max(currentTime, times[i]) + ((long)skill[i] * mana[j]);
+            }
+
+            times[n - 1] = currentTime;
+
+            for (var i = n - 2; i >= 0; i--)
+            {
+                times[i] = times[i + 1] - ((long)skill[i + 1] * mana[j]);
+            }
+        }
+
+        return times[n - 1];
+    }
+}
