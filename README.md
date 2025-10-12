f55a6bb1be9189a1637c1f7ea8eda6e3332f7403
Eugene <eremeeveugene@gmail.com>
2025-10-12 18:53:32 +0300

    Product of Array Except Self

--- README.md
+++ README.md
@@ -140,6 +140,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [232. Implement Queue using Stacks](https://leetcode.com/problems/implement-queue-using-stacks/description/)
 - [234. Palindrome Linked List](https://leetcode.com/problems/palindrome-linked-list/description/)
 - [237. Delete Node in a Linked List](https://leetcode.com/problems/delete-node-in-a-linked-list/description/)
+- [238. Product of Array Except Self](https://leetcode.com/problems/product-of-array-except-self/description/)
 - [241. Different Ways to Add Parentheses](https://leetcode.com/problems/different-ways-to-add-parentheses/description/)
 - [242. Valid Anagram](https://leetcode.com/problems/valid-anagram/description/)
 - [257. Binary Tree Paths](https://leetcode.com/problems/binary-tree-paths/description/)
---
+++ source/Tests/LeetCode.Tests/Algorithms/ProductOfArrayExceptSelf/ProductOfArrayExceptSelfTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ProductOfArrayExceptSelf;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.ProductOfArrayExceptSelf;
+
+public abstract class ProductOfArrayExceptSelfTestsBase<T> where T : IProductOfArrayExceptSelf, new()
+{
+    [TestMethod]
+    [DataRow("[1,2,3,4]", "[24,12,8,6]")]
+    [DataRow("[-1,1,0,-3,3]", "[0,0,9,0,0]")]
+    public void ProductExceptSelf_WithIntegerArray_ReturnsArrayWithProductsExcludingCurrentIndex(string numsJson,
+        string expectedResultJson)
+    {
+        // Arrange
+        var nums = JsonHelper<int[]>.Parse(numsJson);
+        var expectedResult = JsonHelper<int[]>.Parse(expectedResultJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.ProductExceptSelf(nums);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
