6dd9265e4ca061f650741e88656ead776d18f5d2
Eugene <eremeeveugene@gmail.com>
2025-10-15 10:02:26 +0300

    Two Sum II - Input Array Is Sorted

---
+++ source/LeetCode/Algorithms/TwoSum2InputArrayIsSorted/TwoSum2InputArrayIsSortedTwoPointers.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.TwoSum2InputArrayIsSorted;
+
+/// <inheritdoc />
+public class TwoSum2InputArrayIsSortedTwoPointers : ITwoSum2InputArrayIsSorted
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="numbers"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        var left = 0;
+        var right = numbers.Length - 1;
+
+        while (left < right)
+        {
+            var sum = numbers[left] + numbers[right];
+
+            if (sum == target)
+            {
+                break;
+            }
+
+            if (sum < target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return [left + 1, right + 1];
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/TwoSum2InputArrayIsSorted/TwoSum2InputArrayIsSortedTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.TwoSum2InputArrayIsSorted;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.TwoSum2InputArrayIsSorted;
+
+public abstract class TwoSum2InputArrayIsSortedTestsBase<T> where T : ITwoSum2InputArrayIsSorted, new()
+{
+    [TestMethod]
+    [DataRow("[2,7,11,15]", 9, "[1,2]")]
+    [DataRow("[2,3,4]", 6, "[1,3]")]
+    [DataRow("[-1,0]", -1, "[1,2]")]
+    public void TwoSum_WithSortedArrayAndTargetSum_ReturnsOneIndexedPairAddingToTarget(string numbersJson, int target,
+        string expectedResultJson)
+    {
+        // Arrange
+        var numbers = JsonHelper<int[]>.Parse(numbersJson);
+        var expectedResult = JsonHelper<int[]>.Parse(expectedResultJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.TwoSum(numbers, target);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
