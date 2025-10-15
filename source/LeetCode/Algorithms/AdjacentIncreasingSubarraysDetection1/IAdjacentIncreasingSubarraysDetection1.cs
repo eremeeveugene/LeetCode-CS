de9acd30e4409d816cda1a06b1fe736dc5cf2269
Eugene <eremeeveugene@gmail.com>
2025-10-15 20:06:09 +0300

    Adjacent Increasing Subarrays Detection I

---
+++ source/LeetCode/Algorithms/AdjacentIncreasingSubarraysDetection1/IAdjacentIncreasingSubarraysDetection1.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.AdjacentIncreasingSubarraysDetection1;
+
+/// <summary>
+///     https://leetcode.com/problems/adjacent-increasing-subarrays-detection-i/description/
+/// </summary>
+public interface IAdjacentIncreasingSubarraysDetection1
+{
+    bool HasIncreasingSubarrays(IList<int> nums, int k);
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/AdjacentIncreasingSubarraysDetection1/AdjacentIncreasingSubarraysDetection1GreedyTests.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.AdjacentIncreasingSubarraysDetection1;
+
+namespace LeetCode.Tests.Algorithms.AdjacentIncreasingSubarraysDetection1;
+
+[TestClass]
+public class AdjacentIncreasingSubarraysDetection1GreedyTests :
+    AdjacentIncreasingSubarraysDetection1TestsBase<AdjacentIncreasingSubarraysDetection1Greedy>;
