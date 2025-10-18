f2eca797f401f29d3003d68dadce7c73f3a991d3
Eugene <eremeeveugene@gmail.com>
2025-10-18 19:38:58 +0300

    Maximum Number of Distinct Elements After Operations

---
+++ source/LeetCode/Algorithms/MaximumNumberOfDistinctElementsAfterOperations/IMaximumNumberOfDistinctElementsAfterOperations.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumNumberOfDistinctElementsAfterOperations;
+
+/// <summary>
+///     https://leetcode.com/problems/maximum-number-of-distinct-elements-after-operations/description/
+/// </summary>
+public interface IMaximumNumberOfDistinctElementsAfterOperations
+{
+    int MaxDistinctElements(int[] nums, int k);
+}
