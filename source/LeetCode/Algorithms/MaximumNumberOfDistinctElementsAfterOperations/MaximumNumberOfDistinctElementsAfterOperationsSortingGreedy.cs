1882b51a9385da305123bf64e8a6bdd5704d7c83
Eugene <eremeeveugene@gmail.com>
2025-10-18 19:45:10 +0300

    Maximum Number of Distinct Elements After Operations (#1546)

---
+++ source/LeetCode/Algorithms/MaximumNumberOfDistinctElementsAfterOperations/MaximumNumberOfDistinctElementsAfterOperationsSortingGreedy.cs
@@ -0,0 +1,62 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumNumberOfDistinctElementsAfterOperations;
+
+public class MaximumNumberOfDistinctElementsAfterOperationsSortingGreedy :
+    IMaximumNumberOfDistinctElementsAfterOperations
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(log n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int MaxDistinctElements(int[] nums, int k)
+    {
+        var numsLength = nums.Length;
+
+        if (k >= numsLength)
+        {
+            return numsLength;
+        }
+
+        Array.Sort(nums);
+
+        if (nums[0] == nums[^1])
+        {
+            return Math.Min(numsLength, (2 * k) + 1);
+        }
+
+        var previous = nums[0] - k;
+
+        var distinctCount = 1;
+
+        for (var i = 1; i < numsLength; i++)
+        {
+            var num = nums[i];
+
+            var current = Math.Min(Math.Max(previous + 1, num - k), num + k);
+
+            if (current <= previous)
+            {
+                continue;
+            }
+
+            distinctCount++;
+
+            previous = current;
+        }
+
+        return distinctCount;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MaximumNumberOfDistinctElementsAfterOperations/MaximumNumberOfDistinctElementsAfterOperationsTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaximumNumberOfDistinctElementsAfterOperations;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MaximumNumberOfDistinctElementsAfterOperations;
+
+public abstract class MaximumNumberOfDistinctElementsAfterOperationsTestsBase<T>
+    where T : IMaximumNumberOfDistinctElementsAfterOperations, new()
+{
+    [TestMethod]
+    [DataRow("[1,2,2,3,3,4]", 2, 6)]
+    [DataRow("[4,4,4,4]", 1, 3)]
+    public void MaxDistinctElements_WithNumsAndAdjustmentLimit_ReturnsMaximumDistinctCount(string numsJson,
+        int k, int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int[]>.Parse(numsJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaxDistinctElements(nums, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
