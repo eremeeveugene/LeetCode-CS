8fae6978fa8feddd13f056162682e589575d72d8
Eugene <eremeeveugene@gmail.com>
2025-10-19 19:41:09 +0300

    Smallest Missing Multiple of K

---
+++ source/Tests/LeetCode.Tests/Algorithms/SmallestMissingMultipleOfK/SmallestMissingMultipleOfKTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SmallestMissingMultipleOfK;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.SmallestMissingMultipleOfK;
+
+public abstract class SmallestMissingMultipleOfKTestsBase<T>
+    where T : ISmallestMissingMultipleOfK, new()
+{
+    [TestMethod]
+    [DataRow("[8,2,3,4,6]", 2, 10)]
+    [DataRow("[1,4,7,10,15]", 5, 5)]
+    public void MissingMultiple_WithArrayAndMultipleK_ReturnsSmallestMissingPositiveMultipleOfK(string numsJson, int k,
+        int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int[]>.Parse(numsJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MissingMultiple(nums, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
