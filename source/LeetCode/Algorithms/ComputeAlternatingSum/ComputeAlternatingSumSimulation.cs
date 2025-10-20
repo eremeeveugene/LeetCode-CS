e936e0afabf5bf3184f9c9b0705025fa04c20079
Eugene <eremeeveugene@gmail.com>
2025-10-20 12:09:01 +0300

    Compute Alternating Sum

---
+++ source/LeetCode/Algorithms/ComputeAlternatingSum/ComputeAlternatingSumSimulation.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ComputeAlternatingSum;
+
+/// <inheritdoc />
+public class ComputeAlternatingSumSimulation : IComputeAlternatingSum
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int AlternatingSum(int[] nums)
+    {
+        var sum = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var num = nums[i];
+
+            if (i % 2 == 0)
+            {
+                sum += num;
+            }
+            else
+            {
+                sum -= num;
+            }
+        }
+
+        return sum;
+    }
+}
