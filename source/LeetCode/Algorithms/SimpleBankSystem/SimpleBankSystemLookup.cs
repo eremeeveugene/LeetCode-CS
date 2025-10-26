202b3ad33144bb6f0fe238a88bf1f6f3b0f1f7f6
Eugene <eremeeveugene@gmail.com>
2025-10-26 14:05:56 +0200

    Simple Bank System

---
+++ source/LeetCode/Algorithms/SimpleBankSystem/SimpleBankSystemLookup.cs
@@ -0,0 +1,79 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SimpleBankSystem;
+
+/// <inheritdoc />
+public class SimpleBankSystemLookup : ISimpleBankSystem
+{
+    private readonly long[] _balance;
+    private readonly int _balanceLength;
+
+    public SimpleBankSystemLookup(long[] balance)
+    {
+        _balance = balance;
+        _balanceLength = balance.Length;
+    }
+
+    public bool Transfer(int account1, int account2, long money)
+    {
+        if (account1 > _balanceLength || account2 > _balanceLength)
+        {
+            return false;
+        }
+
+        var balance1Index = account1 - 1;
+        var balance2Index = account2 - 1;
+
+        if (_balance[balance1Index] < money)
+        {
+            return false;
+        }
+
+        _balance[balance1Index] -= money;
+        _balance[balance2Index] += money;
+
+        return true;
+    }
+
+    public bool Deposit(int account, long money)
+    {
+        if (account > _balanceLength)
+        {
+            return false;
+        }
+
+        var balanceIndex = account - 1;
+
+        _balance[balanceIndex] += money;
+
+        return true;
+    }
+
+    public bool Withdraw(int account, long money)
+    {
+        if (account > _balanceLength)
+        {
+            return false;
+        }
+
+        var balanceIndex = account - 1;
+
+        if (_balance[balanceIndex] < money)
+        {
+            return false;
+        }
+
+        _balance[balanceIndex] -= money;
+
+        return true;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/SimpleBankSystem/SimpleBankSystemTestsBase.cs
@@ -0,0 +1,74 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.SimpleBankSystem;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.SimpleBankSystem;
+
+public abstract class SimpleBankSystemTestsBase
+{
+    private const string Transfer = "transfer";
+    private const string Deposit = "deposit";
+    private const string Withdraw = "withdraw";
+
+    [TestMethod]
+    [DataRow("[10, 100, 20, 50, 30]",
+        "[\"withdraw\", \"transfer\", \"deposit\", \"transfer\", \"withdraw\"]",
+        "[[3, 10], [5, 1, 20], [5, 20], [3, 4, 15], [10, 50]]",
+        "[true, true, true, false, false]")]
+    public void Test(string balanceJson, string operationsJson, string argumentsJson, string expectedResultJson)
+    {
+        // Arrange
+        var balance = JsonHelper<long[]>.Parse(balanceJson);
+        var operations = JsonHelper<string[]>.Parse(operationsJson);
+        var arguments = JsonHelper<object[][]>.Parse(argumentsJson);
+        var expectedResult = JsonHelper<object[]>.Parse(expectedResultJson);
+
+        var solution = GetSolution(balance);
+
+        // Act
+        var actualResult = new List<object>();
+
+        for (var i = 0; i < operations.Length; i++)
+        {
+            switch (operations[i])
+            {
+                case Transfer:
+                    var transferResult = solution.Transfer((int)arguments[i][0], (int)arguments[i][1],
+                        (int)arguments[i][2]);
+
+                    actualResult.Add(transferResult);
+
+                    break;
+                case Deposit:
+                    var depositResult = solution.Deposit((int)arguments[i][0], (int)arguments[i][1]);
+
+                    actualResult.Add(depositResult);
+
+                    break;
+                case Withdraw:
+                    var withdrawResult = solution.Withdraw((int)arguments[i][0], (int)arguments[i][1]);
+
+                    actualResult.Add(withdrawResult);
+
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException($"Unexpected operation '{operations[i]}' at index {i}.");
+            }
+        }
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+
+    protected abstract ISimpleBankSystem GetSolution(long[] balance);
+}
