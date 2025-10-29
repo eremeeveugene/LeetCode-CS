338b87a9cabf35760a012e7eec2a3fe7d98ba75a
Eugene <eremeeveugene@gmail.com>
2025-10-29 21:48:26 +0200

    Check If Digits Are Equal in String After Operations I

---
+++ source/Tests/LeetCode.Tests/Algorithms/CheckIfDigitsAreEqualInStringAfterOperations1/CheckIfDigitsAreEqualInStringAfterOperations1SimulationOptimizedTests.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CheckIfDigitsAreEqualInStringAfterOperations1;
+
+namespace LeetCode.Tests.Algorithms.CheckIfDigitsAreEqualInStringAfterOperations1;
+
+[TestClass]
+public class CheckIfDigitsAreEqualInStringAfterOperations1SimulationOptimizedTests :
+    CheckIfDigitsAreEqualInStringAfterOperations1TestsBase<
+        CheckIfDigitsAreEqualInStringAfterOperations1SimulationOptimized>;
