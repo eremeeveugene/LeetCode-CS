09bf3616c8e75526936b98fd2d82272be5134fa8
Eugene <eremeeveugene@gmail.com>
2025-11-02 15:29:13 +0200

    Find Missing Elements (#1572)

---
+++ source/LeetCode/Algorithms/FindMissingElements/FindMissingElementsSorting.cs
@@ -0,0 +1,45 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindMissingElements;
+
+/// <inheritdoc />
+public class FindMissingElementsSorting : IFindMissingElements
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(log n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public IList<int> FindMissingElements(int[] nums)
+    {
+        Array.Sort(nums);
+
+        var missingElements = new List<int>();
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            var num = nums[i];
+
+            var missingElement = nums[i - 1] + 1;
+
+            while (missingElement < num)
+            {
+                missingElements.Add(missingElement);
+
+                missingElement++;
+            }
+        }
+
+        return missingElements;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FindMissingElements/FindMissingElementsTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindMissingElements;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.FindMissingElements;
+
+public abstract class FindMissingElementsTestsBase<T> where T : IFindMissingElements, new()
+{
+    [TestMethod]
+    [DataRow("[1,4,2,5]", "[3]")]
+    [DataRow("[7,8,6,9]", "[]")]
+    [DataRow("[5,1]", "[2,3,4]")]
+    public void FindMissingElements_WithArrayContainingGaps_ReturnsMissingSequentialElements(string numsJson,
+        string expectedResultJson)
+    {
+        // Arrange
+        var nums = JsonHelper<int[]>.Parse(numsJson);
+        var expectedResult = JsonHelper<int[]>.Parse(expectedResultJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.FindMissingElements(nums).ToArray();
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
