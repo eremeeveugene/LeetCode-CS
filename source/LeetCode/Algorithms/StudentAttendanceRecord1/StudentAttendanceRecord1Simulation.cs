17fcee4709369f5c9f170fc06fcee0c71c8eea80
Eugene <eremeeveugene@gmail.com>
2025-11-02 15:50:41 +0200

    Student Attendance Record I (#1573)

---
+++ source/LeetCode/Algorithms/StudentAttendanceRecord1/StudentAttendanceRecord1Simulation.cs
@@ -0,0 +1,67 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.StudentAttendanceRecord1;
+
+/// <inheritdoc />
+public class StudentAttendanceRecord1Simulation : IStudentAttendanceRecord1
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public bool CheckRecord(string s)
+    {
+        var lateCount = 0;
+        var absentCount = 0;
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            var c = s[i];
+
+            switch (c)
+            {
+                case 'A':
+                    {
+                        absentCount++;
+
+                        if (absentCount > 1)
+                        {
+                            return false;
+                        }
+
+                        lateCount = 0;
+
+                        break;
+                    }
+                case 'L':
+                    {
+                        lateCount++;
+
+                        if (lateCount > 2)
+                        {
+                            return false;
+                        }
+
+                        break;
+                    }
+                default:
+                    lateCount = 0;
+
+                    break;
+            }
+        }
+
+        return true;
+    }
+}
