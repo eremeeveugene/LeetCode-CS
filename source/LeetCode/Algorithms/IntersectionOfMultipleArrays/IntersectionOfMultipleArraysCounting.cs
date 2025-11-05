7b4d1edae77357b6725e697df2e005277eea82da
Eugene <eremeeveugene@gmail.com>
2025-11-05 19:35:19 +0200

    Intersection of Multiple Arrays (#1581)

---
+++ source/LeetCode/Algorithms/IntersectionOfMultipleArrays/IntersectionOfMultipleArraysCounting.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.IntersectionOfMultipleArrays;
+
+/// <inheritdoc />
+public class IntersectionOfMultipleArraysCounting : IIntersectionOfMultipleArrays
+{
+    /// <summary>
+    ///     Time complexity - O(n * m), where n is the number of arrays in nums and m is the length of arrays in nums
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public IList<int> Intersection(int[][] nums)
+    {
+        Span<int> numsFrequencies = stackalloc int[1001];
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var row = nums[i];
+
+            for (var j = 0; j < row.Length; j++)
+            {
+                var cell = row[j];
+
+                numsFrequencies[cell]++;
+            }
+        }
+
+        var result = new List<int>();
+
+        for (var i = 0; i < numsFrequencies.Length; i++)
+        {
+            var numsFrequency = numsFrequencies[i];
+
+            if (numsFrequency == nums.Length)
+            {
+                result.Add(i);
+            }
+        }
+
+        return result;
+    }
+}
