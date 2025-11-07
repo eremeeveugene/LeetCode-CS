ba5520ec3a0687db90dfcf70f8de29237a36a767
Eugene <eremeeveugene@gmail.com>
2025-11-07 18:22:24 +0200

    Equal Score Substrings

---
+++ source/Tests/LeetCode.Tests/Algorithms/EqualScoreSubstrings/EqualScoreSubstringsTestsBase.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.EqualScoreSubstrings;
+
+namespace LeetCode.Tests.Algorithms.EqualScoreSubstrings;
+
+public abstract class EqualScoreSubstringsTestsBase<T> where T : IEqualScoreSubstrings, new()
+{
+    [TestMethod]
+    [DataRow("adcb", true)]
+    [DataRow("bace", false)]
+    public void ScoreBalance_WithInputString_ReturnsTrueIfSplitExists(string s, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.ScoreBalance(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
