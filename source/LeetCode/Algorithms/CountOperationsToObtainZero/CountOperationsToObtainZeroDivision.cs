ab67b44b2381aacd9a1ce94799b0281958324950
Eugene <eremeeveugene@gmail.com>
2025-11-09 19:54:13 +0200

    Count Operations to Obtain Zero

---
+++ source/LeetCode/Algorithms/CountOperationsToObtainZero/CountOperationsToObtainZeroDivision.cs
@@ -0,0 +1,56 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountOperationsToObtainZero;
+
+/// <inheritdoc />
+public class CountOperationsToObtainZeroDivision : ICountOperationsToObtainZero
+{
+    /// <summary>
+    ///     Time complexity - O(log(min(num1, num2)))
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="num1"></param>
+    /// <param name="num2"></param>
+    /// <returns></returns>
+    public int CountOperations(int num1, int num2)
+    {
+        if (num1 == 0 || num2 == 0)
+        {
+            return 0;
+        }
+
+        if (num1 == num2)
+        {
+            return 1;
+        }
+
+        var count = 0;
+
+        while (num1 != 0 && num2 != 0)
+        {
+            if (num1 > num2)
+            {
+                count += num1 / num2;
+
+                num1 %= num2;
+            }
+            else
+            {
+                count += num2 / num1;
+
+                num2 %= num1;
+            }
+        }
+
+        return count;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountOperationsToObtainZero/CountOperationsToObtainZeroTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountOperationsToObtainZero;
+
+namespace LeetCode.Tests.Algorithms.CountOperationsToObtainZero;
+
+public abstract class CountOperationsToObtainZeroTestsBase<T> where T : ICountOperationsToObtainZero, new()
+{
+    [TestMethod]
+    [DataRow(2, 3, 3)]
+    [DataRow(10, 10, 1)]
+    public void CountOperations_WithTwoNonNegativeIntegers_ReturnsCountOfSubtractionOperations(
+        int num1, int num2, int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.CountOperations(num1, num2);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
