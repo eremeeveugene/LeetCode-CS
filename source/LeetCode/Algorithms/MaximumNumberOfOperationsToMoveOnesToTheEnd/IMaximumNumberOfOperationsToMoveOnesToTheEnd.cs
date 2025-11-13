40ee897188de74bf3626af1598ad830ef710bde2
Eugene <eremeeveugene@gmail.com>
2025-11-13 16:14:00 +0200

    Maximum Number of Operations to Move Ones to the End

---
+++ source/LeetCode/Algorithms/MaximumNumberOfOperationsToMoveOnesToTheEnd/IMaximumNumberOfOperationsToMoveOnesToTheEnd.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumNumberOfOperationsToMoveOnesToTheEnd;
+
+/// <summary>
+///     https://leetcode.com/problems/maximum-number-of-operations-to-move-ones-to-the-end/description/
+/// </summary>
+public interface IMaximumNumberOfOperationsToMoveOnesToTheEnd
+{
+    int MaxOperations(string s);
+}
---
+++ source/LeetCode/Algorithms/MaximumNumberOfOperationsToMoveOnesToTheEnd/MaximumNumberOfOperationsToMoveOnesToTheEndGreedyCounting.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumNumberOfOperationsToMoveOnesToTheEnd;
+
+/// <inheritdoc />
+public class MaximumNumberOfOperationsToMoveOnesToTheEndGreedyCounting : IMaximumNumberOfOperationsToMoveOnesToTheEnd
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public int MaxOperations(string s)
+    {
+        var count = 0;
+
+        var ones = 0;
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '0')
+            {
+                while (i + 1 < s.Length && s[i + 1] == '0')
+                {
+                    i++;
+                }
+
+                count += ones;
+            }
+            else
+            {
+                ones++;
+            }
+        }
+
+        return count;
+    }
+}
