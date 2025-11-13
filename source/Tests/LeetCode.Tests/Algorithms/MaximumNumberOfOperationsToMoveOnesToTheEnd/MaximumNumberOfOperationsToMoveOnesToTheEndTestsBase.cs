a7c9fe548d6cd24dda2421972bbafecf076d75ac
Eugene <eremeeveugene@gmail.com>
2025-11-13 16:19:23 +0200

    Maximum Number of Operations to Move Ones to the End (#1595)

---
+++ source/Tests/LeetCode.Tests/Algorithms/MaximumNumberOfOperationsToMoveOnesToTheEnd/MaximumNumberOfOperationsToMoveOnesToTheEndTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MaximumNumberOfOperationsToMoveOnesToTheEnd;
+
+namespace LeetCode.Tests.Algorithms.MaximumNumberOfOperationsToMoveOnesToTheEnd;
+
+public abstract class MaximumNumberOfOperationsToMoveOnesToTheEndTestsBase<T>
+    where T : IMaximumNumberOfOperationsToMoveOnesToTheEnd, new()
+{
+    [TestMethod]
+    [DataRow("1001101", 4)]
+    [DataRow("00111", 0)]
+    public void MaxOperations_WithBinaryStringContainingZerosAndOnes_ReturnsTotalNumberOfValidMoves(string s,
+        int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaxOperations(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
