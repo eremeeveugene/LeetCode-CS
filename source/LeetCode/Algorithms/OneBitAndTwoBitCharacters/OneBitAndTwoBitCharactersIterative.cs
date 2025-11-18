9b36af2c01a49a254c3212fae661c0565afc7015
Eugene <eremeeveugene@gmail.com>
2025-11-18 17:09:14 +0200

    1-bit and 2-bit Characters

---
+++ source/LeetCode/Algorithms/OneBitAndTwoBitCharacters/OneBitAndTwoBitCharactersIterative.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.OneBitAndTwoBitCharacters;
+
+/// <inheritdoc />
+public class OneBitAndTwoBitCharactersIterative : IOneBitAndTwoBitCharacters
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="bits"></param>
+    /// <returns></returns>
+    public bool IsOneBitCharacter(int[] bits)
+    {
+        var i = 0;
+
+        while (i < bits.Length - 1)
+        {
+            if (bits[i] == 0)
+            {
+                i++;
+            }
+            else
+            {
+                i += 2;
+            }
+        }
+
+        return i == bits.Length - 1;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/OneBitAndTwoBitCharacters/OneBitAndTwoBitCharactersTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.OneBitAndTwoBitCharacters;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.OneBitAndTwoBitCharacters;
+
+public abstract class OneBitAndTwoBitCharactersTestsBase<T> where T : IOneBitAndTwoBitCharacters, new()
+{
+    [TestMethod]
+    [DataRow("[1,0,0]", true)]
+    [DataRow("[1,1,1,0]", false)]
+    public void IsOneBitCharacter_WithBitsArray_ReturnsTrueIfLastIsOneBitCharacter(string bitsJson, bool expectedResult)
+    {
+        // Arrange
+        var bits = JsonHelper<int[]>.Parse(bitsJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsOneBitCharacter(bits);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
