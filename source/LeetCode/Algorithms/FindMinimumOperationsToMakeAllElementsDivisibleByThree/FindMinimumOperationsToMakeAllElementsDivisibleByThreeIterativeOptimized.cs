59e1427ccc27d70e32fac408d7e39ab979a2340c
Eugene <eremeeveugene@gmail.com>
2025-11-23 14:48:01 +0200

    Find Minimum Operations to Make All Elements Divisible by Three (#1609)

---
+++ source/LeetCode/Algorithms/FindMinimumOperationsToMakeAllElementsDivisibleByThree/FindMinimumOperationsToMakeAllElementsDivisibleByThreeIterativeOptimized.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindMinimumOperationsToMakeAllElementsDivisibleByThree;
+
+/// <inheritdoc />
+public class FindMinimumOperationsToMakeAllElementsDivisibleByThreeIterativeOptimized :
+    IFindMinimumOperationsToMakeAllElementsDivisibleByThree
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MinimumOperations(int[] nums)
+    {
+        var count = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var num = nums[i];
+
+            var remainder = num % 3;
+
+            if (remainder == 0)
+            {
+                continue;
+            }
+
+            count++;
+        }
+
+        return count;
+    }
+}
