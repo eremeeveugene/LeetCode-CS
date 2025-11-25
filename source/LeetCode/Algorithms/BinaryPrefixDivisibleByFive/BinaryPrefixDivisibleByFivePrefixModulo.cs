bffdd6f83c11bdd7fe9d9834b60107a704325ac4
Eugene <eremeeveugene@gmail.com>
2025-11-25 09:53:03 +0200

    Binary Prefix Divisible By 5 (#1613)

---
+++ source/LeetCode/Algorithms/BinaryPrefixDivisibleByFive/BinaryPrefixDivisibleByFivePrefixModulo.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.BinaryPrefixDivisibleByFive;
+
+/// <inheritdoc />
+public class BinaryPrefixDivisibleByFivePrefixModulo : IBinaryPrefixDivisibleByFive
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public IList<bool> PrefixesDivBy5(int[] nums)
+    {
+        var result = new bool[nums.Length];
+
+        var prefix = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            prefix = ((prefix << 1) + nums[i]) % 5;
+
+            result[i] = prefix == 0;
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/BinaryPrefixDivisibleByFive/BinaryPrefixDivisibleByFiveTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.BinaryPrefixDivisibleByFive;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.BinaryPrefixDivisibleByFive;
+
+public abstract class BinaryPrefixDivisibleByFiveTestsBase<T> where T : IBinaryPrefixDivisibleByFive, new()
+{
+    [TestMethod]
+    [DataRow("[0,1,1]", "[true,false,false]")]
+    [DataRow("[1,1,1]", "[false,false,false]")]
+    public void PrefixesDivBy5_WithNumsArray_ReturnsTrueIfPrefixDivisibleBy5(string numsJson, string expectedResultJson)
+    {
+        // Arrange
+        var nums = JsonHelper<int[]>.Parse(numsJson);
+        var expectedResult = JsonHelper<bool[]>.Parse(expectedResultJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.PrefixesDivBy5(nums).ToArray();
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
