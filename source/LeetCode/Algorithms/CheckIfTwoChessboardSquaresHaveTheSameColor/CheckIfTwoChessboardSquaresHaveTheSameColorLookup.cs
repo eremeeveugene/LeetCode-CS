e6ac20df5ef9daceb789fa55bd576e1fc4b89ba7
Eugene <eremeeveugene@gmail.com>
2025-12-02 10:26:59 +0200

    Check if Two Chessboard Squares Have the Same Color (#1625)

---
+++ source/LeetCode/Algorithms/CheckIfTwoChessboardSquaresHaveTheSameColor/CheckIfTwoChessboardSquaresHaveTheSameColorLookup.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CheckIfTwoChessboardSquaresHaveTheSameColor;
+
+/// <inheritdoc />
+public class CheckIfTwoChessboardSquaresHaveTheSameColorLookup : CheckIfTwoChessboardSquaresHaveTheSameColorBase
+{
+    private const int BoardSize = 8;
+    private static readonly bool[,] CellsLookup = CreateCellsLookup();
+
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="coordinate1"></param>
+    /// <param name="coordinate2"></param>
+    /// <returns></returns>
+    public override bool CheckTwoChessboards(string coordinate1, string coordinate2)
+    {
+        var (x1, y1) = ParseCoordinate(coordinate1);
+        var (x2, y2) = ParseCoordinate(coordinate2);
+
+        return CellsLookup[x1, y1] == CellsLookup[x2, y2];
+    }
+
+    private static bool[,] CreateCellsLookup()
+    {
+        var cells = new bool[BoardSize, BoardSize];
+
+        for (var x = 0; x < BoardSize; x++)
+        {
+            for (var y = 0; y < BoardSize; y++)
+            {
+                cells[x, y] = GetColor(x, y);
+            }
+        }
+
+        return cells;
+    }
+}
