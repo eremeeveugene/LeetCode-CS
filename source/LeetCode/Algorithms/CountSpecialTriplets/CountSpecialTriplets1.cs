18de18d71304548fc0b2eb7fb28b1c7ed1ee70b6
Eugene <eremeeveugene@gmail.com>
2025-12-09 15:39:46 +0200

    Count Special Triplets

---
+++ source/LeetCode/Algorithms/CountSpecialTriplets/CountSpecialTriplets1.cs
@@ -0,0 +1,73 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountSpecialTriplets;
+
+/// <inheritdoc />
+public sealed class CountSpecialTriplets1 : ICountSpecialTriplets
+{
+    private const int Modulo = 1_000_000_007;
+
+    /// <summary>
+    ///     Time complexity - O(n^3)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int SpecialTriplets(int[] nums)
+    {
+        long result = 0;
+
+        var numsLength = nums.Length;
+
+        var rightNumToCountDictionary = new Dictionary<int, int>();
+
+        foreach (var num in nums)
+        {
+            if (!rightNumToCountDictionary.TryAdd(num, 1))
+            {
+                rightNumToCountDictionary[num]++;
+            }
+        }
+
+        var leftNumToCountDictionary = new Dictionary<int, int>();
+
+        for (var j = 0; j < numsLength; j++)
+        {
+            var num = nums[j];
+
+            rightNumToCountDictionary[num]--;
+
+            var middle = num * 2;
+
+            long leftCount = leftNumToCountDictionary.GetValueOrDefault(middle, 0);
+
+            if (leftCount != 0)
+            {
+                long rightCount = rightNumToCountDictionary.GetValueOrDefault(middle, 0);
+
+                if (rightCount != 0)
+                {
+                    result = (result + (leftCount * rightCount % Modulo)) % Modulo;
+                }
+            }
+
+            if (leftNumToCountDictionary.TryAdd(num, 1))
+            {
+                continue;
+            }
+
+            leftNumToCountDictionary[num]++;
+        }
+
+        return (int)result;
+    }
+}
