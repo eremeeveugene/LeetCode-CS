40e0590cfa8277c55fbd6cb7428bc0e89b085ed5
Eugene <eremeeveugene@gmail.com>
2025-12-19 13:07:24 +0200

    Absolute Difference Between Maximum and Minimum K Elements

---
+++ source/LeetCode/Algorithms/AbsoluteDifferenceBetweenMaximumAndMinimumKElements/AbsoluteDifferenceBetweenMaximumAndMinimumKElementsSorting.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.AbsoluteDifferenceBetweenMaximumAndMinimumKElements;
+
+/// <inheritdoc />
+public sealed class AbsoluteDifferenceBetweenMaximumAndMinimumKElementsSorting :
+    IAbsoluteDifferenceBetweenMaximumAndMinimumKElements
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(log n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int AbsDifference(int[] nums, int k)
+    {
+        Array.Sort(nums);
+
+        var result = 0;
+
+        for (var i = 0; i < k; i++)
+        {
+            result += nums[i];
+            result -= nums[nums.Length - 1 - i];
+        }
+
+        return result;
+    }
+}
