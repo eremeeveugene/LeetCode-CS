9cdd0108c33af0ae08f53d1066dbe850399d2577
Eugene <eremeeveugene@gmail.com>
2025-12-21 17:19:37 +0200

    Delete Columns to Make Sorted II

---
+++ source/LeetCode/Algorithms/DeleteColumnsToMakeSorted2/DeleteColumnsToMakeSorted2Greedy.cs
@@ -0,0 +1,76 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.DeleteColumnsToMakeSorted2;
+
+/// <inheritdoc />
+public sealed class DeleteColumnsToMakeSorted2Greedy : IDeleteColumnsToMakeSorted2
+{
+    /// <summary>
+    ///     Time complexity - O(n * m)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="strs"></param>
+    /// <returns></returns>
+    public int MinDeletionSize(string[] strs)
+    {
+        var result = 0;
+
+        var n = strs.Length;
+        var m = strs[0].Length;
+
+        Span<bool> ignored = stackalloc bool[n - 1];
+
+        for (var i = 0; i < m; i++)
+        {
+            var needDelete = false;
+
+            for (var j = 0; j < n - 1; j++)
+            {
+                if (ignored[j])
+                {
+                    continue;
+                }
+
+                if (strs[j][i] <= strs[j + 1][i])
+                {
+                    continue;
+                }
+
+                needDelete = true;
+
+                break;
+            }
+
+            if (needDelete)
+            {
+                result++;
+
+                continue;
+            }
+
+            for (var j = 0; j < n - 1; j++)
+            {
+                if (ignored[j])
+                {
+                    continue;
+                }
+
+                if (strs[j][i] < strs[j + 1][i])
+                {
+                    ignored[j] = true;
+                }
+            }
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/DeleteColumnsToMakeSorted2/DeleteColumnsToMakeSorted2TestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.DeleteColumnsToMakeSorted2;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.DeleteColumnsToMakeSorted2;
+
+public abstract class DeleteColumnsToMakeSorted2TestsBase<T> where T : IDeleteColumnsToMakeSorted2, new()
+{
+    [TestMethod]
+    [DataRow("[\"xc\",\"yb\",\"za\"]", 0)]
+    [DataRow("[\"ca\",\"bb\",\"ac\"]", 1)]
+    [DataRow("[\"xga\",\"xfb\",\"yfa\"]", 1)]
+    [DataRow("[\"zyx\",\"wvu\",\"tsr\"]", 3)]
+    public void MinDeletionSize_WithStringsOfEqualLength_ReturnsMinimumDeletionsForLexicographicOrder(string strsJson,
+        int expectedResult)
+    {
+        // Arrange
+        var strs = JsonHelper<string[]>.Parse(strsJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinDeletionSize(strs);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
