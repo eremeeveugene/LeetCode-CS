26ea407a5467e23c4ea6480a851273c6915ea739
Eugene <eremeeveugene@gmail.com>
2026-01-10 23:54:41 +0200

    Add assembly info

---
+++ source/Tests/LeetCode.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("a5b62146-073c-4d0a-bfb7-681b3f180ec5")]
+[assembly: Parallelize(Workers = 4, Scope = ExecutionScope.MethodLevel)]
---
+++ source/Tests/LeetCode.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("08820c21-7814-41b7-9ee3-211f8d7b3718")]
+[assembly: Parallelize(Workers = 4, Scope = ExecutionScope.MethodLevel)]
