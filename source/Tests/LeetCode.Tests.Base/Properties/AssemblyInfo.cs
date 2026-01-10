53c64e2eea9cccd53441bfce89d69dbb1810b733
Eugene <eremeeveugene@gmail.com>
2026-01-11 00:01:46 +0200

    Create Directory.Packages.props (#1673)

---
+++ source/Tests/LeetCode.Tests.Base/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: Guid("0225dc36-a6ac-4484-a9a0-339e5a1ba627")]
+[assembly: Parallelize(Workers = 4, Scope = ExecutionScope.MethodLevel)]
