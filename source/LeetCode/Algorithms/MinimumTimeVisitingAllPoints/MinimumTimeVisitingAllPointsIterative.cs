197d038991f6888d04011f117cee6eebd331baa9
Eugene <eremeeveugene@gmail.com>
2026-01-12 12:28:19 +0200

    Minimum Time Visiting All Points

---
+++ source/LeetCode/Algorithms/MinimumTimeVisitingAllPoints/MinimumTimeVisitingAllPointsIterative.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumTimeVisitingAllPoints;
+
+/// <inheritdoc />
+public sealed class MinimumTimeVisitingAllPointsIterative : IMinimumTimeVisitingAllPoints
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="points"></param>
+    /// <returns></returns>
+    public int MinTimeToVisitAllPoints(int[][] points)
+    {
+        var result = 0;
+
+        for (var i = 0; i < points.Length - 1; i++)
+        {
+            var point = points[i];
+            var nextPoint = points[i + 1];
+
+            var dx = Math.Abs(nextPoint[0] - point[0]);
+            var dy = Math.Abs(nextPoint[1] - point[1]);
+
+            result += Math.Max(dx, dy);
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumTimeVisitingAllPoints/MinimumTimeVisitingAllPointsTestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumTimeVisitingAllPoints;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MinimumTimeVisitingAllPoints;
+
+public abstract class MinimumTimeVisitingAllPointsTestsBase<T> where T : IMinimumTimeVisitingAllPoints, new()
+{
+    [TestMethod]
+    [DataRow("[[1,1],[3,4],[-1,0]]", 7)]
+    [DataRow("[[3,2],[-2,2]]", 5)]
+    public void MinTimeToVisitAllPoints_WithPointsArray_ReturnsSumOfStepwiseDistances(string pointsJson,
+        int expectedResult)
+    {
+        // Arrange
+        var points = JsonHelper<int[][]>.Parse(pointsJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinTimeToVisitAllPoints(points);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
