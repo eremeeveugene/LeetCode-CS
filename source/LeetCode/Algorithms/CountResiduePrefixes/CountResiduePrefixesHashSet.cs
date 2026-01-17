c5d367eadb61263fcf6798f75f583a09d0d5bbff
Eugene <eremeeveugene@gmail.com>
2026-01-17 22:01:21 +0200

    Count Residue Prefixes

---
+++ source/LeetCode/Algorithms/CountResiduePrefixes/CountResiduePrefixesHashSet.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CountResiduePrefixes;
+
+/// <inheritdoc />
+public sealed class CountResiduePrefixesHashSet : ICountResiduePrefixes
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public int ResiduePrefixes(string s)
+    {
+        var result = 0;
+
+        var hashSet = new HashSet<char>();
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            hashSet.Add(s[i]);
+
+            if (hashSet.Count == (i + 1) % 3)
+            {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CountResiduePrefixes/CountResiduePrefixesTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CountResiduePrefixes;
+
+namespace LeetCode.Tests.Algorithms.CountResiduePrefixes;
+
+public abstract class CountResiduePrefixesTestsBase<T> where T : ICountResiduePrefixes, new()
+{
+    [TestMethod]
+    [DataRow("abc", 2)]
+    [DataRow("dd", 1)]
+    [DataRow("bob", 2)]
+    public void ResiduePrefixes_WithInputString_ReturnsResiduePrefixesCount(string s,
+        int expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.ResiduePrefixes(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
