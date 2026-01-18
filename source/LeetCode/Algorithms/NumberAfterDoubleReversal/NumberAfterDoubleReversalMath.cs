b72b1cfeb414cf4bb3b8f9d01bf02de51bb9b67a
Eugene <eremeeveugene@gmail.com>
2026-01-18 13:00:25 +0200

    A Number After a Double Reversal (#1684)

---
+++ source/LeetCode/Algorithms/NumberAfterDoubleReversal/NumberAfterDoubleReversalMath.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.NumberAfterDoubleReversal;
+
+/// <inheritdoc />
+public sealed class NumberAfterDoubleReversalMath : INumberAfterDoubleReversal
+{
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+    public bool IsSameAfterReversals(int num)
+    {
+        if (num == 0)
+        {
+            return true;
+        }
+
+        return num % 10 != 0;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/NumberAfterDoubleReversal/NumberAfterDoubleReversalTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.NumberAfterDoubleReversal;
+
+namespace LeetCode.Tests.Algorithms.NumberAfterDoubleReversal;
+
+public abstract class NumberAfterDoubleReversalTestsBase<T> where T : INumberAfterDoubleReversal, new()
+{
+    [TestMethod]
+    [DataRow(0, true)]
+    [DataRow(526, true)]
+    [DataRow(1800, false)]
+    public void IsSameAfterReversals_WithInputNum_ReturnsTrueIfFinalEqualsOriginal(int num, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsSameAfterReversals(num);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
