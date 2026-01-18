4caf45ce947e707b9388a31633a5de559d8eb1cf
Eugene <eremeeveugene@gmail.com>
2026-01-18 18:03:05 +0200

    Reverse String Prefix (#1686)

---
+++ source/LeetCode/Algorithms/ReverseStringPrefix/ReverseStringPrefixTwoPointers.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ReverseStringPrefix;
+
+/// <inheritdoc />
+public sealed class ReverseStringPrefixTwoPointers : IReverseStringPrefix
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public string ReversePrefix(string s, int k)
+    {
+        if (k == 1)
+        {
+            return s;
+        }
+
+        var sCharArray = s.ToCharArray();
+
+        for (var i = 0; i < k / 2; i++)
+        {
+            (sCharArray[i], sCharArray[k - i - 1]) = (sCharArray[k - i - 1], sCharArray[i]);
+        }
+
+        return new string(sCharArray);
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ReverseStringPrefix/ReverseStringPrefixTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ReverseStringPrefix;
+
+namespace LeetCode.Tests.Algorithms.ReverseStringPrefix;
+
+public abstract class ReverseStringPrefixTestsBase<T>
+    where T : IReverseStringPrefix, new()
+{
+    [TestMethod]
+    [DataRow("abcd", 2, "bacd")]
+    [DataRow("xyz", 3, "zyx")]
+    [DataRow("hey", 1, "hey")]
+    public void ReversePrefix_WithKCharacters_ReversesFirstKCharactersOnly(string s, int k, string expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.ReversePrefix(s, k);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
