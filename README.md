0133b6ad0cf405d5cec7b4c6cfecb37ca2f20fe0
Eugene <eremeeveugene@gmail.com>
2026-01-19 14:08:50 +0200

    Best Time to Buy and Sell Stock V (#1688)

--- README.md
+++ README.md
@@ -442,6 +442,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [1630. Arithmetic Subarrays](https://leetcode.com/problems/arithmetic-subarrays/description/)
 - [1636. Sort Array by Increasing Frequency](https://leetcode.com/problems/sort-array-by-increasing-frequency/description/)
 - [1637. Widest Vertical Area Between Two Points Containing No Points](https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/description/)
+- [1642. Best Time to Buy and Sell Stock V](https://leetcode.com/problems/best-time-to-buy-and-sell-stock-v/description/)
 - [1652. Defuse the Bomb](https://leetcode.com/problems/defuse-the-bomb/description/)
 - [1656. Design an Ordered Stream](https://leetcode.com/problems/design-an-ordered-stream/description/)
 - [1662. Check If Two String Arrays are Equivalent](https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/description/)
---
+++ source/LeetCode/Algorithms/BestTimeToBuyAndSellStock5/BestTimeToBuyAndSellStock5DynamicProgramming.cs
@@ -0,0 +1,71 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.BestTimeToBuyAndSellStock5;
+
+/// <inheritdoc />
+public sealed class BestTimeToBuyAndSellStock5DynamicProgramming : IBestTimeToBuyAndSellStock5
+{
+    /// <summary>
+    ///     Time complexity - O(n * k)
+    ///     Space complexity - O(k)
+    /// </summary>
+    /// <param name="prices"></param>
+    /// <param name="maxTransactions"></param>
+    /// <returns></returns>
+    public long MaximumProfit(int[] prices, int maxTransactions)
+    {
+        var days = prices.Length;
+
+        var transactionStatesCount = maxTransactions + 1;
+
+        Span<long> profits = stackalloc long[transactionStatesCount];
+        Span<long> longHoldProfits = stackalloc long[transactionStatesCount];
+        Span<long> shortHoldProfits = stackalloc long[transactionStatesCount];
+
+        var initialLongProfit = -prices[0];
+        var initialShortProfit = prices[0];
+
+        for (var completedTransactions = 1; completedTransactions < transactionStatesCount; completedTransactions++)
+        {
+            longHoldProfits[completedTransactions] = initialLongProfit;
+            shortHoldProfits[completedTransactions] = initialShortProfit;
+        }
+
+        for (var day = 1; day < days; day++)
+        {
+            long currentPrice = prices[day];
+
+            for (var completedTransactions = maxTransactions; completedTransactions > 0; completedTransactions--)
+            {
+                var longHoldProfit = longHoldProfits[completedTransactions];
+                var shortHoldProfit = shortHoldProfits[completedTransactions];
+
+                var profitAfterClosingLong = longHoldProfit + currentPrice;
+                var profitAfterClosingShort = shortHoldProfit - currentPrice;
+
+                var profitAfterClosing = Math.Max(profitAfterClosingLong, profitAfterClosingShort);
+
+                profits[completedTransactions] = Math.Max(profits[completedTransactions], profitAfterClosing);
+
+                var previousTransaction = completedTransactions - 1;
+
+                var openLongFromCash = profits[previousTransaction] - currentPrice;
+                var openShortFromCash = profits[previousTransaction] + currentPrice;
+
+                longHoldProfits[completedTransactions] = Math.Max(longHoldProfit, openLongFromCash);
+                shortHoldProfits[completedTransactions] = Math.Max(shortHoldProfit, openShortFromCash);
+            }
+        }
+
+        return profits[^1];
+    }
+}
