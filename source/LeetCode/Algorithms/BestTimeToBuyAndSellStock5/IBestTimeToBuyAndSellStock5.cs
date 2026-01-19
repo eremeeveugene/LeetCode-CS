fdb85a81f0352b36d9069409242f41639e9e0343
Eugene <eremeeveugene@gmail.com>
2026-01-19 14:05:31 +0200

    Best Time to Buy and Sell Stock V

---
+++ source/LeetCode/Algorithms/BestTimeToBuyAndSellStock5/IBestTimeToBuyAndSellStock5.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.BestTimeToBuyAndSellStock5;
+
+/// <summary>
+///     https://leetcode.com/problems/best-time-to-buy-and-sell-stock-v/description/
+/// </summary>
+public interface IBestTimeToBuyAndSellStock5
+{
+    long MaximumProfit(int[] prices, int maxTransactions);
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/BestTimeToBuyAndSellStock5/BestTimeToBuyAndSellStock5TestsBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.BestTimeToBuyAndSellStock5;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.BestTimeToBuyAndSellStock5;
+
+public abstract class BestTimeToBuyAndSellStock5TestsBase<T> where T : IBestTimeToBuyAndSellStock5, new()
+{
+    [TestMethod]
+    [DataRow("[1,7,9,8,2]", 2, 14)]
+    [DataRow("[12,16,19,19,8,1,19,13,9]", 3, 36)]
+    public void MaximumProfit_WithPricesAndTransactionLimit_ReturnsMaximumTotalProfitConsideringNormalAndShortSelling(
+        string pricesJson, int maxTransactions, long expectedResult)
+    {
+        // Arrange
+        var prices = JsonHelper<int[]>.Parse(pricesJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MaximumProfit(prices, maxTransactions);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
