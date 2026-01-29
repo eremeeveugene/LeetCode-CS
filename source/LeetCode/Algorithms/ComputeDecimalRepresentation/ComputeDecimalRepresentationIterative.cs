6e9b372daf5078d49be9307fa6bdd969c848f633
Eugene <eremeeveugene@gmail.com>
2026-01-29 17:50:36 +0200

    Compute Decimal Representation

---
+++ source/LeetCode/Algorithms/ComputeDecimalRepresentation/ComputeDecimalRepresentationIterative.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ComputeDecimalRepresentation;
+
+/// <inheritdoc />
+public sealed class ComputeDecimalRepresentationIterative : IComputeDecimalRepresentation
+{
+    /// <summary>
+    ///     Time complexity - O(log n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public int[] DecimalRepresentation(int n)
+    {
+        Span<int> buffer = stackalloc int[10];
+
+        var length = 0;
+        var power = 1;
+
+        while (n > 0)
+        {
+            var digit = n % 10;
+
+            if (digit != 0)
+            {
+                buffer[length] = digit * power;
+
+                length++;
+            }
+
+            n /= 10;
+            power *= 10;
+        }
+
+        var result = new int[length];
+
+        for (var i = 0; i < length; i++)
+        {
+            result[i] = buffer[length - i - 1];
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ComputeDecimalRepresentation/ComputeDecimalRepresentationTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ComputeDecimalRepresentation;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.ComputeDecimalRepresentation;
+
+public abstract class ComputeDecimalRepresentationTestsBase<T> where T : IComputeDecimalRepresentation, new()
+{
+    [TestMethod]
+    [DataRow(537, "[500,30,7]")]
+    [DataRow(102, "[100,2]")]
+    [DataRow(6, "[6]")]
+    public void DecimalRepresentation_WithPositiveInteger_ReturnsFewestBase10ComponentsInDescendingOrder(int n,
+        string expectedResultJson)
+    {
+        // Arrange
+        var expectedResult = JsonHelper<int[]>.Parse(expectedResultJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.DecimalRepresentation(n);
+
+        // Assert
+        CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+}
