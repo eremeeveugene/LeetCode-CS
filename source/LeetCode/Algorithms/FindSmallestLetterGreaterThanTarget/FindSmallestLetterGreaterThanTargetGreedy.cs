9a17d30b99f3f37a7bc1bdfdf31e1254c0d61389
Eugene <eremeeveugene@gmail.com>
2026-01-31 16:49:41 +0200

    Find Smallest Letter Greater Than Target

---
+++ source/LeetCode/Algorithms/FindSmallestLetterGreaterThanTarget/FindSmallestLetterGreaterThanTargetGreedy.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindSmallestLetterGreaterThanTarget;
+
+/// <inheritdoc />
+public sealed class FindSmallestLetterGreaterThanTargetGreedy : IFindSmallestLetterGreaterThanTarget
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="letters"></param>
+    /// <param name="target"></param>
+    /// <returns></returns>
+    public char NextGreatestLetter(char[] letters, char target)
+    {
+        foreach (var letter in letters)
+        {
+            if (letter > target)
+            {
+                return letter;
+            }
+        }
+
+        return letters[0];
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/FindSmallestLetterGreaterThanTarget/FindSmallestLetterGreaterThanTargetTestsBase.cs
@@ -0,0 +1,38 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.FindSmallestLetterGreaterThanTarget;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.FindSmallestLetterGreaterThanTarget;
+
+public abstract class FindSmallestLetterGreaterThanTargetTestsBase<T>
+    where T : IFindSmallestLetterGreaterThanTarget, new()
+{
+    [TestMethod]
+    [DataRow("[\"c\",\"f\",\"j\"]", 'a', 'c')]
+    [DataRow("[\"c\",\"f\",\"j\"]", 'c', 'f')]
+    [DataRow("[\"x\",\"x\",\"y\",\"y\"]", 'z', 'x')]
+    public void NextGreatestLetter_WithSortedLettersAndTargetCharacter_ReturnsSmallestLetterGreaterThanTarget(
+        string lettersJson, char target, char expectedResult)
+    {
+        // Arrange
+        var letters = JsonHelper<char[]>.Parse(lettersJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.NextGreatestLetter(letters, target);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
