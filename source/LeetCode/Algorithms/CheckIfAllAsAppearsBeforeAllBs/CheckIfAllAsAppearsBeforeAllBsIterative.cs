ecca8e264b378d5787b710dea03b3d269faa596a
Eugene <eremeeveugene@gmail.com>
2026-02-07 15:46:53 +0200

    Check if All A's Appears Before All B's (#1715)

---
+++ source/LeetCode/Algorithms/CheckIfAllAsAppearsBeforeAllBs/CheckIfAllAsAppearsBeforeAllBsIterative.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CheckIfAllAsAppearsBeforeAllBs;
+
+/// <inheritdoc />
+public sealed class CheckIfAllAsAppearsBeforeAllBsIterative : ICheckIfAllAsAppearsBeforeAllBs
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public bool CheckString(string s)
+    {
+        for (var i = 0; i < s.Length - 1; i++)
+        {
+            if (s[i] > s[i + 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
