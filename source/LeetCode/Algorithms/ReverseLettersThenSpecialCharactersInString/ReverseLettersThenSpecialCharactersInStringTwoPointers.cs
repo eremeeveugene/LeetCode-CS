bba2d17b79be2a92fb445c7999a720009e9dd8ae
Eugene <eremeeveugene@gmail.com>
2026-02-08 16:06:27 +0200

    Reverse Letters Then Special Characters in a String

---
+++ source/LeetCode/Algorithms/ReverseLettersThenSpecialCharactersInString/ReverseLettersThenSpecialCharactersInStringTwoPointers.cs
@@ -0,0 +1,82 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ReverseLettersThenSpecialCharactersInString;
+
+/// <inheritdoc />
+public sealed class ReverseLettersThenSpecialCharactersInStringTwoPointers :
+    IReverseLettersThenSpecialCharactersInString
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public string ReverseByType(string s)
+    {
+        var charArray = s.ToCharArray();
+
+        var left = 0;
+        var right = charArray.Length - 1;
+
+        while (left < right)
+        {
+            while (left < right && !char.IsLetter(charArray[left]))
+            {
+                left++;
+            }
+
+            while (left < right && !char.IsLetter(charArray[right]))
+            {
+                right--;
+            }
+
+            if (left >= right)
+            {
+                break;
+            }
+
+            (charArray[left], charArray[right]) = (charArray[right], charArray[left]);
+
+            left++;
+            right--;
+        }
+
+        left = 0;
+        right = charArray.Length - 1;
+
+        while (left < right)
+        {
+            while (left < right && char.IsLetter(charArray[left]))
+            {
+                left++;
+            }
+
+            while (left < right && char.IsLetter(charArray[right]))
+            {
+                right--;
+            }
+
+            if (left >= right)
+            {
+                continue;
+            }
+
+            (charArray[left], charArray[right]) = (charArray[right], charArray[left]);
+
+            left++;
+            right--;
+        }
+
+        return new string(charArray);
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ReverseLettersThenSpecialCharactersInString/ReverseLettersThenSpecialCharactersInStringTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ReverseLettersThenSpecialCharactersInString;
+
+namespace LeetCode.Tests.Algorithms.ReverseLettersThenSpecialCharactersInString;
+
+public abstract class ReverseLettersThenSpecialCharactersInStringTestsBase<T>
+    where T : IReverseLettersThenSpecialCharactersInString, new()
+{
+    [TestMethod]
+    [DataRow("a", "a")]
+    [DataRow("z", "z")]
+    [DataRow("!", "!")]
+    [DataRow(")ebc#da@f(", "(fad@cb#e)")]
+    [DataRow("!@#$%^&*()", ")(*&^%$#@!")]
+    public void ReverseByType_WithInputString_ReturnsStringWithReversedLetterAndSpecialCharacters(
+        string s, string expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.ReverseByType(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
