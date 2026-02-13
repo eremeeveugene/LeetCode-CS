c452c1d4aeda5f8e367db841903bc266bcc1df73
Eugene <eremeeveugene@gmail.com>
2026-02-13 17:50:58 +0200

    Points That Intersect With Cars (#1719)

---
+++ source/LeetCode/Algorithms/PointsThatIntersectWithCars/PointsThatIntersectWithCarsLookup.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.PointsThatIntersectWithCars;
+
+/// <inheritdoc />
+public sealed class PointsThatIntersectWithCarsLookup : IPointsThatIntersectWithCars
+{
+    /// <summary>
+    ///     Time complexity - O(n * R), where n is the length of nums and R = max(end) - min(start) + 1
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int NumberOfPoints(IList<IList<int>> nums)
+    {
+        Span<bool> lookup = stackalloc bool[100];
+
+        for (var i = 0; i < nums.Count; i++)
+        {
+            var num = nums[i];
+            var start = num[0];
+            var end = num[1];
+
+            for (var j = start; j <= end; j++)
+            {
+                lookup[j - 1] = true;
+            }
+        }
+
+        var result = 0;
+
+        for (var i = 0; i < lookup.Length; i++)
+        {
+            if (lookup[i])
+            {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/PointsThatIntersectWithCars/PointsThatIntersectWithCarsTestsBase.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.PointsThatIntersectWithCars;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.PointsThatIntersectWithCars;
+
+public abstract class PointsThatIntersectWithCarsTestsBase<T> where T : IPointsThatIntersectWithCars, new()
+{
+    [TestMethod]
+    [DataRow("[[3,6],[1,5],[4,7]]", 7)]
+    [DataRow("[[1,3],[5,8]]", 7)]
+    public void NumberOfPoints_WithGivenCarIntervals_ReturnsCountOfCoveredPoints(string numsJson, int expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<IList<IList<int>>>.Parse(numsJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.NumberOfPoints(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
