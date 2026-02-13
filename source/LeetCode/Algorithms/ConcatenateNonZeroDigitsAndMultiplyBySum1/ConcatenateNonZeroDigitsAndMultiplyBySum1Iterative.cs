52e0c07e4618480e04e7f6ee31cf994c1aefb93c
Eugene <eremeeveugene@gmail.com>
2026-02-13 18:17:55 +0200

    Concatenate Non-Zero Digits and Multiply by Sum I

---
+++ source/LeetCode/Algorithms/ConcatenateNonZeroDigitsAndMultiplyBySum1/ConcatenateNonZeroDigitsAndMultiplyBySum1Iterative.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ConcatenateNonZeroDigitsAndMultiplyBySum1;
+
+/// <inheritdoc />
+public sealed class ConcatenateNonZeroDigitsAndMultiplyBySum1Iterative : IConcatenateNonZeroDigitsAndMultiplyBySum1
+{
+    /// <summary>
+    ///     Time complexity - O(log n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public long SumAndMultiply(int n)
+    {
+        long value = 0;
+        var sum = 0;
+        var multiplier = 1;
+
+        while (n > 0)
+        {
+            var digit = n % 10;
+
+            n /= 10;
+
+            if (digit == 0)
+            {
+                continue;
+            }
+
+            sum += digit;
+            value += digit * multiplier;
+
+            multiplier *= 10;
+        }
+
+        return value * sum;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ConcatenateNonZeroDigitsAndMultiplyBySum1/ConcatenateNonZeroDigitsAndMultiplyBySum1TestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ConcatenateNonZeroDigitsAndMultiplyBySum1;
+
+namespace LeetCode.Tests.Algorithms.ConcatenateNonZeroDigitsAndMultiplyBySum1;
+
+public abstract class ConcatenateNonZeroDigitsAndMultiplyBySum1TestsBase<T>
+    where T : IConcatenateNonZeroDigitsAndMultiplyBySum1, new()
+{
+    [TestMethod]
+    [DataRow(1000, 1)]
+    [DataRow(10203004, 12340)]
+    public void SumAndMultiply_WithGivenInteger_ReturnsProductOfNonZeroDigitConcatenationAndDigitSum(int n,
+        long expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.SumAndMultiply(n);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
