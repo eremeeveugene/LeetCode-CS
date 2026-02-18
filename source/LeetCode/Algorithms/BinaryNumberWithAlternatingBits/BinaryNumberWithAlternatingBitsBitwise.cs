f0f62a5b7f686476d6e4880c483a3e79e650d8ae
Eugene <eremeeveugene@gmail.com>
2026-02-18 17:32:47 +0200

    Binary Number with Alternating Bits

---
+++ source/LeetCode/Algorithms/BinaryNumberWithAlternatingBits/BinaryNumberWithAlternatingBitsBitwise.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.BinaryNumberWithAlternatingBits;
+
+/// <inheritdoc />
+public sealed class BinaryNumberWithAlternatingBitsBitwise : IBinaryNumberWithAlternatingBits
+{
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public bool HasAlternatingBits(int n)
+    {
+        var x = n ^ (n >> 1);
+
+        return (x & (x + 1)) == 0;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/BinaryNumberWithAlternatingBits/BinaryNumberWithAlternatingBitsTestsBase.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.BinaryNumberWithAlternatingBits;
+
+namespace LeetCode.Tests.Algorithms.BinaryNumberWithAlternatingBits;
+
+public abstract class BinaryNumberWithAlternatingBitsTestsBase<T> where T : IBinaryNumberWithAlternatingBits, new()
+{
+    [TestMethod]
+    [DataRow(5, true)]
+    [DataRow(7, false)]
+    [DataRow(11, false)]
+    public void HasAlternatingBits_WithGivenPositiveInteger_ReturnsTrueIfAdjacentBitsAlternate(int n,
+        bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.HasAlternatingBits(n);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
