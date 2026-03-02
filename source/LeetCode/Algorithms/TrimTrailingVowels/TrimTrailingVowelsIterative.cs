8ce695aa12a3a1c73dcdfeabc7ad0d1d351b24af
Eugene <eremeeveugene@gmail.com>
2026-03-02 11:14:59 +0200

    Trim Trailing Vowels

---
+++ source/LeetCode/Algorithms/TrimTrailingVowels/TrimTrailingVowelsIterative.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.TrimTrailingVowels;
+
+/// <summary>
+///     Time complexity - O(n)
+///     Space complexity - O(1)
+/// </summary>
+public sealed class TrimTrailingVowelsIterative : ITrimTrailingVowels
+{
+    public string TrimTrailingVowels(string s)
+    {
+        var length = s.Length;
+
+        while (length > 0 && IsVowel(s[length - 1]))
+        {
+            length--;
+        }
+
+        return length == s.Length ? s : s[..length];
+    }
+
+    private static bool IsVowel(char c)
+    {
+        return c is 'a' or 'e' or 'i' or 'o' or 'u';
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/TrimTrailingVowels/TrimTrailingVowelsTestsBase.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.TrimTrailingVowels;
+
+namespace LeetCode.Tests.Algorithms.TrimTrailingVowels;
+
+public abstract class TrimTrailingVowelsTestsBase<T> where T : ITrimTrailingVowels, new()
+{
+    [TestMethod]
+    [DataRow("idea", "id")]
+    [DataRow("day", "day")]
+    [DataRow("aeiou", "")]
+    public void TrimTrailingVowels_WithGivenString_ReturnsStringWithoutTrailingVowels(string s, string expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.TrimTrailingVowels(s);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
