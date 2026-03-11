406b06b59d51931363db7a96b763fdeaaed868d9
Eugene <eremeeveugene@gmail.com>
2026-03-11 17:57:05 +0200

    N-Repeated Element in Size 2N Array

---
+++ source/LeetCode/Algorithms/NRepeatedElementInSize2NArray/NRepeatedElementInSize2NArrayHashSet.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.NRepeatedElementInSize2NArray;
+
+/// <inheritdoc />
+public sealed class NRepeatedElementInSize2NArrayHashSet : INRepeatedElementInSize2NArray
+{
+    /// <summary>
+    ///     Time
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int RepeatedNTimes(int[] nums)
+    {
+        var numsHashSet = new HashSet<int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var num = nums[i];
+
+            if (numsHashSet.Add(num))
+            {
+                continue;
+            }
+
+            return num;
+        }
+
+        return 0;
+    }
+}
---
+++ source/LeetCode/Algorithms/NRepeatedElementInSize2NArray/NRepeatedElementInSize2NArrayLookup.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.NRepeatedElementInSize2NArray;
+
+/// <inheritdoc />
+public sealed class NRepeatedElementInSize2NArrayLookup : INRepeatedElementInSize2NArray
+{
+    private const int MaxValue = 10_000;
+
+    /// <summary>
+    ///     Time
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int RepeatedNTimes(int[] nums)
+    {
+        Span<bool> numsLookup = stackalloc bool[MaxValue + 1];
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var num = nums[i];
+
+            if (numsLookup[num])
+            {
+                return num;
+            }
+
+            numsLookup[num] = true;
+        }
+
+        return 0;
+    }
+}
