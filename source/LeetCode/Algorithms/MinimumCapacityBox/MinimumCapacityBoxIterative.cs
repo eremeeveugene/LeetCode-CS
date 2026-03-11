6c0b82fe4e972de771c3a1aea7d707083cc6adef
Eugene <eremeeveugene@gmail.com>
2026-03-11 17:35:29 +0200

    Minimum Capacity Box (#1739)

---
+++ source/LeetCode/Algorithms/MinimumCapacityBox/MinimumCapacityBoxIterative.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MinimumCapacityBox;
+
+/// <inheritdoc />
+public sealed class MinimumCapacityBoxIterative : IMinimumCapacityBox
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="capacities"></param>
+    /// <param name="itemSize"></param>
+    /// <returns></returns>
+    public int MinimumIndex(int[] capacities, int itemSize)
+    {
+        var minCapacity = int.MaxValue;
+
+        var result = -1;
+
+        for (var i = 0; i < capacities.Length; i++)
+        {
+            var capacity = capacities[i];
+
+            if (capacity < itemSize || capacity >= minCapacity)
+            {
+                continue;
+            }
+
+            minCapacity = capacity;
+
+            result = i;
+        }
+
+        return result;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumCapacityBox/MinimumCapacityBoxTestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.MinimumCapacityBox;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.MinimumCapacityBox;
+
+public abstract class MinimumCapacityBoxTestsBase<T> where T : IMinimumCapacityBox, new()
+{
+    [TestMethod]
+    [DataRow("[1,5,3,7]", 3, 2)]
+    [DataRow("[3,5,4,3]", 2, 0)]
+    [DataRow("[3]", 5, -1)]
+    public void MinimumIndex_WithCapacitiesAndItemSize_ReturnsIndexOfSmallestSufficientCapacity(string capacitiesJson,
+        int itemSize, int expectedResult)
+    {
+        // Arrange
+        var capacities = JsonHelper<int[]>.Parse(capacitiesJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MinimumIndex(capacities, itemSize);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
