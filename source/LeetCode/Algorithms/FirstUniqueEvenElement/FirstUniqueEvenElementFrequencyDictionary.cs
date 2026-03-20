8bc1b8403a825037b54d9d0ac2fd5248f279d035
Eugene <eremeeveugene@gmail.com>
2026-03-20 17:28:59 +0200

    First Unique Even Element

---
+++ source/LeetCode/Algorithms/FirstUniqueEvenElement/FirstUniqueEvenElementFrequencyDictionary.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FirstUniqueEvenElement;
+
+/// <inheritdoc />
+public sealed class FirstUniqueEvenElementFrequencyDictionary : IFirstUniqueEvenElement
+{
+    /// <summary>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int FirstUniqueEven(int[] nums)
+    {
+        var frequencyDictionary = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var num = nums[i];
+
+            if (num % 2 != 0)
+            {
+                continue;
+            }
+
+            if (!frequencyDictionary.TryAdd(num, 1))
+            {
+                frequencyDictionary[num]++;
+            }
+        }
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            var num = nums[i];
+
+            if (num % 2 != 0)
+            {
+                continue;
+            }
+
+            if (frequencyDictionary[num] == 1)
+            {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+}
