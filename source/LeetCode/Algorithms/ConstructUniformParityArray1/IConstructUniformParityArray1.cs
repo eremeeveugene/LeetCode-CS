ebfa6b8687d2a14d922970097f1c73b8236cef5a
Eugene <eremeeveugene@gmail.com>
2026-03-22 17:37:02 +0200

    Construct Uniform Parity Array I

---
+++ source/LeetCode/Algorithms/ConstructUniformParityArray1/IConstructUniformParityArray1.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ConstructUniformParityArray1;
+
+/// <summary>
+///     https://leetcode.com/problems/construct-uniform-parity-array-i/description/
+/// </summary>
+public interface IConstructUniformParityArray1
+{
+    bool UniformArray(int[] nums1);
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/ConstructUniformParityArray1/ConstructUniformParityArray1TestsBase.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.ConstructUniformParityArray1;
+using LeetCode.Core.Helpers;
+
+namespace LeetCode.Tests.Algorithms.ConstructUniformParityArray1;
+
+public abstract class ConstructUniformParityArray1TestsBase<T> where T : IConstructUniformParityArray1, new()
+{
+    [TestMethod]
+    [DataRow("[1,4,7]", true)]
+    [DataRow("[2,3]", false)]
+    [DataRow("[4,6]", true)]
+    public void UniformArray_WithDistinctIntegerArray_ReturnsTrueIfUniformParityArrayCanBeConstructed(string numsJson,
+        bool expectedResult)
+    {
+        // Arrange
+        var nums = JsonHelper<int[]>.Parse(numsJson);
+
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.UniformArray(nums);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
