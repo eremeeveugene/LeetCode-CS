5fbf96ac4a8afbf2973801be4b9e713e6fbd0b18
Eugene <eremeeveugene@gmail.com>
2026-03-25 23:53:48 +0200

    Create ConstructProductMatrix1.cs

---
+++ source/LeetCode/Algorithms/ConstructProductMatrix/ConstructProductMatrix1.cs
@@ -0,0 +1,59 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ConstructProductMatrix;
+
+/// <inheritdoc />
+public sealed class ConstructProductMatrix1 : IConstructProductMatrix
+{
+    public int[][] ConstructProductMatrix(int[][] grid)
+    {
+        const int mod = 12345;
+
+        var m = grid.Length;
+        var n = grid[0].Length;
+
+        var result = new int[m][];
+
+        for (var i = 0; i < m; i++)
+        {
+            result[i] = new int[n];
+        }
+
+        var prefix = 1;
+
+        for (var i = 0; i < m; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                result[i][j] = prefix;
+
+                grid[i][j] %= mod;
+
+                prefix = prefix * grid[i][j] % mod;
+            }
+        }
+
+        var suffix = 1;
+
+        for (var i = m - 1; i >= 0; i--)
+        {
+            for (var j = n - 1; j >= 0; j--)
+            {
+                result[i][j] = result[i][j] * suffix % mod;
+
+                suffix = suffix * grid[i][j] % mod;
+            }
+        }
+
+        return result;
+    }
+}
