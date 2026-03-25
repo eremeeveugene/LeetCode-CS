aa4dd8581a74163b4804f321a1bfe4af8818d120
Eugene <eremeeveugene@gmail.com>
2026-03-25 23:55:17 +0200

    Update SpiralMatrix4TestsBase.cs

--- source/Tests/LeetCode.Tests/Algorithms/SpiralMatrix4/SpiralMatrix4TestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/SpiralMatrix4/SpiralMatrix4TestsBase.cs
@@ -12,7 +12,6 @@
 using LeetCode.Algorithms.SpiralMatrix4;
 using LeetCode.Core.Helpers;
 using LeetCode.Core.Models;
-using LeetCode.Tests.Base.Extensions;
 
 namespace LeetCode.Tests.Algorithms.SpiralMatrix4;
 
@@ -36,6 +35,6 @@ public abstract class SpiralMatrix4TestsBase<T> where T : ISpiralMatrix4, new()
         var actualResult = solution.SpiralMatrix(m, n, head);
 
         // Assert
-        NestedCollectionAssert.AreEqual(expectedResult, actualResult);
+        CollectionAssert.AreEqual(expectedResult, actualResult);
     }
 }
