c48a6ccc9d16d1348ad45c4c7c4c29485ceaec98
Eugene <eremeeveugene@gmail.com>
2026-03-27 17:37:52 +0200

    Maximize Expression of Three Elements (#1758)

---
+++ source/LeetCode/Algorithms/MaximizeExpressionOfThreeElements/MaximizeExpressionOfThreeElementsSorting.cs
@@ -0,0 +1,29 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximizeExpressionOfThreeElements;
+
+/// <inheritdoc />
+public sealed class MaximizeExpressionOfThreeElementsSorting : IMaximizeExpressionOfThreeElements
+{
+    /// <summary>
+    ///     Time complexity - O(n log n)
+    ///     Space complexity - O(log n)
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public int MaximizeExpressionOfThree(int[] nums)
+    {
+        Array.Sort(nums);
+
+        return nums[^2] + nums[^1] - nums[0];
+    }
+}
