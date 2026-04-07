d59552dfff3107c9d5724c9c7c07068f05a757e4
Eugene <eremeeveugene@gmail.com>
2026-04-07 16:50:12 +0300

    Design an ATM Machine (#1769)

---
+++ source/LeetCode/Algorithms/DesignAnATMMachine/DesignAnATMMachineGreedy.cs
@@ -0,0 +1,68 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.DesignAnATMMachine;
+
+/// <inheritdoc />
+public sealed class DesignAnATMMachineGreedy : IDesignAnATMMachine
+{
+    private const int BanknotesCount = 5;
+    private static readonly int[] InvalidOperation = [-1];
+    private readonly int[] _banknotes = [20, 50, 100, 200, 500];
+    private readonly long[] _banknotesCounts = new long[BanknotesCount];
+
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="banknotesCounts"></param>
+    public void Deposit(int[] banknotesCounts)
+    {
+        for (var i = 0; i < 5; i++)
+        {
+            _banknotesCounts[i] += banknotesCounts[i];
+        }
+    }
+
+    /// <summary>
+    ///     Time complexity - O(1)
+    ///     Space complexity - O(1)
+    /// </summary>
+    /// <param name="amount"></param>
+    /// <returns></returns>
+    public int[] Withdraw(int amount)
+    {
+        var used = new int[BanknotesCount];
+
+        var remaining = amount;
+
+        for (var i = used.Length - 1; i >= 0; i--)
+        {
+            var canTakeByAmount = remaining / _banknotes[i];
+            var canTake = (int)Math.Min(_banknotesCounts[i], canTakeByAmount);
+
+            used[i] = canTake;
+            remaining -= used[i] * _banknotes[i];
+        }
+
+        if (remaining != 0)
+        {
+            return InvalidOperation;
+        }
+
+        for (var i = 0; i < BanknotesCount; i++)
+        {
+            _banknotesCounts[i] -= used[i];
+        }
+
+        return used;
+    }
+}
---
+++ source/Tests/LeetCode.Tests.Base/Scenarios/IScenario.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Tests.Base.Scenarios;
+
+/// <summary>
+///     Represents a self-contained test scenario for a given solution type.
+///     A scenario defines a sequence of operations to execute and the expected
+///     results they should produce.
+/// </summary>
+/// <typeparam name="TSolution">
+///     The solution interface under test. Operations are executed against an instance of this type.
+/// </typeparam>
+public interface IScenario<in TSolution>
+{
+    /// <summary>
+    ///     Gets the ordered sequence of operations to execute against the solution.
+    /// </summary>
+    IOperation<TSolution>[] Operations { get; }
+
+    /// <summary>
+    ///     Gets the expected results corresponding to each operation in <see cref="Operations" />.
+    ///     The element at index <c>i</c> is the expected outcome of <c>Operations[i]</c>.
+    /// </summary>
+    IOperationResult[] OperationResults { get; }
+}
