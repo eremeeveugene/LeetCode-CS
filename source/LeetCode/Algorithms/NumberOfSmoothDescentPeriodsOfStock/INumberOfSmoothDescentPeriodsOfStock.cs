27c1b352eef9006e4b45b85d06563e9ed46eefdb
Eugene <eremeeveugene@gmail.com>
2026-04-12 16:19:57 +0300

    Number of Smooth Descent Periods of a Stock

---
+++ source/LeetCode/Algorithms/NumberOfSmoothDescentPeriodsOfStock/INumberOfSmoothDescentPeriodsOfStock.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.NumberOfSmoothDescentPeriodsOfStock;
+
+/// <summary>
+///     https://leetcode.com/problems/number-of-smooth-descent-periods-of-a-stock/description/
+/// </summary>
+public interface INumberOfSmoothDescentPeriodsOfStock
+{
+    long GetDescentPeriods(int[] prices);
+}
