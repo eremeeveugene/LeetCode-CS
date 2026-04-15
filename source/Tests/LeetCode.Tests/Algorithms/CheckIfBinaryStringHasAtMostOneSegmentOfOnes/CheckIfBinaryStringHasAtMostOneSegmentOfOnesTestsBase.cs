0c7e6af74f5cc4fd466c6a00b9f92c2399cec7f8
Eugene <eremeeveugene@gmail.com>
2026-04-15 14:07:55 +0300

    Update CheckIfBinaryStringHasAtMostOneSegmentOfOnesTestsBase.cs

--- source/Tests/LeetCode.Tests/Algorithms/CheckIfBinaryStringHasAtMostOneSegmentOfOnes/CheckIfBinaryStringHasAtMostOneSegmentOfOnesTestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/CheckIfBinaryStringHasAtMostOneSegmentOfOnes/CheckIfBinaryStringHasAtMostOneSegmentOfOnesTestsBase.cs
@@ -18,21 +18,19 @@ public abstract class CheckIfBinaryStringHasAtMostOneSegmentOfOnesTestsBase<T>
 {
     [TestMethod]
     [DataRow("1", true)]
-    [DataRow("0", true)]
     [DataRow("11", true)]
     [DataRow("10", true)]
-    [DataRow("01", true)]
-    [DataRow("111111", true)]
-    [DataRow("000000", true)]
+    [DataRow("110", true)]
+    [DataRow("111", true)]
     [DataRow("111000", true)]
-    [DataRow("000111", true)]
     [DataRow("101", false)]
     [DataRow("1001", false)]
-    [DataRow("110", true)]
     [DataRow("110011", false)]
     [DataRow("10001", false)]
     [DataRow("10111", false)]
     [DataRow("1110111", false)]
+    [DataRow("1001000", false)]
+    [DataRow("1100001", false)]
     public void CheckOnesSegment_WithBinaryString_ReturnsTrueIfAtMostOneContiguousSegmentOfOnes(
         string s,
         bool expectedResult)
