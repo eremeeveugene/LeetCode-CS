f45f1ac5b050482fe9ce44851b406bcc9f8b3260
Eugene <eremeeveugene@gmail.com>
2026-04-19 19:51:26 +0300

    Maximum Distance Between a Pair of Values (#1786)

---
+++ source/LeetCode/Algorithms/MaximumDistanceBetweenPairOfValues/MaximumDistanceBetweenPairOfValuesBruteForce.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumDistanceBetweenPairOfValues;
+
+/// <inheritdoc />
+public sealed class MaximumDistanceBetweenPairOfValuesBruteForce : IMaximumDistanceBetweenPairOfValues
+{
+    /// <inheritdoc />
+    /// <remarks>
+    ///     Time complexity - O(n * m)
+    ///     Space complexity - O(1)
+    /// </remarks>
+    public int MaxDistance(int[] nums1, int[] nums2)
+    {
+        var maxDistance = 0;
+
+        for (var i = 0; i < nums1.Length; i++)
+        {
+            var num1 = nums1[i];
+
+            var distance = 0;
+
+            for (var j = i; j < nums2.Length; j++)
+            {
+                var num2 = nums2[j];
+
+                if (num1 > num2)
+                {
+                    break;
+                }
+
+                distance = j - i;
+            }
+
+            maxDistance = Math.Max(maxDistance, distance);
+        }
+
+        return maxDistance;
+    }
+}
---
+++ source/LeetCode/Algorithms/MaximumDistanceBetweenPairOfValues/MaximumDistanceBetweenPairOfValuesTwoPointers.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.MaximumDistanceBetweenPairOfValues;
+
+/// <inheritdoc />
+public sealed class MaximumDistanceBetweenPairOfValuesTwoPointers : IMaximumDistanceBetweenPairOfValues
+{
+    /// <inheritdoc />
+    /// <remarks>
+    ///     Time complexity - O(n + m)
+    ///     Space complexity - O(1)
+    /// </remarks>
+    public int MaxDistance(int[] nums1, int[] nums2)
+    {
+        var maxDistance = 0;
+
+        var i = 0;
+        var j = 0;
+
+        while (i < nums1.Length && j < nums2.Length)
+        {
+            if (nums1[i] > nums2[j])
+            {
+                i++;
+            }
+            else
+            {
+                var distance = j - i;
+
+                maxDistance = Math.Max(maxDistance, distance);
+
+                j++;
+            }
+        }
+
+        return maxDistance;
+    }
+}
