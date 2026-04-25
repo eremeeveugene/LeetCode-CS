b7450539792bf5d74ca6a83a189c455cfc2c5219
Eugene <eremeeveugene@gmail.com>
2026-04-25 16:45:40 +0300

    Sum of Distances

---
+++ source/LeetCode/Algorithms/SumOfDistances/ISumOfDistances.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.SumOfDistances;
+
+/// <summary>
+///     https://leetcode.com/problems/sum-of-distances/description/
+/// </summary>
+public interface ISumOfDistances
+{
+    /// <summary>
+    ///     Calculates the sum of distances from each index to all other indices with the same value.
+    /// </summary>
+    /// <param name="nums">A 0-indexed integer array.</param>
+    /// <returns>An array where each value is the sum of absolute distances to equal values.</returns>
+    long[] Distance(int[] nums);
+}
---
+++ source/LeetCode/Algorithms/SumOfDistances/SumOfDistancesGroupedPrefixSuffix.cs
@@ -0,0 +1,86 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Runtime.InteropServices;
+
+namespace LeetCode.Algorithms.SumOfDistances;
+
+/// <inheritdoc />
+public sealed class SumOfDistancesGroupedPrefixSuffix : ISumOfDistances
+{
+    /// <inheritdoc />
+    /// <remarks>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(k), where k is the number of distinct numbers
+    /// </remarks>
+    public long[] Distance(int[] nums)
+    {
+        var numsLength = nums.Length;
+
+        var numToStateDictionary = new Dictionary<int, State>(numsLength);
+
+        for (var index = 0; index < numsLength; index++)
+        {
+            var num = nums[index];
+
+            ref var state = ref CollectionsMarshal.GetValueRefOrAddDefault(numToStateDictionary, num, out _);
+
+            state.RightCount++;
+            state.RightSum += index;
+        }
+
+        var result = new long[numsLength];
+
+        for (var index = 0; index < numsLength; index++)
+        {
+            var num = nums[index];
+
+            ref var state = ref CollectionsMarshal.GetValueRefOrAddDefault(numToStateDictionary, num, out _);
+
+            state.RightCount--;
+            state.RightSum -= index;
+
+            result[index] = (state.LeftCount * index) - state.LeftSum + state.RightSum - (state.RightCount * index);
+
+            state.LeftCount++;
+            state.LeftSum += index;
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    ///     Stores cumulative prefix and suffix state for a specific number:
+    ///     left side count/sum and right side count/sum of indices.
+    /// </summary>
+    private struct State
+    {
+        /// <summary>
+        ///     Number of equal elements to the left of the current index.
+        /// </summary>
+        public long LeftCount;
+
+        /// <summary>
+        ///     Sum of indices of equal elements to the left of the current index.
+        /// </summary>
+        public long LeftSum;
+
+        /// <summary>
+        ///     Number of equal elements to the right of the current index.
+        /// </summary>
+        public long RightCount;
+
+        /// <summary>
+        ///     Sum of indices of equal elements to the right of the current index.
+        /// </summary>
+        public long RightSum;
+    }
+}
