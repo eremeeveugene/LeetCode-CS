e0b53d95c34dffc32e7e3aeeb63be62b66a39e3f
Eugene <eremeeveugene@gmail.com>
2026-05-02 22:37:18 +0300

    Update ZigzagGridTraversalWithSkipTestsBase.cs

--- source/Tests/LeetCode.Tests/Algorithms/ZigzagGridTraversalWithSkip/ZigzagGridTraversalWithSkipTestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/ZigzagGridTraversalWithSkip/ZigzagGridTraversalWithSkipTestsBase.cs
@@ -10,23 +10,16 @@
 // --------------------------------------------------------------------------------
 
 using LeetCode.Algorithms.ZigzagGridTraversalWithSkip;
-using LeetCode.Core.Helpers;
 
 namespace LeetCode.Tests.Algorithms.ZigzagGridTraversalWithSkip;
 
 public abstract class ZigzagGridTraversalWithSkipTestsBase<T> where T : IZigzagGridTraversalWithSkip, new()
 {
     [TestMethod]
-    [DataRow("[[1,2],[3,4]]", "[1,4]")]
-    [DataRow("[[2,1],[2,1],[2,1]]", "[2,1,2]")]
-    [DataRow("[[1,2,3],[4,5,6],[7,8,9]]", "[1,3,5,7,9]")]
-    public void ZigzagTraversal_With2DGrid_ReturnsElementsInZigzagOrder(string gridJson,
-        string expectedResultJson)
+    [DynamicData(nameof(TestData))]
+    public void ZigzagTraversal_With2DGrid_ReturnsElementsInZigzagOrder(int[][] grid, int[] expectedResult)
     {
         // Arrange
-        var grid = JsonHelper.Parse<int[][]>(gridJson);
-        var expectedResult = JsonHelper.Parse<int[]>(expectedResultJson);
-
         var solution = new T();
 
         // Act
@@ -34,5 +27,38 @@ public abstract class ZigzagGridTraversalWithSkipTestsBase<T> where T : IZigzagG
 
         // Assert
         CollectionAssert.AreEqual(expectedResult, actualResult);
+    }
+
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return [new[] { new[] { 1, 2 }, new[] { 3, 4 } }, new[] { 1, 4 }];
+
+        yield return [new[] { new[] { 2, 1 }, new[] { 2, 1 }, new[] { 2, 1 } }, new[] { 2, 1, 2 }];
+
+        yield return [new[] { new[] { 1, 2, 3 }, new[] { 4, 5, 6 }, new[] { 7, 8, 9 } }, new[] { 1, 3, 5, 7, 9 }];
+
+        yield return [new[] { new[] { 42 } }, new[] { 42 }];
+
+        yield return [new[] { new[] { 1, 2, 3, 4, 5 } }, new[] { 1, 3, 5 }];
+
+        yield return [new[] { new[] { 1 }, new[] { 2 }, new[] { 3 }, new[] { 4 } }, new[] { 1, 3 }];
+
+        yield return [new[] { new[] { 1, 2, 3 }, new[] { 4, 5, 6 } }, new[] { 1, 3, 5 }];
+
+        yield return [new[] { new[] { 1, 2 }, new[] { 3, 4 }, new[] { 5, 6 } }, new[] { 1, 4, 5 }];
+
+        yield return [new[] { new[] { -1, -2 }, new[] { -3, -4 } }, new[] { -1, -4 }];
+
+        yield return [new[] { new[] { 0, 0, 0 }, new[] { 0, 0, 0 } }, new[] { 0, 0, 0 }];
+
+        yield return [new[] { new[] { 1, 2, 3, 4 }, new[] { 5, 6, 7, 8 }, new[] { 9, 10, 11, 12 }, new[] { 13, 14, 15, 16 } }, new[] { 1, 3, 8, 6, 9, 11, 16, 14 }];
+
+        yield return [new[] { new[] { 10, 20, 30 }, new[] { 40, 50, 60 } }, new[] { 10, 30, 50 }];
+
+        yield return [new[] { new[] { 1, 2, 3, 4, 5, 6 } }, new[] { 1, 3, 5 }];
+
+        yield return [new[] { new[] { 1, 2, 3, 4, 5, 6 }, new[] { 7, 8, 9, 10, 11, 12 } }, new[] { 1, 3, 5, 12, 10, 8 }];
+
+        yield return [new[] { new[] { 1, 2, 3, 4, 5 }, new[] { 6, 7, 8, 9, 10 } }, new[] { 1, 3, 5, 9, 7 }];
     }
 }
