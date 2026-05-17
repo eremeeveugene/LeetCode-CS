5ea7cab380cf165b8f5e172e78a985a294c172fa
Eugene <eremeeveugene@gmail.com>
2026-05-17 14:20:51 +0300

    Update KthLargestElementInAnArrayTestsBase.cs

--- source/Tests/LeetCode.Tests/Algorithms/KthLargestElementInAnArray/KthLargestElementInAnArrayTestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/KthLargestElementInAnArray/KthLargestElementInAnArrayTestsBase.cs
@@ -36,6 +36,17 @@ public abstract class KthLargestElementInAnArrayTestsBase<T> where T : IKthLarge
     [DataRow(new[] { 10000, -10000, 0, 9999, -9999 }, 5, -10000)]
     [DataRow(new[] { 4, 4, 4, 3, 3, 2, 1 }, 4, 3)]
     [DataRow(new[] { 7, 10, 4, 3, 20, 15 }, 3, 10)]
+    [DataRow(new[] { 1, 2, 3, 4, 5 }, 2, 4)]
+    [DataRow(new[] { 5, 4, 3, 2, 1 }, 2, 4)]
+    [DataRow(new[] { 4, 1, 3, 2, 5 }, 3, 3)]
+    [DataRow(new[] { 9, 8, 7, 1, 2, 3 }, 4, 3)]
+    [DataRow(new[] { 1, 2, 3, 4, 0 }, 5, 0)]
+    [DataRow(new[] { 2, 1, 2, 1, 2, 1 }, 3, 2)]
+    [DataRow(new[] { 6, 5, 5, 5, 4, 3 }, 4, 5)]
+    [DataRow(new[] { 0, -1, -1, 2, 2, 2, -3 }, 5, -1)]
+    [DataRow(new[] { -10000, -10000, -9999 }, 2, -10000)]
+    [DataRow(new[] { 9999, 10000, 10000 }, 2, 10000)]
+    [DataRow(new[] { 12, 3, 5, 7, 19, 1, 8, 15, 6, 10 }, 5, 8)]
     public void FindKthLargest_WithGivenArrayAndK_ReturnsKthLargestElement(int[] nums, int k, int expectedResult)
     {
         // Arrange
