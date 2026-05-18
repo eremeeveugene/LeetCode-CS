66efbbdce68877e369f995abc8f3bd33a5dd71c0
Eugene <eremeeveugene@gmail.com>
2026-05-18 17:07:33 +0300

    Check Adjacent Digit Differences (#1827)

---
+++ source/LeetCode/Algorithms/CheckAdjacentDigitDifferences/CheckAdjacentDigitDifferencesIterative.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CheckAdjacentDigitDifferences;
+
+/// <inheritdoc />
+public sealed class CheckAdjacentDigitDifferencesIterative : ICheckAdjacentDigitDifferences
+{
+    /// <inheritdoc />
+    /// <remarks>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </remarks>
+    public bool IsAdjacentDiffAtMostTwo(string s)
+    {
+        for (var i = 0; i < s.Length - 1; i++)
+        {
+            var difference = s[i] - s[i + 1];
+
+            if (difference is < -2 or > 2)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ source/Tests/LeetCode.Tests/Algorithms/CheckAdjacentDigitDifferences/CheckAdjacentDigitDifferencesTestsBase.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.CheckAdjacentDigitDifferences;
+
+namespace LeetCode.Tests.Algorithms.CheckAdjacentDigitDifferences;
+
+public abstract class CheckAdjacentDigitDifferencesTestsBase<T> where T : ICheckAdjacentDigitDifferences, new()
+{
+    [TestMethod]
+    [DataRow("132", true)]
+    [DataRow("129", false)]
+    [DataRow("00", true)]
+    [DataRow("99", true)]
+    [DataRow("02", true)]
+    [DataRow("20", true)]
+    [DataRow("03", false)]
+    [DataRow("30", false)]
+    [DataRow("0123456789", true)]
+    [DataRow("9876543210", true)]
+    [DataRow("02468", true)]
+    [DataRow("86420", true)]
+    [DataRow("0369", false)]
+    [DataRow("9630", false)]
+    [DataRow("111111", true)]
+    [DataRow("909", false)]
+    [DataRow("121212", true)]
+    [DataRow("123454321", true)]
+    [DataRow("13579", true)]
+    [DataRow("97531", true)]
+    public void IsAdjacentDiffAtMostTwo_WithGivenDigitString_ReturnsTrueWhenAllAdjacentDifferencesWithinLimit(string num, bool expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.IsAdjacentDiffAtMostTwo(num);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
