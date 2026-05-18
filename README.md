44ad2a4d7552ca105c1efb0b2aaf99e54832259c
Eugene <eremeeveugene@gmail.com>
2026-05-18 17:00:40 +0300

    Check Adjacent Digit Differences

--- README.md
+++ README.md
@@ -865,6 +865,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [3908. Valid Digit Number](https://leetcode.com/problems/valid-digit-number/description/)
 - [3917. Count Indices With Opposite Parity](https://leetcode.com/problems/count-indices-with-opposite-parity/description/)
 - [3925. Concatenate Array With Reverse](https://leetcode.com/problems/concatenate-array-with-reverse/description/)
+- [3931. Check Adjacent Digit Differences](https://leetcode.com/problems/check-adjacent-digit-differences/description/)
 
   </p>
 </details>
---
+++ source/LeetCode/Algorithms/CheckAdjacentDigitDifferences/ICheckAdjacentDigitDifferences.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.CheckAdjacentDigitDifferences;
+
+/// <summary>
+///     https://leetcode.com/problems/check-adjacent-digit-differences/description/
+/// </summary>
+public interface ICheckAdjacentDigitDifferences
+{
+    /// <summary>
+    ///     Checks whether the absolute difference between every pair of adjacent digits is at most two.
+    /// </summary>
+    /// <param name="s">The numeric string whose adjacent digit differences should be checked.</param>
+    /// <returns><see langword="true" /> if every adjacent digit difference is at most two; otherwise, <see langword="false" />.</returns>
+    bool IsAdjacentDiffAtMostTwo(string s);
+}
