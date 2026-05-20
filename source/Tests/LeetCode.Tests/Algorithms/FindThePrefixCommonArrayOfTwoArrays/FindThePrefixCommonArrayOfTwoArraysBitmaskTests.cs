d0979afffb5c9b748b08855efe206d0b54da728c
Eugene <eremeeveugene@gmail.com>
2026-05-20 18:30:05 +0300

    Find the Prefix Common Array of Two Arrays

--- source/Tests/LeetCode.Tests/Algorithms/FindThePrefixCommonArrayOfTwoArrays/FindThePrefixCommonArrayOfTwoArraysBitmaskTests.cs
+++ source/Tests/LeetCode.Tests/Algorithms/FindThePrefixCommonArrayOfTwoArrays/FindThePrefixCommonArrayOfTwoArraysBitmaskTests.cs
@@ -14,4 +14,4 @@ using LeetCode.Algorithms.FindThePrefixCommonArrayOfTwoArrays;
 namespace LeetCode.Tests.Algorithms.FindThePrefixCommonArrayOfTwoArrays;
 
 [TestClass]
-public sealed class FindThePrefixCommonArrayOfTwoArraysHashSetTests : FindThePrefixCommonArrayOfTwoArraysTestsBase<FindThePrefixCommonArrayOfTwoArraysHashSet>;
+public sealed class FindThePrefixCommonArrayOfTwoArraysBitmaskTests : FindThePrefixCommonArrayOfTwoArraysTestsBase<FindThePrefixCommonArrayOfTwoArraysBitmask>;
--- source/Tests/LeetCode.Tests/Algorithms/FindThePrefixCommonArrayOfTwoArrays/FindThePrefixCommonArrayOfTwoArraysTestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/FindThePrefixCommonArrayOfTwoArrays/FindThePrefixCommonArrayOfTwoArraysTestsBase.cs
@@ -10,24 +10,35 @@
 // --------------------------------------------------------------------------------
 
 using LeetCode.Algorithms.FindThePrefixCommonArrayOfTwoArrays;
-using LeetCode.Core.Helpers;
 
 namespace LeetCode.Tests.Algorithms.FindThePrefixCommonArrayOfTwoArrays;
 
-public abstract class FindThePrefixCommonArrayOfTwoArraysTestsBase<T>
-    where T : IFindThePrefixCommonArrayOfTwoArrays, new()
+public abstract class FindThePrefixCommonArrayOfTwoArraysTestsBase<T> where T : IFindThePrefixCommonArrayOfTwoArrays, new()
 {
     [TestMethod]
-    [DataRow("[2,3,1]", "[3,1,2]", "[0,1,3]")]
-    [DataRow("[1,3,2,4]", "[3,1,2,4]", "[0,2,3,4]")]
-    public void FindThePrefixCommonArray_WithTwoIntegerArrays_ReturnsPrefixCommonCounts(string aJson,
-        string bJson, string expectedResultJson)
+    [DataRow(new[] { 1 }, new[] { 1 }, new[] { 1 })]
+    [DataRow(new[] { 1, 2 }, new[] { 1, 2 }, new[] { 1, 2 })]
+    [DataRow(new[] { 1, 2 }, new[] { 2, 1 }, new[] { 0, 2 })]
+    [DataRow(new[] { 1, 3, 2, 4 }, new[] { 3, 1, 2, 4 }, new[] { 0, 2, 3, 4 })]
+    [DataRow(new[] { 2, 3, 1 }, new[] { 3, 1, 2 }, new[] { 0, 1, 3 })]
+    [DataRow(new[] { 1, 2, 3 }, new[] { 1, 2, 3 }, new[] { 1, 2, 3 })]
+    [DataRow(new[] { 1, 2, 3 }, new[] { 3, 2, 1 }, new[] { 0, 1, 3 })]
+    [DataRow(new[] { 3, 2, 1 }, new[] { 1, 2, 3 }, new[] { 0, 1, 3 })]
+    [DataRow(new[] { 1, 2, 3, 4 }, new[] { 4, 3, 2, 1 }, new[] { 0, 0, 2, 4 })]
+    [DataRow(new[] { 4, 3, 2, 1 }, new[] { 1, 2, 3, 4 }, new[] { 0, 0, 2, 4 })]
+    [DataRow(new[] { 1, 2, 3, 4, 5 }, new[] { 1, 2, 3, 4, 5 }, new[] { 1, 2, 3, 4, 5 })]
+    [DataRow(new[] { 1, 2, 3, 4, 5 }, new[] { 5, 4, 3, 2, 1 }, new[] { 0, 0, 1, 3, 5 })]
+    [DataRow(new[] { 5, 4, 3, 2, 1 }, new[] { 1, 2, 3, 4, 5 }, new[] { 0, 0, 1, 3, 5 })]
+    [DataRow(new[] { 1, 2, 3, 4, 5 }, new[] { 2, 3, 4, 5, 1 }, new[] { 0, 1, 2, 3, 5 })]
+    [DataRow(new[] { 2, 3, 4, 5, 1 }, new[] { 1, 2, 3, 4, 5 }, new[] { 0, 1, 2, 3, 5 })]
+    [DataRow(new[] { 1, 3, 5, 2, 4 }, new[] { 5, 1, 4, 3, 2 }, new[] { 0, 2, 3, 4, 5 })]
+    [DataRow(new[] { 2, 5, 1, 4, 3 }, new[] { 4, 1, 5, 3, 2 }, new[] { 0, 1, 3, 4, 5 })]
+    [DataRow(new[] { 6, 1, 2, 3, 4, 5 }, new[] { 1, 2, 3, 4, 5, 6 }, new[] { 0, 1, 2, 3, 4, 6 })]
+    [DataRow(new[] { 1, 6, 2, 5, 3, 4 }, new[] { 6, 1, 5, 2, 4, 3 }, new[] { 0, 2, 2, 4, 4, 6 })]
+    [DataRow(new[] { 4, 1, 6, 2, 5, 3 }, new[] { 1, 2, 3, 4, 5, 6 }, new[] { 0, 1, 1, 4, 5, 6 })]
+    public void FindThePrefixCommonArray_WithTwoIntegerArrays_ReturnsPrefixCommonCounts(int[] a, int[] b, int[] expectedResult)
     {
         // Arrange
-        var a = JsonHelper.Parse<int[]>(aJson);
-        var b = JsonHelper.Parse<int[]>(bJson);
-        var expectedResult = JsonHelper.Parse<int[]>(expectedResultJson);
-
         var solution = new T();
 
         // Act
