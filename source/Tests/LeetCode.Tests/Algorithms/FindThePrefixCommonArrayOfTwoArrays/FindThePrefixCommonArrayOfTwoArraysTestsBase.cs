99f817f5d6ad5b3195f94358a6136e32cb4a2f76
Eugene <eremeeveugene@gmail.com>
2026-05-20 18:45:12 +0300

    Update FindThePrefixCommonArrayOfTwoArraysTestsBase.cs

--- source/Tests/LeetCode.Tests/Algorithms/FindThePrefixCommonArrayOfTwoArrays/FindThePrefixCommonArrayOfTwoArraysTestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/FindThePrefixCommonArrayOfTwoArrays/FindThePrefixCommonArrayOfTwoArraysTestsBase.cs
@@ -31,11 +31,11 @@ public abstract class FindThePrefixCommonArrayOfTwoArraysTestsBase<T> where T : 
     [DataRow(new[] { 5, 4, 3, 2, 1 }, new[] { 1, 2, 3, 4, 5 }, new[] { 0, 0, 1, 3, 5 })]
     [DataRow(new[] { 1, 2, 3, 4, 5 }, new[] { 2, 3, 4, 5, 1 }, new[] { 0, 1, 2, 3, 5 })]
     [DataRow(new[] { 2, 3, 4, 5, 1 }, new[] { 1, 2, 3, 4, 5 }, new[] { 0, 1, 2, 3, 5 })]
-    [DataRow(new[] { 1, 3, 5, 2, 4 }, new[] { 5, 1, 4, 3, 2 }, new[] { 0, 2, 3, 4, 5 })]
-    [DataRow(new[] { 2, 5, 1, 4, 3 }, new[] { 4, 1, 5, 3, 2 }, new[] { 0, 1, 3, 4, 5 })]
+    [DataRow(new[] { 1, 3, 5, 2, 4 }, new[] { 5, 1, 4, 3, 2 }, new[] { 0, 1, 2, 3, 5 })]
+    [DataRow(new[] { 2, 5, 1, 4, 3 }, new[] { 4, 1, 5, 3, 2 }, new[] { 0, 0, 2, 3, 5 })]
     [DataRow(new[] { 6, 1, 2, 3, 4, 5 }, new[] { 1, 2, 3, 4, 5, 6 }, new[] { 0, 1, 2, 3, 4, 6 })]
     [DataRow(new[] { 1, 6, 2, 5, 3, 4 }, new[] { 6, 1, 5, 2, 4, 3 }, new[] { 0, 2, 2, 4, 4, 6 })]
-    [DataRow(new[] { 4, 1, 6, 2, 5, 3 }, new[] { 1, 2, 3, 4, 5, 6 }, new[] { 0, 1, 1, 4, 5, 6 })]
+    [DataRow(new[] { 4, 1, 6, 2, 5, 3 }, new[] { 1, 2, 3, 4, 5, 6 }, new[] { 0, 1, 1, 3, 4, 6 })]
     public void FindThePrefixCommonArray_WithTwoIntegerArrays_ReturnsPrefixCommonCounts(int[] a, int[] b, int[] expectedResult)
     {
         // Arrange
