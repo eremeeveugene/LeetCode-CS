73d434b527997b574430c0a8cf90ed069b038b63
Eugene <eremeeveugene@gmail.com>
2026-06-03 17:02:22 +0300

    Digit Frequency Score

---
+++ source/LeetCode/Algorithms/DigitFrequencyScore/DigitFrequencyScoreCounting.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.DigitFrequencyScore;
+
+/// <inheritdoc />
+public sealed class DigitFrequencyScoreCounting : IDigitFrequencyScore
+{
+    /// <inheritdoc />
+    /// <remarks>
+    ///     Time complexity - O(log n)
+    ///     Space complexity - O(1)
+    /// </remarks>
+    public int DigitFrequencyScore(int n)
+    {
+        Span<byte> digitsFrequency = stackalloc byte[10];
+
+        while (n > 0)
+        {
+            var digit = n % 10;
+
+            digitsFrequency[digit]++;
+
+            n /= 10;
+        }
+
+        var result = 0;
+
+        for (var digit = 1; digit < 10; digit++)
+        {
+            var digitFrequency = digitsFrequency[digit];
+
+            result += digit * digitFrequency;
+        }
+
+        return result;
+    }
+}
---
+++ source/LeetCode/Algorithms/DigitFrequencyScore/IDigitFrequencyScore.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.DigitFrequencyScore;
+
+/// <summary>
+///     https://leetcode.com/problems/digit-frequency-score/description/
+/// </summary>
+public interface IDigitFrequencyScore
+{
+    /// <summary>
+    ///     Computes the digit frequency score of an integer, defined as the sum of <c>d * freq(d)</c> over all distinct digits.
+    /// </summary>
+    /// <param name="n">The positive integer whose digit frequency score should be computed.</param>
+    /// <returns>The digit frequency score of <paramref name="n" />.</returns>
+    int DigitFrequencyScore(int n);
+}
