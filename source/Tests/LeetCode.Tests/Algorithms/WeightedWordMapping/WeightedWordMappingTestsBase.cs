1b100278e2c49ce909e6334512fef381f98949db
Eugene <eremeeveugene@gmail.com>
2026-06-13 13:10:07 +0300

    Weighted Word Mapping

---
+++ source/Tests/LeetCode.Tests/Algorithms/WeightedWordMapping/WeightedWordMappingTestsBase.cs
@@ -0,0 +1,54 @@
+// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using LeetCode.Algorithms.WeightedWordMapping;
+
+namespace LeetCode.Tests.Algorithms.WeightedWordMapping;
+
+public abstract class WeightedWordMappingTestsBase<T> where T : IWeightedWordMapping, new()
+{
+    [TestMethod]
+    [DataRow(new[] { "abcd", "def", "xyz" }, new[] { 5, 3, 12, 14, 1, 2, 3, 2, 10, 6, 6, 9, 7, 8, 7, 10, 8, 9, 6, 9, 9, 8, 3, 7, 7, 2 }, "rij")]
+    [DataRow(new[] { "a", "b", "c" }, new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, "yyy")]
+    [DataRow(new[] { "abcd" }, new[] { 7, 5, 3, 4, 3, 5, 4, 9, 4, 2, 2, 7, 10, 2, 5, 10, 6, 1, 2, 2, 4, 1, 3, 4, 4, 5 }, "g")]
+    [DataRow(new[] { "z" }, new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, "y")]
+    [DataRow(new[] { "zzzzzzzzzz" }, new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, "p")]
+    [DataRow(new[] { "abcdefghij" }, new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, "p")]
+    [DataRow(new[] { "a" }, new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 }, "y")]
+    [DataRow(new[] { "z" }, new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 }, "z")]
+    [DataRow(new[] { "a" }, new[] { 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }, "z")]
+    [DataRow(new[] { "z" }, new[] { 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 }, "y")]
+    [DataRow(new[] { "abc", "xyz" }, new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 }, "tc")]
+    [DataRow(new[] { "hello", "world" }, new[] { 5, 3, 12, 14, 1, 2, 3, 2, 10, 6, 6, 9, 7, 8, 7, 10, 8, 9, 6, 9, 9, 8, 3, 7, 7, 2 }, "xj")]
+    [DataRow(new[] { "a" }, new[] { 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 }, "d")]
+    [DataRow(new[] { "aa" }, new[] { 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 }, "h")]
+    [DataRow(new[] { "abcdefghij" }, new[] { 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 }, "n")]
+    [DataRow(new[] { "leetcode" }, new[] { 7, 5, 3, 4, 3, 5, 4, 9, 4, 2, 2, 7, 10, 2, 5, 10, 6, 1, 2, 2, 4, 1, 3, 4, 4, 5 }, "v")]
+    [DataRow(new[] { "zzz", "aaa" }, new[] { 5, 3, 12, 14, 1, 2, 3, 2, 10, 6, 6, 9, 7, 8, 7, 10, 8, 9, 6, 9, 9, 8, 3, 7, 7, 2 }, "tk")]
+    [DataRow(new[] { "m" }, new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 }, "m")]
+    [DataRow(new[] { "abcdefghijklmnopqrstuvwxyz" }, new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, "z")]
+    [DataRow(new[] { "abc" }, new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, "w")]
+    [DataRow(new[] { "aaaaa", "bbbbb", "ccccc" }, new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 }, "upk")]
+    [DataRow(new[] { "q", "w", "e", "r", "t", "y" }, new[] { 5, 3, 12, 14, 1, 2, 3, 2, 10, 6, 6, 9, 7, 8, 7, 10, 8, 9, 6, 9, 9, 8, 3, 7, 7, 2 }, "rwyqqs")]
+    [DataRow(new[] { "python", "java" }, new[] { 7, 5, 3, 4, 3, 5, 4, 9, 4, 2, 2, 7, 10, 2, 5, 10, 6, 1, 2, 2, 4, 1, 3, 4, 4, 5 }, "ti")]
+    [DataRow(new[] { "a", "aa", "aaa", "aaaa" }, new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26 }, "yxwv")]
+    public void MapWordWeights_WithWordsAndLetterWeights_ReturnsMappedString(string[] words, int[] weights, string expectedResult)
+    {
+        // Arrange
+        var solution = new T();
+
+        // Act
+        var actualResult = solution.MapWordWeights(words, weights);
+
+        // Assert
+        Assert.AreEqual(expectedResult, actualResult);
+    }
+}
