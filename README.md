cf607277bc97e3d25b72507c7c7ee536e2102d12
Eugene <eremeeveugene@gmail.com>
2026-06-20 21:07:21 +0300

    Modify the Matrix (#1872)

--- README.md
+++ README.md
@@ -742,6 +742,7 @@ Below are the LeetCode problems sorted by category. Click on the category names 
 - [3024. Type of Triangle](https://leetcode.com/problems/type-of-triangle/description/)
 - [3025. Find the Number of Ways to Place People I](https://leetcode.com/problems/find-the-number-of-ways-to-place-people-i/description/)
 - [3027. Find the Number of Ways to Place People II](https://leetcode.com/problems/find-the-number-of-ways-to-place-people-ii/description/)
+- [3033. Modify the Matrix](https://leetcode.com/problems/modify-the-matrix/description/)
 - [3042. Count Prefix and Suffix Pairs I](https://leetcode.com/problems/count-prefix-and-suffix-pairs-i/description/)
 - [3043. Find the Length of the Longest Common Prefix](https://leetcode.com/problems/find-the-length-of-the-longest-common-prefix/description/)
 - [3046. Split the Array](https://leetcode.com/problems/split-the-array/description/)
---
+++ source/LeetCode/Algorithms/ModifyTheMatrix/ModifyTheMatrixIterative.cs
@@ -0,0 +1,48 @@
+// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.ModifyTheMatrix;
+
+/// <inheritdoc />
+public sealed class ModifyTheMatrixIterative : IModifyTheMatrix
+{
+    /// <summary>
+    ///     Time complexity - O(m * n)
+    ///     Space complexity - O(m * n)
+    /// </summary>
+    /// <param name="matrix"></param>
+    /// <returns></returns>
+    public int[][] ModifiedMatrix(int[][] matrix)
+    {
+        var m = matrix.Length;
+        var n = matrix[0].Length;
+
+        for (var j = 0; j < n; j++)
+        {
+            var max = 0;
+
+            for (var i = 0; i < m; i++)
+            {
+                max = Math.Max(max, matrix[i][j]);
+            }
+
+            for (var i = 0; i < m; i++)
+            {
+                if (matrix[i][j] == -1)
+                {
+                    matrix[i][j] = max;
+                }
+            }
+        }
+
+        return matrix;
+    }
+}
