49f63bac531f75af16002ca67fe9927ef0749bde
Eugene <eremeeveugene@gmail.com>
2026-07-11 17:24:04 +0300

    Update ConcatenateNonZeroDigitsAndMultiplyBySum2TestsBase.cs

--- source/Tests/LeetCode.Tests/Algorithms/ConcatenateNonZeroDigitsAndMultiplyBySum2/ConcatenateNonZeroDigitsAndMultiplyBySum2TestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/ConcatenateNonZeroDigitsAndMultiplyBySum2/ConcatenateNonZeroDigitsAndMultiplyBySum2TestsBase.cs
@@ -33,50 +33,54 @@ public abstract class ConcatenateNonZeroDigitsAndMultiplyBySum2TestsBase<T> wher
     {
         yield return ["10203004", new[] { new[] { 0, 7 }, new[] { 1, 3 }, new[] { 4, 6 } }, new[] { 12340, 4, 9 }];
 
-        //yield return ["1000", new[] { new[] { 0, 3 }, new[] { 1, 1 } }, new[] { 1, 0 }];
+        yield return ["1000", new[] { new[] { 0, 3 }, new[] { 1, 1 } }, new[] { 1, 0 }];
 
-        //yield return ["9876543210", new[] { new[] { 0, 9 } }, new[] { 444444137 }];
+        yield return ["9876543210", new[] { new[] { 0, 9 } }, new[] { 444444137 }];
 
-        //yield return ["0", new[] { new[] { 0, 0 } }, new[] { 0 }];
+        yield return ["0", new[] { new[] { 0, 0 } }, new[] { 0 }];
 
-        //yield return ["5", new[] { new[] { 0, 0 } }, new[] { 25 }];
+        yield return ["5", new[] { new[] { 0, 0 } }, new[] { 25 }];
 
-        //yield return ["9", new[] { new[] { 0, 0 } }, new[] { 81 }];
+        yield return ["9", new[] { new[] { 0, 0 } }, new[] { 81 }];
 
-        //yield return ["12", new[] { new[] { 0, 1 } }, new[] { 36 }];
+        yield return ["12", new[] { new[] { 0, 1 } }, new[] { 36 }];
 
-        //yield return ["12", new[] { new[] { 0, 0 }, new[] { 1, 1 } }, new[] { 1, 4 }];
+        yield return ["12", new[] { new[] { 0, 0 }, new[] { 1, 1 } }, new[] { 1, 4 }];
 
-        //yield return ["000", new[] { new[] { 0, 2 } }, new[] { 0 }];
+        yield return ["000", new[] { new[] { 0, 2 } }, new[] { 0 }];
 
-        //yield return ["102", new[] { new[] { 0, 2 }, new[] { 0, 1 }, new[] { 1, 2 } }, new[] { 36, 1, 4 }];
+        yield return ["102", new[] { new[] { 0, 2 }, new[] { 0, 1 }, new[] { 1, 2 } }, new[] { 36, 1, 4 }];
 
-        //yield return ["111", new[] { new[] { 0, 2 } }, new[] { 333 }];
+        yield return ["111", new[] { new[] { 0, 2 } }, new[] { 333 }];
 
-        //yield return ["90", new[] { new[] { 0, 1 }, new[] { 1, 1 } }, new[] { 81, 0 }];
+        yield return ["90", new[] { new[] { 0, 1 }, new[] { 1, 1 } }, new[] { 81, 0 }];
 
-        //yield return ["123456789", new[] { new[] { 0, 8 } }, new[] { 555555470 }];
+        yield return ["123456789", new[] { new[] { 0, 8 } }, new[] { 555555470 }];
 
-        //yield return ["10", new[] { new[] { 0, 0 }, new[] { 0, 1 }, new[] { 1, 1 } }, new[] { 1, 1, 0 }];
+        yield return ["10", new[] { new[] { 0, 0 }, new[] { 0, 1 }, new[] { 1, 1 } }, new[] { 1, 1, 0 }];
 
-        //yield return ["404", new[] { new[] { 0, 2 } }, new[] { 352 }];
+        yield return ["404", new[] { new[] { 0, 2 } }, new[] { 352 }];
 
-        //yield return ["007", new[] { new[] { 0, 2 }, new[] { 0, 1 } }, new[] { 49, 0 }];
+        yield return ["007", new[] { new[] { 0, 2 }, new[] { 0, 1 } }, new[] { 49, 0 }];
 
-        //yield return ["2222", new[] { new[] { 0, 3 }, new[] { 1, 2 } }, new[] { 17776, 88 }];
+        yield return ["2222", new[] { new[] { 0, 3 }, new[] { 1, 2 } }, new[] { 17776, 88 }];
 
-        //yield return ["505050", new[] { new[] { 0, 5 } }, new[] { 8325 }];
+        yield return ["505050", new[] { new[] { 0, 5 } }, new[] { 8325 }];
 
-        //yield return ["31415926", new[] { new[] { 2, 5 } }, new[] { 79021 }];
+        yield return ["31415926", new[] { new[] { 2, 5 } }, new[] { 79021 }];
 
-        //yield return ["606", new[] { new[] { 1, 1 } }, new[] { 0 }];
+        yield return ["606", new[] { new[] { 1, 1 } }, new[] { 0 }];
 
-        //yield return ["1203", new[] { new[] { 0, 3 }, new[] { 2, 3 }, new[] { 0, 1 }, new[] { 1, 2 } }, new[] { 738, 9, 36, 4 }];
+        yield return ["1203", new[] { new[] { 0, 3 }, new[] { 2, 3 }, new[] { 0, 1 }, new[] { 1, 2 } }, new[] { 738, 9, 36, 4 }];
 
-        //yield return ["99999", new[] { new[] { 0, 4 } }, new[] { 4499955 }];
+        yield return ["99999", new[] { new[] { 0, 4 } }, new[] { 4499955 }];
 
-        //yield return ["8", new[] { new[] { 0, 0 }, new[] { 0, 0 } }, new[] { 64, 64 }];
+        yield return ["8", new[] { new[] { 0, 0 }, new[] { 0, 0 } }, new[] { 64, 64 }];
 
-        //yield return ["1010101", new[] { new[] { 0, 6 }, new[] { 2, 4 } }, new[] { 4444, 22 }];
+        yield return ["1010101", new[] { new[] { 0, 6 }, new[] { 2, 4 } }, new[] { 4444, 22 }];
+
+        yield return ["1234567891", new[] { new[] { 1, 9 } }, new[] { 555555025 }];
+
+        yield return ["9999999999", new[] { new[] { 5, 9 } }, new[] { 4499955 }];
     }
 }
