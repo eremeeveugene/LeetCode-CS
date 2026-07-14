7c735db067380e4eecf7fcccceab3a39807c05b6
Eugene <eremeeveugene@gmail.com>
2026-07-14 16:03:35 +0300

    Changes

--- source/Tests/LeetCode.Tests/Algorithms/ImplementTrie/ImplementTrieTestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/ImplementTrie/ImplementTrieTestsBase.cs
@@ -10,48 +10,213 @@
 // --------------------------------------------------------------------------------
 
 using LeetCode.Algorithms.ImplementTrie;
-using LeetCode.Tests.Base.Exceptions;
+using LeetCode.Tests.Base.Scenarios;
 
 namespace LeetCode.Tests.Algorithms.ImplementTrie;
 
 public abstract class ImplementTrieTestsBase<T> where T : IImplementTrie, new()
 {
-    private const string Insert = "insert";
-    private const string Search = "search";
-    private const string StartsWith = "startsWith";
-
     [TestMethod]
-    [DataRow(
-        new[] { "insert", "search", "search", "startsWith", "insert", "search" },
-        new[] { "apple", "apple", "app", "app", "app", "app" },
-        new[] { true, false, true, true })]
-    public void TrieOperations_WithMultipleCommands_ReturnsActionResults(string[] methods, string[] args, bool[] expectedResult)
+    [DynamicData(nameof(GetScenarios))]
+    public void ImplementTrie_WithMixedOperations_ProcessesOperationsAccordingToSpecification(IScenario<IImplementTrie> scenario)
     {
         // Arrange
+        var expectedResult = scenario.OperationResults;
+
         var solution = new T();
 
         // Act
-        var actualResult = new List<bool>();
+        var operations = scenario.Operations;
+        var operationsLength = operations.Length;
 
-        for (var i = 0; i < methods.Length; i++)
+        var actualResult = new IOperationResult[operationsLength];
+
+        for (var i = 0; i < operationsLength; i++)
         {
-            switch (methods[i])
-            {
-                case Insert:
-                    solution.Insert(args[i]);
-                    break;
-                case Search:
-                    actualResult.Add(solution.Search(args[i]));
-                    break;
-                case StartsWith:
-                    actualResult.Add(solution.StartsWith(args[i]));
-                    break;
-                default:
-                    throw new UnexpectedMethodException(methods[i]);
-            }
+            var operation = operations[i];
+
+            actualResult[i] = operation.Execute(solution);
         }
 
         // Assert
         Assert.AreSequenceEqual(expectedResult, actualResult);
+    }
+
+    private static IEnumerable<IScenario<IImplementTrie>[]> GetScenarios()
+    {
+        yield return
+        [
+            new Scenario<IImplementTrie>(
+                [
+                    new InsertOperation("apple"),
+                    new SearchOperation("apple"),
+                    new SearchOperation("app"),
+                    new StartsWithOperation("app"),
+                    new InsertOperation("app"),
+                    new SearchOperation("app")
+                ],
+                [
+                    VoidOperationResult.Instance,
+                    new SearchOperation.Result(true),
+                    new SearchOperation.Result(false),
+                    new StartsWithOperation.Result(true),
+                    VoidOperationResult.Instance,
+                    new SearchOperation.Result(true)
+                ])
+        ];
+
+        yield return
+        [
+            new Scenario<IImplementTrie>(
+                [new SearchOperation("a"), new StartsWithOperation("a")],
+                [new SearchOperation.Result(false), new StartsWithOperation.Result(false)])
+        ];
+
+        yield return
+        [
+            new Scenario<IImplementTrie>(
+                [
+                    new InsertOperation("a"),
+                    new SearchOperation("a"),
+                    new StartsWithOperation("a"),
+                    new SearchOperation("b"),
+                    new StartsWithOperation("b")
+                ],
+                [
+                    VoidOperationResult.Instance,
+                    new SearchOperation.Result(true),
+                    new StartsWithOperation.Result(true),
+                    new SearchOperation.Result(false),
+                    new StartsWithOperation.Result(false)
+                ])
+        ];
+
+        yield return
+        [
+            new Scenario<IImplementTrie>(
+                [
+                    new InsertOperation("abc"),
+                    new SearchOperation("ab"),
+                    new StartsWithOperation("ab"),
+                    new SearchOperation("abcd"),
+                    new StartsWithOperation("abcd"),
+                    new InsertOperation("ab"),
+                    new SearchOperation("ab")
+                ],
+                [
+                    VoidOperationResult.Instance,
+                    new SearchOperation.Result(false),
+                    new StartsWithOperation.Result(true),
+                    new SearchOperation.Result(false),
+                    new StartsWithOperation.Result(false),
+                    VoidOperationResult.Instance,
+                    new SearchOperation.Result(true)
+                ])
+        ];
+    }
+
+    private sealed class InsertOperation : IOperation<IImplementTrie>
+    {
+        private readonly string _word;
+
+        public InsertOperation(string word)
+        {
+            _word = word;
+        }
+
+        public IOperationResult Execute(IImplementTrie implementTrie)
+        {
+            implementTrie.Insert(_word);
+
+            return VoidOperationResult.Instance;
+        }
+    }
+
+    private sealed class SearchOperation : IOperation<IImplementTrie>
+    {
+        private readonly string _word;
+
+        public SearchOperation(string word)
+        {
+            _word = word;
+        }
+
+        public IOperationResult Execute(IImplementTrie implementTrie)
+        {
+            var found = implementTrie.Search(_word);
+
+            return new Result(found);
+        }
+
+        public sealed class Result
+            : IOperationResult,
+                IEquatable<Result>
+        {
+            private readonly bool _found;
+
+            public Result(bool found)
+            {
+                _found = found;
+            }
+
+            public bool Equals(Result? other)
+            {
+                return other is not null && _found == other._found;
+            }
+
+            public override bool Equals(object? obj)
+            {
+                return obj is Result other && Equals(other);
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(_found);
+            }
+        }
+    }
+
+    private sealed class StartsWithOperation : IOperation<IImplementTrie>
+    {
+        private readonly string _prefix;
+
+        public StartsWithOperation(string prefix)
+        {
+            _prefix = prefix;
+        }
+
+        public IOperationResult Execute(IImplementTrie implementTrie)
+        {
+            var found = implementTrie.StartsWith(_prefix);
+
+            return new Result(found);
+        }
+
+        public sealed class Result
+            : IOperationResult,
+                IEquatable<Result>
+        {
+            private readonly bool _found;
+
+            public Result(bool found)
+            {
+                _found = found;
+            }
+
+            public bool Equals(Result? other)
+            {
+                return other is not null && _found == other._found;
+            }
+
+            public override bool Equals(object? obj)
+            {
+                return obj is Result other && Equals(other);
+            }
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(_found);
+            }
+        }
     }
 }
