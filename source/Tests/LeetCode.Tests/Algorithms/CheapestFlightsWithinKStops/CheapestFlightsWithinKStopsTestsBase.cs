f1a7434c0730ef8dd5857f47c7432f761f2dc93e
Eugene <eremeeveugene@gmail.com>
2026-07-28 19:52:50 +0300

    Update CheapestFlightsWithinKStopsTestsBase.cs

--- source/Tests/LeetCode.Tests/Algorithms/CheapestFlightsWithinKStops/CheapestFlightsWithinKStopsTestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/CheapestFlightsWithinKStops/CheapestFlightsWithinKStopsTestsBase.cs
@@ -47,5 +47,41 @@ public abstract class CheapestFlightsWithinKStopsTestsBase<T> where T : ICheapes
         yield return [3, new[] { new[] { 0, 1, 100 }, new[] { 1, 2, 100 }, new[] { 0, 2, 500 } }, 0, 2, 0, 500];
 
         yield return [3, new[] { new[] { 0, 1, 2 }, new[] { 1, 2, 1 }, new[] { 2, 0, 10 } }, 1, 2, 1, 1];
+
+        yield return [3, new[] { new[] { 0, 1, 100 } }, 0, 2, 1, -1];
+
+        yield return [2, new[] { new[] { 0, 1, 50 } }, 0, 1, 0, 50];
+
+        yield return [2, new[] { new[] { 0, 1, 50 } }, 1, 0, 0, -1];
+
+        yield return [2, new[] { new[] { 0, 1, 1 } }, 0, 1, 5, 1];
+
+        yield return [3, new[] { new[] { 0, 1, 100 }, new[] { 1, 2, 100 }, new[] { 0, 2, 500 } }, 0, 2, 2, 200];
+
+        yield return [4, new[] { new[] { 0, 1, 1 }, new[] { 1, 2, 1 }, new[] { 2, 3, 1 } }, 0, 3, 2, 3];
+
+        yield return [4, new[] { new[] { 0, 1, 1 }, new[] { 1, 2, 1 }, new[] { 2, 3, 1 } }, 0, 3, 1, -1];
+
+        yield return [5, new[] { new[] { 0, 1, 10 }, new[] { 1, 4, 10 }, new[] { 0, 2, 3 }, new[] { 2, 3, 4 }, new[] { 3, 4, 5 } }, 0, 4, 3, 12];
+
+        yield return [5, new[] { new[] { 0, 1, 10 }, new[] { 1, 4, 10 }, new[] { 0, 2, 3 }, new[] { 2, 3, 4 }, new[] { 3, 4, 5 } }, 0, 4, 1, 20];
+
+        yield return [3, new[] { new[] { 0, 1, 5 }, new[] { 1, 2, 5 } }, 0, 2, 0, -1];
+
+        yield return [3, new[] { new[] { 0, 1, 5 }, new[] { 1, 2, 5 } }, 0, 2, 1, 10];
+
+        yield return [2, new[] { new[] { 0, 1, 100 }, new[] { 0, 1, 50 } }, 0, 1, 0, 50];
+
+        yield return [4, new[] { new[] { 0, 1, 1 }, new[] { 0, 2, 5 }, new[] { 1, 2, 1 }, new[] { 2, 3, 1 } }, 0, 3, 2, 3];
+
+        yield return [4, new[] { new[] { 0, 1, 1 }, new[] { 0, 2, 5 }, new[] { 1, 2, 1 }, new[] { 2, 3, 1 } }, 0, 3, 1, 6];
+
+        yield return [3, new[] { new[] { 0, 1, 100 }, new[] { 1, 2, 100 } }, 2, 0, 5, -1];
+
+        yield return [4, new[] { new[] { 0, 1, 10 }, new[] { 1, 2, 10 }, new[] { 2, 3, 10 }, new[] { 0, 3, 40 } }, 0, 3, 0, 40];
+
+        yield return [4, new[] { new[] { 0, 1, 10 }, new[] { 1, 2, 10 }, new[] { 2, 3, 10 }, new[] { 0, 3, 40 } }, 0, 3, 3, 30];
+
+        yield return [5, new[] { new[] { 0, 1, 5 }, new[] { 1, 2, 5 }, new[] { 2, 3, 5 }, new[] { 3, 4, 5 } }, 0, 4, 10, 20];
     }
 }
