0d4f112dfd09d422de7c057fe50c3af102379b06
Eugene <eremeeveugene@gmail.com>
2026-07-29 19:39:13 +0300

    Update CountOfSubstringsContainingEveryVowelAndKConsonants2TestsBase.cs

--- source/Tests/LeetCode.Tests/Algorithms/CountOfSubstringsContainingEveryVowelAndKConsonants2/CountOfSubstringsContainingEveryVowelAndKConsonants2TestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/CountOfSubstringsContainingEveryVowelAndKConsonants2/CountOfSubstringsContainingEveryVowelAndKConsonants2TestsBase.cs
@@ -21,6 +21,23 @@ public abstract class CountOfSubstringsContainingEveryVowelAndKConsonants2TestsB
     [DataRow("aeiou", 0, 1)]
     [DataRow("ieaouqqieaouqq", 1, 3)]
     [DataRow("iqeaouqi", 2, 3)]
+    [DataRow("aaeiou", 0, 2)]
+    [DataRow("aaeeiioouu", 0, 4)]
+    [DataRow("aaeeiioouu", 1, 0)]
+    [DataRow("aeioaeiou", 0, 5)]
+    [DataRow("aeioub", 0, 1)]
+    [DataRow("aeioub", 1, 1)]
+    [DataRow("aeiou", 1, 0)]
+    [DataRow("aeiou", 2, 0)]
+    [DataRow("uoiea", 0, 1)]
+    [DataRow("iuoea", 0, 1)]
+    [DataRow("iuoea", 1, 0)]
+    [DataRow("baeiou", 0, 1)]
+    [DataRow("baeiou", 1, 1)]
+    [DataRow("aeioubc", 0, 1)]
+    [DataRow("aeioubc", 1, 1)]
+    [DataRow("aeioubc", 2, 1)]
+    [DataRow("qqqqq", 1, 0)]
     public void CountOfSubstrings_WithWordAndK_ReturnsSubstringCount(string word, int k, int expectedResult)
     {
         // Arrange
