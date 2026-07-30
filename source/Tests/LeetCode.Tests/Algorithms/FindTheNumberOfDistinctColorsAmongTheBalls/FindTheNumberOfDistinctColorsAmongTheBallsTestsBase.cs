b5179eddf33575d96a96e2cdb86d7c3fbd531dde
Eugene <eremeeveugene@gmail.com>
2026-07-31 00:16:11 +0300

    Update FindTheNumberOfDistinctColorsAmongTheBallsTestsBase.cs

--- source/Tests/LeetCode.Tests/Algorithms/FindTheNumberOfDistinctColorsAmongTheBalls/FindTheNumberOfDistinctColorsAmongTheBallsTestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/FindTheNumberOfDistinctColorsAmongTheBalls/FindTheNumberOfDistinctColorsAmongTheBallsTestsBase.cs
@@ -34,5 +34,7 @@ public abstract class FindTheNumberOfDistinctColorsAmongTheBallsTestsBase<T> whe
         yield return [4, new[] { new[] { 1, 4 }, new[] { 2, 5 }, new[] { 1, 3 }, new[] { 3, 4 } }, new[] { 1, 2, 2, 3 }];
 
         yield return [4, new[] { new[] { 0, 1 }, new[] { 1, 2 }, new[] { 2, 2 }, new[] { 3, 4 }, new[] { 4, 5 } }, new[] { 1, 2, 2, 3, 4 }];
+
+        yield return [4, new[] { new[] { 1, 4 }, new[] { 2, 4 }, new[] { 1, 3 } }, new[] { 1, 1, 2 }];
     }
 }
