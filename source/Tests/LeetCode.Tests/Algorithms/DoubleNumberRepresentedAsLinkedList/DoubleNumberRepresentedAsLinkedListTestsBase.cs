da5b79e0bdf6018686706b62525dcd63a83391ea
Eugene <eremeeveugene@gmail.com>
2026-07-31 21:57:39 +0300

    Update DoubleNumberRepresentedAsLinkedListTestsBase.cs

--- source/Tests/LeetCode.Tests/Algorithms/DoubleNumberRepresentedAsLinkedList/DoubleNumberRepresentedAsLinkedListTestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/DoubleNumberRepresentedAsLinkedList/DoubleNumberRepresentedAsLinkedListTestsBase.cs
@@ -18,6 +18,7 @@ namespace LeetCode.Tests.Algorithms.DoubleNumberRepresentedAsLinkedList;
 public abstract class DoubleNumberRepresentedAsLinkedListTestsBase<T> where T : IDoubleNumberRepresentedAsLinkedList, new()
 {
     [TestMethod]
+    [DataRow(new int[] { }, new int[] { })]
     [DataRow(new[] { 1, 8, 9 }, new[] { 3, 7, 8 })]
     [DataRow(new[] { 9, 9, 9 }, new[] { 1, 9, 9, 8 })]
     public void ListNode_DoubleIt_GivenListNode_ReturnsDoubledValuesListNode(int[] headArray, int[] expectedResultArray)
