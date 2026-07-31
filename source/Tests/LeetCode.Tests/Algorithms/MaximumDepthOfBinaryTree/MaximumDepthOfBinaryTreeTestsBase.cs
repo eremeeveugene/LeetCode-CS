388029714a8f51d3ad6dcc15f11c17110d020cf4
Eugene <eremeeveugene@gmail.com>
2026-07-31 21:18:15 +0300

    Update MaximumDepthOfBinaryTreeTestsBase.cs

--- source/Tests/LeetCode.Tests/Algorithms/MaximumDepthOfBinaryTree/MaximumDepthOfBinaryTreeTestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/MaximumDepthOfBinaryTree/MaximumDepthOfBinaryTreeTestsBase.cs
@@ -39,5 +39,7 @@ public abstract class MaximumDepthOfBinaryTreeTestsBase<T> where T : IMaximumDep
         yield return [new int?[] { 3, 9, 20, null, null, 15, 7 }, 3];
 
         yield return [new int?[] { 1, null, 2 }, 2];
+
+        yield return [new int?[] { 2, 1 }, 2];
     }
 }
