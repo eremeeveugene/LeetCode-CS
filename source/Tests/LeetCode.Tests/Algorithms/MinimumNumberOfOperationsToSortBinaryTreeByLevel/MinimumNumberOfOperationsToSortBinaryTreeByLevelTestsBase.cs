58bd61b88deb0adc2c61cc0bc875d1e9e4c9d166
Eugene <eremeeveugene@gmail.com>
2026-07-31 20:45:51 +0300

    Update MinimumNumberOfOperationsToSortBinaryTreeByLevelTestsBase.cs

--- source/Tests/LeetCode.Tests/Algorithms/MinimumNumberOfOperationsToSortBinaryTreeByLevel/MinimumNumberOfOperationsToSortBinaryTreeByLevelTestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/MinimumNumberOfOperationsToSortBinaryTreeByLevel/MinimumNumberOfOperationsToSortBinaryTreeByLevelTestsBase.cs
@@ -34,6 +34,8 @@ public abstract class MinimumNumberOfOperationsToSortBinaryTreeByLevelTestsBase<
 
     private static IEnumerable<object[]> GetTestData()
     {
+        yield return [new int?[] { 1 }, 0];
+
         yield return [new int?[] { 1, 2, 3, 4, 5, 6 }, 0];
 
         yield return [new int?[] { 1, 3, 2, 7, 6, 5, 4 }, 3];
