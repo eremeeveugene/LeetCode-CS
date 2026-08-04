a9bc7e9a416ffab1bea9c24d15092e9e125f5a09
Eugene <eremeeveugene@gmail.com>
2026-08-04 18:20:59 +0300

    Find Missing Elements

---
+++ source/LeetCode/Algorithms/FindMissingElements/FindMissingElementsLookup.cs
@@ -0,0 +1,57 @@
+// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace LeetCode.Algorithms.FindMissingElements;
+
+/// <inheritdoc />
+public sealed class FindMissingElementsLookup : IFindMissingElements
+{
+    private const int MaxValue = 100;
+
+    /// <inheritdoc />
+    /// <remarks>
+    ///     Time complexity - O(n)
+    ///     Space complexity - O(1)
+    /// </remarks>
+    public IList<int> FindMissingElements(int[] nums)
+    {
+        var n = nums.Length;
+
+        Span<bool> seen = stackalloc bool[MaxValue + 1];
+
+        var min = int.MaxValue;
+        var max = int.MinValue;
+
+        for (var i = 0; i < n; i++)
+        {
+            var num = nums[i];
+
+            seen[num] = true;
+
+            min = Math.Min(min, num);
+            max = Math.Max(max, num);
+        }
+
+        var missingElements = new List<int>();
+
+        for (var i = min + 1; i < max; i++)
+        {
+            if (seen[i])
+            {
+                continue;
+            }
+
+            missingElements.Add(i);
+        }
+
+        return missingElements;
+    }
+}
--- source/Tests/LeetCode.Tests/Algorithms/FindMissingElements/FindMissingElementsTestsBase.cs
+++ source/Tests/LeetCode.Tests/Algorithms/FindMissingElements/FindMissingElementsTestsBase.cs
@@ -19,6 +19,23 @@ public abstract class FindMissingElementsTestsBase<T> where T : IFindMissingElem
     [DataRow(new[] { 1, 4, 2, 5 }, new[] { 3 })]
     [DataRow(new[] { 7, 8, 6, 9 }, new int[] { })]
     [DataRow(new[] { 5, 1 }, new[] { 2, 3, 4 })]
+    [DataRow(new[] { 1, 2 }, new int[] { })]
+    [DataRow(new[] { 2, 1 }, new int[] { })]
+    [DataRow(new[] { 1, 3 }, new[] { 2 })]
+    [DataRow(new[] { 3, 1 }, new[] { 2 })]
+    [DataRow(new[] { 1, 5 }, new[] { 2, 3, 4 })]
+    [DataRow(new[] { 2, 4 }, new[] { 3 })]
+    [DataRow(new[] { 10, 20 }, new[] { 11, 12, 13, 14, 15, 16, 17, 18, 19 })]
+    [DataRow(new[] { 1, 2, 3, 4, 5 }, new int[] { })]
+    [DataRow(new[] { 1, 3, 5, 7, 9 }, new[] { 2, 4, 6, 8 })]
+    [DataRow(new[] { 50, 51 }, new int[] { })]
+    [DataRow(new[] { 50, 55 }, new[] { 51, 52, 53, 54 })]
+    [DataRow(new[] { 1, 20 }, new[] { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 })]
+    [DataRow(new[] { 99, 100 }, new int[] { })]
+    [DataRow(new[] { 95, 100 }, new[] { 96, 97, 98, 99 })]
+    [DataRow(new[] { 1, 6, 2, 3, 5 }, new[] { 4 })]
+    [DataRow(new[] { 8, 3, 5 }, new[] { 4, 6, 7 })]
+    [DataRow(new[] { 30, 10, 20 }, new[] { 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 24, 25, 26, 27, 28, 29 })]
     public void FindMissingElements_WithArrayContainingGaps_ReturnsMissingSequentialElements(int[] nums, int[] expectedResult)
     {
         // Arrange
