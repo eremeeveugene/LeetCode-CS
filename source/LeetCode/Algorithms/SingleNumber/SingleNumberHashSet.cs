011a7ec5901cb005a785106324968f8878ef2a6d
Eugene <eremeeveugene@gmail.com>
2026-08-09 13:37:19 +0300

    Update SingleNumberHashSet.cs

--- source/LeetCode/Algorithms/SingleNumber/SingleNumberHashSet.cs
+++ source/LeetCode/Algorithms/SingleNumber/SingleNumberHashSet.cs
@@ -23,8 +23,10 @@ public sealed class SingleNumberHashSet : ISingleNumber
     {
         var numsHashSet = new HashSet<int>();
 
-        foreach (var num in nums)
+        for (var i = 0; i < nums.Length; i++)
         {
+            var num = nums[i];
+
             if (!numsHashSet.Add(num))
             {
                 numsHashSet.Remove(num);
