34ba2d0889a82ff1dfa922f23914ce23096e27ba
Eugene <eremeeveugene@gmail.com>
2026-08-15 18:28:46 +0300

    Update MedianOfTwoSortedArraysBinarySearch.cs

--- source/LeetCode/Algorithms/MedianOfTwoSortedArrays/MedianOfTwoSortedArraysBinarySearch.cs
+++ source/LeetCode/Algorithms/MedianOfTwoSortedArrays/MedianOfTwoSortedArraysBinarySearch.cs
@@ -21,80 +21,85 @@ public sealed class MedianOfTwoSortedArraysBinarySearch : IMedianOfTwoSortedArra
     /// </remarks>
     public double FindMedianSortedArrays(int[] nums1, int[] nums2)
     {
-        while (true)
+        if (nums1.Length > nums2.Length)
         {
-            if (nums1.Length > nums2.Length)
+            (nums1, nums2) = (nums2, nums1);
+        }
+
+        var low = 0;
+        var high = nums1.Length;
+        var halfLen = (nums1.Length + nums2.Length + 1) / 2;
+
+        var result = 0.0;
+
+        while (low <= high)
+        {
+            var i = (low + high) / 2;
+            var j = halfLen - i;
+
+            if (i < nums1.Length && nums2[j - 1] > nums1[i])
             {
-                (nums1, nums2) = (nums2, nums1);
+                low = i + 1;
 
                 continue;
             }
 
-            var low = 0;
-            var high = nums1.Length;
-            var halfLen = (nums1.Length + nums2.Length + 1) / 2;
-
-            var result = 0.0;
-
-            while (low <= high)
+            if (i > 0 && nums1[i - 1] > nums2[j])
             {
-                var i = (low + high) / 2;
-                var j = halfLen - i;
+                high = i - 1;
 
-                if (i < nums1.Length && nums2[j - 1] > nums1[i])
-                {
-                    low = i + 1;
-                }
-                else if (i > 0 && nums1[i - 1] > nums2[j])
-                {
-                    high = i - 1;
-                }
-                else
-                {
-                    int maxLeft;
-
-                    if (i == 0)
-                    {
-                        maxLeft = nums2[j - 1];
-                    }
-                    else if (j == 0)
-                    {
-                        maxLeft = nums1[i - 1];
-                    }
-                    else
-                    {
-                        maxLeft = Math.Max(nums1[i - 1], nums2[j - 1]);
-                    }
-
-                    if ((nums1.Length + nums2.Length) % 2 == 1)
-                    {
-                        result = maxLeft;
-
-                        break;
-                    }
-
-                    int minRight;
-
-                    if (i == nums1.Length)
-                    {
-                        minRight = nums2[j];
-                    }
-                    else if (j == nums2.Length)
-                    {
-                        minRight = nums1[i];
-                    }
-                    else
-                    {
-                        minRight = Math.Min(nums1[i], nums2[j]);
-                    }
-
-                    result = (maxLeft + minRight) / 2.0;
-
-                    break;
-                }
+                continue;
             }
 
-            return result;
+            result = GetMedian(nums1, nums2, i, j);
+
+            break;
         }
+
+        return result;
+    }
+
+    private static double GetMedian(int[] nums1, int[] nums2, int i, int j)
+    {
+        var maxLeft = GetMaxLeft(nums1, nums2, i, j);
+
+        if ((nums1.Length + nums2.Length) % 2 == 1)
+        {
+            return maxLeft;
+        }
+
+        var minRight = GetMinRight(nums1, nums2, i, j);
+
+        return (maxLeft + minRight) / 2.0;
+    }
+
+    private static int GetMaxLeft(int[] nums1, int[] nums2, int i, int j)
+    {
+        if (i == 0)
+        {
+            return nums2[j - 1];
+        }
+
+        if (j == 0)
+        {
+            return nums1[i - 1];
+        }
+
+        return Math.Max(nums1[i - 1], nums2[j - 1]);
+    }
+
+    private static int GetMinRight(int[] nums1, int[] nums2, int i, int j)
+    {
+        if (i == nums1.Length)
+        {
+            return nums2[j];
+        }
+
+        if (j == nums2.Length)
+        {
+            return nums1[i];
+        }
+
+        return Math.Min(nums1[i], nums2[j]);
     }
 }
