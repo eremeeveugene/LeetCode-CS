f62c6aebb0e869f7a1906296d74d380f17add6c5
Eugene <eremeeveugene@gmail.com>
2026-08-16 15:44:32 +0300

    Resolve Open Issues on SonarCloud (#2000)

--- source/LeetCode.Core/Models/TreeNode.cs
+++ source/LeetCode.Core/Models/TreeNode.cs
@@ -10,6 +10,7 @@
 // --------------------------------------------------------------------------------
 
 // ReSharper disable InconsistentNaming
+#pragma warning disable S101
 
 using LeetCode.Core.Exceptions;
 
--- source/LeetCode/Algorithms/TwentyFourGame/TwentyFourGameBruteForce.cs
+++ source/LeetCode/Algorithms/TwentyFourGame/TwentyFourGameBruteForce.cs
@@ -208,7 +208,6 @@ public sealed class TwentyFourGameBruteForce : ITwentyFourGame
                 result = left * right;
 
                 return true;
-            case Operation.Division:
             default:
                 if (Math.Abs(right) < Tolerance)
                 {
