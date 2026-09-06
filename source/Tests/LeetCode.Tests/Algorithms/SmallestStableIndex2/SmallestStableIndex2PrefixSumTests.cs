fac3f50761eedca22110c88147d3d516c9b1a989
Eugene <eremeeveugene@gmail.com>
2026-09-06 11:44:19 +0300

    Update SmallestStableIndex2PrefixSumTests.cs

--- source/Tests/LeetCode.Tests/Algorithms/SmallestStableIndex2/SmallestStableIndex2PrefixSumTests.cs
+++ source/Tests/LeetCode.Tests/Algorithms/SmallestStableIndex2/SmallestStableIndex2PrefixSumTests.cs
@@ -14,21 +14,4 @@ using LeetCode.Algorithms.SmallestStableIndex2;
 namespace LeetCode.Tests.Algorithms.SmallestStableIndex2;
 
 [TestClass]
-public sealed class SmallestStableIndex2PrefixSumTests : SmallestStableIndex2TestsBase<SmallestStableIndex2PrefixSum>
-{
-    [TestMethod]
-    public void FirstStableIndex_WithMaximumLengthArray_ReturnsLastIndex()
-    {
-        // Arrange
-        var nums = new int[100_000];
-        nums[0] = 1_000_000_000;
-        nums[^1] = 1_000_000_000;
-        var solution = new SmallestStableIndex2PrefixSum();
-
-        // Act
-        var actualResult = solution.FirstStableIndex(nums, 0);
-
-        // Assert
-        Assert.AreEqual(nums.Length - 1, actualResult);
-    }
-}
+public sealed class SmallestStableIndex2PrefixSumTests : SmallestStableIndex2TestsBase<SmallestStableIndex2PrefixSum>;
