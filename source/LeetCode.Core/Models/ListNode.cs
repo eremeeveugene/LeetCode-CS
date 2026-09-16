9301d66777ad27aa6d96a48412eaaf87b0522ad1
Eugene <eremeeveugene@gmail.com>
2026-09-16 23:31:55 +0300

    Update ListNode.cs

--- source/LeetCode.Core/Models/ListNode.cs
+++ source/LeetCode.Core/Models/ListNode.cs
@@ -10,6 +10,7 @@
 // --------------------------------------------------------------------------------
 
 // ReSharper disable InconsistentNaming
+
 #pragma warning disable S1104
 
 using LeetCode.Core.Exceptions;
